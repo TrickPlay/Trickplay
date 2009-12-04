0df7d48bb4c71d7b74bd659fb9132bebef9610e7
Craig R. Hughes <craig.git@rungie.com>
2009-12-04 15:35:55 -0800

    Make the demo more interesting (slightly) by using values in the DB to draw circles...

--- test.lua
+++ test.lua
@@ -1,41 +1,42 @@
 
-local_db = LocalDB.new('data_file')
+local_db = LocalDB.new('circles_demo.tch')
 
+--[[
+local_db:nuke('REALLY_NUKE')
 
----[[
-value=''
+local_db:put('x', 1392)
+local_db:put('y', 783)
 
-for i=1,5000 do
-	value = value..i*2
-	local_db:put(i,value)
-end
+local_db:put('r', 0xe0)
+local_db:put('g', 0xc2)
+local_db:put('b', 0xfc)
+
+local_db:put('time', 6000)
+
+local_db:put('n_circles', 3)
+local_db:put('circle_width', 128)
+local_db:put('circle_gap', 16)
+local_db:put('circle_segments', 3)
 --]]
 
 ---[[
-print(1234,local_db:get(1234))
 print(local_db)
 --]]
 
 ---[[
-for j=1,10 do
-	for i=1,20000 do local_db:get(i) end
-end
---]]
 
---[[
-url_fetcher = URLFetcher.new()
+x,y = local_db:get('x'), local_db:get('y')
+r,g,b = local_db:get('r'), local_db:get('g'), local_db:get('b')
 
-x,y = 640, 540
-r,g,b = 0xe0, 0xc2, 0xfc
 stage = Stage.new(x,y,r,g,b)
 
-t = 6000
-timeline = Timeline.new(t)
+time = local_db:get('time')
+timeline = Timeline.new(time)
 
-n_circles = 3
-circle_width = 128
-circle_gap = 16
-circle_segments = 3
+n_circles = local_db:get('n_circles')
+circle_width = local_db:get('circle_width')
+circle_gap = local_db:get('circle_gap')
+circle_segments = local_db:get('circle_segments')
 stage:circles(timeline, n_circles, circle_width, circle_gap, circle_segments)
 
 --]]
