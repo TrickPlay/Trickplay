ab13c60d90d4fde8edc24d4c58ec03c3453ae3b3
Craig R. Hughes <craig.git@rungie.com>
2009-12-23 16:15:22 -0800

    Add my storage stuff as a new .lb file

---
+++ test/storage.h
@@ -0,0 +1,53 @@
+#ifndef __TRICKPLAY_STORAGE__
+#define __TRICKPLAY_STORAGE__
+
+#include <string>
+
+extern "C" {
+	#include "tcutil.h"
+	#include "tchdb.h"
+}
+
+
+namespace Storage
+{
+	typedef std::string String;
+	
+	class LocalHash
+	{
+		public:
+			LocalHash();
+			~LocalHash();
+
+			String name;
+
+			void connect();
+
+			String get(String & key);
+			void   put(String & key, String & value);
+			void   del(String & key);
+
+			// Remove all key/value pairs from the DB
+			void nuke();
+
+			// Transaction stuff
+			void begin();
+			void commit();
+			void abort();
+
+			// Flush to backing store			
+			void flush();
+
+			// Count number of key/value pairs in the DB
+			uint64_t count();
+
+		protected:
+			TCHDB * db;
+	};
+	
+	
+	// Count the number of records in the database
+	uint64_t count(LocalHash & db);
+};
+
+#endif __TRICKPLAY_STORAGE__
