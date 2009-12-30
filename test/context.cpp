a4a60e0a88fcb248804ce13c769e26c5e091990d
Pablo Pissanetzky <pablo@trickplay.com>
2009-12-29 18:12:45 -0800

    Parses app metadata file. Added global app

--- test/context.cpp
+++ test/context.cpp
@@ -4,6 +4,7 @@
 #include "lb.h"
 
 #include <cstdio>
+#include <cstring>
 
 #include "glib.h"
 #include "curl/curl.h"
@@ -18,6 +19,7 @@ extern void luaopen_timer(lua_State*L);
 extern void luaopen_url_request(lua_State*L);
 extern void luaopen_storage(lua_State*L);
 extern void luaopen_globals(lua_State*L);
+extern void luaopen_app(lua_State*L);
 
 //-----------------------------------------------------------------------------
 // Internal context
@@ -42,12 +44,12 @@ void TPContext::set(const char * key,const char * value)
     config.insert(std::make_pair(String(key),String(value)));
 }
 
-const char * TPContext::get(const char * key)
+const char * TPContext::get(const char * key,const char * def)
 {    
     StringMap::const_iterator it = config.find(String(key));
     
     if (it==config.end())
-        return NULL;
+        return def;
     return it->second.c_str();
 }
 
@@ -55,6 +57,21 @@ int TPContext::run()
 {    
     // So that run cannot be called while we are running
     g_assert(!L);
+    
+    // Get the base path for the app
+    const char * app_path = get( APP_PATH );
+    
+    if (!app_path)
+    {
+	gchar * c = g_get_current_dir();
+	set( APP_PATH , c );
+	g_free(c);
+	
+	app_path = get(APP_PATH);
+    }
+    
+    if (!load_app_metadata(app_path))
+	return 1;
     
     // Start up a lua state
     L = lua_open();
@@ -73,6 +90,7 @@ int TPContext::run()
     luaopen_url_request(L);
     luaopen_storage(L);
     luaopen_globals(L);
+    luaopen_app(L);
     
     // Run the script
     int result = luaL_dofile(L,"tp.lua");
@@ -103,7 +121,7 @@ void TPContext::quit()
     clutter_main_quit();
 }
    
-TPContext * get_from_lua(lua_State * L)
+TPContext * TPContext::get_from_lua(lua_State * L)
 {
     g_assert(L);
     lua_pushstring(L,"tp_context");
@@ -113,6 +131,119 @@ TPContext * get_from_lua(lua_State * L)
     g_assert(result);
     return result;
 }
+
+bool TPContext::load_app_metadata(const char * app_path)
+{
+    g_assert(app_path);
+    
+    // Open a state with no libraries - not even the base one
+    
+    lua_State * L=lua_open();
+    
+    g_assert(L);
+    
+    try
+    {
+	// Build the path to the metadata file and test that it exists
+	
+	gchar * path = g_build_filename(app_path,APP_METADATA_FILENAME,NULL);
+	
+	if (!g_file_test(path,G_FILE_TEST_IS_REGULAR))
+	{
+	    g_free(path);
+	    throw String("App metadata file does not exist");
+	}
+	
+	// Now, run it with Lua
+	
+	int result = luaL_dofile(L,path);
+	
+	// Free the path
+	
+	g_free(path);
+	
+	// Check that it ran OK
+	
+	if(result)
+	    throw String("Failed to parse app metadata : ") + lua_tostring(L,-1);
+	    
+	// Look for the 'app' global
+	
+	lua_getglobal(L,APP_TABLE_NAME);	
+	if (!lua_istable(L,-1))
+	    throw String("Missing or invalid app table");
+	    
+	// Look for the id
+	lua_getfield(L,-1,APP_FIELD_ID);
+	if (lua_type(L,-1)!=LUA_TSTRING)
+	    throw String("Missing or invalid app id");
+	    
+	// Validate the id
+	
+	size_t len;
+	const char * s=lua_tolstring(L,-1,&len);
+	
+	if (len>64)
+	    throw String("App id is too long");
+	
+	static const char * valid_id_characters = "_-.";
+	
+	for(const char * c=s;*c;++c)
+	{
+	    if (!g_ascii_isalnum(*c))
+	    {
+		if(!strchr(valid_id_characters,*c))
+		    throw String("App id contains invalid characters");
+	    }
+	}
+	
+	if (strstr(s,".."))
+	    throw String("App id contains two dots");
+
+	if (strstr(s,"--"))
+	    throw String("App id contains two dashes");
+	    
+	if (strstr(s,"__"))
+	    throw String("App id contains two underscores");
+	    
+	
+	// Store it
+	set(APP_ID,s);
+	lua_pop(L,1);
+
+	// Look for the other fields
+	lua_getfield(L,-1,APP_FIELD_NAME);
+	if (lua_type(L,-1)!=LUA_TSTRING)
+	    throw String("Missing or invalid app name");
+	set(APP_NAME,lua_tostring(L,-1));
+	lua_pop(L,1);
+	
+	lua_getfield(L,-1,APP_FIELD_DESCRIPTION);
+	if(lua_isstring(L,-1))
+	    set(APP_DESCRIPTION,lua_tostring(L,-1));
+	lua_pop(L,1);
+	
+	lua_getfield(L,-1,APP_FIELD_AUTHOR);
+	if(lua_isstring(L,-1))
+	    set(APP_AUTHOR,lua_tostring(L,-1));
+	lua_pop(L,1);
+	
+	lua_getfield(L,-1,APP_FIELD_COPYRIGHT);
+	if(lua_isstring(L,-1))
+	    set(APP_COPYRIGHT,lua_tostring(L,-1));
+	lua_pop(L,1);
+	
+	lua_close(L);
+	return true;	
+    }
+    catch( const String & e)
+    {
+	lua_close(L);
+	g_warning("Failed to load app metadata from '%s' : %s" , app_path , e.c_str() );
+	return false;	
+    }
+}
+
 //-----------------------------------------------------------------------------
 
 void log_handler(const gchar * log_domain,GLogLevelFlags log_level,const gchar *message,gpointer user_data)
--- test/context.h
+++ test/context.h
@@ -15,6 +15,24 @@ typedef std::string String;
 typedef std::map<String,String> StringMap;
 
 //-----------------------------------------------------------------------------
+// Configuration variable names
+
+#define APP_PATH                "app.path"
+#define APP_ID                  "app.id"
+#define APP_NAME                "app.name"
+#define APP_DESCRIPTION         "app.description"
+#define APP_AUTHOR              "app.author"
+#define APP_COPYRIGHT           "app.copyright"
+
+//-----------------------------------------------------------------------------
+#define APP_METADATA_FILENAME   "app"
+#define APP_TABLE_NAME          "app"
+#define APP_FIELD_ID            "id"
+#define APP_FIELD_NAME          "name"
+#define APP_FIELD_DESCRIPTION   "description"
+#define APP_FIELD_AUTHOR        "author"
+#define APP_FIELD_COPYRIGHT     "copyright"
+//-----------------------------------------------------------------------------
 
 struct TPContext
 {
@@ -24,18 +42,22 @@ public:
     ~TPContext();
     
     void set(const char * key,const char * value);
-    const char * get(const char * key);
+    const char * get(const char * key,const char * def = NULL);
     
     int run();    
     void quit();
     
     static TPContext * get_from_lua(lua_State * L);
     
+protected:
+    
+    bool load_app_metadata(const char * app_path);
+    
 private:
     
     TPContext(const TPContext&);
     
-    StringMap config;
+    StringMap   config;
     lua_State * L;
 };
 
