48d0f44b2c319770abb00317616bbd92972e3b93
Pablo Pissanetzky <pablo@trickplay.com>
2009-12-30 17:16:17 -0800

    Takes an argument for the app path

--- clients/desktop/main.c
+++ clients/desktop/main.c
@@ -7,6 +7,9 @@ int main(int argc,char * argv[])
     
     TPContext * context = tp_context_new();
     
+    if (argc>1)
+        tp_context_set(context,"app.path",argv[1]);
+    
     int result = tp_context_run(context);
     
     tp_context_free(context);
