30fa4f00733b5ba402a1ec512bcfa7d84f2bac99
Craig R. Hughes <craig@trickplay.com>
2010-02-01 11:52:55 -0800

    Convert trickplay logo example app into an accelerometer example

--- apps/examples/trickplay-logo/main.lua
+++ apps/examples/trickplay-logo/main.lua
@@ -1,169 +1,49 @@
---------------------------------------------------------------------------------
--- FROM http://lua-users.org/wiki/TableSerialization
---------------------------------------------------------------------------------
-
-function table_print (tt, indent, done)
-  done = done or {}
-  indent = indent or 0
-  if type(tt) == "table" then
-    local sb = {}
-    for key, value in pairs (tt) do
-      table.insert(sb, string.rep (" ", indent)) -- indent it
-      if type (value) == "table" and not done [value] then
-        done [value] = true
-        table.insert(sb, "{\n");
-        table.insert(sb, table_print (value, indent + 2, done))
-        table.insert(sb, string.rep (" ", indent)) -- indent it
-        table.insert(sb, "}\n");
-      elseif "number" == type(key) then
-        table.insert(sb, string.format("\"%s\"\n", tostring(value)))
-      else
-        table.insert(sb, string.format(
-            "%s = \"%s\"\n", tostring (key), tostring(value)))
-       end
-    end
-    return table.concat(sb)
-  else
-    return tt .. "\n"
-  end
-end
-
-function to_string( tbl )
-    if  "nil"       == type( tbl ) then
-        return tostring(nil)
-    elseif  "table" == type( tbl ) then
-        return table_print(tbl)
-    elseif  "string" == type( tbl ) then
-        return tbl
-    else
-        return tostring(tbl)
-    end
-end
-
---------------------------------------------------------------------------------
---------------------------------------------------------------------------------
-
-
-screen:set{ color = "000000FF" , size = { 960 , 540 } }
+screen:set{ color = "00000000" , size = { 960 , 540 } }
 screen:show_all()
 
-local g = Group
-    {
-        size = { 800 , 500 } ,
-        position = { 10 , 10 },
-        children =
-        {
-            Rectangle{ color = { 255 , 255 , 255 } , border_width = 2 , border_color = { 255 , 0 , 255 } , size = { 240 , 120 } , position = { 90 , 90 } },
-            Text{ font = "Sans 38px" , markup = "Hello <i>World</i>!" , color = { 255 , 0 , 0 , 128 } , position = { 100 , 100 } }
-        }
-        
-    }
+local logo = Image { size = { screen.w, screen.h }, src = "trickplay_logo_dark_bg.svg" }
 
-screen:add( g )
+-- We move the anchor point to the center of the logo so it'll spin in place when we rotate it below
+logo:move_anchor_point( logo.size[1]/2, logo.size[2]/2 )
 
-local ticks = 0
+-- And now position the anchor point in the middle of the screen
+logo.position = { screen.w/2, screen.h/2 }
 
-local r = Rectangle{ color = { 255 , 0 , 0 , 255 } , size = { 100 , 100 } , position = { 106 , 10 } }
+screen:add( logo )
 
-local r2 =
+function controllers.on_controller_connected(controllers,controller)
+    if(controller.has_accelerometer) then
+    	controller:start_accelerometer("L",0.01)
+    	
+    	function controller.on_accelerometer(controller, x, y, z)
+    		--[[
+				Decompose rotation into 2 rotations, about y-axis onto x-z plane, then about x-axis onto negative y-axis
+				Then rotate about z-axis to align tilt
+    		]]--
 
-Group{
-    size = { 100 , 100 },
-    position = { 106 , 10 },
-    clip = { 0 , 0 , 100 , 100 },
-    children = { 
+    		-- Accelerometer measurements are based on axes from http://www.switchonthecode.com/sites/default/files/825/images/device_axes.png
 
-        Rectangle{ color = "AF0000" , size = { 100 , 100 } , position = { 0 , 0 } , z_rotation = { 45 , 50 , 50 } }
-    }
-}
+    		-- Angle to the x-z plane
+    		local theta_to_xz_plane = math.deg(math.atan2(x, z))
+    		-- Correct now for quadrant
+    		if z <= 0 then
+    			theta_to_xz_plane = 180 - theta_to_xz_plane
+    		end
 
-g.children = { r , r2 , Text{ font = "Sans 46px" , markup = "TrickPlay" , color = "FFFFFF" , position = { 0 , 50 } } }
+    		-- Angle to the x-axis is the atan.  Then we move another 90º to hit the negative y-axis (which is where gravity lives)
+    		local theta_to_z_axis = math.deg(math.atan2(y, math.sqrt(x^2 + z^2)))
+    		local theta_to_negative_y_axis = theta_to_z_axis + 90
+    		-- Correct now for quadrant
+    		if z > 0 then
+    			theta_to_negative_y_axis = - theta_to_negative_y_axis
+    		end
 
-print( to_string( screen.perspective , 2 ) )
+			-- Angle the phone is titled relative to ground
+			local theta_for_tilt = math.deg(x, math.sqrt(x^2 + z^2))
 
-local p = screen.perspective
-p[1]=30
-screen.perspective = p
-
-local tim = Timeline{ duration = 5000 }
-
---tim:add_marker( "foo" , 1000 )
---tim:add_marker( "bar" , 1500 )
---tim:add_marker( "caca" , 1000 )
-
-
-print( "MARKERS" , to_string( tim:list_markers( -1 ) ) )
-
-
-local a = Alpha{ timeline = tim , mode = "EASE_IN_CUBIC" }
-
-
-print( a.mode )
-
---[[
-function a.on_alpha(self,progress)
-    print( "PROGRESS" , progress )
-    return 0.5
-end
-]]
-
-local iv = Interval( 10 , 610 )
-print (iv:get_value( 1 ) )
-
-print( a.mode )
-
-tim.loop = true
-
-function tim.on_new_frame( self , msecs )
-    --print( "NEW FRAME" , msecs , self.progress , self.delta , a.alpha )
-    
-    --g.opacity = 255 - ( self.progress * 255 )
-
-    --g.x = iv:get_value( a.alpha )
-    
-    --g.y = 10 + ( a.alpha * 300 )
-    
-    --g.x_rotation = { 90 * a.alpha , g.w / 2 , 0 }
-    g.y_rotation = { 360 * self.progress , 106 , -500 }
-    
-    --g.z_rotation = { 360 * self.progress , 206 , 100 }
-
-    
-    --g.depth = -5000 * a.alpha
-    
-end
-
-function tim.on_started(self)
-    print("Started" , self.duration )
-end
-
-function tim.on_paused(self)
-    print( "PAUSED" )
-end
-
-function tim.on_marker_reached(self,marker,msecs)
-    print( "REACHED MARKER" , marker , msecs )
-    if marker == "foo" then
---        self:rewind()
+			logo.y_rotation = { theta_to_xz_plane, 0, 0 }
+			logo.x_rotation = { theta_to_negative_y_axis, 0, 0 }
+			logo.z_rotation = { theta_for_tilt, 0, 0 }
+    	end
     end
 end
-
---[[
-function tim.on_completed(self)
-    print("COMPLETED")
-    self.on_new_frame = nil
-    self.on_started = nil
-    self.on_paused = nil
-    self.on_marker_reached = nil
-    self.on_completed = nil
-    tim = nil
-end
-]]
-
-local h = { from = 10 , to = 12 }
-
---tim.loop = true
-
-Timer{ interval = 0.5 , on_timer = function() tim:start() return false end }
-
-
