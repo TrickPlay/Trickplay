19e035faacd714f06049e723e671fbb794b3f9eb
Pablo Pissanetzky <pablo@trickplay.com>
2010-02-02 19:58:27 -0800

    Updated mediaplayer OEM interface

--- engine/public/include/tp/mediaplayer.h
+++ engine/public/include/tp/mediaplayer.h
@@ -29,11 +29,16 @@ extern "C" {
                     
             get_viewport_geometry
             set_viewport_geometry
+            get_audio_volume
+            set_audio_volume
+            get_audio_mute
+            set_audio_mute
                 -> do not change state
                 
         MediaPlayer can call:
         
-            no callbacks
+            tp_media_player_get_state
+                -> does not change state
             
             
     LOADING
@@ -46,14 +51,21 @@ extern "C" {
             
             get_viewport_geometry
             set_viewport_geometry
+            get_audio_volume
+            set_audio_volume
+            get_audio_mute
+            set_audio_mute
                 -> do not change state
 
         MediaPlayer can call:
         
-            on_error
+            tp_media_player_get_state
+                -> does not change state
+                
+            tp_media_player_error
                 -> goes to IDLE state
                 
-            on_loaded
+            tp_media_player_loaded
                 -> goes to READY state
             
     READY
@@ -88,11 +100,16 @@ extern "C" {
             get_buffered_duration
             get_video_size
             get_tags
+            get_audio_volume
+            set_audio_volume
+            get_audio_mute
+            set_audio_mute
                 -> do not change state
             
         MediaPlayer can call:
         
-            no callbacks
+            tp_media_player_get_state
+                -> does not change state
             
     PLAYING
     -------
@@ -134,16 +151,21 @@ extern "C" {
             get_buffered_duration
             get_video_size
             get_tags
+            get_audio_volume
+            set_audio_volume
+            get_audio_mute
+            set_audio_mute
                 -> do not change state
             
         MediaPlayer can call:
         
-            on_end_of_stream
-            
+            tp_media_player_get_state
+                -> does not change state
+
+            tp_media_player_end_of_stream            
                 -> goes to PAUSED state (position is reset to 0, rate is reset to 1)
                 
-            on_error
-            
+            tp_media_player_error            
                 -> goes to PAUSED state (position and rate do not change)
                 
     PAUSED
@@ -178,11 +200,16 @@ extern "C" {
             get_buffered_duration
             get_video_size
             get_tags
+            get_audio_volume
+            set_audio_volume
+            get_audio_mute
+            set_audio_mute
                 -> do not change state
                 
         MediaPlayer can call:
         
-            No callbacks
+            tp_media_player_get_state            
+                -> does not change state
                     
  
 */
@@ -193,7 +220,7 @@ extern "C" {
 */
 //-----------------------------------------------------------------------------
 /*
-    Constants: States
+    Constants: Media Player States
     
     TP_MEDIAPLAYER_IDLE     - Idle state, when there is no resource loaded.     
     TP_MEDIAPLAYER_LOADING  - The media player is trying to load a resource.
@@ -202,30 +229,57 @@ extern "C" {
     TP_MEDIAPLAYER_PAUSED   - The media player is paused.
 */
 
-#define TP_MEDIAPLAYER_IDLE         1
-#define TP_MEDIAPLAYER_LOADING      2
-#define TP_MEDIAPLAYER_READY        3
-#define TP_MEDIAPLAYER_PLAYING      4
-#define TP_MEDIAPLAYER_PAUSED       5
+#define TP_MEDIAPLAYER_IDLE         0x01
+#define TP_MEDIAPLAYER_LOADING      0x02
+#define TP_MEDIAPLAYER_READY        0x04
+#define TP_MEDIAPLAYER_PLAYING      0x08
+#define TP_MEDIAPLAYER_PAUSED       0x10
+
+#define TP_MEDIAPLAYER_ANY_STATE    (TP_MEDIAPLAYER_IDLE|TP_MEDIAPLAYER_LOADING|TP_MEDIAPLAYER_READY|TP_MEDIAPLAYER_PLAYING|TP_MEDIAPLAYER_PAUSED)
+
+//-----------------------------------------------------------------------------
+/*
+    Constants: Media Types
+    
+    TP_MEDIA_TYPE_AUDIO         - The media resource has audio.
+    TP_MEDIA_TYPE_VIDEO         - The media resource has video.
+    TP_MEDIA_TYPE_AUDIO_VIDEO   - The media resource has both audio and video.
+*/
+
+#define TP_MEDIA_TYPE_AUDIO         0x01
+#define TP_MEDIA_TYPE_VIDEO         0x02
+#define TP_MEDIA_TYPE_AUDIO_VIDEO   (TP_MEDIA_TYPE_AUDIO|TP_MEDIA_TYPE_VIDEO)
+
+//-----------------------------------------------------------------------------
+
+#define TP_MEDIAPLAYER_ERROR_NOT_IMPLEMENTED    -1    
+#define TP_MEDIAPLAYER_ERROR_INVALID_STATE      -2
+#define TP_MEDIAPLAYER_ERROR_BAD_PARAMETER      -3
+#define TP_MEDIAPLAYER_ERROR_NO_MEDIAPLAYER     -4
 
 //-----------------------------------------------------------------------------
 // Forward declarations
 
-typedef struct TPMediaPlayer            TPMediaPlayer;
+typedef struct TPMediaPlayer    TPMediaPlayer;
 
 //-----------------------------------------------------------------------------
 
 /*
     Function: TPMediaPlayerConstructor
-    This is the prototype for a function that creates a new media player.
+    This is the prototype for a function that initializes a new media player. You
+    should populate all of its functions.
+    
+    Parameters:
+    
+    mp -    A pointer to an uninitialized TPMediaPlayer structure. 
     
     Returns:
     
-    NULL -  If a new media player cannot be created.
-    other - A pointer to an initialized TPMediaPlayer structure.
+    0 -     If the media player was initialized.
+    other - The media player cannot be initialized.
 */
 
-typedef TPMediaPlayer*(*TPMediaPlayerConstructor)();
+typedef int (*TPMediaPlayerConstructor)(TPMediaPlayer * mp);
 
 /*
     Function: tp_context_set_media_player_constructor
@@ -284,9 +338,16 @@ void tp_media_player_end_of_stream(TPMediaPlayer * mp);
 struct TPMediaPlayer
 {
 /*
+    Property: user_data
+    A pointer to user data, which is opaque to TrickPlay.
+*/
+
+    void * user_data;
+    
+/*
     Function: destroy
-    Should release all resources associated with this media player and free the
-    structure itself.
+    Should release all resources associated with this media player. It should not
+    free the TPMediaPlayer structure, as it belongs to TrickPlay.
     
     Parameters:
     
@@ -642,8 +703,28 @@ struct TPMediaPlayer
     
 /*
     Function: get_media_type
-    TODO
+    Returns the type of media; whether it has audio, video or both.
+    
+    Parameters:
+    
+    mp  -       The TPMediaPlayer instance.
+    
+    type -      A pointer to hold the type.
+    
+    Returns:
+    
+    0 -         The media type is known and type was set.
+    
+    other -     The media type is not known.
+    
+    Valid States:
+    
+    - READY
+    - PLAYING
+    - PAUSED
 */
+
+    int (*get_media_type)(TPMediaPlayer * mp,int * type);
 
 /*
     Function: get_media_tags
@@ -651,18 +732,126 @@ struct TPMediaPlayer
 */
 
 /*
+    Function: get_audio_volume
+    Returns the current audio volume as a floating point number between 0 and
+    1, where 0 is the lowest volume.
+    
+    Parameters:
+    
+    mp  -       The TPMediaPlayer instance.
+    
+    volume -    A pointer to hold the volume.
+    
+    Returns:
+    
+    0 -         The volume is known and was set.
+    
+    other -     The volume is not known.
+    
+    Valid States:
+    
+    - IDLE
+    - LOADING
+    - READY
+    - PLAYING
+    - PAUSED
+*/
+
+    int (*get_audio_volume)(TPMediaPlayer * mp,double * volume);
+
+/*
+    Function: set_audio_volume
+    Sets the audio volume as a floating point number between 0 and
+    1, where 0 is the lowest volume.
+    
+    Parameters:
+    
+    mp  -       The TPMediaPlayer instance.
+    
+    volume -    The new volume, between 0 and 1 inclusive.
+    
+    Returns:
+    
+    0 -         The volume was set.
+    
+    other -     There was a problem setting the volume.
+    
+    Valid States:
+    
+    - IDLE
+    - LOADING
+    - READY
+    - PLAYING
+    - PAUSED
+*/
+
+    int (*set_audio_volume)(TPMediaPlayer * mp,double volume);
+
+
+/*
+    Function: get_audio_mute
+    Returns a value indicating whether audio is muted.
+    
+    Parameters:
+    
+    mp  -       The TPMediaPlayer instance.
+    
+    mute -      Whether audio is muted. A value of zero, means audio is NOT muted;
+                anything else means it is MUTED.
+    
+    Returns:
+    
+    0 -         Mute was set successfully.
+    
+    other -     There was a problem getting the mute value.
+    
+    Valid States:
+    
+    - IDLE
+    - LOADING
+    - READY
+    - PLAYING
+    - PAUSED
+*/
+
+    int (*get_audio_mute)(TPMediaPlayer * mp,int * mute);
+
+
+/*
+    Function: set_audio_mute
+    Mutes audio.
+    
+    Parameters:
+    
+    mp  -       The TPMediaPlayer instance.
+    
+    mute -      Whether audio should be muted. A value of zero, means audio should
+                NOT be muted; anything else means it should be MUTED.
+    
+    Returns:
+    
+    0 -         Mute was set successfully.
+    
+    other -     There was a problem setting the mute value.
+    
+    Valid States:
+    
+    - IDLE
+    - LOADING
+    - READY
+    - PLAYING
+    - PAUSED
+*/
+
+    int (*set_audio_mute)(TPMediaPlayer * mp,int mute);
+    
+/*
     Function: get_viewport_texture
     Should return NULL.
 */
 
     void * (*get_viewport_texture)(TPMediaPlayer * mp);
     
-/*
-    Property: user_data
-    A pointer to user data, which is opaque to TrickPlay.
-*/
-
-    void * user_data;
 };
 
 
