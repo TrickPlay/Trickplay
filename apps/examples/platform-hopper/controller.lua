447954acaf9e33ab1687ecc85c47cd4cc6a53161
Craig R. Hughes <craig@trickplay.com>
2010-02-05 18:09:58 -0800

    Initial platform hopper commit

---
+++ apps/examples/platform-hopper/controller.lua
@@ -0,0 +1,31 @@
+--[[
+	The controller's accelerometer will control only the on-screen X momentum of the player.
+]]--
+
+local SAMPLE_PERIOD		= 0.02
+local MOMENTUM_DAMPING	= 10
+local MAX_MOMENTUM		= 1000
+
+function controllers.on_controller_connected(controllers,controller)
+    if(controller.has_accelerometer) then
+    	controller:start_accelerometer("H", SAMPLE_PERIOD)
+
+    	function controller.on_accelerometer(controller, x, y, z)
+    		-- Accelerometer measurements are based on axes from http://www.switchonthecode.com/sites/default/files/825/images/device_axes.png
+
+			-- Use 1/10 of the original momentum when adjusting to a new momentum
+			player.horizontal_momentum =	(
+												MOMENTUM_DAMPING*player.horizontal_momentum
+												+ (MAX_MOMENTUM * x)
+											) / ( 1 + MOMENTUM_DAMPING )
+
+			if(player.horizontal_momentum > MAX_MOMENTUM) then
+				player.horizontal_momentum = MAX_MOMENTUM
+			end
+
+			if(player.horizontal_momentum < -MAX_MOMENTUM) then
+				player.horizontal_momentum = -MAX_MOMENTUM
+			end
+    	end
+    end
+end
---
+++ apps/examples/platform-hopper/main.lua
@@ -0,0 +1,113 @@
+screen.size = { 960 , 540 }
+screen:show_all()
+
+math.randomseed(os.time())
+
+Timer{ interval = 2 , on_timer = function() collectgarbage() end }
+
+local Settings = {
+					BGROUND_IMAGE			= "grid.gif",
+					JUMPER_IMAGE			= "jumper.png",
+					GREEN_PLATFORM_IMAGE	= "platforms/green.png",
+
+					JUMP_TIME				=	300,
+					JUMP_HEIGHT				=	screen.h/5,
+				}
+
+local bground = Image {
+				src = Settings.BGROUND_IMAGE,
+				tile = {true, true},
+				size = { screen.w, screen.h },
+}
+
+local green_platform = Image {
+						src = Settings.GREEN_PLATFORM_IMAGE,
+						keep_aspect_ratio = true,
+						width = 100,
+						position = {-100, -100 },
+					}
+
+screen:add(bground)
+screen:add(green_platform)
+
+player =	{
+					-- Horizontal momentum is measured in pixels per second
+					horizontal_momentum = 0,
+
+					jumper = Image {
+						src = Settings.JUMPER_IMAGE,
+						size = { 60, 60 }
+					},
+
+					score = 0,
+				}
+-- Handle the jumper by his bottom-left corner (to align bottom with top of platforms more easily)
+player.jumper:move_anchor_point( 0, player.jumper.h )
+
+local platforms = Group {}
+for i = 1,10 do
+	local platform = Clone {
+								source = green_platform,
+	
+								position =
+								{
+									(screen.w - green_platform.size[1]) * math.random(),
+									(screen.h - green_platform.size[2]) * math.random()
+								},
+							}
+	platforms:add(platform)
+end
+
+local start_platform = Clone { source = green_platform }
+start_platform.position = { screen.w/2, 5 * screen.h / 6 }
+platforms:add(start_platform)
+
+screen:add(platforms)
+
+
+player.jumper.position =	{
+								start_platform.x + (start_platform.w - player.jumper.w)/2,
+								start_platform.y
+							}
+
+screen:add(player.jumper)
+
+
+local spin = false
+
+local bounce_up_timeline = Timeline { duration = Settings.JUMP_TIME }
+local bounce_up_interval = Interval ( player.jumper.y, player.jumper.y - Settings.JUMP_HEIGHT )
+local bounce_up_alpha = Alpha { timeline = bounce_up_timeline, mode = "EASE_OUT_QUAD" }
+
+function bounce_up_timeline.on_new_frame( t , msecs )
+    player.jumper.y = bounce_up_interval:get_value( bounce_up_alpha.alpha )
+
+    player.jumper.x = player.jumper.x + t.delta * player.horizontal_momentum/1000
+    if(player.jumper.x > screen.w) then
+    	player.jumper.x = 0
+    elseif player.jumper.x < 0 then
+    	player.jumper.x = screen.w
+    end
+
+    if spin then
+    	player.jumper.z_rotation = { -bounce_up_alpha.alpha * 360, player.jumper.w/2, -player.jumper.w/2 }
+    end
+end
+
+function bounce_up_timeline.on_completed( )
+	bounce_up_timeline:reverse()
+	if bounce_up_timeline.direction == "FORWARD" then
+		if math.random() < 0.2 then
+			spin = true
+		else
+			spin = false
+		end
+	else
+		spin = false
+	end
+	bounce_up_timeline:start()
+end
+
+bounce_up_timeline:start()
+
+dofile('controller.lua')
