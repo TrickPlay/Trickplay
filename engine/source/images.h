1ddc215cfcd2b89c482858aae06d3da0f472a627
Pablo Pissanetzky <pablo@trickplay.com>
2010-04-19 15:37:16 -0700

    Context now implements an icon cache - it saves uncompressed images to the data directory

--- engine/source/images.h
+++ engine/source/images.h
@@ -7,6 +7,11 @@ namespace Images
 {
     bool load_texture_from_data( ClutterTexture * texture, const void * data, size_t length );
     bool load_texture_from_file( ClutterTexture * texture, const char * file_name );
+
+    // Takes the data passed in and decodes it to an image. Results must be
+    // freed with g_free.
+
+    unsigned char * decode_image( const void * data, size_t length, int & width, int & height, int & pitch, int & depth, int & bgr );
 };
 
 #endif // _TRICKPLAY_IMAGES_H
