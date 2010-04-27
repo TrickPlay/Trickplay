63348bbab566ba80d050d47f3d287273fe6dab7b
Pablo Pissanetzky <pablo@trickplay.com>
2010-04-27 12:24:07 -0700

    Pulling EventGroup out of app.h/cpp

---
+++ engine/source/event_group.cpp
@@ -0,0 +1,137 @@
+
+#include "event_group.h"
+
+class EventGroup::IdleClosure
+{
+public:
+
+    static guint add_idle( EventGroup * eg, gint priority, GSourceFunc f, gpointer d, GDestroyNotify dn )
+    {
+        return g_idle_add_full( priority, idle_callback, new IdleClosure( eg, f, d, dn ), destroy_callback );
+    }
+
+private:
+
+    IdleClosure( EventGroup * eg, GSourceFunc f, gpointer d, GDestroyNotify dn )
+        :
+        event_group( eg ),
+        function( f ),
+        data( d ),
+        destroy_notify( dn )
+    {
+        event_group->ref();
+    }
+
+    ~IdleClosure()
+    {
+        event_group->unref();
+    }
+
+    static gboolean idle_callback( gpointer ic )
+    {
+        IdleClosure * closure = ( IdleClosure * )ic;
+
+        GSource * source = g_main_current_source();
+
+        guint id = g_source_get_id( source );
+
+        if ( !g_source_is_destroyed( source ) )
+        {
+            closure->function( closure->data );
+        }
+        else
+        {
+            g_debug( "NOT FIRING SOURCE %d", id );
+        }
+
+        closure->event_group->remove( id );
+
+        return FALSE;
+    }
+
+    static void destroy_callback( gpointer ic )
+    {
+        IdleClosure * closure = ( IdleClosure * )ic;
+
+        if ( closure->destroy_notify )
+        {
+            closure->destroy_notify( closure->data );
+        }
+
+        delete closure;
+    }
+
+private:
+
+    EventGroup *    event_group;
+    GSourceFunc     function;
+    gpointer        data;
+    GDestroyNotify  destroy_notify;
+};
+
+EventGroup::EventGroup()
+    :
+    mutex( g_mutex_new() )
+{
+}
+
+EventGroup::~EventGroup()
+{
+    cancel_all();
+    g_mutex_free( mutex );
+}
+
+guint EventGroup::add_idle( gint priority, GSourceFunc function, gpointer data, GDestroyNotify notify )
+{
+    Util::GMutexLock lock( mutex );
+    g_assert( function );
+
+    guint id = IdleClosure::add_idle( this, priority, function, data, notify );
+
+    source_ids.insert( id );
+
+    return id;
+}
+
+void EventGroup::cancel( guint id )
+{
+    Util::GMutexLock lock( mutex );
+    std::set<guint>::iterator it = source_ids.find( id );
+
+    if ( it == source_ids.end() )
+    {
+        g_debug( "CANNOT CANCEL SOURCE %d", id );
+    }
+    else
+    {
+        g_debug( "CANCELLING SOURCE %d", id );
+
+        source_ids.erase( it );
+
+        g_source_remove( id );
+    }
+}
+
+void EventGroup::cancel_all()
+{
+    Util::GMutexLock lock( mutex );
+
+    if ( !source_ids.empty() )
+    {
+        g_debug( "CANCELLING %" G_GSIZE_FORMAT " SOURCE(S)", source_ids.size() );
+
+        for ( std::set<guint>::iterator it = source_ids.begin(); it != source_ids.end(); ++it )
+        {
+            g_source_remove( ( *it ) );
+        }
+
+        source_ids.clear();
+    }
+}
+
+void EventGroup::remove( guint id )
+{
+    Util::GMutexLock lock( mutex );
+    source_ids.erase( id );
+}
+
---
+++ engine/source/event_group.h
@@ -0,0 +1,39 @@
+
+#ifndef _TRICKPLAY_EVENT_GROUP_H
+#define _TRICKPLAY_EVENT_GROUP_H
+
+#include "common.h"
+#include "util.h"
+
+//-----------------------------------------------------------------------------
+// An event group lets us track idle sources, so we can neuter them when an
+// app is closed (so that they won't fire once the lua state is gone).
+
+class EventGroup : public RefCounted
+{
+public:
+
+    EventGroup();
+
+    guint add_idle( gint priority, GSourceFunc function, gpointer data, GDestroyNotify notify );
+
+    void cancel( guint id );
+
+    void cancel_all();
+
+    void remove( guint id );
+
+protected:
+
+    ~EventGroup();
+
+private:
+
+    class IdleClosure;
+
+    GMutex     *    mutex;
+    std::set<guint> source_ids;
+};
+
+
+#endif  // _TRICKPLAY_EVENT_GROUP_H
