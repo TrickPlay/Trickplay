af6228cf7f01e716a00d2bbaa0ae16cc7b45a43b
Pablo Pissanetzky <pablo@trickplay.com>
2010-04-28 15:51:30 -0700

    Added delegates, progress and timing to downloads
    Former-commit-id: 331d97c1b9eebe05e194c4297d93da3e60a2d7bb

--- engine/source/downloads.h
+++ engine/source/downloads.h
@@ -34,6 +34,10 @@ public:
         :
             id( 0 ),
             status( FAILED ),
+            content_length( 0 ),
+            written( 0 ),
+            elapsed_seconds( 0 ),
+            seconds_left( -1 ),
             code( 0 )
         {}
 
@@ -43,13 +47,69 @@ public:
             owner( _owner ),
             file_name( _file_name ),
             status( RUNNING ),
+            content_length( 0 ),
+            written( 0 ),
+            elapsed_seconds( 0 ),
+            seconds_left( -1 ),
             code( 0 )
         {}
+
+        gdouble percent_downloaded() const
+        {
+            if ( content_length > 0 )
+            {
+                return 100.0 * ( gdouble( written ) / gdouble( content_length ) );
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        void progress( guint64 _content_length, guint64 _written, gdouble _seconds )
+        {
+            content_length = _content_length;
+            written = _written;
+            elapsed_seconds += _seconds;
+
+            if ( status == RUNNING )
+            {
+                if ( content_length > 0 && written > 0 )
+                {
+                    seconds_left = ( elapsed_seconds * ( 1 / ( gdouble( written ) / gdouble( content_length ) ) ) ) - elapsed_seconds ;
+                }
+                else
+                {
+                    seconds_left = -1;
+                }
+            }
+            else
+            {
+                seconds_left = 0;
+            }
+        }
+
+        void finished( const Network::Response & response )
+        {
+            status = response.failed ? FAILED : FINISHED;
+
+            code = response.code;
+            message = response.status;
+            headers = response.headers;
+            seconds_left = 0;
+        }
 
         unsigned int    id;
         String          owner;
         String          file_name;
         Status          status;
+        guint64         content_length;
+        guint64         written;
+        gdouble         elapsed_seconds;
+        gdouble         seconds_left;
+
+        // These come from the response, once the download is done
+
         int             code;
         String          message;
         StringMultiMap  headers;
@@ -59,6 +119,22 @@ public:
     // Get the information for a download
 
     bool get_download_info( unsigned int id, Info & info );
+
+    //..........................................................................
+    // Delegate class, to get callbacks. Note that all delegates get callbacks
+    // for all downloads. Callbacks happen in the main thread and the
+    // download_progress callback frequency is throttled.
+
+    class Delegate
+    {
+    public:
+
+        virtual void download_progress( const Info & info ) = 0;
+        virtual void download_finished( const Info & info ) = 0;
+    };
+
+    void add_delegate( Delegate * delegate );
+    void remove_delegate( Delegate * delegate );
 
 private:
 
@@ -74,7 +150,8 @@ private:
             file( _file ),
             stream( _stream ),
             content_length( 0 ),
-            written( 0 )
+            written( 0 ),
+            timer( g_timer_new() )
         {}
 
         ~Closure()
@@ -86,7 +163,7 @@ private:
 
             close( true );
 
-            g_debug( "DESTROYED CLOSURE FOR DOWNLOAD %d", id );
+            g_timer_destroy( timer );
         }
 
         void close( bool delete_file )
@@ -124,7 +201,41 @@ private:
         GFileOutputStream * stream;
         guint64             content_length;
         guint64             written;
+        GTimer *            timer;
     };
+
+    //..........................................................................
+    // Structure to pass progress information from the network thread to the
+    // main thread.
+
+    struct Progress
+    {
+        static Progress * make( Closure * closure )
+        {
+            Progress * result = g_slice_new( Progress );
+
+            result->downloads = closure->downloads;
+            result->id = closure->id;
+            result->content_length = closure->content_length;
+            result->written = closure->written;
+            result->seconds = g_timer_elapsed( closure->timer, NULL );
+
+            return result;
+        }
+
+        static void destroy( gpointer progress )
+        {
+            g_slice_free( Progress, progress );
+        }
+
+        Downloads *     downloads;
+        unsigned int    id;
+        guint64         content_length;
+        guint64         written;
+        gdouble         seconds;
+    };
+
+    static gboolean progress_callback( gpointer progress );
 
     //..........................................................................
     // Network callback. This gets called in the network thread while the request
@@ -143,6 +254,14 @@ private:
     typedef std::map<unsigned int,Info> InfoMap;
 
     InfoMap                 info_map;
+
+    //..........................................................................
+    // Delegates
+
+    typedef std::set<Delegate*> DelegateSet;
+
+    DelegateSet             delegates;
+
 };
 
 
