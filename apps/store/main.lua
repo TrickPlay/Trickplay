9400c910cea37eeae447fe83bdedfcad6ce396a8
Pablo Pissanetzky <pablo@trickplay.com>
2010-05-07 18:07:39 -0700

    App store checkpoint - not working 100% yet
    Former-commit-id: 3b36c7acff6a5b29922e3cc17e2270e5370d4e9e

--- apps/store/main.lua
+++ apps/store/main.lua
@@ -124,20 +124,37 @@ end
 
 ui =
 {
+    main =
+    {
+        background          = { f = "background-blue-curtain.jpg" , position = { 0 , 0 } },
+        floor               = { f = "floor.png" ,                   position = { 0 , 0 } },
+        featured            = { f = "featured-apps.png",            position = { 0 , 0 } },
+        castle_info         = { f = "featured-app-castle-info.png", position = { 0 , 0 } , name = "castle_info" },
+        zombie_info         = { f = "featured-app-zombie-info.png", position = { 0 , 0 } , name = "zombie_info" },
+        stroke              = { f = "featured-apps-stroke-white.png", position = { 0 , 0 } },
+        categories          = { f = "categories.png" ,              position = { 0 , 0 } },
+        focus               = { f = "focus-yellow-all.png" ,        position = { 0 , 0 } }
+        
+    },
+    
     app =
     {
         background          = { f = "background.jpg" ,            position = { 0 , 0 }      },
         buy_off             = { f = "button-buy-off.png",         position = { 96 , 919 }   , name = "buy_off"  , other = "buy_on" },
-        buy_on              = { f = "button-buy-on.png",          position = { 96 , 919 }   , name = "buy_on"   , other = "buy_off" , nav = { u = "menu_games_app_off" , r = "screen1" } },
+        buy_on              = { f = "button-buy-on.png",          position = { 96 , 919 }   , name = "buy_on"   , other = "buy_off" , nav = { u = "menu_off" , r = "screen1" } },
         loading             = { f = "button-loading.png",         position = { 96 , 919 }   , name = "loading"  },
         play_off            = { f = "button-play-off.png",        position = { 96 , 919 }   , name = "play_off" , other = "play_on"  },
-        play_on             = { f = "button-play-on.png",         position = { 96 , 919 }   , name = "play_on"  , other = "play_off" , nav = { u = "menu_games_app_off" , r = "screen1" } },
-        menu_search_off     = { f = "menu-0-search-off.png" ,     position = { 40 , 14 }    , name = "menu_search_off" },
-        menu_apps_off       = { f = "menu-1-apps-off.png",        position = { 121 , 14 }   , name = "menu_apps_off" },
-        menu_games_on       = { f = "menu-2-games-on.png",        position = { 260,  14 }   , name = "menu_games_on" }, 
-        menu_games_app_off  = { f = "menu-2-games-app-off.png",   position = { 260 , 14 }   , name = "menu_games_app_off" , other = "menu_games_app_on" },
-        menu_games_app_on   = { f = "menu-2-games-app-on.png",    position = { 260 , 14 }   , name = "menu_games_app_on" , other = "menu_games_app_off" , nav = { d = "buy_off" } },
-        screen1_on          = { f = "screenshot-1-on.png",        position = { 1285 , 609 } , name = "screen1_on" , other = "screen1" , nav = { u = "menu_games_app_off" , d = "screen2" , l = "buy_off" } },
+        play_on             = { f = "button-play-on.png",         position = { 96 , 919 }   , name = "play_on"  , other = "play_off" , nav = { u = "menu_off" , r = "screen1" } },
+        
+        search_off          = { f = "menu-search-off.png",        position = { 40 , 14 }    , name = "search_off" , other = "search_on" },
+        search_on           = { f = "menu-search-on.png",         position = { 40 , 14 }    , name = "search_on"  , other = "search_off" , nav = { d = "buy_off" , r = "menu_off" } },
+        
+        menu_off            = { f = "menu-all-off.png" ,          position = { 121 , 14 }   , name = "menu_off" , other = "menu_apps_on" , nav = { d = "buy_off"  } },
+        menu_apps_on        = { f = "menu-apps-on.png" ,          position = { 121 , 14 }   , name = "menu_apps_on" , other = "menu_off" , nav = { l = "search_off" , d = "buy_off" , r = { hide = true , target = "menu_games_on" } } },
+        
+        menu_games_on       = { f = "menu-games-on.png",          position = { 121 , 14 }   , name = "menu_games_on" , other = "menu_off" , nav = { d = "buy_off" , l = { hide = true, target = "menu_apps_on" } } },
+        
+        screen1_on          = { f = "screenshot-1-on.png",        position = { 1285 , 609 } , name = "screen1_on" , other = "screen1" , nav = { u = "menu_off" , d = "screen2" , l = "buy_off" } },
         screen2_on          = { f = "screenshot-2-on.png",        position = { 1285 , 753 } , name = "screen2_on" , other = "screen2" , nav = { u = "screen1" , d = "screen3" , l = "buy_off" } },
         screen3_on          = { f = "screenshot-3-on.png",        position = { 1285 , 897 } , name = "screen3_on" , other = "screen3" , nav = { u = "screen2" , l = "buy_off" } },
         screen1             = { f = "screenshot-1.png",           position = { 1285 , 609 } , name = "screen1" , other = "screen1_on" },
@@ -155,56 +172,62 @@ ui =
     
     app_screen_focus = "buy_on",
     
+    prepare_image = 
+    
+        function( self , base , style , hide , no_scale )
+        
+            local image = Image()
+            
+            if not no_scale then
+            
+                image.scale = { self.x_scale , self.y_scale }
+                
+            end
+            
+            image.src = base.."/"..style.f
+            
+            image.x = style.position[ 1 ] * self.x_scale
+            image.y = style.position[ 2 ] * self.y_scale
+            
+            if style.name then
+                
+                image.name = style.name
+                
+            end
+            
+            image.extra.other = style.other
+            
+            image.extra.nav = style.nav
+            
+            if hide then
+                image:hide()
+            end
+            
+            return image
+        
+        end,
+    
     load_app_screen =
     
         function( self , app_id )
             
-            local scale = { self.x_scale , self.y_scale }
-            
-            local function prepare_image( style , hide )
-            
-                local image = Image{ scale = scale }
-                
-                image.src = app_id.."/"..style.f
-                
-                image.x = style.position[ 1 ] * scale[ 1 ]
-                image.y = style.position[ 2 ] * scale[ 2 ]
-                
-                if style.name then
-                    
-                    image.name = style.name
-                    
-                end
-                
-                image.extra.other = style.other
-                
-                image.extra.nav = style.nav
-                
-                if hide then
-                    image:hide()
-                end
-                
-                return image
-            
-            end
-            
-            
             self.app_screen = Group{ size = screen.size , position = { 0 , 0 } }
             
             self.app_screen:add(
-                prepare_image( self.app.background ),
-                prepare_image( self.app.menu_search_off ),
-                prepare_image( self.app.menu_apps_off ),
-                prepare_image( self.app.menu_games_app_off ),
-                prepare_image( self.app.menu_games_app_on , true ),
-                prepare_image( self.app.buy_on ),
-                prepare_image( self.app.buy_off , true ),
-                prepare_image( self.app.screen1 ),
-                prepare_image( self.app.screen1_on , true ),
-                prepare_image( self.app.screen2 ),
-                prepare_image( self.app.screen2_on , true ),
-                prepare_image( self.app.screen3 ),
-                prepare_image( self.app.screen3_on , true )
+                self:prepare_image( app_id, self.app.background ),
+                self:prepare_image( app_id, self.app.search_off ),
+                self:prepare_image( app_id, self.app.search_on , true ),
+                self:prepare_image( app_id, self.app.menu_off ),
+                self:prepare_image( app_id, self.app.menu_apps_on , true ),
+                self:prepare_image( app_id, self.app.menu_games_on , true ),
+                self:prepare_image( app_id, self.app.buy_on ),
+                self:prepare_image( app_id, self.app.buy_off , true ),
+                self:prepare_image( app_id, self.app.screen1 ),
+                self:prepare_image( app_id, self.app.screen1_on , true ),
+                self:prepare_image( app_id, self.app.screen2 ),
+                self:prepare_image( app_id, self.app.screen2_on , true ),
+                self:prepare_image( app_id, self.app.screen3 ),
+                self:prepare_image( app_id, self.app.screen3_on , true )
             )
             
             screen:add( self.app_screen )
@@ -213,7 +236,7 @@ ui =
         
     app_screen_swap =
     
-        function( self , name )
+        function( self , name , hide )
         
             if not self.app_screen then
             
@@ -227,24 +250,208 @@ ui =
             
                 e:hide()
                 
-                e = self.app_screen:find_child( e.extra.other )
+                local other = e.extra.other 
                 
-                if e then
+                if other then
                 
-                    e:show()
+                    e = self.app_screen:find_child( other )
+                        
+                    if e then
                     
-                    return e.name
-
+                        if not hide then
+                        
+                            e:show()
+                            
+                        end
+                        
+                        return e.name
+    
+                    end
+            
                 end
+                
+            end
+        
+        end,
+        
+    main_focus = nil,
+    
+    main_focus_change =
+    
+        function( self , old , new )
+        
+            print( "FOCUS CHANGE" , old , new )
+        
+            local info_down = nil
+            
+            local info_up = nil
+        
+            if old == 1 then
+            
+                info_down = self.main_screen:find_child( "zombie_info" )
+                
+            elseif old == 2 then
+            
+                info_down = self.main_screen:find_child( "castle_info" )
             
             end
+
+            if new == 1 then
+            
+                info_up = self.main_screen:find_child( "zombie_info" )
+                
+            elseif new == 2 then
+            
+                info_up = self.main_screen:find_child( "castle_info" )
+            
+            end
+            
+            if info_up then
+            
+                info_up:animate{ duration = 150 , y = -140 }
+            
+            end
+            
+            if info_down then
+            
+                info_down:animate{ duration = 150 , y = 0 }
+                
+            end
+        
+        end,
+        
+    main_set_focus =
+    
+        function( self , what )
+        
+            old_focus = self.main_focus
+        
+            self.main_focus = what
+        
+            if self.main_focus == 1 then self.main_focus_rings.clip = { 39 , 123 , 911 , 454 }
+            elseif self.main_focus == 2 then self.main_focus_rings.clip = { 966 , 123 , 911 , 454 }
+            elseif self.main_focus == 3 then self.main_focus_rings.clip = { 823 , 591 , 277 , 75 }
+            elseif self.main_focus == 4 then self.main_focus_rings.clip = { 823 , 686 , 277 , 275 }
+            end
+        
+            self:main_focus_change( old_focus , self.main_focus )
+        end,
+        
+    load_main_screen =
+    
+        function( self )
+            
+            local base = "main"
+        
+            self.main_screen = Group{ size = screen.size , position = { 0 , 0 } }
+                        
+            self.main_screen:add(
+                self:prepare_image( base , self.main.background ),
+                self:prepare_image( base , self.main.floor ),
+                self:prepare_image( base , self.main.featured ),
+                
+                Group{
+                    size = screen.size ,
+                    position = { 0 , 0 } ,
+                    scale = { self.x_scale , self.y_scale } ,
+                    clip = { 53 , 0 , 885 , 559 } ,
+                    children = { self:prepare_image( base , self.main.zombie_info , false , true ) } },
+
+                Group{
+                    size = screen.size ,
+                    position = { 0 , 0 } ,
+                    scale = { self.x_scale , self.y_scale } ,
+                    clip = { 978 , 0 , 886 , 559 },
+                    children = { self:prepare_image( base , self.main.castle_info , false , true ) } },
+                
+                self:prepare_image( base , self.main.stroke ),
+                self:prepare_image( base , self.main.categories )
+            )
+            
+            
+            local tile_group =
+                
+                Group{
+                    name = "tiles",
+                    size = { screen.w , 275 },
+                    position = { 0 , 696 * self.y_scale },
+                    scale = { self.x_scale , self.y_scale }
+                }
+            
+            local x = 0
+            
+            local image = self:prepare_image( base .."/tiles" , { f = "1.png" , position = { 0 , 0 } } , false , true )
+            
+            image.anchor_point = { image.w / 2 , 0 }
+            
+            image.x = 960
+            
+            print( image.w , screen.w , image.x )
+            
+            tile_group:add( image )
+            
+            
+            local images = {}
+
+            local lx = image.x - image.w * self.x_scale + 2
+            
+            local rx = image.x + image.w - 2
+            
+            for i = 2 , 2 do
+            
+                image = self:prepare_image( base .."/tiles" , { f = tostring( i )..".png" , position = { lx , 31 } } , false , true )
+            
+                image.anchor_point = { image.w / 2 , 0 }
+                
+                image.scale = { 0.75 , 0.75 }
+                
+                tile_group:add( image )
+
+                lx = lx - ( ( image.w * 0.75 ) + 14 * self.x_scale )
+
+            end
+if false then            
+            for i = 12 , 20 do
+            
+                image = self:prepare_image( base .."/tiles" , { f = tostring( i )..".png" , position = { rx , 31 } } , false , true )
+            
+                image.anchor_point = { image.w / 2 , 0 }
+                
+                image.scale = { 0.75 , 0.75 }
+                
+                tile_group:add( image )
+
+                rx = rx + ( ( image.w * 0.75 ) + 14 )
+
+            end
+end            
+            
+            
+            self.main_screen:add( tile_group )
+            
+            self.main_focus_rings =
+
+                Group{
+                    name = "focus",
+                    size = screen.size,
+                    position = { 0 , 0 },
+                    scale = { self.x_scale , self.y_scale } ,
+                    children = { self:prepare_image( base , self.main.focus , false , true ) } 
+                }
+            
+            self.main_screen:add( self.main_focus_rings )
+                        
+                        
+            self:main_set_focus( 4 )                        
+            
+            screen:add( self.main_screen )
         
         end,
         
     on_key_down =
     
         function( self , keyval )
-
+            
             if self.app_screen then
             
                 local focused = self.app_screen:find_child( self.app_screen_focus )
@@ -265,14 +472,54 @@ ui =
                         
                         if new_one then
                         
-                            self:app_screen_swap( self.app_screen_focus )
+                            local hide = false
                             
-                            self.app_screen_focus = self:app_screen_swap( new_one )
+                            if type( new_one ) == "table" then
+                            
+                                hide = new_one.hide or false
+                                
+                                new_one = new_one.target
+                            
+                            end
+    
+                        
+                            self:app_screen_swap( self.app_screen_focus , hide )
+                            
+                            if hide then
+                            
+                                self.app_screen_focus = new_one
+                                
+                                self.app_screen:find_child( new_one ):show()
+                                
+                            else
+                            
+                                self.app_screen_focus = self:app_screen_swap( new_one )
+                                
+                            end
                             
                         end
                     
                     end
                 
+            
+                
+                end
+                
+            elseif self.main_screen then
+            
+                local main_nav = {
+                    { [ keys.Right ] = 2 , [ keys.Down ] = 3 },    -- left featured
+                    { [ keys.Left ] = 1 , [ keys.Down ] = 3 },    -- right featured
+                    { [ keys.Up ] = 1 , [ keys.Down ] = 4 },    -- categories
+                    { [ keys.Up ] = 3 }     -- tiles               
+                }
+                
+                local target = main_nav[ self.main_focus ][ keyval ]
+                
+                if target then
+                
+                    self:main_set_focus( target )
+                    
                 end
                 
             end
@@ -281,7 +528,9 @@ ui =
 }
 
 
-ui:load_app_screen( "com.trickplay.1945" )
+--ui:load_app_screen( "com.trickplay.1945" )
+
+ui:load_main_screen()
 
 screen:show()
 
