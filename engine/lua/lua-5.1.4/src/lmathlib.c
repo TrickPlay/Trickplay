362c604ae1916ffe153db6d56a40a5605b4c48ed
Pablo Pissanetzky <pablo@trickplay.com>
2010-05-18 16:48:16 -0700

    Patched Lua to get around LG's rand() bugs. This patch is harmless so it can be used for all of our builds

--- engine/lua/lua-5.1.4/src/lmathlib.c
+++ engine/lua/lua-5.1.4/src/lmathlib.c
@@ -181,7 +181,9 @@ static int math_max (lua_State *L) {
 static int math_random (lua_State *L) {
   /* the `%' avoids the (rare) case of r==1, and is needed also because on
      some systems (SunOS!) `rand()' may return a value larger than RAND_MAX */
-  lua_Number r = (lua_Number)(rand()%RAND_MAX) / (lua_Number)RAND_MAX;
+//  lua_Number r = (lua_Number)(rand()%RAND_MAX) / (lua_Number)RAND_MAX;
+  // PABLO
+  lua_Number r = (lua_Number)(((unsigned int)rand())%RAND_MAX) / (lua_Number)RAND_MAX;
   switch (lua_gettop(L)) {  /* check number of arguments */
     case 0: {  /* no arguments */
       lua_pushnumber(L, r);  /* Number between 0 and 1 */
