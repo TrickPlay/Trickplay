0170b099200926826b6937795b5fd4fe4d6a3eaa
Pablo Pissanetzky <pablo@trickplay.com>
2010-05-26 15:30:13 -0700

    Removed old example code and added boilerplate for external image decoding.
    Former-commit-id: 1727ad63d5e5a4d216d6336e19bfa079e4450432

--- clients/desktop/main.c
+++ clients/desktop/main.c
@@ -3,61 +3,55 @@
 #include <stdio.h>
 
 #include "trickplay/trickplay.h"
+#include "trickplay/image.h"
+
+#define CUSTOM_IMAGE_DECODER 0
 
 //-----------------------------------------------------------------------------
-// Example of a console command handler. It will be invoked when you type /test
-// at the console.
 
-void test_console_command_handler(const char * command,const char * parameters,void * data)
+#if CUSTOM_IMAGE_DECODER
+
+int my_image_decoder( void * buffer, unsigned long int size, TPImage * image, void * data )
 {
-//    printf("This is the test command\n");
+
+    if ( ! image )
+    {
+        // Should look at size bytes in buffer and attempt to detect the image format.
+        // If it is supported, return TP_IMAGE_SUPPORTED_FORMAT
+
+        return TP_IMAGE_UNSUPPORTED_FORMAT;
+    }
+
+    // Should decode the image and return TP_IMAGE_DECODE_OK, TP_IMAGE_UNSUPPORTED_FORMAT
+    // or TP_IMAGE_DECODE_FAILED
+
+    return TP_IMAGE_DECODE_FAILED;
 }
 
-//-----------------------------------------------------------------------------
-// Example notification handler
-
-void app_notification_handler(const char * subject,void * data)
-{
-//    printf("Got app notification '%s'\n",subject);
-}
-
-//-----------------------------------------------------------------------------
-// Example request handler
-
-int keyboard_request_handler(const char * subject,void * data)
-{
-//    printf("Got request '%s'\n",subject);
-    
-    // Grant the request
-    return 1;
-}
+#endif
 
 //-----------------------------------------------------------------------------
 
-int main(int argc,char * argv[])
+int main( int argc, char * argv[ ] )
 {
-    tp_init(&argc,&argv);
-    
+    tp_init( &argc, &argv );
+
     TPContext * context = tp_context_new();
-    
-    if (argc>1)
-        tp_context_set(context,"app.path",argv[1]);
-    
-    // This is completely optional
-    
-    tp_context_add_console_command_handler(context,"test",test_console_command_handler,0);
-    
-    tp_context_add_notification_handler(context,TP_NOTIFICATION_APP_LOADING,app_notification_handler,0);
-    tp_context_add_notification_handler(context,TP_NOTIFICATION_APP_LOAD_FAILED,app_notification_handler,0);
-    tp_context_add_notification_handler(context,TP_NOTIFICATION_APP_LOADED,app_notification_handler,0);
-    tp_context_add_notification_handler(context,TP_NOTIFICATION_APP_CLOSING,app_notification_handler,0);
-    tp_context_add_notification_handler(context,TP_NOTIFICATION_APP_CLOSED,app_notification_handler,0);
-    
-    tp_context_set_request_handler(context,TP_REQUEST_ACQUIRE_KEYBOARD,keyboard_request_handler,0);
-    
-    int result = tp_context_run(context);
-    
-    tp_context_free(context);
-    
+
+    if ( argc > 1 )
+    {
+        tp_context_set( context, "app.path", argv[ 1 ] );
+    }
+
+#if CUSTOM_IMAGE_DECODER
+
+    tp_context_set_image_decoder( context, my_image_decoder, NULL );
+
+#endif
+
+    int result = tp_context_run( context );
+
+    tp_context_free( context );
+
     return result;
 }
