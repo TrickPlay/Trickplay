7d3fa624d8e6e67e145c27e7a9dd3e123686a7b9
Pablo Pissanetzky <pablo@trickplay.com>
2010-05-31 17:55:55 -0700

    Signature verification code - not hooked up to anything
    Former-commit-id: 3151e9937acc049055b3664975b03f6431030892

---
+++ engine/source/signature.h
@@ -0,0 +1,31 @@
+#ifndef _TRICKPLAY_SIGNATURE_H
+#define _TRICKPLAY_SIGNATURE_H
+
+#include <iostream>
+
+#include "common.h"
+
+namespace Signature
+{
+    struct Info
+    {
+        typedef std::list< Info > List;
+
+        String  fingerprint;
+        String  common_name;
+    };
+
+    // A TRUE result means that either the file has no signatures, or that all
+    // signatures are OK. TRUE with an empty list, means no signatures.
+    //
+    // FALSE means that something went wrong checking the signatures, or that
+    // at least one of them is invalid.
+
+    bool get_signatures( const gchar * filename, Info::List & signatures );
+
+    bool get_signatures( gpointer data, gsize size, Info::List & signatures );
+
+    bool get_signatures( std::istream & stream, Info::List & signatures );
+};
+
+#endif // _TRICKPLAY_SIGNATURE_H
