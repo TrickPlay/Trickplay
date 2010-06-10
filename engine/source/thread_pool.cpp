156130b1d22b61d98a264910a1760f33d2fae4d8
Pablo Pissanetzky <pablo@trickplay.com>
2010-06-09 17:58:47 -0700

    Added timing information to thread pool tasks - to debug flickr lock-up.

--- engine/source/thread_pool.cpp
+++ engine/source/thread_pool.cpp
@@ -2,8 +2,30 @@
 
 //=============================================================================
 
+ThreadPool::Task::Task()
+{
+#if TP_THREADPOOL_DEBUG
+
+    static guint64 next_id = 1;
+
+    id = next_id++;
+
+#endif
+}
+
+
 ThreadPool::Task::~Task()
 {
+#if TP_THREADPOOL_DEBUG
+
+    g_debug( "TASK %4.4" G_GUINT64_FORMAT " : %7.3f : %7.3f : %7.3f : %7.3f : TOTAL %8.3f",
+            id,
+            time_to_pool * 1000,
+            time_to_process * 1000,
+            time_to_main_thread * 1000,
+            end_time * 1000,
+            ( time_to_pool + time_to_process + time_to_main_thread + end_time ) * 1000 );
+#endif
 }
 
 //.............................................................................
@@ -16,6 +38,50 @@ void ThreadPool::Task::process()
 
 void ThreadPool::Task::process_main_thread()
 {
+}
+
+//.............................................................................
+
+void ThreadPool::Task::do_process()
+{
+#if TP_THREADPOOL_DEBUG
+
+    time_to_pool = timer.elapsed();
+
+    timer.reset();
+
+#endif
+
+    process();
+
+#if TP_THREADPOOL_DEBUG
+
+    time_to_process = timer.elapsed();
+
+    timer.reset();
+
+#endif
+}
+
+//.............................................................................
+
+void ThreadPool::Task::do_process_main_thread()
+{
+#if TP_THREADPOOL_DEBUG
+
+    time_to_main_thread = timer.elapsed();
+
+    timer.reset();
+
+#endif
+
+    process_main_thread();
+
+#if TP_THREADPOOL_DEBUG
+
+    end_time = timer.elapsed();
+
+#endif
 }
 
 //=============================================================================
@@ -42,16 +108,16 @@ ThreadPool::~ThreadPool()
 
 void ThreadPool::thread_function( gpointer task, gpointer )
 {
-    ( ( ThreadPool::Task * ) task )->process();
+    ( ( ThreadPool::Task * ) task )->do_process();
 
-    g_idle_add_full( G_PRIORITY_DEFAULT_IDLE, idle_function, task, destroy_task );
+    g_idle_add_full( G_PRIORITY_LOW, idle_function, task, destroy_task );
 }
 
 //.............................................................................
 
 gboolean ThreadPool::idle_function( gpointer task )
 {
-    ( ( ThreadPool::Task * ) task )->process_main_thread();
+    ( ( ThreadPool::Task * ) task )->do_process_main_thread();
 
     return FALSE;
 }
@@ -61,6 +127,12 @@ gboolean ThreadPool::idle_function( gpointer task )
 void ThreadPool::push( Task * task )
 {
     g_assert( task );
+
+#if TP_THREADPOOL_DEBUG
+
+    task->timer.reset();
+
+#endif
 
     g_thread_pool_push( pool, task, NULL );
 }
