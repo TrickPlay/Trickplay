f8cacd1d1d8188892e9b610df0811707218fd9d4
Pablo Pissanetzky <pablo@trickplay.com>
2010-06-28 18:22:12 -0700

    We have a (crude) debugger
    Former-commit-id: f2dcf98fefcb0f1612228acf055154de7aa0322a

---
+++ engine/source/debugger.h
@@ -0,0 +1,42 @@
+#ifndef _TRICKPLAY_DEBUGGER_H
+#define _TRICKPLAY_DEBUGGER_H
+
+#include "common.h"
+
+class App;
+
+class Debugger
+{
+public:
+
+    Debugger( App * app );
+
+    ~Debugger();
+
+    void install();
+
+    void break_next_line();
+
+private:
+
+    static void command_handler( const char * command, const char * parameters, void * me );
+
+    void handle_command( const char * parameters );
+
+    static void lua_hook( lua_State * L, lua_Debug * ar );
+
+    void debug_break( lua_State * L, lua_Debug * ar );
+
+    App *           app;
+    bool            installed;
+
+    bool            break_next;
+
+    typedef std::pair< String, int > Breakpoint;
+
+    typedef std::list< Breakpoint > BreakpointList;
+
+    BreakpointList  breakpoints;
+};
+
+#endif // _TRICKPLAY_DEBUGGER_H
