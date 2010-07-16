ec68a0223966f90b531cee78cf6a80f9f379ab0b
Viraaj Marathe <viraaj.marathe@trickplay.com>
2010-07-16 14:57:16 -0700

    Skeleton working towers can be added

---
+++ apps/td/Tower.lua
@@ -0,0 +1,34 @@
+Tower = {}
+
+function Tower:new(args)
+	local towerType = args.towerType
+	local damage = args.damage
+	local range = args.range
+	local direction = args.direction
+	local cooldown = args.cooldown
+	local towerImage = Image {src = towerType}
+
+	local object = {
+		towerType = towerType,
+		damage = damage,
+		range = range,
+		direction = direction,
+		cooldown = cooldown,
+		towerImage = towerImage
+   }
+   setmetatable(object, self)
+   self.__index = self
+   return object
+end
+
+function Tower:destroy()
+
+end
+
+function Tower:attack()
+
+end
+
+function Tower:render()
+	
+end
