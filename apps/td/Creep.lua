63a575560e3a5f368f4ce5d1f249eef4b4ba452c
Viraaj Marathe <viraaj.marathe@trickplay.com>
2010-07-19 13:13:31 -0700

    path finding works before entering the screen

--- apps/td/Creep.lua
+++ apps/td/Creep.lua
@@ -1,21 +1,19 @@
 Creep = { }
 
 function Creep:new(args, x, y)
-	local x = x
-	local y = y
 	local creepType = args.creepType
 	local hp = args.hp
 	local speed = args.speed
-	local direction = args.direction or {1,0}
-	local creepImage = Image { src = creepType , x = x, y = y}
+	local direction = args.direction or {0,1}
+	local creepImage = Image { src = creepType , x = x, y = y-60}
+	local path = {}
 	local object = {
-		x = x,
-		y = y,
 		hp = hp,
 		speed = speed,
 		direction = direction,
 		creepType = creepType,
-		creepImage = creepImage
+		creepImage = creepImage,
+		path = path
    }
    setmetatable(object, self)
    self.__index = self
@@ -23,9 +21,27 @@ function Creep:new(args, x, y)
 end
 
 function Creep:render(seconds)
-	if (not self.creepImage.is_animating) then 	
-		self.creepImage:animate {duration = 1/self.speed * 10000, x = self.creepImage.x + self.direction[1] * 60}
+
+	if (self.creepImage.x >= 0) then
+		if (#self.path==0) then
+			local c = game.board:getPathData()
+			c[7][1] = 1
+			recordPath(c, {7, 1} , {7, 32}, 1)
+			self.path = {}
+			tracePath(c, {7, 32}, c[7][32], self.path)
+		end
 	end
---	self.creepImage.x = self.creepImage.x + self.direction[1]*seconds*self.speed
+
+	if (not self.creepImage.is_animating) then
+		local path = self.path
+		if #path > 1 then self.direction = {  - path[#path][1] + path[#path-1][1], - path[#path][2] + path[#path-1][2] } path[#path] = nil end
+--		print (self.direction[1], self.direction[2])
+		self.creepImage:animate {duration = 1/self.speed * 10000, x = self.creepImage.x + self.direction[2] * 60, y = self.creepImage.y + self.direction[1] * 60}
+--		self.creepImage.x = self.creepImage.x + self.direction[2]*60*seconds
+--		self.creepImage.y = self.creepImage.y + self.direction[1]*60*seconds
+
+	end
+	
+	--	self.creepImage.x = self.creepImage.x + self.direction[1]*seconds*self.speed
 --	self.creepImage.y = self.creepImage.y + self.direction[2]*seconds*self.speed
 end
