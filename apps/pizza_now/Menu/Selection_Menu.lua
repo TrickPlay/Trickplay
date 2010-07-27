426ff0282cb5a58437e7535c922b28b05369c32f
Alex Indaco <alex.indaco@trickplay.com>
2010-07-27 11:11:47 -0700

    forgot one
    Former-commit-id: cd12af67f845da9f9bbafe6885e7a1b55d7939e3

---
+++ apps/pizza_now/Menu/Selection_Menu.lua
@@ -0,0 +1,21 @@
+Selection_Menu = Class(function(self)
+   self.Options = {}
+end)
+
+   SP = Selection_Menu()
+   SP.Options = {
+      {Name = "Honolulu Hawaiian Pizza"   },
+      {Name = "Philly Cheese Steak Pizza" },
+      {Name = "Pacific Veggie Pizza"      },
+      {Name = "Cali Chicken Ranch Pizza"  },
+      {Name = "Fiery Hawaiian Pizza"      },
+      {Name = "Buffalo Chicken Pizza"     },
+      {Name = "Memphis BBQ Chicken Pizza" },
+      {Name = "Americas Favorite Feast"   },
+      {Name = "Bacon Cheeseburger Feast"  },
+      {Name = "Deluxe Feast"              },
+      {Name = "ExtravaganZZa Feast"       },
+      {Name = "MeatZZa Feast"             },
+      {Name = "Ultimate Pepperoni Feast"  }
+   }
+
