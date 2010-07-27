cad2a68b567428bab68d1f352957d4c3667ed6e2
Rex Fenley <rex.fenley@trickplay.com>
2010-07-27 11:33:33 -0700

    Fixed the broken wind mill
    Former-commit-id: baae3bea7034a2bacbe5e35884415dba09db89a0

--- apps/pizza_now/views/WindMillView.lua
+++ apps/pizza_now/views/WindMillView.lua
@@ -188,7 +188,6 @@ WindMillView = Class(View, function(view, model, ...)
         local comp = model:get_active_component()
         if comp == Components.CUSTOMIZE_ITEM then
             print("Showing Customize Item UI")
-	    set()
             CustomizeItemAnimations[controller:get_selected_index()]()
         else
             print("Hiding Customize Item UI")
