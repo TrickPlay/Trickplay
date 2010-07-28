97e78fe6f91e54d8fab4fffb2e15cdc4bfae1ad2
Rex Fenley <rex.fenley@trickplay.com>
2010-07-28 15:00:46 -0700

    topping_dropping folder includes fixed functions in main.lua
    Former-commit-id: f8e99104b4370f5ba308ff826ac92f61a7c3d076

--- apps/pizza_now/topping_dropping/main.lua
+++ apps/pizza_now/topping_dropping/main.lua
@@ -34,23 +34,35 @@ pizzagroup:add(pizza)
 pizzagroup:add(sauce)
 pizzagroup:add(cheese)
 screen:add(pizzagroup)
-function distribute_topping(topping, side, amount, group)
+function distribute_topping(topping, side, amount, group, pizzagroup)
     --set up random variables
     local distribution = 1
     local slices = 8
-    if(side == Sides.RIGHT or side == Sides.LEFT) then
-        distribution = .5
-    end
 
-    slices = slices * distribution
-    local range = 180*distribution/slices
+    local range = 180/slices
     local toppingsPerSlice = 3
-    local toppingLightGroup = Group{name = "topping_light"}
-    local toppingNormalGroup = Group{name = "topping_normal"}
-    local toppingExtraGroup = Group{name = "topping_extra"}
-    group:add(toppingLightGroup)
-    group:add(toppingNormalGroup)
-    group:add(toppingExtraGroup)
+    --Groups for the left and right side of the pizza && amount of topping
+    local toppingLightRightGroup = Group{name = "topping_light_right"}
+    local toppingNormalRightGroup = Group{name = "topping_normal_right"}
+    local toppingExtraRightGroup = Group{name = "topping_extra_right"}
+    local toppingRightGroup = Group{name = "right_side"}
+
+    local toppingLightLeftGroup = Group{name = "topping_light_left"}
+    local toppingNormalLeftGroup = Group{name = "topping_normal_left"}
+    local toppingExtraLeftGroup = Group{name = "topping_extra_left"}
+    local toppingLeftGroup = Group{name = "left_side"}
+
+    toppingRightGroup:add(toppingLightRightGroup)
+    toppingRightGroup:add(toppingNormalRightGroup)
+    toppingRightGroup:add(toppingExtraRightGroup)
+
+    toppingLeftGroup:add(toppingLightLeftGroup)
+    toppingLeftGroup:add(toppingNormalLeftGroup)
+    toppingLeftGroup:add(toppingExtraLeftGroup)
+
+    group:add(toppingRightGroup)
+    group:add(toppingLeftGroup)
+
     pizzagroup:add(group)
 
     --determine distribution
@@ -60,9 +72,7 @@ function distribute_topping(topping, side, amount, group)
             local radius = top * 140 * (seed+toppingsPerSlice+1)/(toppingsPerSlice+4)
             local degrees = math.random(range)
             local angle = (degrees + range*(slice-1)) * math.pi/180
-            if(side == Sides.LEFT) then
-                angle = angle + math.pi * .5
-            end
+
             local clone = Clone{source = topping}
             local x = radius*math.cos(angle)+400
             local y = -1*radius*math.sin(angle)+400
@@ -70,11 +80,25 @@ function distribute_topping(topping, side, amount, group)
             print("radians: "..angle..", degrees: "..degrees..", radius: "..radius)
             local groupseed = math.random(3)
             if(Amount.LIGHT == groupseed) then
-                toppingLightGroup:add(clone)
+                if(slice <= 4) then
+                    toppingLightRightGroup:add(clone)
+                else
+                    toppingLightLeftGroup:add(clone)
+                end
             elseif(Amount.NORMAL == groupseed) then
-                toppingNormalGroup:add(clone)
+                if(slice <= 4) then
+                    toppingNormalRightGroup:add(clone)
+                    print("here1")
+                else
+                    print("here2")
+                    toppingNormalLeftGroup:add(clone)
+                end
             elseif(Amount.EXTRA == groupseed) then
-                toppingExtraGroup:add(clone)
+                if(slice <= 4) then
+                    toppingExtraRightGroup:add(clone)
+                else
+                    toppingExtraLeftGroup:add(clone)
+                end
             else
                 error("error: a topping was not added correctly!")
             end
@@ -83,14 +107,15 @@ function distribute_topping(topping, side, amount, group)
     --TODO: add one in the very center
 
 end
-function topping_dropping(topping, side, amount, group)
+function topping_dropping(topping, side, amount, toppinggroup, pizzagroup)
     assert(topping)
     assert(side)
     assert(amount)
+    assert(pizzagroup)
     --add group for type
-    if(not group) then
-        group = Group()
-        distribute_topping(topping, side, amount, group)
+    if(not toppinggroup) then
+        toppinggroup = Group()
+        distribute_topping(topping, side, amount, toppinggroup, pizzagroup)
     end
     --add topping to screen to make it clonable
     if not topping.parent then
@@ -100,19 +125,34 @@ function topping_dropping(topping, side, amount, group)
 
         
     --determine how many should be shown
-    group:hide_all()
-    group:show()
+    toppinggroup:find_child("right_side"):hide_all()
+    toppinggroup:find_child("left_side"):hide_all()
+    --show the correct side of the pizza
+    if(Sides.RIGHT == side or Sides.WHOLE == side) then
+        toppinggroup:find_child("right_side"):show()
+    end
+    if(Sides.LEFT == side or Sides.WHOLE == side) then
+        toppinggroup:find_child("left_side"):show()
+    end
+    --show the correct amount
     if(Amount.LIGHT == amount) then
-        assert(group:find_child("topping_light"))
-        group:find_child("topping_light"):show()
+        toppinggroup:find_child("topping_light_left"):show()
+        toppinggroup:find_child("topping_light_right"):show()
     elseif(Amount.NORMAL == amount) then
-        group:find_child("topping_light"):show()
-        group:find_child("topping_normal"):show()
+        toppinggroup:find_child("topping_light_left"):show()
+        toppinggroup:find_child("topping_light_right"):show()
+        toppinggroup:find_child("topping_normal_left"):show()
+        toppinggroup:find_child("topping_normal_right"):show()
     elseif(Amount.EXTRA == amount) then
-        group:show_all()
+        toppinggroup:find_child("topping_light_left"):show()
+        toppinggroup:find_child("topping_light_right"):show()
+        toppinggroup:find_child("topping_normal_left"):show()
+        toppinggroup:find_child("topping_normal_right"):show()
+        toppinggroup:find_child("topping_extra_left"):show()
+        toppinggroup:find_child("topping_extra_right"):show()
     end
 end
 
 pepporonigroup = nil
-topping_dropping(topping, Sides.LEFT, Amount.NORMAL, pepporonigroup)
+topping_dropping(topping, Sides.RIGHT, Amount.EXTRA, pepporonigroup, pizzagroup)
 screen:show()
