6f21b29cce4860d540b4c11f144a5434f5199e23
Devin Abbott <devin.abbott@trickplay.com>
2010-07-28 12:19:17 -0700

    Bullets added, not moving ones yet
    Former-commit-id: 2adc5d2edf78cc492ff4e5d81daab53797a3b4d4

--- apps/td/themes/pacman/pacman.lua
+++ apps/td/themes/pacman/pacman.lua
@@ -1,9 +1,13 @@
 local pacman = {
 		themeName = "pacman",
 		mainMenuBackground = "",
+		bullets = {
+			{id = 1, frames = 5, im = "Ringwave"},
+			{id = 2, im = "Normal"},
+		},
 		creeps = {
-				{ hp = 100, speed = 30, bounty = 10, flying = false, frames = 3, name = "NormalCreep"},
-				{ hp = 300, speed = 50, bounty = 15, flying = false, frames = 3, name = "MediumCreep"},
+			{ hp = 100, speed = 30, bounty = 10, flying = false, frames = 3, name = "NormalCreep"},
+			{ hp = 300, speed = 50, bounty = 15, flying = false, frames = 3, name = "MediumCreep"},
             { hp = 1000, speed = 40, bounty = 15, flying = false, frames = 3, name = "HardCreep"},
             { hp = 20000, speed = 40, bounty = 200, flying = false, frames = 3, name = "BossCreep"},
             { hp = 80, speed = 35, bounty = 40, flying = true, frames = 3, name = "FlyingCreep"}
@@ -24,7 +28,7 @@ local pacman = {
 										}
 						},
 				nukeTower =
-						{ damage = 2000, range = 600, cooldown = 5, cost = 500, slowammount = 100, slow = true, splash = true, frames = 8, splashradius = 360, name = "NukeTower"
+						{ damage = 400, range = 600, cooldown = 2, cost = 500, slowammount = 100, slow = true, splash = true, frames = 8, splashradius = 360, name = "NukeTower", bullet = 1, bulletframes = 5,
 						},
 				wall =
 						{ damage = 0, range = 0, cooldown = 1000, cost = 5, slowammount = 0, frames = 1, slow = false, name = "Wall"
@@ -36,11 +40,11 @@ local pacman = {
 		wave = dofile("themes/pacman/round1.lua"),
 		
 		waveTable = {
-			["NormalCreep"] = 1,
-			["MediumCreep"] = 2,
-         ["HardCreep"] = 3,
-         ["BossCreep"] = 4,
-         ["FlyingCreep"] = 5,
+		["NormalCreep"] = 1,
+		["MediumCreep"] = 2,
+        ["HardCreep"] = 3,
+        ["BossCreep"] = 4,
+        ["FlyingCreep"] = 5,
 		}           
 }
 
