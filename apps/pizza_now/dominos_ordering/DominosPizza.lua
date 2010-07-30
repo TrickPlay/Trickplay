4b5f91ba567343524b8725595a4e9a3f35141974
Darren Yin <darren.yin@trickplay.com>
2010-07-29 18:18:07 -0700

    Second part of my stuff, I'm almost finished making it callable.
    Created my own Globals.lua, which we'll merge on Friday.

--- apps/pizza_now/dominos_ordering/DominosPizza.lua
+++ apps/pizza_now/dominos_ordering/DominosPizza.lua
@@ -1,80 +1,8 @@
-local Crusts = {
-   HAND_TOSSED = {name="handtossed", canon="HANDTOSS"},
-   DEEP_DISH = {name="deep dish", canon="DEEPDISH"},
-   CRUNCHY_THIN = {name="crunchy thin", canon="THIN"},
-   BROOKLYN_STYLE = {name="brooklyn style", canon="BK"}
-}
-local Sizes = {
-   SMALL = {name="small", diam=10},
-   MEDIUM = {name="medium", diam=12},
-   LARGE = {name="large", diam=14},
-   XLARGE = {name="x-large", diam=16}
-}
-
--- roughly a struct.
-local function Topping(name, crypt_code)
-   assert(name)
-   assert(crypt_code)
-   return {name=name, crypt_code=crypt_code, isTopping=true}
+if not GLOBALS_LOADED then
+   dofile("Globals.lua")
 end
 
-local Toppings = {
-   -- MEATS
-   PEPPERONI = Topping("Pepperoni","P"),
-   XL_PEPPERONI = Topping("Extra Large Pepperoni","Pl"),
-   SLICED_ITALIAN_SAUSAGE = Topping("Sliced Italian Sausage","Sb"),
-   ITALIAN_SAUSAGE = Topping("Italian Sausage","S"),
-   BEEF = Topping("Beef", "B"),
-   HAM = Topping("Ham","H"),
-   BACON = Topping("Bacon","K"),
-   CHICKEN = Topping("Premium Chicken","Du"),
-   SALAMI = Topping("Salami","Sa"),
-   PHILLY_STEAK = Topping("philly-steak","Pm"),
-   -- UNMEATS
-   GREEN_PEPPERS = Topping("Green Peppers","G"),
-   BLACK_OLIVES = Topping("Black Olives","R"),
-   PINEAPPLE = Topping("Pineapple","N"),
-   MUSHROOMS = Topping("Mushrooms","M"),
-   ONIONS = Topping("Onions","O"),
-   JALAPENOS = Topping("Jalapeno Peppers","J"),
-   BANANA_PEPPERS = Topping("Banana Peppers","Z"),
-   SPINACH = Topping("Spinach", "Si"),
-   RED_PEPPERS = Topping("Roasted Red Peppers","Rr"),
-   CHEDDAR_CHEESE = Topping("Cheddar Cheese","E"),
-   PROVOLONE = Topping("Shredded Provolone Cheese","Cp"),
-   PARMESAN = Topping("Shredded Parmesan","Cs"),
-   FETA = Topping("Feta Cheese","Fe"),
-   GARLIC = Topping("Garlic","F"),
-   TOMATOES = Topping("Diced Tomatoes","Td"),
-   HOT_SAUCE = Topping("Hot Sauce","Ht")
-}
-
-local function Sauce(name,crypt_code)
-   assert(name)
-   assert(crypt_code)
-   return {name=name, crypt_code=crypt_code, isSauce = true}
-end
-
-local Sauces = {
-   ROBUST = Sauce("Inspired Robust Sauce","X"),
-   WHITE = Sauce("White Sauce","Xw"),
-   MARINARA = Sauce("Hearty Marinara Sauce","Xm"),
-   BBQ = Sauce("BBQ Sauce","Bq")
-}
-
-local Placement = {
-   WHOLE = "W",
-   LEFT = "1",
-   RIGHT = "2"
-}
-
-local Coverage = {
-   NORMAL = "1",
-   EXTRA = "1.5",
-   LIGHT = ".5"
-}
-
-local size_crust_avlblty = {
+local size_crust_avlble = {
    [Crusts.HAND_TOSSED] = {
       [Sizes.SMALL] = true,
       [Sizes.MEDIUM] = true,
@@ -94,7 +22,7 @@ local size_crust_avlblty = {
    }
 }
 
-local DEFAULT_CRUST = Crusts.HANDTOSSED
+local DEFAULT_CRUST = Crusts.HAND_TOSSED
 local DEFAULT_SIZE = Sizes.LARGE
 local DEFAULT_CHEESE_CUSTOMIZATION = {enabled=true, placement=Placement.WHOLE, coverage=Coverage.NORMAL}
 local DEFAULT_SAUCE_CUSTOMIZATION = {enabled=true, sauce=Sauces.ROBUST, coverage=Coverage.NORMAL}
@@ -107,25 +35,49 @@ DominosPizza = Class(nil,
       self.sauce_customization = sauce_customization or DEFAULT_SAUCE_CUSTOMIZATION
       self.toppings = toppings or {}
       
-      -- DEBUGGING PURPOSES, take out in production code
-      assert(size_crust_avlble[self.crust][self.size], "Illegal crust/size combination: " .. self.crust .. "/" .. self.size.name)
-      for topping, tweaks in pairs(self.toppings) do
-         assert(topping.isTopping, tostring(topping) .. " is not topping")
-         local coverage_found = false
-         for k, v in pairs(Coverage) do
-            if tweaks.coverage == v then
-               coverage_found = true
-               break
+      local function checkRep()
+         assert(size_crust_avlble[self.crust][self.size], "Illegal crust/size combination: " .. self.crust.name .. "/" .. self.size.name)
+         for topping, tweaks in pairs(self.toppings) do
+            assert(topping.isTopping, tostring(topping) .. " is not topping")
+            local coverage_found = false
+            for k, v in pairs(Coverage) do
+               if tweaks.coverage == v then
+                  coverage_found = true
+                  break
+               end
             end
-         end
-         assert(coverage_found, "Invalid Coverage for topping " .. topping.name)
-         local placement_found = false
-         for k, v in pairs(Placement) do
-            if tweaks.placement == v then
-               placement_found = true
-               break
+            assert(coverage_found, "Invalid Coverage for topping " .. topping.name)
+            local placement_found = false
+            for k, v in pairs(Placement) do
+               if tweaks.placement == v then
+                  placement_found = true
+                  break
+               end
             end
+            assert(placement_found, "Placement not valid for topping " .. topping.name)
          end
-         assert(placement_found, "Placement not valid for topping " .. topping.name)
       end
+
+      -- debug
+      checkRep()
    end)
+
+CHEESE_PIZZA = DominosPizza(
+   DEFAULT_CRUST,
+   DEFAULT_SIZE,
+   DEFAULT_CHEESE_CUSTOMIZATION,
+   DEFAULT_SAUCE_CUSTOMIZATION
+)
+
+PEPPERONI_PIZZA = DominosPizza(
+   DEFAULT_CRUST,
+   DEFAULT_SIZE,
+   DEFAULT_CHEESE_CUSTOMIZATION,
+   DEFAULT_SAUCE_CUSTOMIZATION,
+   {
+      [Toppings.PEPPERONI] = {
+         coverage = Coverage.NORMAL,
+         placement = Placement.WHOLE
+      }
+   }
+)
--- apps/pizza_now/dominos_ordering/Utils.lua
+++ apps/pizza_now/dominos_ordering/Utils.lua
@@ -86,3 +86,119 @@ function form_print(formdata)
    table.sort(temp_array)
    print("\n" .. table.concat(temp_array,"\n"))
 end
+
+function get_response(url, formdata)
+   local request = URLRequest{url=url}
+   if formdata then
+      request.method="POST"
+      request.body=urlencode(formdata)
+   end
+
+   local response = request:perform()
+   assert(not response.failed)
+   return response
+end
+
+function parse_form(html, form_id, debug)
+   local match = string.match
+   local gmatch = string.gmatch
+   local formdata = {}
+   local id
+   local input_name
+   local input_value
+   local input_type
+   local input_checked
+   local input_selected
+
+   local print =
+      function(...)
+         if debug then print(...) end
+      end
+
+   -- radio INPUT elements unchecked and checked
+   local unchecked = {}
+   local checked = {}
+
+   for form_attrs, form_html in gmatch(html, "<form (.-)>(.-)</form>") do
+      id = match(form_attrs, "id=['\"](.-)['\"]")
+      local debugstr = "form with id " .. id .. " found"
+      local width = string.len(debugstr)
+      print("\n" ..
+         string.rep("=", width+4) .. "\n" ..
+         "= " .. debugstr .. " =\n" ..
+         string.rep("=", width+4) .. "\n")
+      if (not form_id) or id == form_id then
+         for input_attrs in gmatch(form_html, "<input (.-)>") do
+            input_name = match(input_attrs, "name=['\"](.-)['\"]")
+            input_value = match(input_attrs, "value=['\"](.-)['\"]") or ""
+            input_type = match(input_attrs, "type=['\"](.-)['\"]")
+            input_checked = match(input_attrs, "checked=['\"](.-)['\"]") or match(input_attrs, " checked")
+
+            if not input_type then
+               print("No input type attr in input_attrs: " .. input_attrs)
+               input_type = "text"
+            end
+
+            assert(input_name, "No input name attr in input_attrs: " .. input_attrs)
+            
+            if input_type == "hidden" then
+               print(input_name, "=", input_value)
+               formdata[input_name] = input_value
+            elseif input_type == "text" then
+               print(input_name, "=", input_value)
+               formdata[input_name] = input_value
+            elseif input_type == "radio" then
+               if input_checked then
+                  assert(not checked[input_name], "radio input " .. input_name .. " already checked")
+                  print(input_name, "=", input_value)
+                  formdata[input_name] = input_value
+                  checked[input_name] = true
+                  if unchecked[input_name] then
+                     unchecked[input_name] = nil
+                  end
+               else
+                  if checked[input_name] then
+                     assert(not unchecked[input_name])
+                  elseif not unchecked[input_name] then
+                     unchecked[input_name] = input_value
+                  end
+               end
+            elseif input_type == "checkbox" then
+               if input_checked then
+                  print(input_name, "=", input_value)
+                  formdata[input_name] = input_value
+               end
+            end
+         end -- matching input tags
+
+         for select_attrs, select_html in gmatch(form_html, "<select (.-)>(.-)</select>") do
+            input_name = match(select_attrs, "name=['\"](.-)['\"]")
+            
+            local opt_selected = false
+            local first_option_val = nil
+            for option_attrs, fallback_val in gmatch(select_html, "<option (.-)>(.-)</option>") do
+               input_value = match(option_attrs, "value=['\"](.-)['\"]") or fallback_val
+               input_selected = match(option_attrs, "selected=['\"](.-)['\"]") or match(option_attrs, " selected")
+               if input_selected and not formdata[input_name] then
+                  print(input_name, "=", input_value)
+                  formdata[input_name] = input_value
+                  opt_selected = true
+               elseif not first_option_val then
+                  first_option_val = input_value
+               end
+            end
+            -- if not opt_selected then
+            --    print("SELECT element " .. input_name .. " has no value selected, setting to " .. first_option_val)
+            --    print(input_name, "=", first_option_val)
+            --    formdata[input_name] = first_option_val
+            -- end
+         end
+      end
+   end
+   -- for k,v in pairs(unchecked) do
+   --    print("Radio input with name " .. k .. " left unchecked, defaulting to " .. v)
+   --    formdata[k] = v
+   -- end
+
+   return formdata
+end
