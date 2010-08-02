c2dee35a8eb923b927a9f1c46d8b58db9f0130c7
Devin Abbott <devin.abbott@trickplay.com>
2010-08-02 16:45:44 -0700

    Robots vs. Zombies 1.0

--- apps/td/themes/robot/coop.lua
+++ apps/td/themes/robot/coop.lua
@@ -132,6 +132,168 @@ wave = {
                 },
                 size = 1,
         },
+        
+         -- Wave 11
+        {
+                {
+                        {name = "MediumCreep"},
+                        size = 10,
+                        speed = 1.5,
+                        buffs = {hp = 3.5, speed = 1.2},
+                },
+                size = 10,
+        },
+        
+        -- Wave 12
+        {
+                {
+                        {name = "MediumCreep"},
+                        size = 10,
+                        speed = 1.5,
+                        buffs = {hp = 2.5, speed = 1.2},
+                },
+                {
+                        {name = "NormalCreep"},
+                        {name = "NormalCreep"},
+                        size = 30,
+                        speed = 1.5,
+                        buffs = {hp = 3.5, speed = 1.2},
+                },
+                size = 40,
+        },
+        
+        -- Wave 13
+        {
+                {
+                        {name = "MediumCreep"},
+                        {name = "HardCreep"},
+                        size = 20,
+                        speed = 2,
+                        buffs = {hp = 2.5, speed = 1.2},
+                },
+                size = 20,
+        },
+        
+        -- Wave 14
+        {
+                {
+                        {name = "NormalCreep"},
+                        {name = "MediumCreep"},
+                        size = 20,
+                        speed = 1.5,
+                        buffs = {hp = 4, speed = 1.2},
+                },
+                {
+                        {name = "HardCreep"},
+                        size = 5,
+                        speed = 2,
+                        buffs = {hp = 10, speed = .4},
+                },
+                size = 25,
+        },
+        
+        -- Wave 15
+        {
+                {
+                        {name = "MediumCreep"},
+                        size = 20,
+                        speed = 1,
+                        buffs = { hp = 15 }
+                },
+                {
+                        {name = "HardCreep"},
+                        size = 10,
+                        speed = 1.5,
+                        buffs = { hp = 5 }
+                },
+                size = 30,
+        },
+        
+        
+        
+        
+        
+        
+        
+        
+         -- Wave 16
+        {
+                {
+                        {name = "MediumCreep"},
+                        size = 10,
+                        speed = 1.5,
+                        buffs = {hp = 7, speed = 1.5},
+                },
+                size = 10,
+        },
+        
+        -- Wave 17
+        {
+                {
+                        {name = "MediumCreep"},
+                        size = 10,
+                        speed = 1.5,
+                        buffs = {hp = 5, speed = 1.2},
+                },
+                {
+                        {name = "NormalCreep"},
+                        {name = "NormalCreep"},
+                        size = 30,
+                        speed = 1.5,
+                        buffs = {hp = 9, speed = 1.2},
+                },
+                size = 40,
+        },
+        
+        -- Wave 18
+        {
+                {
+                        {name = "MediumCreep"},
+                        {name = "HardCreep"},
+                        size = 20,
+                        speed = 2,
+                        buffs = {hp = 6, speed = 1.2},
+                },
+                size = 20,
+        },
+        
+        -- Wave 19
+        {
+                {
+                        {name = "NormalCreep"},
+                        {name = "MediumCreep"},
+                        size = 20,
+                        speed = 1.5,
+                        buffs = {hp = 8, speed = 1.2},
+                },
+                {
+                        {name = "HardCreep"},
+                        size = 5,
+                        speed = 2,
+                        buffs = {hp = 20, speed = .4},
+                },
+                size = 25,
+        },
+        
+        -- Wave 20
+        {
+                {
+                        {name = "NormalCreep"},
+                        {name = "MediumCreep"},
+                        {name = "HardCreep"},
+                        {name = "FlyingCreep"},
+                        size = 100,
+                        speed = 1.5,
+                        buffs = { hp = 15 }
+                },
+                {
+                        {name = "BossCreep"},
+                        size = 10,
+                        speed = 4,
+                        buffs = { hp = 5 }
+                },
+                size = 110,
+        },
 
 
 }
