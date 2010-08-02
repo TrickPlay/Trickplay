bfec187bb44ba796c022c966578d72eb075f595e
Rex Fenley <rex.fenley@trickplay.com>
2010-08-02 16:51:32 -0700

    ProviderSelectionView/Controller improved

--- apps/pizza_now/controllers/ProvidersCarouselController.lua
+++ apps/pizza_now/controllers/ProvidersCarouselController.lua
@@ -6,14 +6,19 @@ ProvidersCarouselController = Class(Controller, function(self, view, ...)
     local CarouselKeyTable = {
         [keys.Left]   = function(self) self:move_left()  end,
         [keys.Right]  = function(self) self:move_right() end,
-       --[[ 
+        ---[[ 
         [keys.Return] =
         function(self)
+            self:get_model():set_active_component(Components.FOOD_SELECTION)
+            self:get_model():notify()
             -- compromise so that there's not a full-on lua panic,
             -- but the error message still displays on screen
+            --[[
             local success, error_msg = pcall(MenuItemCallbacks[selected], self)
             if not success then print(error_msg) end
-        end--]]
+            --]]
+        end
+        --]]
     }
 
     function self:on_key_down(k)
