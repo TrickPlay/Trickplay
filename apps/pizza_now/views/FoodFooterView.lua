e3c9eb84a02c939ca03f4dc2123be4f65f12b807
Rex Fenley <rex.fenley@trickplay.com>
2010-08-05 17:08:57 -0700

    Food selection bottom bar updated
    Former-commit-id: 3f8ac89c432e1e3ccd95cb1f7250812dc604b713

--- apps/pizza_now/views/FoodFooterView.lua
+++ apps/pizza_now/views/FoodFooterView.lua
@@ -7,27 +7,49 @@ FoodFooterView = Class(View, function(view, model, ...)
     view.ui=Group{name="Food Footer UI", position={0,960}, opacity=255}
     view.prev_cart_size = #model.cart
     view.bar = Image{
-                position = {0,0},
-                width = 1920,
-                tile = {false,true},
-                src="assets/OrderBarBase.png"
+            position = {0,0},
+            width = 1920,
+            tile = {false,true},
+            src="assets/OrderBarBase.png"
     }
-    view.back = Text{
-            position={0, 0},
-            font  = DEFAULT_FONT,
-            color = Colors.BLACK,
-            text = "Go Back"
-        }
-    view.checkout = Text{
-            position={1700, 0},
-            font  = DEFAULT_FONT,
-            color = Colors.BLACK,
-            text = "Checkout"
-        }
+    view.backText = Text{
+        position={0, 0},
+        font  = DEFAULT_FONT,
+        color = Colors.BLACK,
+        text = "Go Back"
+    }
+    view.checkoutText = Text{
+        position={1700, 0},
+        font  = DEFAULT_FONT,
+        color = Colors.BLACK,
+        text = "Checkout"
+    }
+    --interactive images
+    view.back = FocusableImage(30, 30,
+        "assets/BackArrow.png",
+        "assets/BackArrowFocus.png"
+    )
+    view.checkout = FocusableImage(1700, 30,
+        "assets/CartButton.png",
+        "assets/CartButtonFocus.png"
+    )
+    view.back_pressed = Image{
+        position = {30, 30},
+        src = "assets/BackArrowPress.png",
+        opacity = 0
+    }
+    view.checkout_pressed = Image{
+        position={1700,30},
+        src="assets/CartButtonPress.png",
+        opacity = 0
+    }
 
     view.items = {view.back, view.checkout}
+    view.pressed_items = {view.back_pressed, view.checkout_pressed}
     view.ui:add(view.bar)
-    view.ui:add(unpack(view.items))
+    view.ui:add(view.back.group, view.checkout.group,
+        view.backText, view.checkoutText, view.back_pressed, view.checkout_pressed
+    )
     --screen:add(view.ui)
     function view:initialize()
         self:set_controller(FoodFooterController(self))
@@ -40,7 +62,6 @@ FoodFooterView = Class(View, function(view, model, ...)
             print("Showing FoodFooterView UI")
             if view.prev_cart_size ~= #model.cart then
                 print("\n\nUpdating Cart, size is now",#model.cart,"... adding "..model.cart[#model.cart].Name)
-                view.ui:clear()
                 view.items = {}
                 local j = 1
                 view.items[j] = view.back
@@ -70,21 +91,19 @@ FoodFooterView = Class(View, function(view, model, ...)
             end
             for i,item in ipairs(view.items) do
                 if i == controller:get_selected_index() then
-                    print("\t",i,"opacity to 255")
-                    item:animate{duration=CHANGE_VIEW_TIME, opacity=255}
+                    if(item.on_focus) then
+                        item:on_focus()
+                    else
+                        item:animate{duration=CHANGE_VIEW_TIME, opacity=255}
+                    end
                 else
-                    print("\t",i,"opacity to 0")
-                    item:animate{duration=CHANGE_VIEW_TIME, opacity=100}
+                    if(item.out_focus) then
+                        item:out_focus()
+                    else
+                        item:animate{duration=CHANGE_VIEW_TIME, opacity=100}
+                    end
                 end
             end
-    --[[    elseif comp == Components.CUSTOMIZE or comp == Components.TAB or
-               comp == Components.CUSTOMIZE_ITEM then
-            print("Graying FoodFooterView UI")
-            controller:reset_index()
-            view.ui:raise_to_top()
-            view.ui:complete_animation()
-            view.items[controller:get_selected_index()].opacity = 100
---]]
         else
             print("Hiding FoodFooterView UI")
             controller:reset_index()
