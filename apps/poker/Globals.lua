7676bfbfb8c18c72dc4bff7936577e0b6db9ba6e
Darren Yin <darren.yin@trickplay.com>
2010-08-10 10:25:49 -0700

    Basic PokerRules list added.

---
+++ apps/poker/Globals.lua
@@ -0,0 +1,28 @@
+Directions = {
+   RIGHT = {1,0},
+   LEFT = {-1,0},
+   DOWN = {0,1},
+   UP = {0,-1}
+}
+
+Colors={
+   SLATE_GRAY                   ="708090",
+   WHITE                        ="FFFFFF",
+   FIRE_BRICK                   ="B22222",
+   LIME_GREEN                   ="32CD32",
+   TURQUOISE                    ="40E0D0",
+   BLACK                        ="000000",
+   RED                          ="FF0000",
+   YELLOW                       ="FFFF00",
+   GREEN                        ="00FF00",
+   BLUE                         ="0000FF",
+   MAGENTA                      ="FF00FF",
+   CYAN                         ="00FFFF",
+   ORANGE                       ="FFA500",
+   PURPLE                       ="A020F0",
+   PERU                         ="CD853F",
+   FOCUS_RED                    ="602020"
+}
+
+DEFAULT_FONT = "DejaVu Serif 40px"
+DEFAULT_COLOR = Colors.WHITE
--- apps/poker/main.lua
+++ apps/poker/main.lua
@@ -1,44 +1,53 @@
+math.randomseed(os.time())
 dofile("Class.lua")
 dofile("Cards.lua")
+dofile("Globals.lua")
 local deck = Deck()
 deck:shuffle()
 local hand1 = deck:deal(5)
 local hand2 = deck:deal(5)
-print("\n\n\n\n\n\n")
-print(#hand1)
-for _,card in ipairs(hand1) do
-   print(card.name)
-end
-
 
 local hand1_group = Group{position={0,0}}
-local y = 0
-for _,card in ipairs(hand1) do
-   local card_text = Text{
-      y=y,
-      text=card.name,
-      color="FFFFFF"
-   }
-   hand1_group:add(card_text)
-   y = y+card_text.h+10
-end
 local hand2_group = Group{position={960,0}}
-local y = 0
-for _,card in ipairs(hand2) do
-   print(card.name)
-   local card_text = Text{
-      y=y,
-      text=card.name,
-      color="FFFFFF"
-   }
-   hand2_group:add(card_text)
-   y = y+card_text.h+10
-end
 screen:add(hand1_group, hand2_group)
 screen:show()
+function display_cards()
+   hand1_group:clear()
+   hand2_group:clear()
+   local y = 0
+   for _,card in ipairs(hand1) do
+      local card_text = Text{
+         y=y,
+         text=card.name,
+         color="FFFFFF",
+         font="Sans 40px"
+      }
+      hand1_group:add(card_text)
+      y = y+card_text.h+10
+   end
+
+   local y = 0
+   for _,card in ipairs(hand2) do
+      print(card.name)
+      local card_text = Text{
+         y=y,
+         text=card.name,
+         color="FFFFFF",
+         font="Sans 40px"
+      }
+      hand2_group:add(card_text)
+      y = y+card_text.h+10
+   end
+end
+
+display_cards()
 
 function screen:on_key_down(k)
    if k == keys.r then
+      deck:reset()
       deck:shuffle()
+      hand1=deck:deal(5)
+      hand2=deck:deal(5)
+      display_cards()
    end
 end
