f835a0ddf5751ce422c87ffebb1cd93aa3d202ff
Darren Yin <darren.yin@trickplay.com>
2010-08-11 15:39:16 -0700

    shit shit shit checkout CountOutsTest to see outs counting IN ACTION.

--- apps/poker/Utils.lua
+++ apps/poker/Utils.lua
@@ -1,3 +1,5 @@
+
+
 function sort_hand(hand)
    local hand_copy = {}
    for _,card in ipairs(hand) do
@@ -10,4 +12,76 @@ function sort_hand(hand)
       end
    )
    return hand_copy
+end
+
+---
+-- Returns the index of the first occurrence of the specified element
+-- in hand, or -1 if hand does not contain the element. More formally,
+-- returns the lowest index i such that card:equals(get(i)), or -1 if
+-- there is no such index.
+--
+-- @param hand array  Array of cards
+-- @param card Card  card to search for
+--
+-- @returns the index of the last occurrence of the specified element
+-- in hand, or -1 if hand does not contain the element
+function indexOf(hand, card)
+   assert(type(hand)=="table")
+   for i,hand_card in ipairs(hand) do
+      if hand_card:equals(card) then
+         return i
+      end
+   end
+   return -1
+end
+
+---
+-- Returns true if hand contains the specified element. More formally,
+-- returns true if and only if hand contains at least one element e
+-- such that card:equals(e).
+--
+-- @param hand array  Array of cards
+-- @param card Card  card whose presence in hand is to be tested
+-- @returns true if hand contains the specified card
+function contains(hand, card)
+   assert(type(hand)=="table")
+   for i,hand_card in ipairs(hand) do
+      if hand_card:equals(card) then
+         return true
+      end
+   end
+   return false
+end
+
+function count_outs(hand)
+   -- make a local copy of h so any user of this interface doesn't go
+   -- wtf when his hand turns into potatoes.
+   local h = {}
+   for _,card in ipairs(hand) do
+      table.insert(h,card)
+   end
+
+   local targets = {}
+   local out_table = {}
+   for _, poker_hand in ipairs(PokerHands) do
+      if poker_hand.present_in(h) then
+         print(poker_hand.name .. " in hand!")
+      else
+         table.insert(targets, poker_hand)
+         out_table[poker_hand] = 0
+      end
+   end
+
+   for _, card in ipairs(Cards) do
+      if not contains(h, card) then
+         table.insert(h, card)
+         for __, poker_hand in ipairs(targets) do
+            if poker_hand.present_in(h) then
+               out_table[poker_hand] = out_table[poker_hand] + 1
+            end
+         end
+         table.remove(h)
+      end
+   end
+   return out_table
 end
