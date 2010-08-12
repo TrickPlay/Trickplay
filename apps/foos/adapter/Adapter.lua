bbec242976062d9384a08af610befdf30c69df52
Viraaj Marathe <viraaj.marathe@trickplay.com>
2010-08-12 13:39:44 -0700

    started url adapters

--- apps/foos/adapter/Adapter.lua
+++ apps/foos/adapter/Adapter.lua
@@ -1,14 +1,24 @@
 adaptersTable = { "Yahoo", "Google" }
+picsTable = {}
+
 
 START_INDEX = 1
 NUM_SLIDESHOW_IMAGES = 10
-selection = 2
 
-dofile(adaptersTable[selection].."/adapter.lua")
+function startAdapter(selection)
+	dofile(adaptersTable[selection].."/adapter.lua")
+end
+
+function getPictureUrl()
+	return picsTable
+end
 
 function getUrls(source)
 	-- this function would call something in the Slideshow App
+	picsTable = source
 	for k,v in pairs(source) do
 		print(k,v) 
 	end
 end
+
+startAdapter(2)
