d8202fe3548125f454d108e725700168009c6d13
Alex Indaco <alex.indaco@trickplay.com>
2010-08-12 11:55:22 -0700

    2.0
    Former-commit-id: a9264d3b21d970e47eb19ca62e565b4985ad408a

--- apps/foos/FrontPageController.lua
+++ apps/foos/FrontPageController.lua
@@ -6,54 +6,19 @@ Directions = {
 }
 
 
-FrontPageController = Class(Controller, function(self, view, view_grid, ...)
+FrontPageController = Class(Controller, function(self, view, ...)
     self._base.init(self, view, Components.FRONT_PAGE)
 
     -- the default selected index
     local selected = {1,1}
-    local grid = view_grid
-
-    function self:refresh_grid(g)
-        grid = g
-    end
+    local prev_index = {1,1}
 
     local MenuKeyTable = {
         [keys.Up]    = function(self) self:move_selector(Directions.UP) end,
         [keys.Down]  = function(self) self:move_selector(Directions.DOWN) end,
         [keys.Left]  = function(self) self:move_selector(Directions.LEFT) end,
         [keys.Right] = function(self) self:move_selector(Directions.RIGHT) end,
-        [keys.r]     = function(self) view.refresh() end,
-        [keys.h]     = function(self)
-            self:get_model():set_active_component(Components.HELP_MENU)
-            self:get_model():notify()
- end,
-
-        [keys.m]     = function(self)
-            self:get_model():set_active_component(Components.MAIN_MENU)
-            self:get_model():notify() 
-        end,
-
         [keys.Return] = function(self) 
-            --text goes on left side if too close to the right
-            if selected[2] > NUM_COLS/2+1 then
-                self:get_model().pic_text = {
-                         (selected[2]-1)*screen.width/NUM_COLS - 250,
-                         (selected[1]-1)*screen.height/NUM_ROWS
-                }
-                print("text left",self:get_model().pic_text[1],self:get_model().pic_text[2])
-
-            --defaults the the right otherwise
-            else
-                self:get_model().pic_text = {
-                         (selected[2]-1)*screen.width/NUM_COLS +
-                         view.menu_items[selected[1]][selected[2]].width*
-                         view.menu_items[selected[1]][selected[2]].scale[1]+20,
-                         (selected[1]-1)*screen.height/NUM_ROWS
-                }
-                print("text right",self:get_model().pic_text[1],self:get_model().pic_text[2])
-
-            end
-            --self:get_model().selected_picture = {selected[1],selected[2]}
             self:get_model():set_active_component(Components.ITEM_SELECTED)
             self:get_model():notify()
         end
@@ -78,43 +43,27 @@ FrontPageController = Class(Controller, function(self, view, view_grid, ...)
         return selected
     end
 
+    function self:get_prev_index()
+        return prev_index
+    end
+    function self:set_prev_index(r,c)
+        prev_index = {r,c}
+    end
+
 
 
     function self:move_selector(dir)
-        local next_spot = {selected[1],selected[2]}
+        prev_index = {selected[1],selected[2]}
+        local next_spot = {selected[1]+dir[2],selected[2]+dir[1]}
+        if model.vis_pics[next_spot[1]]               ~= nil and
+           model.vis_pics[next_spot[1]][next_spot[2]] ~= nil then
 
-
-        --Pressing up when you are on the top row of images
-        --brings the upper menu
-        if next_spot[1] + dir[2] == 0 then
-            self:get_model():set_active_component(Components.MAIN_MENU)       
-
-
-        --Regular movement/navigation within the picture grid
-        else
-            --move to the next image (i.e. if the one you are on is 
-            --not a 1x1 or the one you are moving to is not a 1x1)
-            while grid[next_spot[1]]               ~= nil and
-                  grid[next_spot[1]][next_spot[2]] ~= nil and
-
-                  grid[next_spot[1]][next_spot[2]][1] == 
-                  grid[selected[1]][selected[2]][1]       and
-
-                  grid[next_spot[1]][next_spot[2]][2] == 
-                  grid[selected[1]][selected[2]][2]       do
-
-
-                next_spot = {next_spot[1] + dir[2],
-                             next_spot[2] + dir[1]}
-            end
-
-            --if you didn't reach an edge then, move to it
-            if grid[next_spot[1]]               ~= nil and
-               grid[next_spot[1]][next_spot[2]] ~= nil then
-                selected[1] = grid[next_spot[1]][next_spot[2]][1]
-                selected[2] = grid[next_spot[1]][next_spot[2]][2]
-            end
-
+            selected[1] = next_spot[1]
+            selected[2] = next_spot[2]
+        elseif dir == Directions.RIGHT then
+            view:move_right()
+        elseif dir == Directions.LEFT  then
+            view:move_left()
         end
 
         self:get_model():notify()
