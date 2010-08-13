b56d3575144c130ffb0641665363dd3db5722b0a
Viraaj Marathe <viraaj.marathe@trickplay.com>
2010-08-13 10:44:52 -0700

    adapter stuff
    Former-commit-id: 6597f0ff2e4ebc95382ae25ef5db72b18e1ad557

--- apps/foos/adapter/Adapter.lua
+++ apps/foos/adapter/Adapter.lua
@@ -10,15 +10,17 @@ function startAdapter(selection)
 end
 
 function getPictureUrl()
+	
+	for k,v in pairs(picsTable) do
+		print(k,v) 
+	end
 	return picsTable
 end
 
-function getUrls(source)
+function setUrls(source)
 	-- this function would call something in the Slideshow App
 	picsTable = source
-	for k,v in pairs(source) do
-		print(k,v) 
-	end
 end
 
 startAdapter(2)
+getPictureUrl()
