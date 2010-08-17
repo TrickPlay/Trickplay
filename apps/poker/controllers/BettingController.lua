b2d6510217f79b1cc91bb45a32368f784b19b2d1
Darren Yin <darren.yin@trickplay.com>
2010-08-17 14:31:04 -0700

    basic betting works

--- apps/poker/controllers/BettingController.lua
+++ apps/poker/controllers/BettingController.lua
@@ -28,12 +28,14 @@ BettingController = Class(Controller, function(self, view, ...)
     --the number of the current player selecting a seat
     local playerCounter = 1
 
+    local betCallback = function() end
+
     local PlayerCallbacks = {
         [SubGroups.LEFT] = function(self)
         --[[
-            local current_bet = model.players[ model.currentPlayer ].bet
-            model.players[ model.currentPlayer ].bet = 0
-            model.players[ model.currentPlayer ].money = model.players[ model.currentPlayer ].money - current_bet
+            local current_bet = model.currentPlayer.bet
+            model.currentPlayer.bet = 0
+            model.currentPlayer.money = model.currentPlayer.money - current_bet
         --]]
         end,
         [SubGroups.MIDDLE] = function(self)
@@ -47,15 +49,23 @@ BettingController = Class(Controller, function(self, view, ...)
         [keys.Down] = function(self) self:move_selector(Directions.DOWN) end,
         [keys.Left] = function(self) self:move_selector(Directions.LEFT) end,
         [keys.Right] = function(self) self:move_selector(Directions.RIGHT) end,
-        [keys.Return] = function(self)
-        
-            self:get_model():set_active_component(Components.CHARACTER_SELECTION)
-            self:get_model():notify()
-        
+        [keys.Return] =
+        function(self)
+           local bet = model.currentPlayer.bet
+           betCallback(bet)
+           betCallback = function() end
+           self:get_model():set_active_component(Components.GAME)
+           self:get_model():notify()
         end,
     }
 
+    function self:set_callback(cb)
+       print("callback set")
+       betCallback = cb
+    end
+
     function self:on_key_down(k)
+       print("bettingcontroller on_key_down")
         if PlayerSelectionKeyTable[k] then
             PlayerSelectionKeyTable[k](self)
         end
@@ -79,12 +89,12 @@ BettingController = Class(Controller, function(self, view, ...)
             end
         -- Change bet
         elseif(0 ~= dir[2]) and subselection == SubGroups.RIGHT then
-            local new_money = model.players[ model.currentPlayer ].money + ( dir[2] * model.bet.BIG_BLIND )
-            local new_bet = model.players[ model.currentPlayer ].bet + ( - dir[2] * model.bet.BIG_BLIND )
+            local new_money = model.currentPlayer.money + ( dir[2] * model.bet.BIG_BLIND )
+            local new_bet = model.currentPlayer.bet + ( - dir[2] * model.bet.BIG_BLIND )
             if new_bet > 0 and new_money >= 0 then
-                model.players[ model.currentPlayer ].bet = new_bet
-                model.players[ model.currentPlayer ].money = new_money
-                print("Current bet:", model.players[ model.currentPlayer ].bet, "Current money:", model.players[ model.currentPlayer ].money)
+                model.currentPlayer.bet = new_bet
+                model.currentPlayer.money = new_money
+                print("Current bet:", model.currentPlayer.bet, "Current money:", model.currentPlayer.money)
             end
         end
         self:get_model():notify()
