37949c0ae9741f6db2dc4813fde36513704cdaad
Rex Fenley <rex.fenley@trickplay.com>
2010-08-18 12:26:30 -0700

    deleted a print statement

--- apps/poker/Player.lua
+++ apps/poker/Player.lua
@@ -90,7 +90,6 @@ Player = Class(function(player, args, ...)
    -- @param round
    -- @returns fold boolean  true if player should fold
    -- @returns bet number  quantity of bet, if fold then bet should be 0
-   --function player:get_move(hole, community_cards, position, call_bet, min_raise, current_bet, pot, round)
    local last_move = Moves.FOLD
    function player:get_move(state)
       
@@ -161,7 +160,6 @@ Player = Class(function(player, args, ...)
                a_move = Moves.CALL
             end
          end
-         print("\na_move: "..a_move.."\n")
          return a_move, math.random(RaiseFactor.R, RaiseFactor.RR)
       end
 
