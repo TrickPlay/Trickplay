cd7453509047f8a79e387555283456fee8e82d42
Viraaj Marathe <viraaj.marathe@trickplay.com>
2010-08-26 11:26:54 -0700

    no search results bug
    Former-commit-id: 40f53adc847dfb038bee1b2bdb033bcb18c8bd2c

--- apps/foos/adapter/Adapter.lua
+++ apps/foos/adapter/Adapter.lua
@@ -1,4 +1,4 @@
-adapterTypes = {"Google","Yahoo","Flickr","Yahoo","Google","Yahoo","Google","Yahoo"}
+adapterTypes = {"Google","Yahoo","Flickr", "Google", "Yahoo", "Flickr"}
 adaptersTable = settings.adaptersTable or { "Google", "Yahoo", "Yahoo", "Google", "Google", "Yahoo", "Google"}
 searches = settings.searches or {"space", "dinosaurs", "dog", "cat", "jessica%20alba","national%20geographic", "NFL"}
 user_ids = settings.user_ids or {}
