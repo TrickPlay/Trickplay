7466bc3322bc7a915d0cc5a8af0ce7bdb5186db6
Craig R. Hughes <craig@trickplay.com>
2010-09-08 11:08:01 -0700

    Add trace along the curve so tanks can move

--- apps/tanks/main.lua
+++ apps/tanks/main.lua
@@ -9,6 +9,7 @@ draw_terrain()
 function screen:on_key_down(key)
 	if key == keys.Down then terrain_refine = math.max(2,terrain_refine-1)
 	elseif key == keys.Up then terrain_refine = math.min(100,terrain_refine+1)
+	elseif key == keys.Right then trace_terrain() return
 	elseif key ~= keys.Return then return
 	end
 
--- apps/tanks/terrain.lua
+++ apps/tanks/terrain.lua
@@ -11,37 +11,6 @@ local terrain_canvas = Canvas { size = { screen.w, screen.h } }
 screen:add(terrain_canvas)
 screen:show()
 
-local function calc_p1(p3,p0)
-    
-end
-
-local function calc_p2(p0,p3)
-end
-
-local function calculate_bezier_x()
-end
-
-local function find_y_for_x(segments, x)
-    assert(x>=0 and x<=screen.w, "Point "..x.." is not located in our range of hills")
-
-    for segment_number=1,#segments do
-        if(x >= segments[segment_number].start.x and x <= segments[segment_number].fin.x) then
-            -- This is our segment, so iterate over t in the interval [0,1] to find the point
-            for bezier_t = 0,1,0.01 do
-                if x < calculate_bezier_x(segments[segment_number], bezier_t) then
-                    -- We just passed our X, so we found our t -- now calculate the y for this t and return it
-                    return calculate_bezier_y(segments[segment_number], bezier_t)
-                end
-            end
-            -- If we get out of that loop without returning, then we're at the tip of the segment, so return the endpoint's y
-            return segments[segment_number].fin.y
-        end
-    end
-
-    assert(false, "Somehow our point wasn't on the curve.  That is very unusual.")
-    return 0
-end
-
 --[[
 
     The hillside is made up of a chain of cubic bezier curves.  Each curve is defined by 4 points,
@@ -52,10 +21,41 @@ end
 
     The array of points we've come up algorithmically for the hillside is just the connecting points, not the control points (ie the P0's and P3's, but no P1's or P2's).  So we'll now come up with some P1's and P2's to satisfy the continuity issue, while making a nice smooth hillside.  Note these are calculated heuristically as a function of the connection points, so we can do math later to figure out the x-y shape of the curve, so that tanks and bombs can know what the y-altitude of the hillside is at every point along the x-direction.
 
-    Wikipedia gives the parametric form of a cubic Bézier curve as:
-    B(t) = (1-t)³.P0 + 3(1-t)².t.P1 + 3(1-t).t².P2 + t³.P3, t∈[0,1]
-
 ]]--
+
+local function control_points(segments, i)
+    local p = {}
+    p[0] = {
+            x = segments[i].start.x,
+            y = segments[i].start.y
+         }
+    p[3] = {
+            x = segments[i].fin.x,
+            y = segments[i].fin.y,
+         }
+    -- p1 which we head towards from when moving from p0, is built as a weighted average
+    -- of 2 parts the previous start point and 1 part our own end point
+    -- If there was no previous segment, then don't curve away from the starting point
+    p[1] = segments[i-1] and {
+                x = 2*p[0].x-(2*segments[i-1].start.x+p[3].x)/3,
+                y = 2*p[0].y-(2*segments[i-1].start.y+p[3].y)/3,
+             } or {
+                x = p[0].x,
+                y = p[0].y,
+             }
+
+    -- p2 which we come from when moving towards p3, is built as a weighted average
+    -- of 2 parts our own start point and 1 part the end point of the next segment
+    -- If there is no next point, then don't curve into the end point
+    p[2] = segments[i+1] and {
+                x = (2*p[0].x+segments[i+1].fin.x)/3,
+                y = (2*p[0].y+segments[i+1].fin.y)/3,
+             } or {
+                x = p[3].x,
+                y = p[3].y,
+             }
+    return p
+end
 
 local function display_line_segments(segments)
 	terrain_canvas:begin_painting()
@@ -63,39 +63,11 @@ local function display_line_segments(segments)
 	terrain_canvas:move_to(-grass_width, terrain_canvas.height+grass_width)
 	terrain_canvas:line_to(segments[1].start.x, segments[1].start.y)
 	for i = 1,#segments do
-        local p0 = {
-                x = segments[i].start.x,
-                y = segments[i].start.y
-             }
-        local p3 = {
-                x = segments[i].fin.x,
-                y = segments[i].fin.y,
-             }
-        -- p1 which we head towards from when moving from p0, is built as a weighted average
-        -- of 2 parts the previous start point and 1 part our own end point
-        -- If there was no previous segment, then don't curve away from the starting point
-        local p1 = segments[i-1] and {
-                    x = 2*p0.x-(2*segments[i-1].start.x+p3.x)/3,
-                    y = 2*p0.y-(2*segments[i-1].start.y+p3.y)/3,
-                 } or {
-                    x = p0.x,
-                    y = p0.y,
-                 }
-
-        -- p2 which we come from when moving towards p3, is built as a weighted average
-        -- of 2 parts our own start point and 1 part the end point of the next segment
-        -- If there is no next point, then don't curve into the end point
-        local p2 = segments[i+1] and {
-                    x = (2*p0.x+segments[i+1].fin.x)/3,
-                    y = (2*p0.y+segments[i+1].fin.y)/3,
-                 } or {
-                    x = p3.x,
-                    y = p3.y,
-                 }
+	    local p = control_points(segments,i)
         terrain_canvas:curve_to(
-									p1.x, p1.y,
-									p2.x, p2.y,
-									p3.x, p3.y
+									p[1].x, p[1].y,
+									p[2].x, p[2].y,
+									p[3].x, p[3].y
 		)
 	end
 	terrain_canvas:line_to(terrain_canvas.width+grass_width, terrain_canvas.height+grass_width)
@@ -160,4 +132,33 @@ function make_terrain(n)
 		line_segments = new_segments
 		random_range = random_range / math.exp(roughness)
 	end
+end
+
+
+--[[
+    Wikipedia gives the parametric form of a cubic Bézier curve as:
+    B(t) = (1-t)³.P0 + 3(1-t)².t.P1 + 3(1-t).t².P2 + t³.P3, t∈[0,1]
+]]--
+
+local marker = Rectangle { size = { grass_width, grass_width }, color = "ff0000", anchor_point = { grass_width/2, grass_width/2 } }
+function trace_terrain()
+    screen:add(marker)
+    marker.position = { 0, screen.h/2 }
+    local t = Timeline {
+        duration = 3000,
+        on_new_frame = function(t, msec, progress)
+            local seg = math.min(math.floor(progress*#line_segments)+1,#line_segments)
+            local t = (progress*#line_segments+1)-seg
+            local p = control_points(line_segments, seg)
+            local point = {
+                x = (1-t)^3*p[0].x + 3*(1-t)^2*t*p[1].x + 3*(1-t)*t^2*p[2].x + t^3*p[3].x,
+                y = (1-t)^3*p[0].y + 3*(1-t)^2*t*p[1].y + 3*(1-t)*t^2*p[2].y + t^3*p[3].y
+            }
+            marker.position = { point.x, point.y }
+        end,
+        on_completed = function()
+            marker:unparent()
+        end
+    }
+    t:start()
 end
