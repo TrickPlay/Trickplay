8d264282daf25b1da3f4a8510e111340a44440ec
Craig R. Hughes <craig@trickplay.com>
2010-09-09 14:57:09 -0700

    Amend control point calculation

--- apps/tanks/terrain.lua
+++ apps/tanks/terrain.lua
@@ -16,44 +16,47 @@ screen:show()
     The hillside is made up of a chain of cubic bezier curves.  Each curve is defined by 4 points,
     P0, P1, P2, P3
     The curve starts at P0 going toward P1 and arrives at P3 coming from the direction of P2
-    To guarantee smoothness, the control point at which two curves meet must be on the line between the two control points on either side.
-    In other words, segment 1 of P0_1, P1_1, P2_1, P3_1 and segment 2 of P0_2, P1_2, P2_2, P3_2, then P3_1=P0_2 must be on the line from P2_1 to P1_2
 
     The array of points we've come up algorithmically for the hillside is just the connecting points, not the control points (ie the P0's and P3's, but no P1's or P2's).  So we'll now come up with some P1's and P2's to satisfy the continuity issue, while making a nice smooth hillside.  Note these are calculated heuristically as a function of the connection points, so we can do math later to figure out the x-y shape of the curve, so that tanks and bombs can know what the y-altitude of the hillside is at every point along the x-direction.
 
 ]]--
 
+
+--[[
+    To guarantee smoothness, the control point at which two curves meet must be on the line between the two control points on either side.
+    In other words, segment 1 of P0_1, P1_1, P2_1, P3_1 and segment 2 of P0_2, P1_2, P2_2, P3_2, then P3_1=P0_2 must be on the line from P2_1 to P1_2, ie. P3_1=P0_2=k(P2_1+P1_2) for some k
+    Therefore P1_2=P0_2/k - P2_1 and P2_1 = P0_2/k - P1_2
+
+    Obviously, we still have a lot of control over the "flatness" of the curve, which is basically the distance of the P1 and P2 control points off the line from P0 to P3.  We will express this deflection as a perpendicular to the P0-P3 line, the magnitude being equal to some fraction of the distance from P0 to P3 -- the higher the fraction, the more deflection off flat there is.
+]]--
+
+local DEFLECTION = 0.3
 local function control_points(segments, i)
     local p = {}
-    p[0] = {
-            x = segments[i].start.x,
-            y = segments[i].start.y
-         }
-    p[3] = {
-            x = segments[i].fin.x,
-            y = segments[i].fin.y,
-         }
-    -- p1 which we head towards from when moving from p0, is built as a weighted average
-    -- of 2 parts the previous start point and 1 part our own end point
-    -- If there was no previous segment, then don't curve away from the starting point
-    p[1] = segments[i-1] and {
-                x = 2*p[0].x-(2*segments[i-1].start.x+p[3].x)/3,
-                y = 2*p[0].y-(2*segments[i-1].start.y+p[3].y)/3,
-             } or {
-                x = p[0].x,
-                y = p[0].y,
-             }
+    p[0] = segments[i].start
+    p[3] = segments[i].fin
 
-    -- p2 which we come from when moving towards p3, is built as a weighted average
-    -- of 2 parts our own start point and 1 part the end point of the next segment
-    -- If there is no next point, then don't curve into the end point
-    p[2] = segments[i+1] and {
-                x = (2*p[0].x+segments[i+1].fin.x)/3,
-                y = (2*p[0].y+segments[i+1].fin.y)/3,
-             } or {
-                x = p[3].x,
-                y = p[3].y,
-             }
+    -- Calculate some root of the square of the length of the line segment.  DEFLECTION=0.5 means deflection is equal to line length
+    local control_offset = math.pow((p[3].x-p[0].x)^2+(p[3].y-p[0].y)^2, DEFLECTION)
+
+    --[[ So now calculate a point that's control_offset away from p[0] orthogonally to p[3] ]]--
+    local diff = { x = p[3].x-p[0].x, y = p[3].y-p[3].x }
+    local scale = math.pow(control_offset^2 / (diff.x^2 + diff.y^2),0.5)
+    p[1] = { x = p[0].x+diff.y*scale, y = p[0].y-diff.x*scale }
+    -- Now correct heading -- we want to move vertically away from where we're heading eventually
+    if(diff.y > 0 and p[1].y > 0) then p[1].x,p[1].y = -p[1].x,-p[1].y end
+
+    --[[ This point will be orthogonal to the NEXT line segment, or just nothing ]]--
+    if(not segments[i+1]) then
+        p[2] = p[3]
+    else
+        local s2 = segments[i+1]
+        control_offset = math.pow((s2.fin.x-s2.start.x)^2+(s2.fin.y-s2.start.y)^2, DEFLECTION)
+        diff = { x = s2.fin.x-s2.start.x, y = s2.fin.y-s2.start.y }
+        scale = math.pow(control_offset^2 / (diff.x^2 + diff.y^2),0.5)
+        p[2] = { x = p[3].x-diff.y*scale, y = p[3].y+diff.x*scale }
+        if(diff.y > 0 and p[2].y < 0) then p[2].x,p[2].y = -p[2].x,-p[2].y end
+    end
     return p
 end
 
