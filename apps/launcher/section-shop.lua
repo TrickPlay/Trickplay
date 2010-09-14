3a137679228bd5b23e456021e43292ffc080143c
Pablo Pissanetzky <pablo@trickplay.com>
2010-09-13 19:20:12 -0700

    Added shop API and plumbing for full-screen app shop
    Former-commit-id: 3360e9852a61ce77882065c6007085fc1784cfb8
    Former-commit-id: 10d04ec9bc6f67ae78a6dc76ef835bb02aef5e3c

--- apps/launcher/section-shop.lua
+++ apps/launcher/section-shop.lua
@@ -2,7 +2,17 @@
 return
 function( section )
 
+    local ui = section.ui
+
     function section.on_enter()
-    end   
+        -- grab key focus
+        -- return true
+    end
+    
+    function section.on_default_action()
+        --local fs_shop = dofile( "fs-shop" )( ui )
+        --ui:on_section_full_screen( fs_shop )
+        --return true
+    end
 
 end
