5a1c6fcd1414cbb1042e1591046360099af5b3e6
Alex Indaco <alex.indaco@trickplay.com>
2010-09-24 09:01:36 -0700

    assets are all doubled, text splash is in place, score works, lives work

--- apps/1945/main.lua
+++ apps/1945/main.lua
@@ -1,6 +1,74 @@
 
 dofile( "controller.lua" )
 
+
+-- Alex's code
+local number_of_lives = 3
+local high_score = 0
+local point_counter = 0
+function info_text()
+	return	"Lives: "..number_of_lives..
+		"\t\t\tCurrent Score: "..point_counter..
+		"\tHigh Score: "..high_score
+end
+local my_plane_sz = 65*2
+local score = Text{text = info_text(),font="Sans Bold 36px",color="FFFFFF",x=30,y=20,z=1}
+local topbar = Rectangle{color="0000007F",w=1920,h=100,z=1}
+local game_started = false
+local splash = Group{z=10}
+splash:add(
+
+	--shadows
+	Text{
+		text = "1945!",
+		font = "Sans 70px",
+		color = "000000",
+		opacity = 150,
+		x = 505,
+		y = 405
+	},
+	Text{
+		text = "Enter - Shoots\nArrows - Moves\nSpace - Pauses",
+		font = "Sans 32px",
+		color = "000000",
+		opacity = 150,
+		x = 905,
+		y = 505,
+	},
+	Text{
+		text = "Press Enter to begin!",
+		font = "Sans 32px",
+		color = "000000",
+		opacity = 150,
+		x = 505,
+		y = 705,
+	},
+
+	Text{
+		text = "1945!",
+		font = "Sans 70px",
+		color = "FFFFFF",
+		x = 500,
+		y = 400
+	},
+	Text{
+		text = "Enter - Shoots\nArrows - Moves\nSpace - Pauses",
+		font = "Sans 32px",
+		color = "FFFFFF",
+		x = 900,
+		y = 500,
+	},
+	Text{
+		text = "Press Enter to begin!",
+		font = "Sans 32px",
+		color = "FFFFFF",
+		x = 500,
+		y = 700,
+	}
+
+)
+screen:add(topbar,score,splash)
+
 -------------------------------------------------------------------------------
 
 function clamp( v , min , max )
@@ -327,7 +395,7 @@ my_plane =
     
     speed_bump = 200,
         
-    group = Group{ size = { 65 , 65 } , clip = { 0 , 0 , 65 , 65 } },
+    group = Group{ size = { --[[65 , 65]]my_plane_sz,my_plane_sz } , clip = { 0 , 0 , my_plane_sz,my_plane_sz--[[65 , 65]] } },
     
     image = Clone{ source = assets.my_plane_strip },
     
@@ -358,7 +426,7 @@ my_plane =
             
             screen:add( self.group )
             
-            self.group.position = { screen.w / 2 - 65 / 2 , screen.h - 65 }
+            self.group.position = { screen.w / 2 - my_plane_sz / 2 , screen.h - my_plane_sz }
             
         end,
         
@@ -371,9 +439,9 @@ my_plane =
             -- We just move the image within the group, which has a clipping
             -- area set.
             
-            local x = self.image.x - 65
+            local x = self.image.x - my_plane_sz
             
-            if x == -195 then
+            if x == -390 then
             
                 x = 0
                 
@@ -393,9 +461,9 @@ my_plane =
                 
                     local x = self.group.x + ( self.h_speed * seconds )
                     
-                    if x > screen.w - 65 then
+                    if x > screen.w - my_plane_sz then
                     
-                        x = screen.w - 65
+                        x = screen.w -my_plane_sz 
                         
                         self.h_speed = 0
                     
@@ -430,9 +498,9 @@ my_plane =
     
                     local y = self.group.y + ( self.v_speed * seconds )
                     
-                    if y > screen.h - 65 then
+                    if y > screen.h - my_plane_sz then
                     
-                        y = screen.h - 65
+                        y = screen.h -my_plane_sz 
                         
                         self.v_speed = 0
                     
@@ -468,15 +536,19 @@ my_plane =
             -- when dead
             
             else
-            
+--           TODO: decrement life counter
                 -- Figure the total time we have been dead
-                
+--[[
+                if number_of_lives <= 0 then
+		exit()
+		end 
+--]]
                 self.dead_time = self.dead_time + seconds
                 
                 -- If it is the maximum time, we go back to being alive
                 
                 if self.dead_time >= self.max_dead_time then
-                
+--number_of_lives = number_of_lives - 1
                     self.dead = false
                     
                     self.dead_time = 0
@@ -634,6 +706,8 @@ my_plane =
     collision =
     
         function( self , other )
+number_of_lives = number_of_lives - 1
+score.text = info_text()
 
             self.dead = true
             
@@ -955,6 +1029,9 @@ enemies =
                 
                     function( self , other )
                     
+--TODO increment the point counter
+point_counter = point_counter+10
+score.text = info_text()
                         screen:remove( self.group )
                         
                         remove_from_render_list( self )
@@ -1068,22 +1145,25 @@ enemies =
 
 -------------------------------------------------------------------------------
 -- This table contains all the things that are moving on the screen
+function start_game()
+add_to_render_list( my_plane )
+end
+add_to_render_list( enemies )
 
 add_to_render_list( water )
-add_to_render_list( my_plane )
-add_to_render_list( enemies )
+
+paused = false
 
 -------------------------------------------------------------------------------
 
 screen:show()
 
-paused = false
 
 -------------------------------------------------------------------------------
 -- Game loop, renders everything in the render list
 
 function idle.on_idle( idle , seconds )
-   
+    --score.text = info_text()
     if not paused then
     
         for _ , item in ipairs( render_list ) do
@@ -1103,10 +1183,13 @@ end
 
 function screen.on_key_down( screen , key )
 
-    if key == keys.space then
+    if not game_started then
+        start_game()
+	splash.opacity = 0
+	game_started = true
+    elseif key == keys.space then
         
         paused = not paused
-                
     elseif not paused then
     
         for _ , item in ipairs( render_list ) do
@@ -1119,5 +1202,4 @@ function screen.on_key_down( screen , key )
 end
 
 -------------------------------------------------------------------------------
-
 math.randomseed( os.time() )
