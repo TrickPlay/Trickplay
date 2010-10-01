e4759d782f25133a0f679944757d6b414905f248
Alex Indaco <alex.indaco@trickplay.com>
2010-10-01 10:24:31 -0700

    almost all the functionality is there

--- apps/sudoku/Game.lua
+++ apps/sudoku/Game.lua
@@ -1,13 +1,13 @@
 local TILE_WIDTH  = 100
-local TILE_GUTTER = 10
-local SET_GUTTER  = 20
-local TOP_GAP     = (TILE_WIDTH*9 + TILE_GUTTER*6 + SET_GUTTER*2)/2
+local TILE_GUTTER = 12
+local SET_GUTTER  = 10
+local TOP_GAP     = 30--(screen.h - (TILE_WIDTH*9 + TILE_GUTTER*8 + SET_GUTTER*2))/2
 assert(TOP_GAP >= SET_GUTTER, "flawed #defines for the board..." )
 
 
 
 function BoardGen(number_of_givens)
-    assert(number_of_givens <= 60, "in BoardGen, number_of_givens is too large")
+    assert(number_of_givens <= 80, "in BoardGen, number_of_givens is too large")
     assert(number_of_givens >= 25, "in BoardGen, number_of_givens is too small")
     local base = 
     {
@@ -16,7 +16,7 @@ function BoardGen(number_of_givens)
         {7,8,9,1,2,3,4,5,6},
         {2,3,4,5,6,7,8,9,1},
         {5,6,7,8,9,1,2,3,4},
-        {8,9,1,2,3,4,5,6,6},
+        {8,9,1,2,3,4,5,6,7},
         {3,4,5,6,7,8,9,1,2},
         {6,7,8,9,1,2,3,4,5},
         {9,1,2,3,4,5,6,7,8}
@@ -132,28 +132,98 @@ end
 
 function DevelopBoard(group,givens)
 	local t
+	local guess_g
 	---------------------------------
 	for r = 1,9 do     for c = 1,9 do
 	---------------------------------
 		
+		t = Rectangle {name = "Tile "..r.." "..c,color = "202020",w = TILE_WIDTH,h = TILE_WIDTH, anchor_point = {TILE_WIDTH/2,TILE_WIDTH/2},opacity=0}
+			t.x = (c-1)*TILE_WIDTH + 
+				math.floor((c-1)/3)*SET_GUTTER +
+				(c-1)*TILE_GUTTER
+			t.y = (r-1)*TILE_WIDTH + 
+				math.floor((r-1)/3)*SET_GUTTER +
+				(r-1)*TILE_GUTTER+TOP_GAP+TILE_WIDTH/2
 
+		group:add(t)
 		if givens[r][c] ~= 0 then
+			t = Text
+			{
+				name = "Given_s "..r.." "..c,
+				text = givens[r][c],
+				font = "DejaVu Bold 82px",
+					color = "000000"
+			}
+			t.anchor_point={t.w/2,t.h/2}
+			t.x = (c-1)*TILE_WIDTH + 
+				math.floor((c-1)/3)*SET_GUTTER +
+				(c-1)*TILE_GUTTER-1
+			t.y = (r-1)*TILE_WIDTH + 
+				math.floor((r-1)/3)*SET_GUTTER +
+				(r-1)*TILE_GUTTER+TOP_GAP+TILE_WIDTH/2-1
+			group:add(t)
+
 			t = Text
 			{
 				name = "Given "..r.." "..c,
 				text = givens[r][c],
-				font = "Sans 36px",
+				font = "DejaVu Bold 80px",
+					color = "FFFFFF"
 			}
 			t.anchor_point={t.w/2,t.h/2}
 			t.x = (c-1)*TILE_WIDTH + 
-				math.floor(c/3)*SET_GUTTER +
-				(c%3)*TILE_GUTTER
+				math.floor((c-1)/3)*SET_GUTTER +
+				(c-1)*TILE_GUTTER
 			t.y = (r-1)*TILE_WIDTH + 
-				math.floor(r/3)*SET_GUTTER +
-				(r%3)*TILE_GUTTER
+				math.floor((r-1)/3)*SET_GUTTER +
+				(r-1)*TILE_GUTTER+TOP_GAP+TILE_WIDTH/2
 			group:add(t)
 		else
-			
+			guess_g = Group{name = "Guess "..r.." "..c,opacity=0}
+			group:add(guess_g)
+
+			for g = 1,9 do
+				t = Text
+				{
+					name    = "Guess "..r.." "..c.." "..g,
+					text    = g,
+					font    = "DejaVu ExtraLight 55px",
+					color   = "FFFFFF",
+					opacity = 0,
+					--z=1
+				}
+				t.scale = {1/2,1/2}
+				t.anchor_point={t.w/2,t.h/2}
+			t.x = (c-1)*TILE_WIDTH + 
+				math.floor((c-1)/3)*SET_GUTTER +
+				(c-1)*TILE_GUTTER+ ((g-1)%3-1)*TILE_WIDTH/3
+			t.y = (r-1)*TILE_WIDTH + 
+				math.floor((r-1)/3)*SET_GUTTER +
+				(r-1)*TILE_GUTTER+ (math.floor((g-1)/3)-1)*
+				TILE_WIDTH/3+TOP_GAP+TILE_WIDTH/2
+
+				group:add(t)
+				t = Text
+				{
+					text    = g,
+					font    = "DejaVu ExtraLight 55px",
+					color   = "707070",
+					opacity = 150
+					
+				}
+				t.scale = {1/2,1/2}
+				t.anchor_point={t.w/2,t.h/2}
+			t.x = (c-1)*TILE_WIDTH + 
+				math.floor((c-1)/3)*SET_GUTTER +
+				(c-1)*TILE_GUTTER+ ((g-1)%3-1)*TILE_WIDTH/3
+			t.y = (r-1)*TILE_WIDTH + 
+				math.floor((r-1)/3)*SET_GUTTER +
+				(r-1)*TILE_GUTTER+ (math.floor((g-1)/3)-1)*
+				TILE_WIDTH/3+TOP_GAP+TILE_WIDTH/2
+
+				guess_g:add(t)
+
+			end
 		end
 
 	---------------------------------
@@ -161,8 +231,27 @@ function DevelopBoard(group,givens)
 	---------------------------------
 
 end
+--[[
+local sel_menu = Group{}
+local m_items = {
+	Text{text="PENCIL",color="FFFFFF",font="Sans 36px",x=10,y=10},
+	Text{text="PEN",color="FFFFFF",font="Sans 36px",x=10,y=90},
+	Text{text="CLEAR",color="FFFFFF",font="Sans 36px",x=10,y=170},
+	Text{text="BACK",color="FFFFFF",font="Sans 36px",x=10,y=250}
 
+}
+sel_menu:add(
+	Rectangle{w = 200,h=300,color = "101010"}
+)
+sel_menu:add(unpack(m_items))
+sel_menu.anchor_point={TILE_WIDTH/2,TILE_WIDTH/2}
+--]]
 Game = Class(function(g,number_of_givens, ...)
+	local error_checking = false
+
+	local error_list = {}
+	local undo_list = {}
+	local redo_list = {}
 	local givens = BoardGen(number_of_givens)
 	local guesses = 
 	{
@@ -176,16 +265,485 @@ Game = Class(function(g,number_of_givens, ...)
 		{{},{},{},{},{},{},{},{},{}},
 		{{},{},{},{},{},{},{},{},{}}
 	}
+
+	for r = 1,9 do     for c = 1,9 do
+
+		for i = 1,9 do
+			guesses[r][c][i] = false
+		end 
+		guesses[r][c].sz = 0
+
+    end                end
+
 	g.board = Group{}
+	screen:add(g.board)
 	DevelopBoard(g.board,givens)
-	function g:add_guess(r,c,guess)
-		if givens[r][c] ~= 0 then
-			table.insert(guesses[r][c],guess)
+	--g.board.anchor_point = {g.board.w/2,g.board.h/2}
+	g.board.position = {500,0}--{screen.w/2+40,screen.h/2}
+	function g:toggle_guess(r,c,guess,status)
+		if type(guess) == "table" then
+			for i = 1,#guess do
+			g:toggle_guess(r,c,guess[i],status)
+			end
+			return
+		end
+		if givens[r][c] == 0 then
+			if guesses[r][c][guess] then
+				guesses[r][c].sz = guesses[r][c].sz - 1
+				guesses[r][c][guess] = false
+				g.board:find_child("Guess "..r.." "..c..
+					" "..guess).opacity = 0
+				if status ~= "REDO" and status ~= "UNDO" then
+					table.insert(undo_list,{r,c,guess})
+					if #undo_list > 100 then
+						table.remove(undo_list,1)
+					end
+				elseif status ~= "REDO" and status ~= "UNDO" then
+					redo_list = {}
+				end
+				if error_checking then
+				--have to search backwards in order to update the
+				--table at the same time
+				local e
+				for i =#error_list,1,-1 do
+					e = error_list[i]
+					if (e[1][1] == r and e[1][2] == c 
+					                 and e[1][3] == guess) or
+					   (e[2][1] == r and e[2][2] == c 
+					                 and e[2][3] == guess) then
+						--table.remove(error_list,i)
+
+						if #e[1] == 2 then
+							g.board:find_child("Given "..e[1][1].." "
+								..e[1][2]).color = "FFFFFF"
+						elseif #e[1] == 3 then
+							g.board:find_child("Guess "..e[1][1].." "
+								..e[1][2].." "..e[1][3]).color = "FFFFFF"
+						else
+							error("this should never happen,"..
+								" i did something wrong")
+						end
+						if #e[2] == 2 then
+							g.board:find_child("Given "..e[2][1].." "
+								..e[2][2]).color = "FFFFFF"
+						elseif #e[2] == 3 then
+							g.board:find_child("Guess "..e[2][1].." "
+								..e[2][2].." "..e[2][3]).color = "FFFFFF"
+						else
+							error("this should never happen,"..
+								" i did something wrong")
+						end
+
+						table.remove(error_list,i)
+					end
+				end
+				for i =#error_list,1,-1 do
+					e = error_list[i]
+
+						if #e[1] == 2 then
+							g.board:find_child("Given "..e[1][1].." "
+								..e[1][2]).color = "FFFFFF"
+						elseif #e[1] == 3 then
+							g.board:find_child("Guess "..e[1][1].." "
+								..e[1][2].." "..e[1][3]).color = "FFFFFF"
+						else
+							error("this should never happen,"..
+								" i did something wrong")
+						end
+						if #e[2] == 2 then
+							g.board:find_child("Given "..e[2][1].." "
+								..e[2][2]).color = "FF0000"
+						elseif #e[2] == 3 then
+							g.board:find_child("Guess "..e[2][1].." "
+								..e[2][2].." "..e[2][3]).color = "FF0000"
+						else
+							error("this should never happen,"..
+								" i did something wrong")
+						end
+				end	
+				end			
+			else
+				guesses[r][c].sz = guesses[r][c].sz + 1
+				guesses[r][c][guess] = true
+				g.board:find_child("Guess "..r.." "..c..
+					" "..guess).opacity = 255
+				if status ~= "REDO" and status ~= "UNDO" then
+					table.insert(undo_list,{r,c,guess})
+					if #undo_list > 100 then
+						table.remove(undo_list,1)
+					end
+				elseif status ~= "REDO" and status ~= "UNDO" then
+					redo_list = {}
+				end
+				--Error Check
+				if error_checking then
+				for rr = 1,9 do
+					if givens[rr][c] == guess then
+						table.insert(error_list,
+							{{r,c,guess},{rr,c}})
+					elseif guesses[rr][c][guess] and 
+						rr ~= r then
+						table.insert(error_list,
+							{{r,c,guess},{rr,c,guess}})
+					end
+				end
+				for cc = 1,9 do
+					if givens[r][cc] == guess then
+						table.insert(error_list,
+							{{r,c,guess},{r,cc}})
+					elseif guesses[r][cc][guess] and 
+						cc ~= c then
+						table.insert(error_list,
+							{{r,c,guess},{r,cc,guess}})
+					end
+				end
+				for rr = math.floor((r-1)/3)*3+1, math.ceil(r/3)*3 do
+					for cc = math.floor((c-1)/3)*3+1, math.ceil(c/3)*3 do
+						if givens[rr][cc] == guess then
+							table.insert(error_list,
+								{{r,c,guess},{rr,cc}})
+						elseif guesses[rr][cc][guess] and 
+							(cc ~= c or rr ~= r) then
+							table.insert(error_list,
+								{{r,c,guess},{rr,cc,guess}})
+						end
+					end
+				end
+				for i,e in ipairs(error_list) do
+					if #e[1] == 2 then
+						g.board:find_child("Given "..e[1][1].." "
+							..e[1][2]).color = "FF0000"
+					elseif #e[1] == 3 then
+						g.board:find_child("Guess "..e[1][1].." "
+							..e[1][2].." "..e[1][3]).color = "FF0000"
+					else
+						error("this should never happen,"..
+							" i did something wrong")
+					end
+
+					if #e[2] == 2 then
+						g.board:find_child("Given "..e[2][1].." "
+							..e[2][2]).color = "FF0000"
+					elseif #e[2] == 3 then
+						g.board:find_child("Guess "..e[2][1].." "
+							..e[2][2].." "..e[2][3]).color = "FF0000"
+					else
+						error("this should never happen,"..
+							" i did something wrong")
+					end
+				end
+				end
+			end
 			return true
 		else
 			return false
 		end
 	end
+	function g:error_check()
+		if error_checking then
+			error_checking = false
+				for i =#error_list,1,-1 do
+					e = error_list[i]
+						--table.remove(error_list,i)
+
+					if #e[1] == 2 then
+						g.board:find_child("Given "..e[1][1].." "
+							..e[1][2]).color = "FFFFFF"
+					elseif #e[1] == 3 then
+						g.board:find_child("Guess "..e[1][1].." "
+							..e[1][2].." "..e[1][3]).color = "FFFFFF"
+					else
+						error("this should never happen,"..
+							" i did something wrong")
+					end
+					if #e[2] == 2 then
+						g.board:find_child("Given "..e[2][1].." "
+							..e[2][2]).color = "FFFFFF"
+					elseif #e[2] == 3 then
+						g.board:find_child("Guess "..e[2][1].." "
+							..e[2][2].." "..e[2][3]).color = "FFFFFF"
+					else
+						error("this should never happen,"..
+							" i did something wrong")
+					end
+					table.remove(error_list,i)
+				end
+			return
+		end
+		error_checking = true
+		error_list = {}
+		for r = 1,9 do
+			for c = 1,9 do
+				if givens[r][c] == 0 then
+					for guess = 1,9 do
+						if guesses[r][c][guess] then
+							for rr = 1,9 do
+								if givens[rr][c] == guess then
+									table.insert(error_list,
+										{{r,c,guess},{rr,c}})
+								elseif guesses[rr][c][guess] and 
+									rr ~= r then
+									table.insert(error_list,
+										{{r,c,guess},{rr,c,guess}})
+								end
+							end
+							for cc = 1,9 do
+								if givens[r][cc] == guess then
+									table.insert(error_list,
+										{{r,c,guess},{r,cc}})
+								elseif guesses[r][cc][guess] and 
+									cc ~= c then
+									table.insert(error_list,
+										{{r,c,guess},{r,cc,guess}})
+								end
+							end
+							for rr = math.floor((r-1)/3)*3+1, 
+							         math.ceil(r/3)*3 do
+								for cc = math.floor((c-1)/3)*3+1, 
+								         math.ceil(c/3)*3 do
+									if givens[rr][cc] == guess then
+										table.insert(error_list,
+											{{r,c,guess},{rr,cc}})
+									elseif guesses[rr][cc][guess] and 
+										(cc ~= c or rr ~= r) then
+										table.insert(error_list,
+											{{r,c,guess},{rr,cc,guess}})
+									end
+								end
+							end
+						end
+					end
+---[[ uncomment to see if the givens produced by the board generator are messed up
+				else
+					for rr = 1,9 do
+						if givens[rr][c] == givens[r][c] and rr ~= r then
+							table.insert(error_list,
+								{{r,c},{rr,c}})
+						elseif guesses[rr][c][ givens[r][c] ] then
+							table.insert(error_list,
+								{{r,c},{rr,c,givens[r][c]}})
+						end
+					end
+					for cc = 1,9 do
+						if givens[r][cc] == givens[r][c] and cc ~= c then
+							table.insert(error_list,
+								{{r,c},{r,cc}})
+						elseif guesses[r][cc][ givens[r][c] ] then
+							table.insert(error_list,
+								{{r,c},{r,cc,givens[r][c]}})
+						end
+					end
+					for rr = math.floor((r-1)/3)*3+1, 
+					         math.ceil(r/3)*3 do
+						for cc = math.floor((c-1)/3)*3+1, 
+						         math.ceil(c/3)*3 do
+							if givens[rr][cc] == givens[r][c] and 
+								(cc ~= c or rr ~= r) then
+								table.insert(error_list,
+									{{r,c},{rr,cc}})
+							elseif guesses[rr][cc][ givens[r][c] ] then
+								table.insert(error_list,
+									{{r,c},{rr,cc,givens[r][c]}})
+							end
+						end
+					end
+--]]
+				end
+			end
+		end
+
+
+		for i,e in ipairs(error_list) do
+			if #e[1] == 2 then
+				g.board:find_child("Given "..e[1][1].." "
+					..e[1][2]).color = "FF0000"
+			elseif #e[1] == 3 then
+				g.board:find_child("Guess "..e[1][1].." "
+					..e[1][2].." "..e[1][3]).color = "FF0000"
+			else
+				error("this should never happen,"..
+					" i did something wrong")
+			end
+			if #e[2] == 2 then
+
+				if givens[e[2][1]][e[2][2]] == 0 then
+					print(e[1][1],e[1][2],"   ",e[2][1],e[2][2])
+
+					debug()
+				end
+				g.board:find_child("Given "..e[2][1].." "
+					..e[2][2]).color = "FF0000"
+			elseif #e[2] == 3 then
+				g.board:find_child("Guess "..e[2][1].." "
+					..e[2][2].." "..e[2][3]).color = "FF0000"
+			else
+				error("this should never happen,"..
+					" i did something wrong")
+			end
+		end
+	end
+	function g:undo(r,c)
+		if #undo_list > 0 then
+			params = table.remove(undo_list)
+			table.insert(redo_list,{params[1],params[2],params[3]})
+			g:toggle_guess(params[1],params[2],params[3],"UNDO")
+			if r ~= params[1] or c ~= params[2] then
+				r = params[1]
+				c = params[2]
+				if guesses[r][c].sz == 1 then
+					local i = 1
+					for k = 1,9 do
+						if guesses[r][c][k] then
+							i=k	
+							print(i)			
+						end
+					end
+					local guess = g.board:find_child("Guess "..
+						r.." "..c.." "..i)
+					guess.scale = {1,1}
+					guess.x = (c-1)*TILE_WIDTH + 
+						math.floor((c-1)/3)*SET_GUTTER +
+						(c-1)*TILE_GUTTER
+					guess.y = (r-1)*TILE_WIDTH + 
+						math.floor((r-1)/3)*SET_GUTTER +
+						(r-1)*TILE_GUTTER+TOP_GAP+
+						TILE_WIDTH/2
+				end
+
+			end
+		end
+	end
+	function g:redo(r,c)
+		if #redo_list > 0 then
+			params = table.remove(redo_list)
+			table.insert(undo_list,{params[1],params[2],params[3]})
+			g:toggle_guess(params[1],params[2],params[3],"REDO")
+			if r ~= params[1] or c ~= params[2] then
+				r = params[1]
+				c = params[2]
+				if guesses[r][c].sz == 1 then
+					local i = 1
+					for k = 1,9 do
+						if guesses[r][c][k] then
+							i=k	
+							print(i)			
+						end
+					end
+					local guess = g.board:find_child("Guess "..
+						r.." "..c.." "..i)
+					guess.scale = {1,1}
+					guess.x = (c-1)*TILE_WIDTH + 
+						math.floor((c-1)/3)*SET_GUTTER +
+						(c-1)*TILE_GUTTER
+					guess.y = (r-1)*TILE_WIDTH + 
+						math.floor((r-1)/3)*SET_GUTTER +
+						(r-1)*TILE_GUTTER+TOP_GAP+
+						TILE_WIDTH/2
+				
+				elseif guesses[r][c].sz > 1 then
+					for i = 1,9 do
+						if guesses[r][c][i] then
+							local guess = g.board:find_child("Guess "..
+								r.." "..c.." "..i)
+							guess.scale = {1/2,1/2}
+							guess.x = (c-1)*TILE_WIDTH + 
+								math.floor((c-1)/3)*SET_GUTTER +
+								(c-1)*TILE_GUTTER+ ((i-1)%3-1)*
+								TILE_WIDTH/3
+							guess.y = (r-1)*TILE_WIDTH + 
+								math.floor((r-1)/3)*SET_GUTTER +
+								(r-1)*TILE_GUTTER+ (math.floor(
+								(i-1)/3)-1)*TILE_WIDTH/3+TOP_GAP+
+								TILE_WIDTH/2
+						end
+					end
+				end
+			end
+		end
+	end
+
+	function g:out_focus(r,c)
+		--g.board:find_child("Tile "..r.." "..c).color = "404040"
+		g.board:find_child("Tile "..r.." "..c).opacity = 0
+		
+		if  givens[r][c] == 0 then
+			g.board:find_child("Guess "..r.." "..c).opacity = 0
+			if guesses[r][c].sz == 1 then
+				local i = 1
+				for k = 1,9 do
+					if guesses[r][c][k] then
+						i=k	
+						print(i)			
+						--break
+					end
+				end
+				local guess = g.board:find_child("Guess "..
+					r.." "..c.." "..i)
+				guess.scale = {1,1}
+				guess.x = (c-1)*TILE_WIDTH + 
+					math.floor((c-1)/3)*SET_GUTTER +
+					(c-1)*TILE_GUTTER
+				guess.y = (r-1)*TILE_WIDTH + 
+					math.floor((r-1)/3)*SET_GUTTER +
+					(r-1)*TILE_GUTTER+TOP_GAP+
+					TILE_WIDTH/2
+			end
+		end
+	end
+	function g:on_focus(r,c)
+		--g.board:find_child("Tile "..r.." "..c).color = "000000"
+		g.board:find_child("Tile "..r.." "..c).opacity = 255
+		
+		if givens[r][c] == 0 then
+			g.board:find_child("Guess "..r.." "..c).opacity = 255
+			if guesses[r][c].sz == 1 then
+				local i = 1
+				for k = 1,9 do
+					if guesses[r][c][k] then
+						i=k
+						break
+					end
+				end
+				local guess = g.board:find_child("Guess "..
+					r.." "..c.." "..i)
+				guess.scale = {1/2,1/2}
+				guess.x = (c-1)*TILE_WIDTH + 
+					math.floor((c-1)/3)*SET_GUTTER +
+					(c-1)*TILE_GUTTER+ ((i-1)%3-1)*
+					TILE_WIDTH/3
+				guess.y = (r-1)*TILE_WIDTH + 
+					math.floor((r-1)/3)*SET_GUTTER +
+					(r-1)*TILE_GUTTER+ (math.floor(
+					(i-1)/3)-1)*TILE_WIDTH/3+TOP_GAP+
+					TILE_WIDTH/2
+			end
+
+		end
+	end
+	function g:clear_tile(r,c)
+		local params = {}
+		for i = 1,9 do
+			if guesses[r][c][i] then
+				guesses[r][c][i] = false
+				local guess = g.board:find_child("Guess "..
+						r.." "..c.." "..i)
+				guess.color = "FFFFFF"
+				guess.opacity = 0
+				table.insert(params,i)
+			end
+		end
+		table.insert(undo_list,{r,c,params})
+	end
+--[[
+	function g:enter_menu(r,c)
+		sel_menu.x = (c)*TILE_WIDTH + 
+			math.floor((c-1)/3)*SET_GUTTER +
+			(c-1)*TILE_GUTTER
+		sel_menu.y = (r-1)*TILE_WIDTH + 
+			math.floor((r-1)/3)*SET_GUTTER +
+			(r-1)*TILE_GUTTER+TOP_GAP+TILE_WIDTH/2
+	end
+--]]
 end)
 
 
--- apps/sudoku/main.lua
+++ apps/sudoku/main.lua
@@ -1,6 +1,87 @@
 math.randomseed(os.time())
+local bg = Image{src="assets/bg.jpg"}
+screen:add(bg)
+screen:add(
+	Text{
+		text = "N - New Game\n\nU - Undo\nR - Redo\n"..
+		        "C - Clear Tile\nE - Error \n     Checking",
+		font = "DejaVu Bold 60px",
+		color = "FFFFFF",
+		x = 1600,
+		y = 200
+	}
+)
+local num_givens = 50
+local splash     = Group{}
+splash:add(
+	Text{
+		name  = "title_s",
+		text  = "Sudoku",
+		font  = "DejaVu Bold 100px",
+		color = "000000",
+		x     = screen.w/2 + 3,
+		y     = screen.h/2 - 200 +3
+	},
+	Text{
+		name  = "title",
+		text  = "Sudoku",
+		font  = "DejaVu Bold 100px",
+		color = "FFFFFF",
+		x     = screen.w/2,
+		y     = screen.h/2 - 200
+	},
+	Text{
+		name  = "new_s",
+		text  = "New Game\nNumber of Givens",
+		font  = "DejaVu Bold 100px",
+		color = "000000",
+		x     = screen.w/2+150+3,
+		y     = screen.h/2+3
+	},
+	Text{
+		name  = "new",
+		text  = "New Game\nNumber of Givens",
+		font  = "DejaVu Bold 100px",
+		color = "FFFFFF",
+		x     = screen.w/2+150,
+		y     = screen.h/2
+	},
+	Text{
+		name  = "cont_s",
+		text  = "Continue Old Game",
+		font  = "DejaVu Bold 100px",
+		color = "000000",
+		x     = screen.w/2-150+3,
+		y     = screen.h/2+3
+	},
 
+	Text{
+		name  = "cont",
+		text  = "Continue Old Game",
+		font  = "DejaVu Bold 100px",
+		color = "FFFFFF",
+		x     = screen.w/2-150,
+		y     = screen.h/2
+	},
+	Text{
+		name  = "givens_s",
+		text  = num_givens,
+		font  = "DejaVu Bold 100px",
+		color = "000000",
+		x     = screen.w/2+250+3,
+		y     = screen.h/2+3
+	},
 
+	Text{
+		name  = "givens",
+		text  = num_givens,
+		font  = "DejaVu Bold 100px",
+		color = "FFFFFF",
+		x     = screen.w/2+250,
+		y     = screen.h/2
+	}
+)
+screen:add(splash)
 Directions = {
    RIGHT = { 1, 0},
    LEFT  = {-1, 0},
@@ -11,7 +92,7 @@ Directions = {
 dofile("Class.lua") -- Must be declared before any class definitions.
 
 dofile("Game.lua")
-
+--[[
 dofile("MVC.lua")
 dofile("FocusableImage.lua")
 
@@ -60,4 +141,224 @@ function reset_keys()
     screen.on_key_down = model.keep_keys
 end
 model:start_app(Components.SPLASH)
+--]]
+local game_on = false
+game = nil
+local ind = {r=1,c=1}
+local mode = {"TRAVERSAL","CLEAR","PEN","PENCIL","BACK"}
+local curr_mode = "TRAVERSAL"
+function game_on_key_down(k)
+	local key = 
+	{
+		[keys.Down] = function()
+			if ind.r < 9 then
+				game:out_focus(ind.r,ind.c)
+				ind.r=ind.r+1
+				game:on_focus(ind.r,ind.c)
+			end
+		end,
+		[keys.Up] = function()
+			if ind.r > 1 then
+				game:out_focus(ind.r,ind.c)
+				ind.r=ind.r-1
+				game:on_focus(ind.r,ind.c)
 
+			end
+		end,
+		[keys.Right] = function()
+			if ind.c < 9 then
+				game:out_focus(ind.r,ind.c)
+				ind.c=ind.c+1
+				game:on_focus(ind.r,ind.c)
+			end
+		end,
+		[keys.Left] = function()
+
+			if ind.c > 1 then
+				game:out_focus(ind.r,ind.c)
+				ind.c=ind.c-1
+				game:on_focus(ind.r,ind.c)
+			end
+
+		end,
+--[[
+		[keys.Return] = function()
+			local enter_presses = {
+				["TRAVERSAL"] = function ()
+					game:enter_menu(ind.r,ind.c)
+					curr_mode = "PENCIL"
+				end,
+				["CLEAR"] = function ()
+					game:clear_guesses()
+				end,
+				["PEN"] = function ()
+				end,
+				["PENCIL"] = function ()
+				end,
+				["BACK"] = function ()
+					game:close_menu()
+					curr_mode = "TRAVERSAL"
+				end,				
+			}
+			if enter_presses[curr_mode] then
+				enter_presses[curr_mode]()
+			else
+				error("aw shit")
+			end
+		end,
+--]]
+		[keys["1"] ] = function()
+			game:toggle_guess(ind.r,ind.c,1,"REG")
+		end,
+		[keys["2"] ] = function()
+			game:toggle_guess(ind.r,ind.c,2,"REG")
+		end,
+		[keys["3"] ] = function()
+			game:toggle_guess(ind.r,ind.c,3,"REG")
+		end,
+		[keys["4"] ] = function()
+			game:toggle_guess(ind.r,ind.c,4,"REG")
+		end,
+		[keys["5"] ] = function()
+			game:toggle_guess(ind.r,ind.c,5,"REG")
+		end,
+		[keys["6"] ] = function()
+			game:toggle_guess(ind.r,ind.c,6,"REG")
+		end,
+		[keys["7"] ] = function()
+			game:toggle_guess(ind.r,ind.c,7,"REG")
+		end,
+		[keys["8"] ] = function()
+			game:toggle_guess(ind.r,ind.c,8,"REG")
+		end,
+		[keys["9"] ] = function()
+			game:toggle_guess(ind.r,ind.c,9,"REG")
+		end,
+
+
+		[keys["KP_End"] ] = function()
+			game:toggle_guess(ind.r,ind.c,1,"REG")
+		end,
+		[keys["KP_Down"] ] = function()
+			game:toggle_guess(ind.r,ind.c,2,"REG")
+		end,
+		[keys["KP_Page_Down"] ] = function()
+			game:toggle_guess(ind.r,ind.c,3,"REG")
+		end,
+		[keys["KP_Left"] ] = function()
+			game:toggle_guess(ind.r,ind.c,4,"REG")
+		end,
+		[keys["KP_Begin"] ] = function()
+			game:toggle_guess(ind.r,ind.c,5,"REG")
+		end,
+		[keys["KP_Right"] ] = function()
+			game:toggle_guess(ind.r,ind.c,6,"REG")
+		end,
+		[keys["KP_Home"] ] = function()
+			game:toggle_guess(ind.r,ind.c,7,"REG")
+		end,
+		[keys["KP_Up"] ] = function()
+			game:toggle_guess(ind.r,ind.c,8,"REG")
+		end,
+		[keys["KP_Page_Up"] ] = function()
+			game:toggle_guess(ind.r,ind.c,9,"REG")
+		end,
+		
+		[keys["u"] ] = function()
+			game:undo(ind.r,ind.c)
+		end,
+		[keys["r"] ] = function()
+			game:redo(ind.r,ind.c)
+		end,
+		[keys["n"] ] = function()
+			game.board:clear()
+			game = Game(80)
+			game:on_focus(1,1)
+			ind = {r=1,c=1}
+		end,
+		[keys["c"] ] = function()
+			game:clear_tile(ind.r,ind.c)
+		end,
+		[keys["e"] ] = function()
+			game:error_check()
+		end,
+
+
+	}
+	if key[k] then key[k]() end
+end
+local splash_hor_index = 2
+if settings.g then
+	game = settings.g
+	screen:add(game.board)
+	splash_hor_index = 1
+			splash:find_child("cont").color    = "FF0000"
+			splash:find_child("new").color   = "FFFFFF"
+
+else
+	game = nil
+				splash:find_child("cont").color    = "FFFFFF"
+				splash:find_child("new").color   = "FF0000"
+
+end
+function splash_on_key_down(k)
+	local key = 
+	{
+		[ keys["Return"] ] = function()
+			splash.opacity = 0			
+
+			if splash_hor_index == 2 then
+				if settings.g then
+					game.board:clear()
+				end
+				game = Game(num_givens)
+			end
+			game_on = true
+			game:on_focus(1,1)
+			ind = {r=1,c=1}
+		end,
+		[keys.Down] = function()
+			if num_givens > 25 then
+				num_givens = num_givens - 1
+				splash:find_child("givens").text = num_givens
+				splash:find_child("givens_s").text = num_givens
+			end
+		end,
+		[keys.Up] = function()
+			if num_givens < 60 then
+				num_givens = num_givens + 1
+				splash:find_child("givens").text = num_givens
+				splash:find_child("givens_s").text = num_givens
+			end
+
+		end,
+		[keys.Right] = function()
+			splash_hor_index = 2
+			splash:find_child("new").color    = "FF0000"
+			splash:find_child("cont").color   = "FFFFFF"
+
+		end,
+		[keys.Left] = function()
+
+			if settings.g then
+				splash_hor_index = 1
+				splash:find_child("new").color    = "FFFFFF"
+				splash:find_child("cont").color   = "FF0000"
+			end
+		end,
+
+	}
+	if key[k] then key[k]() end	
+end
+function screen:on_key_down(k)
+	if game_on then
+		game_on_key_down(k)
+	else
+		splash_on_key_down(k)
+	end
+end
+function app:on_close()
+	settings.g = game
+end
+--game:on_focus(1,1)
+screen:show()
