8e3af24737b6989a29f72893da92d8e3be60d282
Pablo Pissanetzky <pablo@trickplay.com>
2010-10-12 17:27:33 -0700

    Chain test
    Former-commit-id: dc4d37e17c2a43e91ac0053ace85b0c5fa4190f1
    Former-commit-id: 86b929bf349623ab881136b0b62f5af2535a264d

---
+++ tests/apps/physics/test_chain.lua
@@ -0,0 +1,169 @@
+
+-------------------------------------------------------------------------------
+
+local ground = Rectangle
+{
+    color = "00FF00",
+    size  = { screen.w , 30 },
+    position = { 0 , screen.h - 30 }
+}
+
+ground = physics:Body
+{
+    source = ground,
+    type = "static",
+    density = 1,
+    friction = 0.5,
+    bounce = 0.1
+}
+
+screen:add( ground.source )
+
+-------------------------------------------------------------------------------
+
+local bumper
+
+bumper = physics:Body
+{
+    source =
+    
+        Rectangle
+        {
+            color = "00FF00",
+            size = { screen.h * 1.2 , screen.h / 4 },
+        },
+        
+    type = "static"
+    
+}
+
+bumper.position = { 0 , screen.h / 2 }
+bumper.angle = 75
+
+screen:add( bumper.source )
+
+
+bumper = physics:Body
+{
+    source =
+    
+        Rectangle
+        {
+            color = "00FF00",
+            size = { screen.h * 1.2 , screen.h / 4 },
+        },
+        
+    type = "static"
+    
+}
+
+bumper.position = { screen.w , screen.h / 2 }
+bumper.angle = -75
+
+screen:add( bumper.source )
+
+-------------------------------------------------------------------------------
+
+local POLE_HEIGHT = 800
+
+local pole = Rectangle
+{
+    color = "473232",
+    size = { 30 , POLE_HEIGHT },
+    position = { screen.w / 2 - 15 , screen.h - ( POLE_HEIGHT + ground.source.h ) }
+}
+
+screen:add( pole )
+
+pole = physics:Body
+{
+    source = pole,
+    density = 1,
+    type = "static",
+    filter = { category = 1 }
+}
+
+-------------------------------------------------------------------------------
+
+local CHAIN_LENGTH = POLE_HEIGHT 
+local LINKS = 30
+local LINK_WIDTH = 20
+local LINK_COLOR = "FFFFFF"
+
+local last_link = pole
+
+for i = 1 , LINKS do
+
+    local link = Rectangle
+    {
+        color = LINK_COLOR,
+        size = { LINK_WIDTH , CHAIN_LENGTH / LINKS },
+    }
+    
+    link.anchor_point = link.center
+    link.z_rotation = { 90 , 0 , 0 }
+    
+    link.x = pole.source.x + link.h / 2 + ( ( link.h ) * ( i - 1 ) )
+    link.y = pole.source.y - pole.source.h / 2 
+
+    screen:add( link )
+    
+    link = physics:Body
+    {
+        source = link,
+        density = 2,
+        friction = 0.5,
+        bounce = 0.5,
+        filter = { category = 2 , mask = { 0 , 3 } }
+    }
+
+    link:RevoluteJoint( last_link , { link.x - link.source.w / 2 , link.y } ,
+    {
+        enable_limit = true,
+        lower_angle = -90,
+        upper_angle = 90
+    }
+    )
+    
+    last_link = link
+end
+
+-------------------------------------------------------------------------------
+
+local BLOCK_COUNT = 20
+
+local GLOBE_SCALE = 0.2
+local globe = Image{ src = "images/globe.png" , opacity = 0 }
+screen:add( globe )
+
+for i = 1 , BLOCK_COUNT do
+
+    local block = physics:Body
+    {
+        source = Clone
+        {
+            source = globe ,
+            opacity = 255,
+            scale = { GLOBE_SCALE , GLOBE_SCALE },
+            position = { screen.w / BLOCK_COUNT * i  , - 50 }
+        },
+        type = "dynamic",
+        density = 0.2,
+        bounce = 0.8,
+        shape = physics:Circle( ( globe.w / 2 ) * GLOBE_SCALE ),
+        filter = { category = 3 , mask = { 0 , 2 , 3 } }
+    }
+    
+    screen:add( block.source )
+end
+
+-------------------------------------------------------------------------------
+
+physics.gravity = { 0 , 6 }
+
+function idle.on_idle()
+
+    physics:step()
+    --physics:draw_debug( 255 )
+    
+end
