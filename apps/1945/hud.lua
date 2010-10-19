d02130b0ea4c58f2fc5865b574b30f2ab6cd510a
Alex Indaco <alex.indaco@trickplay.com>
2010-10-19 08:30:22 -0700

    broke up stuff
    Former-commit-id: 179bb4ce3c5e3817657b7fa5f9a2dc3df279f9eb
    Former-commit-id: 182f9868a563e21a347c6104ed976d8ac043eaa2

---
+++ apps/1945/hud.lua
@@ -0,0 +1,77 @@
+number_of_lives = 3
+high_score = settings.high_score or 0
+point_counter = 0
+my_plane_sz = 128
+topbar = Group{z=1}
+topbar:add( 
+	Rectangle{
+		name = "BG",
+		color="0000007F",
+		w=1920,
+		h=80
+	},
+    --[[
+	Text{
+		name = "LIVES",
+		text = "Lives:",
+		font="Highway Gothic Wide Bold 36px",
+		color="FFFFFF",
+		x=30,
+		y=20
+	},
+    --]]
+	Text{
+		name = "SCORE",
+		text = "Current Score:",
+		font="Highway Gothic Wide Bold 36px",
+		color="FFFFFF",
+		y = 20
+	},
+	Text{
+		name = "HIGHSCORE",
+		text = "High Score:",
+		font="Highway Gothic Wide Bold 36px",
+		color="FFFFFF",
+		y = 20
+	}
+
+)
+local score_txt = Text{
+		text = "",
+		font="Highway Gothic Wide Bold 36px",
+		color="FFFFFF",
+		x = screen.w/2 +20,
+		y = 20
+}
+local h_score_txt = Text{
+		text = "",
+		font="Highway Gothic Wide Bold 36px",
+		color="FFFFFF",
+		y = 20
+}
+function redo_score_text()
+	score_txt.text   = string.format("%06d",point_counter)
+	h_score_txt.text = string.format("%06d",high_score) 
+end
+
+redo_score_text()
+h_score_txt.x = screen.w-h_score_txt.w-20
+
+topbar:add(score_txt,h_score_txt)
+topbar:find_child("SCORE").x = screen.w/2 - topbar:find_child("SCORE").w - 20
+topbar:find_child("HIGHSCORE").x = h_score_txt.x - topbar:find_child("HIGHSCORE").w - 20
+
+local life = Image{src = "assets/life.png",opacity=0,z=10}
+screen:add(life)
+lives =
+{
+	Clone{name="life1",source=life,x=20,y=15,z=10},
+	Clone{name="life2",source=life,x=80,y=15,z=10},
+	Clone{name="life3",source=life,x=140,y=15,z=10},
+	Clone{name="life4",source=life,x=200,y=15,z=10,opacity=0},
+	Clone{name="life5",source=life,x=260,y=15,z=10,opacity=0},
+}
+
+screen:add(topbar,score)
+screen:add(unpack(lives))
+
