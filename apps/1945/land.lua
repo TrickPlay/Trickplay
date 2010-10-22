f144156330f4042584928b218642bef6d7a4007a
Alex Indaco <alex.indaco@trickplay.com>
2010-10-22 16:37:21 -0700

    level 1 is done

--- apps/1945/land.lua
+++ apps/1945/land.lua
@@ -1,143 +1,78 @@
+
+
+
 water =
 {
-    speed = 100, -- pixels per second
-    
+    speed  = 100, -- pixels per second
     strips = {},
-    
-    top_y = 0,
-    
-    time = 0,
-    
-    island_time = 0.5, -- seconds
-    
-    setup =
-    
-        function( self )
+    top_y  = 0,
+    setup = function( self )
                 
             local tile = assets.water
-            
-            --tile:set{ w = screen.w , tile = { false  , false } }
-                        
+            tile:set{ w = screen.w , tile = { true  , false } }
             for i = 1 , math.ceil( screen.h / tile.h ) + 3 do
-                   
                 table.insert( self.strips , Clone{ source = tile } )
-                
             end
-            
             local top = - ( tile.h * 2 )
-            
             self.top_y = top
-            
             for _ , strip in ipairs( self.strips ) do
-            
                 strip.position = { 0 , top }
-                
                 top = top + tile.h - 1
-            
                 screen:add( strip )
-                
             end
+    end,
 
-        end,
-            
-    render =
-    
-        function( self , seconds )
+	add_doodad = function( self, index, xxx, x_rot, y_rot, z_rot )
 
-                
-            -- reposition all the water strips
-            
-            local dy = self.speed * seconds
-            
-            local maxy = screen.h
-            
-            self.top_y = self.top_y + dy    
-            
-            for _ , strip in ipairs( self.strips ) do
 
+        local island =
             
-                strip.y = strip.y + dy
+            {
+                speed = self.speed,
+                image = Clone{ 
+					source       = assets[ "island"..tostring( index ) ] , 
+					x_rotation   = { x_rot , 0, 0},
+					y_rotation   = { y_rot , 0, 0},
+					z_rotation   = { z_rot , 0, 0},
+					opacity      = 255 
+				},
+                setup = function( self )
+                        screen:add( self.image )
+						self.image:lower_to_bottom()
+						self.image.anchor_point = {  self.image.w / 2 ,  self.image.h / 2 }
+						self.image.position     = {               xxx , -self.image.h / 2 }
 
-                if strip.y > maxy then
-                
-                    strip.y = self.top_y - strip.h + 1   
+                        for _ , strip in ipairs( water.strips ) do
+    				        strip:lower_to_bottom()
+    					end
+                end,
                     
-                    self.top_y = strip.y
-                
-                end
+                render = function( self , seconds )
+                        self.image.y = self.image.y + self.speed * seconds
+                        if self.image.y > (screen.h+self.image.h) then
+                            remove_from_render_list( self )
+                            screen:remove( self.image )
+                        end
+                        
+                end,
+            }
             
-            end
+        add_to_render_list( island )
+	end,
             
-            -- see if we should drop an island
-            
-            self.time = self.time + seconds
-            
-            if false then
+    render = function( self , seconds )
+            -- reposition all the water strips
+            local dy   = self.speed * seconds
+            local maxy = screen.h
+            self.top_y = self.top_y + dy    
 
---self.time >= self.island_time then
-            
-                self.time = self.time - self.island_time
-                
-                if math.random( 100 ) < 50 then
-                               
-                    local island =
-                        
-                        {
-                            speed = self.speed,
-                            
-                            image = Clone{ source = assets[ "island"..tostring( math.random( 1 , 3 ) ) ] , opacity = 255 },
-                            
-                            setup =
-                            
-                                function( self )
-                                
-                                    self.image.position = { math.random( 0 , screen.w ) , - self.image.h }
-                                    
-                                    if math.random( 100 ) > 50 then
-                                    
-                                        self.image.y_rotation = { 180 , self.image.w / 2 , 0 }
-                                    
-                                    end
-                                    
-                                    if math.random( 100 ) < 50 then
-                                    
-                                        self.image.x_rotation = { 180 , self.image.h / 2 , 0 }
-                                    
-                                    end
-                                    
-                                    self.image.z_rotation = { math.random( 180 ) , self.image.w / 2 , self.image.h / 2 }
-                                    
-                                    screen:add( self.image )
-                                
-                                end,
-                                
-                            render =
-                            
-                                function( self , seconds )
-                                
-                                    local y = self.image.y + self.speed * seconds
-                                    
-                                    if y > screen.h then
-                                    
-                                        remove_from_render_list( self )
-                                        
-                                        screen:remove( self.image )
-                                        
-                                    else
-                                    
-                                        self.image.y = y
-                                        
-                                    end
-                                    
-                                end,
-                        }
-                        
-                    add_to_render_list( island )
-                
+            for _ , strip in ipairs( self.strips ) do
+                strip.y = strip.y + dy
+                if strip.y > maxy then
+                    strip.y    = self.top_y - strip.h + 1   
+                    self.top_y = strip.y
                 end
-            
             end
-            
-        end,        
+    end,        
 }
 
