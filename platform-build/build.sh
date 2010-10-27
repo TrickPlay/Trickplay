0d4c3f254e59bc3c27d3d97d106aca971e8260a9
Pablo Pissanetzky <pablo@trickplay.com>
2010-10-27 13:10:40 -0700

    Everything built with -fPIC. Added output include directory to link test.

--- platform-build/build.sh
+++ platform-build/build.sh
@@ -42,7 +42,7 @@ GLIB_V="${GLIB_MV}.4"
 GLIB_URL="http://ftp.acc.umu.se/pub/GNOME/sources/glib/${GLIB_MV}/glib-${GLIB_V}.tar.gz"
 GLIB_DIST="glib-${GLIB_V}.tar.gz"
 GLIB_SOURCE="glib-${GLIB_V}"
-GLIB_COMMANDS="glib_cv_stack_grows=no glib_cv_uscore=no ac_cv_func_posix_getpwuid_r=yes ac_cv_func_posix_getgrgid_r=yes ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared ${GLIB_ICONV} && make install"
+GLIB_COMMANDS="glib_cv_stack_grows=no glib_cv_uscore=no ac_cv_func_posix_getpwuid_r=yes ac_cv_func_posix_getgrgid_r=yes ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared ${GLIB_ICONV} --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # sqlite
@@ -77,7 +77,7 @@ CARES_V="1.7.0"
 CARES_URL="http://c-ares.haxx.se/c-ares-${CARES_V}.tar.gz"
 CARES_DIST="c-ares-${CARES_V}.tar.gz"
 CARES_SOURCE="c-ares-${CARES_V}"
-CARES_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+CARES_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # curl
@@ -86,7 +86,7 @@ CURL_V="7.20.0"
 CURL_URL="http://curl.haxx.se/download/curl-${CURL_V}.tar.gz"
 CURL_DIST="curl-${CURL_V}.tar.gz"
 CURL_SOURCE="curl-${CURL_V}"
-CURL_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --enable-ares --with-ssl --with-zlib --without-random --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp --disable-manual --disable-dict && make install"
+CURL_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic --enable-ares --with-ssl --with-zlib --without-random --disable-file --disable-ldap --disable-ldaps --disable-rtsp --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smtp --disable-manual --disable-dict && make install"
 CURL_DEPENDS="CARES ZLIB OPENSSL"
 #------------------------------------------------------------------------------
 # bzip
@@ -95,7 +95,7 @@ BZIP_V="1.0.5"
 BZIP_URL="http://www.bzip.org/${BZIP_V}/bzip2-${BZIP_V}.tar.gz"
 BZIP_DIST="bzip2-${BZIP_V}.tar.gz"
 BZIP_SOURCE="bzip2-${BZIP_V}"
-BZIP_COMMANDS="make CC=\"$CC\" AR=\"$AR\" RANLIB=\"$RANLIB\" LDFLAGS=\"$LDFLAGS\" CFLAGS=\"$CFLAGS\" PREFIX=\"$PREFIX\" install"
+BZIP_COMMANDS="make CC=\"$CC\" AR=\"$AR\" RANLIB=\"$RANLIB\" LDFLAGS=\"$LDFLAGS\" CFLAGS=\"-fPIC $CFLAGS\" PREFIX=\"$PREFIX\" install"
 
 #------------------------------------------------------------------------------
 # tokyo (DEPRECATED IN 0.0.8)
@@ -113,7 +113,7 @@ EXPAT_V="2.0.1"
 EXPAT_URL="http://sourceforge.net/projects/expat/files/expat/${EXPAT_V}/expat-${EXPAT_V}.tar.gz/download"
 EXPAT_DIST="expat-${EXPAT_V}.tar.gz"
 EXPAT_SOURCE="expat-${EXPAT_V}"
-EXPAT_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+EXPAT_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # freetype
@@ -122,7 +122,7 @@ FREETYPE_V="2.3.12"
 FREETYPE_URL="http://download.savannah.gnu.org/releases/freetype/freetype-${FREETYPE_V}.tar.gz"
 FREETYPE_DIST="freetype-${FREETYPE_V}.tar.gz"
 FREETYPE_SOURCE="freetype-${FREETYPE_V}"
-FREETYPE_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+FREETYPE_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # fontconfig
@@ -131,7 +131,7 @@ FONTCONFIG_V="2.8.0"
 FONTCONFIG_URL="http://fontconfig.org/release/fontconfig-${FONTCONFIG_V}.tar.gz"
 FONTCONFIG_DIST="fontconfig-${FONTCONFIG_V}.tar.gz"
 FONTCONFIG_SOURCE="fontconfig-${FONTCONFIG_V}"
-FONTCONFIG_COMMANDS="./autogen.sh --prefix=$PREFIX --host=$HOST --build=$BUILD --with-arch=$ARCH --disable-shared --with-freetype-config=\"$PREFIX/bin/freetype-config\" && make install"
+FONTCONFIG_COMMANDS="./autogen.sh --prefix=$PREFIX --host=$HOST --build=$BUILD --with-arch=$ARCH --disable-shared --with-pic --with-freetype-config=\"$PREFIX/bin/freetype-config\" && make install"
 
 #------------------------------------------------------------------------------
 # pixman
@@ -140,7 +140,7 @@ PIXMAN_V="0.17.6"
 PIXMAN_URL="http://cgit.freedesktop.org/pixman/snapshot/pixman-${PIXMAN_V}.tar.gz"
 PIXMAN_DIST="pixman-${PIXMAN_V}.tar.gz"
 PIXMAN_SOURCE="pixman-${PIXMAN_V}"
-PIXMAN_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --disable-gtk && make install"
+PIXMAN_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic --disable-gtk && make install"
 
 #------------------------------------------------------------------------------
 # png
@@ -149,7 +149,7 @@ PNG_V="1.2.42"
 PNG_URL="http://sourceforge.net/projects/libpng/files/00-libpng-stable/${PNG_V}/libpng-${PNG_V}.tar.gz/download"
 PNG_DIST="libpng-${PNG_V}.tar.gz"
 PNG_SOURCE="libpng-${PNG_V}"
-PNG_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+PNG_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # cairo
@@ -158,7 +158,7 @@ CAIRO_V="1.8.10"
 CAIRO_URL="http://cairographics.org/releases/cairo-${CAIRO_V}.tar.gz"
 CAIRO_DIST="cairo-${CAIRO_V}.tar.gz"
 CAIRO_SOURCE="cairo-${CAIRO_V}"
-CAIRO_COMMANDS="CFLAGS=\"${CFLAGS} -DPNG_SKIP_SETJMP_CHECK=1\" ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --disable-xlib --disable-ps --disable-pdf --disable-svg && make install"
+CAIRO_COMMANDS="CFLAGS=\"${CFLAGS} -DPNG_SKIP_SETJMP_CHECK=1\" ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic --disable-xlib --disable-ps --disable-pdf --disable-svg && make install"
 CAIRO_DEPENDS="PIXMAN PNG"
 
 #------------------------------------------------------------------------------
@@ -169,7 +169,7 @@ PANGO_V="${PANGO_MV}.2"
 PANGO_URL="http://ftp.gnome.org/pub/GNOME/sources/pango/${PANGO_MV}/pango-${PANGO_V}.tar.gz"
 PANGO_DIST="pango-${PANGO_V}.tar.gz"
 PANGO_SOURCE="pango-${PANGO_V}"
-PANGO_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --without-x --disable-shared --with-included-modules=yes && make install"
+PANGO_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --without-x --disable-shared --with-pic --with-included-modules=yes && make install"
 PANGO_DEPENDS="CAIRO FREETYPE FONTCONFIG"
 #------------------------------------------------------------------------------
 # jpeg
@@ -178,7 +178,7 @@ JPEG_V="8b"
 JPEG_URL="http://www.ijg.org/files/jpegsrc.v${JPEG_V}.tar.gz"
 JPEG_DIST="jpegsrc.v${JPEG_V}.tar.gz"
 JPEG_SOURCE="jpeg-${JPEG_V}"
-JPEG_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+JPEG_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # tiff
@@ -187,7 +187,7 @@ TIFF_V="3.9.4"
 TIFF_URL="ftp://ftp.remotesensing.org/pub/libtiff/tiff-${TIFF_V}.tar.gz"
 TIFF_DIST="tiff-${TIFF_V}.tar.gz"
 TIFF_SOURCE="tiff-${TIFF_V}"
-TIFF_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+TIFF_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # gif
@@ -196,7 +196,7 @@ GIF_V="4.1.6"
 GIF_URL="http://sourceforge.net/projects/giflib/files/giflib%204.x/giflib-${GIF_V}/giflib-${GIF_V}.tar.gz/download"
 GIF_DIST="giflib-${GIF_V}.tar.gz"
 GIF_SOURCE="giflib-${GIF_V}"
-GIF_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared && make install"
+GIF_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic && make install"
 
 #------------------------------------------------------------------------------
 # json
@@ -206,7 +206,7 @@ JSON_V="${JSON_MV}.4"
 JSON_URL="http://ftp.gnome.org/pub/GNOME/sources/json-glib/${JSON_MV}/json-glib-${JSON_V}.tar.gz"
 JSON_DIST="json-glib-${JSON_V}.tar.gz"
 JSON_SOURCE="json-glib-${JSON_V}"
-JSON_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --disable-glibtest && make install"
+JSON_COMMANDS="./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic --disable-glibtest && make install"
 JSON_DEPENDS="GLIB"
 
 #------------------------------------------------------------------------------
@@ -217,7 +217,7 @@ CLUTTER_V="${CLUTTER_MV}.8"
 CLUTTER_URL="http://source.clutter-project.org/sources/clutter/${CLUTTER_MV}/clutter-${CLUTTER_V}.tar.gz"
 CLUTTER_DIST="clutter-${CLUTTER_V}.tar.gz"
 CLUTTER_SOURCE="clutter-${CLUTTER_V}"
-CLUTTER_COMMANDS="ac_cv_lib_GLES_CM_eglInitialize=yes ac_cv_func_malloc_0_nonnull=yes ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-flavour=eglnative --with-gles=${GLES} --with-imagebackend=internal && make install" 
+CLUTTER_COMMANDS="ac_cv_lib_GLES_CM_eglInitialize=yes ac_cv_func_malloc_0_nonnull=yes ./configure --prefix=$PREFIX --host=$HOST --build=$BUILD --disable-shared --with-pic --with-flavour=eglnative --with-gles=${GLES} --with-imagebackend=internal && make install" 
 CLUTTER_DEPENDS="GLIB PANGO FREETYPE CAIRO FONTCONFIG"
 
 #------------------------------------------------------------------------------
@@ -227,7 +227,7 @@ AVAHI_V="0.6.25"
 AVAHI_URL="http://avahi.org/download/avahi-${AVAHI_V}.tar.gz"
 AVAHI_DIST="avahi-${AVAHI_V}.tar.gz"
 AVAHI_SOURCE="avahi-${AVAHI_V}"
-AVAHI_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable-shared --disable-qt3 --disable-qt4 --disable-gtk --disable-dbus --disable-gdbm --disable-libdaemon --disable-python --disable-pygtk --disable-python-dbus --disable-mono --disable-monodoc --disable-autoipd --disable-doxygen-doc --disable-doxygen-dot --disable-doxygen-xml --with-distro=none --disable-nls --disable-shared && make install"
+AVAHI_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable-shared --with-pic --disable-qt3 --disable-qt4 --disable-gtk --disable-dbus --disable-gdbm --disable-libdaemon --disable-python --disable-pygtk --disable-python-dbus --disable-mono --disable-monodoc --disable-autoipd --disable-doxygen-doc --disable-doxygen-dot --disable-doxygen-xml --with-distro=none --disable-nls --disable-shared && make install"
 AVAHI_DEPENDS="GLIB"
 
 #------------------------------------------------------------------------------
@@ -236,7 +236,7 @@ AVAHI_DEPENDS="GLIB"
 UPNP_V="1.6.6"
 UPNP_DIST="libupnp-${UPNP_V}.tar.bz2"
 UPNP_SOURCE="libupnp-${UPNP_V}"
-UPNP_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable-shared && make && make install"
+UPNP_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable-shared --with-pic && make && make install"
 
 #------------------------------------------------------------------------------
 # uriparser
@@ -244,7 +244,7 @@ UPNP_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disabl
 URI_V="0.7.5"
 URI_DIST="uriparser-${URI_V}.tar.gz"
 URI_SOURCE="uriparser-${URI_V}"
-URI_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable-shared --disable-test --disable-doc && make && make install"
+URI_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable-shared --with-pic --disable-test --disable-doc && make && make install"
 
 #------------------------------------------------------------------------------
 # uuid
@@ -252,7 +252,7 @@ URI_COMMANDS="./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --disable
 UUID_V="1.6.2"
 UUID_DIST="uuid-${UUID_V}.tar.gz"
 UUID_SOURCE="uuid-${UUID_V}"
-UUID_COMMANDS="sed -i \"s/-c -s -m/-c -m/\" Makefile.in && ac_cv_va_copy=no ./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --includedir=$PREFIX/include/ossp --disable-shared && make && make install"
+UUID_COMMANDS="sed -i \"s/-c -s -m/-c -m/\" Makefile.in && ac_cv_va_copy=no ./configure --host=$HOST --prefix=$PREFIX --build=$BUILD --includedir=$PREFIX/include/ossp --disable-shared --with-pic && make && make install"
 
 #------------------------------------------------------------------------------
 
@@ -467,6 +467,7 @@ ${CXX} -o ${HERE}/test \
     -L ${PREFIX}/lib \
     -L ${HERE}/tp-build/engine \
     -I ${THERE}/../engine/public/include \
+    -I ${PREFIX}/include \
     -Wl,--start-group \
     -ltpcore \
 	-ljson-glib-1.0 \
