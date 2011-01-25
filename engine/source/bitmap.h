da4178d8a68c37477e8401d3659daf1c561f1552
Pablo Pissanetzky <pablo@trickplay.com>
2011-01-24 16:23:57 -0800

    [engine] Moved bitmap guts to its own source file.

---
+++ engine/source/bitmap.h
@@ -0,0 +1,37 @@
+#ifndef _TRICKPLAY_BITMAP_H
+#define _TRICKPLAY_BITMAP_H
+
+#include "common.h"
+#include "images.h"
+#include "app.h"
+
+class Bitmap : public RefCounted
+{
+public:
+
+    Bitmap( lua_State * L , const char * _src , bool _async );
+
+    guint width() const;
+    guint height() const;
+
+    bool loaded() const;
+
+    static Image * get_image( lua_State * L , int index );
+
+protected:
+
+    virtual ~Bitmap();
+
+private:
+
+    static void callback( Image * image , gpointer me );
+
+    static void destroy_notify( gpointer me );
+
+    String          src;
+    Image *         image;
+    LuaStateProxy * lsp;
+};
+
+
+#endif // _TRICKPLAY_BITMAP_H
