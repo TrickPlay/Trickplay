0acb242dae7de095e158499352e0692ccb9e2433
Alex Indaco <alex.indaco@trickplay.com>
2011-01-25 16:32:06 -0800

    [kids-memory] Rectangle version of the app.
    Everything is pretty much working in terms of functionality. Selecting Medium and Hard will most-likely crash the app since i didn't feel like hard-coding that many Rectangle animations. Otherwise, you can play the game, it saves... thats about it.
    
    
    Former-commit-id: 2bfb3c4bbcd8c14d8734a7228d7a34c01820b06e
    Former-commit-id: d2fa207f6b7c4f9d8d46f6d2fd7382126bea5062

---
+++ apps/kids-memory/main.lua
@@ -0,0 +1,131 @@
+--Kid's Memory Game
+--
+-- main.lua - this file merely sets up a few globals, loads the files
+-- and handles delegation of key presses
+
+--display the screen
+screen:show()
+
+---------------------
+---- Globals
+--------------------
+
+--reduce number of calls to get_w() and get_h()
+screen_w = screen.w
+screen_h = screen.h
+
+--wrapper function for play_sound() to allow quick muting
+play_sound_wrapper = function(sound)
+    mediaplayer:play_sound(sound)
+end
+
+--holds important game information
+game_state={in_game=false,board={}}
+
+
+
+
+--index variable for key handlers
+local curr_handler = nil
+local key_handlers = {}
+local fade_out_f   = {}
+local fade_in_f    = {}
+--wrapper function for assignment of a new key handler
+give_keys = function(new_handler)
+    assert(
+        key_handlers[new_handler] ~= nil,
+        "Invalid handler assignment, "..new_handler.." is not a key handler"
+    )
+    fade_out_f[curr_handler]()
+    curr_handler = new_handler
+    fade_in_f[curr_handler]()
+end
+
+--that file that defines "Class" in lua
+dofile("Class.lua")
+--file containing the various objects used
+dofile("Objects.lua")
+--file for managing the tiles faces
+dofile("Tiles.lua")
+--file for managing the splash screen
+dofile("Splash_Screen.lua")
+--file for managing the game screen
+dofile("Game_Screen.lua")
+
+
+
+---------------------------
+---- Key Handler
+---------------------------
+
+--all valid key handlers
+key_handlers["SPLASH"] = splash_on_key_down
+key_handlers["GAME"]   = game_on_key_down
+
+fade_out_f["SPLASH"] = splash_fade_out
+fade_out_f["GAME"]   = game_fade_out
+
+fade_in_f["SPLASH"] = splash_fade_in
+fade_in_f["GAME"]   = game_fade_in
+--delegate the key presses
+function screen:on_key_down(key)
+    assert(
+        key_handlers[curr_handler] ~= nil,
+        "Invalid handler call, "..curr_handler.." is not a key handler."
+    )
+    
+    key_handlers[curr_handler](key)
+end
+
+
+
+-- if there is a save file load it
+if settings.board ~= nil and #settings.board ~= 0 then
+    
+    print("Loading from save file")
+    
+    game_state.difficulty = settings.difficulty
+    game_state.in_game    = true
+    
+    curr_handler = "GAME"
+    game_fade_in(settings.board)
+    
+-- otherwise just load splash screen
+else
+    print("No save file to load from")
+    
+    curr_handler = "SPLASH"
+    splash_fade_in()
+end
+
+---------------------------
+---- Save the game
+---------------------------
+
+app.on_closing = function()
+    
+    settings = {}
+    
+    --only save if you are in the middle of a game
+    if game_state.in_game then
+        
+        -- build a temp table to save the tiles
+        local board = {}
+        for i = 1, #game_state.board do
+            board[i] = {}
+            for j = 1, #game_state.board[i] do
+            
+                -- 0 indicates that  spot is empty
+                if game_state.board[i][j]==0 then
+                    board[i][j] = 0
+                else
+                    board[i][j] = game_state.board[i][j].index
+                end
+            end
+        end
+        
+        -- assigin it to the save file
+        settings.board = board
+        settings.difficulty = game_state.difficulty
+    end
+end
