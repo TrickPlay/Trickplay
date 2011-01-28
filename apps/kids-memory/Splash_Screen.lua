75a4965c12919809bede261e5533607cdfad8f27
Alex Indaco <alex.indaco@trickplay.com>
2011-01-27 16:42:35 -0800

    [kids-memory] inserted transition animations
    Former-commit-id: 6ec69894ec6c7bb2d0024ab988c3345a9db59e95
    Former-commit-id: 732b39872aaf0368d78c295dea15dc217b52fa06

--- apps/kids-memory/Splash_Screen.lua
+++ apps/kids-memory/Splash_Screen.lua
@@ -1,5 +1,5 @@
 --umbrella group for all members of the splash screen
-local images = Group{}
+local splash_screen = Group{}
 local button_backing = Image{src="assets/button-start.png"}
 screen:add(button_backing)
 button_backing:hide()
@@ -23,26 +23,93 @@ t = Image{src="assets/level-start-hard.png",position={button_backing.w/2,button_
 t.anchor_point={t.w/2,t.h/2}
 difficulty_items[3]:add(t)
 local diff_i = 1
-images:add(Image{src="assets/background-start.jpg",scale={2,2}})
-images:add(unpack(difficulty_items))
-screen:add(images)
-images:hide()
+splash_screen:add(Image{src="assets/background-start.jpg",scale={2,2}})
+screen:add(splash_screen)
+splash_screen:hide()
 
 local focus = Image{src="assets/focus-rectangle-start.png",x=difficulty_items[1].x+button_backing.w/2,y=difficulty_items[1].y+button_backing.h/2}
 focus.anchor_point={focus.w/2,focus.h/2}
-images:add(focus)
+splash_screen:add(focus)
+splash_screen:add(unpack(difficulty_items))
 
 local exit_focus  = Image{src="assets/focus-exit-btn.png",x=29,y=978}
 local exit_button = Image{src="assets/button-exit.png",x=29,y=978}
-exit_focus:hide()
-images:add(exit_focus,exit_button)
+exit_focus.opacity=0
+splash_screen:add(exit_focus,exit_button)
 
 function splash_fade_in()
-    images:show()
+    splash_screen:show()
 end
 function splash_fade_out()
-    images:hide()
+    splash_screen:hide()
 end
+
+local focus_tl = nil
+
+local function anim_focus(targ_x,targ_y)
+    if focus_tl ~= nil then
+        focus_tl:stop()
+        focus_tl:on_completed()
+        focus_tl=nil
+    end
+    focus_tl = Timeline{duration=200}
+    local ani_mode = Alpha{timeline=focus_tl,mode="EASE_OUT_CIRC"}
+    local curr_x = focus.x
+    local curr_y = focus.y
+    function focus_tl:on_new_frame(_,p)
+        local p = ani_mode.alpha
+        focus.x = curr_x + (targ_x-curr_x)*p
+        focus.y = curr_y + (targ_y-curr_y)*p
+    end
+    function focus_tl:on_completed()
+        
+        focus.x = targ_x
+        focus.y = targ_y
+        focus_tl = nil
+    end
+
+    focus_tl:start()
+end
+local function corner_get_focus()
+    if focus_tl ~= nil then
+        focus_tl:stop()
+        focus_tl:on_completed()
+        focus_tl=nil
+    end
+    focus_tl = Timeline{duration=200}
+    function focus_tl:on_new_frame(_,p)
+        exit_focus.opacity = 255*p
+        focus.opacity = 255*(1-p)
+    end
+    function focus_tl:on_completed()
+        
+        focus.opacity = 0
+        exit_focus.opacity = 255
+        focus_tl = nil
+    end
+    focus_tl:start()
+end
+local function corner_lose_focus()
+    if focus_tl ~= nil then
+        focus_tl:stop()
+        focus_tl:on_completed()
+        focus_tl=nil
+    end
+    focus_tl = Timeline{duration=200}
+    function focus_tl:on_new_frame(_,p)
+        exit_focus.opacity = 255*(1-p)
+        focus.opacity = 255*(p)
+    end
+    function focus_tl:on_completed()
+        
+        focus.opacity = 255
+        exit_focus.opacity = 0
+        focus_tl = nil
+    end
+    focus_tl:start()
+end
+
+
 
 
 local exit_sel = false
@@ -52,8 +119,7 @@ local exit_button_key_handler = {
         exit()
     end,
     [keys.Right] = function()
-        exit_focus:hide()
-        focus:show()
+        corner_lose_focus()
         exit_sel = false
     end,
 }
@@ -68,18 +134,17 @@ local key_handler = {
     [keys.Down] = function()
         if diff_i < 3 then
             diff_i = diff_i + 1
-            focus.y = focus.y+button_backing.h+38
+            anim_focus(focus.x, focus.y+button_backing.h+38)
         end
     end,
     [keys.Up] = function()
         if diff_i > 1 then
             diff_i = diff_i - 1
-            focus.y = focus.y-(button_backing.h+38)
+            anim_focus(focus.x, focus.y-(button_backing.h+38))
         end
     end,
     [keys.Left] = function()
-        exit_focus:show()
-        focus:hide()
+        corner_get_focus()
         exit_sel = true
     end,
 }
