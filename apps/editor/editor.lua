406b34dc365555eb52ed5b98570923523450a472
Alex Indaco <alex.indaco@trickplay.com>
2011-02-22 13:23:13 -0800

    [editor] beginning tabbar

--- apps/editor/editor.lua
+++ apps/editor/editor.lua
@@ -2492,18 +2492,19 @@ function editor.bring_forward()
 end
 
 local widget_map = {
-	["Button"] = function () return widget.button()  end, 
-	["TextInputField"] = function () return widget.textField() end, 
-	["DialogBox"] = function () return widget.dialogBox() end, 
-	["ToastBox"] = function () return widget.toastBox() end,   
-	["RadioButton"] = function () return widget.radioButton() end, 
-	["CheckBox"] = function () return widget.checkBox()  end, 
-	["ButtonPicker"] = function () return widget.buttonPicker()  end, 
-	["LoadingDots"] = function () return widget.loadingdots() end, 
-	["LoadingBar"] = function () return widget.loadingbar() end,
-    ["MenuBar"] = function () return widget.dropDownBar() end,
-	["3D_List"] = function () return widget.threeDlist() end,
-	["ScrollImage"] = function () return widget.scrollWindow() end, 
+	["Button"]         = function () return widget.button()       end, 
+	["TextInputField"] = function () return widget.textField()    end, 
+	["DialogBox"]      = function () return widget.dialogBox()    end, 
+	["ToastBox"]       = function () return widget.toastBox()     end,   
+	["RadioButton"]    = function () return widget.radioButton()  end, 
+	["CheckBox"]       = function () return widget.checkBox()     end, 
+	["ButtonPicker"]   = function () return widget.buttonPicker() end, 
+	["LoadingDots"]    = function () return widget.loadingdots()  end, 
+	["LoadingBar"]     = function () return widget.loadingbar()   end,
+    ["MenuBar"]        = function () return widget.dropDownBar()  end,
+	["3D_List"]        = function () return widget.threeDlist()   end,
+	["ScrollImage"]    = function () return widget.scrollWindow() end,
+    ["TabBar"]         = function () return widget.tabBar()       end,
 }
 
 
@@ -2584,6 +2585,8 @@ function editor.widgets()
 		si=new_widget
          elseif (new_widget.extra.type == "MenuBar") then 
 		mb=new_widget
+         elseif (new_widget.extra.type == "TabBar") then 
+		tb=new_widget
 	      end
 --imsi 
 	      while (is_available(new_widget.name..tostring(item_num)) == false) do  
@@ -2627,6 +2630,8 @@ function editor.widgets()
 		si=new_widget
          elseif (new_widget.extra.type == "MenuBar") then 
 		mb=new_widget
+         elseif (new_widget.extra.type == "TabBar") then 
+		tb=new_widget
 	      end
 --imsi 
  	      while (is_available(new_widget.name..tostring(item_num)) == false) do  
