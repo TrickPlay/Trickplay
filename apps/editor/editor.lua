17cee201839b9edf087fa4700b60275f944b65c3
Heejunk Kim <hjkim@trickplay.com>
2011-03-18 10:09:47 -0700

    sdk/editor
    Former-commit-id: 9580b88a521073362e17829dfb281d66702af191
    Former-commit-id: 21d46015bb5af7a21dd36b538c630c216189e12d

--- apps/editor/editor.lua
+++ apps/editor/editor.lua
@@ -1188,7 +1188,7 @@ function editor.inspector(v, x_pos, y_pos, scroll_y_pos)
 
 	-- inspector scroll function 
 	if v.extra then 
-	       si = ui_element.scrollPane{visible_w = item_group.w + 40, virtual_w = item_group.w, virtual_h = item_group.h, visible_w = 480, border_is_visible = false} 
+	       si = ui_element.scrollPane{visible_w = item_group.w + 40, virtual_w = item_group.w, virtual_h = item_group.h, visible_h = 480, border_is_visible = false, frame_thickness = 0} 
 	       si.content = item_group
 	       si.position = {0,82,0}
 	       si.name ="si"
