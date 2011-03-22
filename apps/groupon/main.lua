b7e0e7d3d97975630cf481ed77fec20ed49873eb
Craig R. Hughes <craig@trickplay.com>
2011-03-22 15:11:29 -0700

    [Groupon] Just some simple cleanup and timing; fade out offers and loop
    Former-commit-id: 44747c6f07a16a33736c7ac5bc46e57379a983ef
    Former-commit-id: bf3bca7522b26ad971ea42bdb816eec61822935a

--- apps/groupon/main.lua
+++ apps/groupon/main.lua
@@ -5,7 +5,7 @@ local deals = GROUPON:get_deals()
 local make_tile = function (image, title)
     local i = Image { src = image }
     local t = Text { text = title, color = 'ffffff', font = 'Diavlo 36px' }
-    local r = Rectangle { size = { t.w, t.h }, color = "000000", opacity = 100 }
+    local r = Rectangle { size = { t.w, t.h }, color = "000000", opacity = 150 }
     local g = Group { }
     g:add(i)
     g:add(r)
@@ -15,11 +15,25 @@ local make_tile = function (image, title)
     r.x, r.y = t.x, t.y
 
     g.x = math.random(10,screen.w-g.w-10)
-    g.y = math.random(10,screen.h-g.h-10)
+    g.y = math.random(10 + 100 ,screen.h-g.h-10)
 
     screen:add(g)
-    g.y_rotation = { 180, g.w/2, 0 }
-    g:animate({duration = 1000, y_rotation = 0 })
+    g.x_rotation = { -90, 0, 0 }
+    g:animate({
+                duration = 500,
+                x_rotation = 0,
+                mode = "EASE_OUT_BOUNCE",
+                on_completed = function ()
+                                    g:animate({
+                                                    duration = 10000,
+                                                    opacity = 0,
+                                                    mode = "EASE_IN_EXPO",
+                                                    on_completed = function ()
+                                                                        g:unparent()
+                                                                    end,
+                                                })
+                                end,
+            })
 end
 
 
@@ -32,17 +46,19 @@ screen:show()
 
 local deal
 function main()
-    deal = next(deals.deals, deal)
+    repeat
+        deal = next(deals.deals, deal)
 
-    local v = deals.deals[deal]
+        if(deal) then
+            local v = deals.deals[deal]
 
-    if(v) then
-        local image = v.largeImageUrl or v.mediumImageUrl or v.smallImageUrl
-    
-        local title = v.announcementTitle
-    
-        make_tile(image, title)
-    end
+            local image = v.largeImageUrl or v.mediumImageUrl or v.smallImageUrl
+
+            local title = v.announcementTitle
+
+            make_tile(image, title)
+        end
+    until deal
 end
 
 local t = Timer(2200)
