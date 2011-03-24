52b6ed2b617bc794e05311cb0602769bc2d571de
Alex Indaco <alex.indaco@trickplay.com>
2011-03-24 10:04:03 -0700

    [burberry] the front page is almost done,

--- apps/burberry/main.lua
+++ apps/burberry/main.lua
@@ -1,24 +1,30 @@
 screen:show()
-
+screen_w = screen.w
+screen_h = screen.h
 local curr_page = "front_page"
 function change_page_to(next_page)
 	
-	assert(_g[next_page] ~= nil,"Next page \""..next_page.."\" does not exist")
+	assert(_G[next_page] ~= nil,"Next page \""..next_page.."\" does not exist")
 	
-	_g[curr_page].func_tbls.fade_out_to[next_page]
-	_g[next_page].func_tbls.fade_in_from[curr_page]
+	animate_list[_G[curr_page].func_tbls.fade_out_to[next_page]]  = _G[curr_page]
+	animate_list[_G[next_page].func_tbls.fade_in_from[curr_page]] = _G[next_page]
 	
 	curr_page = next_page
 	
 end
 
 dofile("App_Loop.lua")
-dofile("FrontPage.lua")
+dofile("Utils.lua")
+dofile("FrontPage.lua") -- global: front_page
+
+screen:add(
+	front_page.group
+)
 
 function screen:on_key_down(key)
-	if _g[curr_page].keys[key] == nil then
-		print("Page \""..curr_page.."\" does not support that key.")
+	if _G[curr_page].keys[key] == nil then
+		print("\""..curr_page.."\" does not support that key.")
 	else
-		_g[curr_page].keys[key]()
+		_G[curr_page].keys[key](_G[curr_page])
 	end
 end
