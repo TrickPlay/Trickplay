01e45edc789e6f20710846054c83706661cb8edc
Rex Fenley <rex.fenley@trickplay.com>
2011-03-29 14:15:31 -0700

    [poker-iphone] fixed a bug with the splash timer where it did not get deleted if the user pressed a key surpassing the splash screen.

--- apps/poker_v2/controllers/SplashController.lua
+++ apps/poker_v2/controllers/SplashController.lua
@@ -4,17 +4,22 @@ function(ctrl, router, ...)
     router:attach(ctrl, Components.SPLASH)
 
     local view = SplashView(ctrl)
+    local splash_timer = Timer()
 
     function ctrl:on_key_down(k)
         router:set_active_component(Components.CHARACTER_SELECTION)
         router:notify()
         ctrlman:choose_dog()
+        if splash_timer then
+            splash_timer:stop()
+            splash_timer.on_timer = nil
+            splash_timer = nil
+        end
     end
 
     function ctrl:notify(event) view:update(event) end
 
     -- This will get rid of the splash screen after 7 seconds
-    splash_timer = Timer()
     splash_timer.interval = 7000
     function splash_timer:on_timer()
         splash_timer:stop()
