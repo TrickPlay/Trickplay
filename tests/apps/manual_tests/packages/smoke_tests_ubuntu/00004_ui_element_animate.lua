07f6b0338823522b65340716645407cddaa768b6
Craig R. Hughes <craig@trickplay.com>
2011-03-28 18:16:26 -0700

    Import manual tests to clean branch
    Former-commit-id: 1f55c1ce7e2f8e5d2dae1957edc6db0986b48882
    Former-commit-id: a329038b85f22a0d9449d4909e9e0093d46a52c8

---
+++ tests/apps/manual_tests/packages/smoke_tests_ubuntu/00004_ui_element_animate.lua
@@ -0,0 +1,54 @@
+--[[
+Filename: 0001_rectangle_basic.lua
+Author: Peter von dem Hagen
+Date: January 19, 2011
+Test type: Manual GUI Test
+Description: Create several rectangles and verify that they display as expected
+--]]
+
+-- Test Set up --
+
+test_question = "Does the code image animate and finish looking like the baseline?"
+
+function generate_test_image ()
+
+
+	 local g = Group
+	 {
+	        size = { screen.w , screen.h },
+	        children = {
+	            Rectangle {
+	            	color = "FFFFFF",
+	            	size = { screen.w, screen.h }
+	            },
+	         	Image {
+					name = "animated_png",
+	         		src = test_folder.."/".."assets/logo.png",
+	         		position = { 200, 200 },
+	         		size = { 100, 100 }
+	         	}
+	         	
+	 		 }
+	 }
+
+	g:find_child("animated_png"):animate{
+									duration=2000,
+									loop=false,
+									position = { screen.w/2, screen.h/2 },
+									h=400,
+									w=400,
+								 	z_rotation=90
+								 	}
+	return g
+end
+
+
+
+
+
+
+
+
+
+
+
