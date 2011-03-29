eb8ede069e6dafd8df6b05fc141d427cf1de110b
Craig R. Hughes <craig@trickplay.com>
2011-03-28 17:44:56 -0700

    Import manual tests to clean branch
    Former-commit-id: 2cc5fd4af845ac12830a484851162545f8e4d254
    Former-commit-id: f4000da1c71af49611a81170424bde4d8b6c9994

---
+++ tests/apps/manual_tests/00003_rectangle_basic3.lua
@@ -0,0 +1,63 @@
+--[[
+Filename: 0001_rectangle_basic.lua
+Author: Peter von dem Hagen
+Date: January 19, 2011
+Test type: Manual GUI Test
+Description: Create several rectangles and verify that they display as expected
+--]]
+
+-- Test Set up --
+
+test_question = "Do the two images match three?"
+
+function generate_test_image ()
+
+	 local g = Group
+	 {
+	        size = { screen.w , screen.h },
+	        children = {
+	            Rectangle {
+	            	color = "FFFFFF",
+	            	size = { screen.w, screen.h }
+	            },
+	            Rectangle {
+	            	color = { 153, 51, 85 },
+	            	border_width = 5,
+	            	border_color = "FF0066",
+	            	size = { 400, 400 },
+					position = { screen.w/2 , screen.h/2 }
+	            },
+	             Rectangle {
+	            	color = "F5B800",
+	            	size = { 100, 100 },
+	            	position = { 100,100 }
+	            },
+	             Rectangle {
+	            	color = "F5B800",
+	            	size = { 200, 200 },
+	            	position = { 100,100 }
+	            } ,
+	             Rectangle {
+	            	color = "F5B800",
+	            	size = { 500, 500 },
+	            	position = { 100,100 }
+	            },
+	            Text {
+	            	font = "sans 40px",
+	            	text = "0003"
+	            }
+	         }
+		}
+	return g
+end
+
+
+
+
+
+
+
+
+
+
+
