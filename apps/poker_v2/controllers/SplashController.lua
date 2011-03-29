5a26eb3a88af6b167dbfd1f380b75164611d8c90
Rex Fenley <rex.fenley@trickplay.com>
2011-03-29 14:12:54 -0700

    [poker-iphone] Added a timer to the splash screen so it goes away automatically in 7 seconds.

--- apps/poker_v2/controllers/SplashController.lua
+++ apps/poker_v2/controllers/SplashController.lua
@@ -13,4 +13,15 @@ function(ctrl, router, ...)
 
     function ctrl:notify(event) view:update(event) end
 
+    -- This will get rid of the splash screen after 7 seconds
+    splash_timer = Timer()
+    splash_timer.interval = 7000
+    function splash_timer:on_timer()
+        splash_timer:stop()
+        splash_timer.on_timer = nil
+        splash_timer = nil
+        ctrl:on_key_down(keys.Return)
+    end
+    splash_timer:start()
+
 end)
