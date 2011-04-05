1fef68e04e7c1d776a6caafc8f8265907d0b90d1
Craig R. Hughes <craig@trickplay.com>
2011-04-05 13:44:48 +0900

    Turn off desaturate; enable page curl

--- apps/physics-showcase/main.lua
+++ apps/physics-showcase/main.lua
@@ -19,13 +19,20 @@ balloon.extra.blur = dummy
 balloon.extra.unblur = dummy
 balloon.extra.saturate = dummy
 balloon.extra.desaturate = dummy
+balloon.extra.pageturn = dummy
 
 lottery.extra.blur = dummy
 lottery.extra.unblur = dummy
 lottery.extra.saturate = dummy
 lottery.extra.desaturate = dummy
+balloon.extra.pageturn = dummy
 
 screen:add( focus , lottery , balloon )
+lottery:pageturn(1.0)
+
+local curl_timeline = Timeline { duration = 750 }
+local balloon_on_fn = function(_,_,progress) balloon:pageturn(1-progress,5) lottery:pageturn(progress,5) end
+local lottery_on_fn = function(_,_,progress) balloon:pageturn(progress,5) lottery:pageturn(1-progress,5) end
 
 local function set_focus( s )
     if s == "balloon" then
@@ -33,17 +40,23 @@ local function set_focus( s )
         focus.extra.focus = s
         g_focus = s
         balloon:unblur()
-        balloon:saturate()
+--        balloon:saturate()
         lottery:blur()
-        lottery:desaturate(0.8)
+--        lottery:desaturate(0.8)
+        curl_timeline:rewind()
+        curl_timeline.on_new_frame = balloon_on_fn
+        curl_timeline:start()
     elseif s == "lottery" then
         focus.position = { lottery.x - 20 , lottery.y - 20 }
         focus.extra.focus = s
         g_focus = s
         balloon:blur()
-        balloon:desaturate(0.8)
+--        balloon:desaturate(0.8)
         lottery:unblur()
-        lottery:saturate()
+--        lottery:saturate()
+        curl_timeline:rewind()
+        curl_timeline.on_new_frame = lottery_on_fn
+        curl_timeline:start()
     end
 end
 
