5f6bb1bc9d985cbd3840397dadea0ff5c5f395cb
Alex Indaco <alex.indaco@trickplay.com>
2011-04-06 13:23:04 -0700

    [car_racing] user can now walk on grass

--- apps/car_racing/Level.lua
+++ apps/car_racing/Level.lua
@@ -1,8 +1,22 @@
 local ADD_THRESH
 local DEL_THRESH
-sections = {}
+local TILE_W=240*4
+local TILE_H=240*4
+
+
+
 section_i = 1
 active_sections = {}
+local ground = Image{
+    src="grass1.png",
+    tile={true,true},
+    size={20000,20000},
+    scale={4,4},
+    position={screen.w/2,screen.h},
+    x_rotation={90,0,0},
+}
+ground.anchor_point={ground.w/2,ground.h/2}
+
 world = Group{x_rotation={90,0,0}}
 
 local end_point = {0,0}
@@ -30,13 +44,17 @@ function world:add_next_section()
         active_sections[#active_sections].y+end_point[2],
     }
     
+    --new end_point
     end_point[1] = end_point[1] + sections[section_i].end_point[1]
     end_point[2] = end_point[2] + sections[section_i].end_point[2]
     
     section_i = section_i + 1
+    
 end
 
 function world:remove_oldest_section()
+    
+    table.remove(active_sections,1):unparent()
     
 end
 
@@ -51,17 +69,26 @@ function world:rotate_by(deg)
     
     y_rot = y_rot+deg
     self.y_rotation={y_rot,0,0}
-    
+    ground.y_rotation={y_rot,0,0}
 end
 
-local ap_x = 0
-local ap_y = 0
+local w_ap_x = 0
+local w_ap_y = 0
+local g_dx = 0
+local g_dy = 0
+local g_cent = ground.w/2
 -- move forward or backward
-function world:move_by(delta)
+function world:move(delta)
     
-    ap_x = ap_x-delta*math.sin(math.pi/180*y_rot)
-    ap_y = ap_y+delta*math.cos(math.pi/180*y_rot)
+    w_ap_x = w_ap_x-delta*math.sin(math.pi/180*y_rot)
+    w_ap_y = w_ap_y+delta*math.cos(math.pi/180*y_rot)
     
-    self.anchor_point = { ap_x, ap_y }
+    self.anchor_point = { w_ap_x, w_ap_y }
     
+    g_dx = (g_dx+delta*math.sin(math.pi/180*y_rot))%TILE_W
+    g_dy = (g_dy-delta*math.cos(math.pi/180*y_rot))%TILE_H
+    
+    ground.anchor_point = { g_cent + g_dx,  g_cent + g_dy }
 end
+
+screen:add(ground,world)
