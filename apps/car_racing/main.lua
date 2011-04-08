7dc0078f29ad45fc43ef42cc9483ea9af60a3060
Alex Indaco <alex.indaco@trickplay.com>
2011-04-08 10:53:18 -0700

    [car_racing] a full rectangular track is now working!!!

--- apps/car_racing/main.lua
+++ apps/car_racing/main.lua
@@ -3,15 +3,16 @@ screen:show()
 clone_sources = Group{name="clone_sources"}
 screen:add(clone_sources)
 clone_sources:hide()
-dofile("Level.lua")
+dofile( "Sections.lua" )
+dofile(    "Level.lua" )
 
 
 local keys = {
 	[keys.Up] = function()
-		world:move(60)
+		world:move(100)
 	end,
 	[keys.Down] = function()
-		world:move(-20)
+		world:move(-100)
 	end,
 	[keys.Left] = function()
 		
@@ -19,6 +20,13 @@ local keys = {
 	end,
 	[keys.Right] = function()
 		world:rotate_by(5)
+	end,
+	[keys.q] = function()
+		print("curve added")
+		table.insert(
+            sections,
+            make_curved_section()
+        )
 	end,
 }
 function screen:on_key_down(k)
