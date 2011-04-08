449800790fddc01fe59f7144bc55a7f90c73ee15
Alex Indaco <alex.indaco@trickplay.com>
2011-04-08 10:53:18 -0700

    [car_racing] a full rectangular track is now working!!!
    Former-commit-id: 7001e9740559a21d4e207c873e96f0dcdd441510
    Former-commit-id: 4fe79767577f723174e2bc929ab80416b9e4a05a

--- apps/car_racing/Level.lua
+++ apps/car_racing/Level.lua
@@ -3,25 +3,34 @@ local DEL_THRESH
 local TILE_W=240*4
 local TILE_H=240*4
 
-
+local end_marker = Rectangle{
+    w=100, h=100, color = "ff0000", anchor_point = {50,50}
+}
+local prev_end_marker = Rectangle{
+    w=100, h=100, color = "ffff00", anchor_point = {50,50}
+}
+local sky = Rectangle{name="THE SKY",w=screen.w,h=screen.h,color="87cefa"}
+local car = Image{name="THE CAR",src="car.png",position={screen.w/2,5*screen.h/6}}
+car.anchor_point = {car.w/2,car.h/2}
 
 section_i = 1
 active_sections = {}
 local ground = Image{
-    src="grass1.png",
-    tile={true,true},
-    size={20000,20000},
-    scale={4,4},
+    name  = "THE GROUND",
+    src   = "grass1.png",
+    tile  = {true,true},
+    size  = {40000,40000},
+    scale = {4,4},
     position={screen.w/2,screen.h},
     x_rotation={90,0,0},
 }
 ground.anchor_point={ground.w/2,ground.h/2}
 
-world = Group{x_rotation={90,0,0}}
-
-local end_point = {0,0}
-
-
+world = Group{name = "THE WORLD",x_rotation={90,0,0},position={screen.w/2,screen.h},scale={4,4}}
+world:add(prev_end_marker,end_marker)
+local end_point = {0,0,0}
+local dist_to_end_point = {0,0}
+local dist_to_start_point = {0,0}
 
 
 
@@ -43,18 +52,72 @@ function world:add_next_section()
         active_sections[#active_sections].x+end_point[1],
         active_sections[#active_sections].y+end_point[2],
     }
+    active_sections[#active_sections].z_rotation={end_point[3],0,0}
     
     --new end_point
-    end_point[1] = end_point[1] + sections[section_i].end_point[1]
-    end_point[2] = end_point[2] + sections[section_i].end_point[2]
+    prev_end_marker.position = end_marker.position
+    prev_end_marker:raise_to_top()
+    --dumptable(end_point)
+    --dumptable(sections[section_i].end_point)
+    end_point[1] = end_point[1] +
+        sections[section_i].end_point[1]*math.cos(math.pi/180*end_point[3]) -
+        sections[section_i].end_point[2]*math.sin(math.pi/180*end_point[3])
     
+    --print("x",sections[section_i].end_point[1]*math.cos(math.pi/180*end_point[3]),
+    --      sections[section_i].end_point[2]*math.sin(math.pi/180*end_point[3]))
+        
+    end_point[2] = end_point[2] +
+        sections[section_i].end_point[1]*math.sin(math.pi/180*end_point[3]) +
+        sections[section_i].end_point[2]*math.cos(math.pi/180*end_point[3])
+    --print("y",sections[section_i].end_point[1]*math.sin(math.pi/180*end_point[3]),
+    --    sections[section_i].end_point[2]*math.cos(math.pi/180*end_point[3]))
+    end_point[3] = end_point[3] + sections[section_i].end_point[3]
+    end_marker.position = {end_point[1],end_point[2]}
+    dist_to_end_point[1] = dist_to_end_point[1] + sections[section_i].end_point[1]*math.cos(math.pi/180*end_point[3]) -
+        sections[section_i].end_point[2]*math.sin(math.pi/180*end_point[3])
+    dist_to_end_point[2] = dist_to_end_point[2] + sections[section_i].end_point[1]*math.sin(math.pi/180*end_point[3]) +
+        sections[section_i].end_point[2]*math.cos(math.pi/180*end_point[3])
+    end_marker:raise_to_top()
     section_i = section_i + 1
-    
+    if section_i > #sections then
+        section_i = 1
+    end
+    print("END ADD NEXT SECTION")
 end
 
+local new_pos = {}
+local w_ap_x = 0
+local w_ap_y = 0
 function world:remove_oldest_section()
     
     table.remove(active_sections,1):unparent()
+    
+    new_pos.x = active_sections[1].x
+    new_pos.y = active_sections[1].y
+    
+    dist_to_start_point[1] = dist_to_start_point[1] - new_pos.x
+    dist_to_start_point[2] = dist_to_start_point[2] - new_pos.y
+    
+    end_point[1] = end_point[1] - new_pos.x
+    end_point[2] = end_point[2] - new_pos.y
+    
+    self.anchor_point = { dist_to_start_point[1], dist_to_start_point[2] }
+    w_ap_x = dist_to_start_point[1]
+    w_ap_y = dist_to_start_point[2]
+    for _,section in ipairs(active_sections) do
+        section.x = section.x - new_pos.x
+        section.y = section.y - new_pos.y
+    end
+    
+    prev_end_marker.x = prev_end_marker.x - new_pos.x
+    prev_end_marker.y = prev_end_marker.y - new_pos.y
+    
+    end_marker.x = end_marker.x - new_pos.x
+    end_marker.y = end_marker.y - new_pos.y
+    
+    print("delete, num active sections is: ",#active_sections)
+    
+    
     
 end
 
@@ -72,23 +135,52 @@ function world:rotate_by(deg)
     ground.y_rotation={y_rot,0,0}
 end
 
-local w_ap_x = 0
-local w_ap_y = 0
+
 local g_dx = 0
 local g_dy = 0
 local g_cent = ground.w/2
+
+local delta_x = 0
+local delta_y = 0
 -- move forward or backward
 function world:move(delta)
     
-    w_ap_x = w_ap_x-delta*math.sin(math.pi/180*y_rot)
-    w_ap_y = w_ap_y+delta*math.cos(math.pi/180*y_rot)
+    delta_x = delta*math.sin(math.pi/180*y_rot)
+    delta_y = delta*math.cos(math.pi/180*y_rot)
+    
+    w_ap_x = w_ap_x+delta_x
+    w_ap_y = w_ap_y-delta_y
     
     self.anchor_point = { w_ap_x, w_ap_y }
     
-    g_dx = (g_dx+delta*math.sin(math.pi/180*y_rot))%TILE_W
-    g_dy = (g_dy-delta*math.cos(math.pi/180*y_rot))%TILE_H
+    g_dx = (g_dx+delta_x)%TILE_W
+    g_dy = (g_dy-delta_y)%TILE_H
     
     ground.anchor_point = { g_cent + g_dx,  g_cent + g_dy }
+    
+    dist_to_end_point[1] = dist_to_end_point[1] -delta_x
+    dist_to_end_point[2] = dist_to_end_point[2] +delta_y
+    
+    dist_to_start_point[1] = dist_to_start_point[1] +delta_x
+    dist_to_start_point[2] = dist_to_start_point[2] -delta_y
+---dumptable(dist_to_end_point)
+    if math.abs(dist_to_end_point[1]) < 10000 and math.abs(dist_to_end_point[2]) < 10000 then
+        --[[
+        table.insert(
+            sections,
+            make_straight_section()
+        )
+        --]]
+        world:add_next_section()
+    end
+    
+    if math.abs(dist_to_start_point[1]) > 20000 or math.abs(dist_to_start_point[2]) > 20000 then
+        --dumptable(dist_to_start_point)
+        world:remove_oldest_section()
+    end
 end
 
-screen:add(ground,world)
+screen:add(sky,ground,world,car)
+world:add_next_section()
+
+--world:set_bounds()
--- apps/car_racing/Sections.lua
+++ apps/car_racing/Sections.lua
@@ -1,41 +1,154 @@
 local base = {
-    straight_road = Image{src="road-straight.png"},
-    straight_road = Image{src="road-curve2.png"}
+    straight_road = Image{ src="road-straight.png", tile={false,true}, h=50*250},
+    curve_road    = Image{ src="road-curve-2.png"   },
+    straight_rail = Image{ src="guardrail.png",     tile={true,false}, w=50*250},
 }
-for _,v in ipairs(base) do
-    clone_sources:add(base)
+for _,v in pairs(base) do
+    clone_sources:add(v)
 end
+
 sections = {}
 
-local function make_straight_section(tiles)
+function make_straight_section()
     local section = {
-        endpoint = {0,0},
+        end_point = {0,-base.straight_road.h,0},
+        
         line_up  = {0,0},
         setup = function(self)
-            self.group = Group{}
-            
+            self.group = Group{
+                name = "A Straight Section",
+                children = {
+                    Clone{
+                        name   = "Road",
+                        source = base.straight_road,
+                        x_rotation = {180,0,0},
+                        ---[[
+                        anchor_point = {
+                             base.straight_road.w/2,
+                            0
+                        },--]]
+                    },
+                    Clone{
+                        name   = "Left Rail",
+                        source = base.straight_rail,
+                        anchor_point = {
+                            0,
+                            base.straight_rail.h
+                        },
+                        x=-base.straight_road.w/2,
+                        z_rotation = {-90,0,0},
+                        x_rotation={-90,0,0}
+                    },
+                    Clone{
+                        name   = "Right Rail",
+                        source = base.straight_rail,
+                        anchor_point = {
+                            base.straight_rail.w,
+                            base.straight_rail.h
+                        },
+                        x = base.straight_road.w/2,
+                        z_rotation = {90,0,0},
+                        x_rotation={-90,0,0}
+                    },
+                }
+            }
+            dumptable(self.end_point)
+            return self.group
         end,
     }
     
     return section
 end
 
-local function make_curved_section(dir)
+local function make_curved_to_straight_section(dir)
     local section = {
-        endpoint = {0,0},
+        endpoint = {0,0,0},
         line_up  = {0,0},
         setup = function(self)
-            
+            self.group = Group{
+                name = "A Straight Section",
+                children = {
+                    Clone{
+                        name   = "Road",
+                        source = base.straight_road,
+                        x_rotation = {180,0,0},
+                        
+                        --[[
+                        anchor_point = {
+                             base.straight_road.w/2,
+                            -base.straight_road.h
+                        },--]]
+                    },
+                    Clone{
+                        name   = "Left Rail",
+                        source = base.straight_rail,
+                        anchor_point = {
+                            0,
+                            base.straight_rail.h
+                        },
+                        --x=-base.straight_road.w/2,
+                        z_rotation = {-90,0,0},
+                        x_rotation={-90,0,0}
+                    },
+                    Clone{
+                        name   = "Right Rail",
+                        source = base.straight_rail,
+                        anchor_point = {
+                            base.straight_rail.w,
+                            base.straight_rail.h
+                        },
+                        x = base.straight_road.w,
+                        z_rotation = {90,0,0},
+                        x_rotation={-90,0,0}
+                    },
+                }
+            }
+            return self.group
         end,
     }
     
     return section
 end
 
-sections[1] = {
-    endpoint = {0,0},
-    line_up  = {0,0},
-    setup = function(self)
-        Clone
-    end
-}
+function make_curved_section()
+    local section = {
+        end_point = {base.curve_road.w-base.straight_road.w/2,-base.curve_road.h+base.straight_road.w/2,90},
+        line_up  = {0,0},
+        setup = function(self)
+            self.group = Group{
+                name = "A Straight Section",
+                children = {
+                    Clone{
+                        name   = "Road",
+                        source = base.curve_road,
+                        anchor_point = {
+                             base.straight_road.w/2,
+                            base.curve_road.h
+                        },
+                        --[[
+                        anchor_point = {
+                             base.straight_road.w/2,
+                            -base.straight_road.h
+                        },--]]
+                    },
+                }
+            }
+            return self.group
+        end,
+    }
+    
+    return section
+end
+
+sections[1] = make_straight_section()
+sections[2] = make_straight_section()
+sections[3] = make_curved_section()
+sections[4] = make_straight_section()
+sections[5] = make_straight_section()
+sections[6] = make_curved_section()
+sections[7] = make_straight_section()
+sections[8] = make_straight_section()
+sections[9] = make_curved_section()
+sections[10] = make_straight_section()
+sections[11] = make_straight_section()
+sections[12] = make_curved_section()
