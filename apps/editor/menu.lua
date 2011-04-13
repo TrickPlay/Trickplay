3630d1f3a50a83e4ab7138ecf22d06a434fe8a95
Heejunk Kim <hjkim@trickplay.com>
2011-04-13 14:49:37 -0700

    sdk/editor
    Former-commit-id: 0992aa1a07200cc4ac1b90ef20fea1da9e14c5b4
    Former-commit-id: fc09f27b44255227a22d6fd9974f845e6a4ca05e

--- apps/editor/menu.lua
+++ apps/editor/menu.lua
@@ -73,8 +73,8 @@ local file_icon_text_shadow = Text{
 ]]
 menuButton_file.insert_item(1,{type="item", string="New\t\t\t\t     N", bg=assets("assets/menu-item.png"), focus=assets("assets/menu-item-focus.png"), f=editor.close,})
 menuButton_file.insert_item(2,{type="item", string="Open ...\t\t\t     O", bg=assets("assets/menu-item.png"), focus= assets("assets/menu-item-focus.png"), f=editor.the_open})
-menuButton_file.insert_item(3,{type="item", string="Save ...\t\t\t     S", bg=assets("assets/menu-item.png"), focus= assets("assets/menu-item-focus.png"), f=editor.save}) 
-menuButton_file.insert_item(4,{type="item", string="Save As ...\t\t     A", bg=assets("assets/menu-item.png"), focus= assets("assets/menu-item-focus.png"), f=editor.save})
+menuButton_file.insert_item(3,{type="item", string="Save ...\t\t\t     S", bg=assets("assets/menu-item.png"), focus= assets("assets/menu-item-focus.png"), f=editor.save, parameter=true}) 
+menuButton_file.insert_item(4,{type="item", string="Save As ...\t\t     A", bg=assets("assets/menu-item.png"), focus= assets("assets/menu-item-focus.png"), f=editor.save, parameter=false})
 menuButton_file.insert_item(5,{type="item", string="Quit\t\t\t\t     Q", bg=assets("assets/menu-item-bottom.png"), focus= assets("assets/menu-item-focus.png"), f=exit}) 
 
 
