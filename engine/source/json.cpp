36b5b400f25d35bf846d21e51a996e9d9b782d39
Pablo Pissanetzky <pablo@trickplay.com>
2011-04-12 18:51:33 -0700

    [engine] Adding our own (nifty) JSON wrapper.
    json-glib is gross.
    
    
    Former-commit-id: cb1111f0672701fe8a6ddcdb700f81f54ba02137
    Former-commit-id: 30d228b6059f0a8b6967c0ae51e4eadd164fcb71

--- engine/source/json.cpp
+++ engine/source/json.cpp
@@ -1,4 +1,8 @@
 
+#include <cstring>
+#include <sstream>
+#include <iomanip>
+
 #include "json.h"
 #include "lb.h"
 
@@ -329,4 +333,854 @@ String JSON::stringify( lua_State * L, int index, bool pretty )
     json_node_free( root );
 
     return result;
+}
+
+
+namespace JSON
+{
+    static String encode_string( const String & string )
+    {
+        std::stringstream result;
+
+        result << "\"";
+
+        for ( String::const_iterator it = string.begin(); it != string.end(); ++it )
+        {
+            switch( *it )
+            {
+                case '"':
+                    result << "\\\"";
+                    break;
+                case '\\':
+                    result << "\\\\";
+                    break;
+                case '/':
+                    result << "\\/";
+                    break;
+                case '\b':
+                    result << "\\b";
+                    break;
+                case '\f':
+                    result << "\\f";
+                    break;
+                case '\n':
+                    result << "\\n";
+                    break;
+                case '\r':
+                    result << "\\r";
+                    break;
+                case '\t':
+                    result << "\\t";
+                    break;
+                default:
+                    if ( *it >= 0 && ( *it < 32 || *it == 127 ) )
+                    {
+                        result << "\\u" << std::setw(4) << std::setfill( '0' ) << std::hex <<  int( *it ) ;
+                    }
+                    else if ( *it >= 32 && *it < 127 )
+                    {
+                        result << * it;
+                    }
+                    else
+                    {
+                        unsigned char b1 = *it;
+
+                        int length = ( b1 & 0xE0 ) == 0xE0 ? 2 : 1;
+
+                        unsigned int x = b1 & 0x1F;
+
+                        ++it;
+
+                        for ( int i = 0; i < length && it != string.end(); ++i , ++it )
+                        {
+                            x = ( x << 6 ) + ( ( ( unsigned char ) *it ) & 0x3F );
+                        }
+
+                        --it;
+
+                        result << "\\u" ;
+                        result << std::setw(4) << std::setfill( '0' ) << std::hex << x;
+                    }
+                    break;
+            }
+
+            if ( it == string.end() )
+            {
+                break;
+            }
+        }
+
+        result << "\"";
+
+        return result.str();
+    }
+
+    //=============================================================================
+
+    Value::Value( )
+    :
+        type( T_NULL )
+    {
+        memset( & value , 0 , sizeof( value ) );
+    }
+
+    Value::Value( const char * _value )
+    :
+        type( T_STRING )
+    {
+        value.string_value = new String( _value ? _value : "" );
+    }
+
+    Value::Value( const String & _value )
+    :
+        type( T_STRING )
+    {
+        value.string_value = new String( _value );
+    }
+
+    Value::Value( long long _value )
+    :
+        type( T_INT )
+    {
+        value.int_value = _value ;
+    }
+
+    Value::Value( int _value )
+    :
+        type( T_INT )
+    {
+        value.int_value = _value ;
+    }
+
+    Value::Value( double _value )
+    :
+        type( T_DOUBLE )
+    {
+        value.double_value = _value;
+    }
+
+    Value::Value( bool _value )
+    :
+        type( T_BOOL )
+    {
+        value.boolean_value = _value;
+    }
+
+    Value::Value( const Object & _value )
+    :
+        type( T_OBJECT )
+    {
+        value.object_value = new Object( _value );
+    }
+
+    Value::Value( const Array & _value )
+    :
+        type( T_ARRAY )
+    {
+        value.array_value = new Array( _value );
+    }
+
+    Value::Value( const Value & _value )
+    :
+        type( _value.type ),
+        value( _value.value )
+    {
+        switch( type )
+        {
+            case T_STRING:
+                value.string_value = new String( * value.string_value );
+                break;
+            case T_OBJECT:
+                value.object_value = new Object( * value.object_value );
+                break;
+            case T_ARRAY:
+                value.array_value = new Array( * value.array_value );
+                break;
+            default:
+                break;
+        }
+    }
+
+    Value::~Value()
+    {
+        clear();
+    }
+
+    void Value::clear()
+    {
+        switch( type )
+        {
+            case T_STRING:
+                delete value.string_value;
+                break;
+            case T_OBJECT:
+                delete value.object_value;
+                break;
+            case T_ARRAY:
+                delete value.array_value;
+                break;
+            default:
+                break;
+        }
+
+        memset( & value , 0 , sizeof( value ) );
+
+        type = T_NULL;
+    }
+
+    bool Value::empty() const
+    {
+        return type == T_NULL;
+    }
+
+    template <> bool Value::is< Null >() const
+    {
+        return type == T_NULL;
+    }
+
+    template <> bool Value::is< String >() const
+    {
+        return type == T_STRING;
+    }
+
+    template <> bool Value::is< int >() const
+    {
+        return type == T_INT;
+    }
+
+    template <> bool Value::is< long long >() const
+    {
+        return type == T_INT;
+    }
+
+    template <> bool Value::is< double >() const
+    {
+        return type == T_DOUBLE;
+    }
+
+    template <> bool Value::is< bool >() const
+    {
+        return type == T_BOOL;
+    }
+
+    template <> bool Value::is< Object >() const
+    {
+        return type == T_OBJECT;
+    }
+
+    template <> bool Value::is< Array >() const
+    {
+        return type == T_ARRAY;
+    }
+
+
+    template <> String & Value::as< String >()
+    {
+        if ( type != T_STRING )
+        {
+            clear();
+
+            type = T_STRING;
+
+            value.string_value = new String();
+        }
+
+        return * value.string_value;
+    }
+
+    template <> long long & Value::as< long long >()
+    {
+        if ( type != T_INT )
+        {
+            clear();
+
+            type = T_INT;
+        }
+
+        return value.int_value;
+    }
+
+    template <> double & Value::as< double >()
+    {
+        if ( type != T_DOUBLE )
+        {
+            clear();
+
+            type = T_DOUBLE;
+        }
+
+        return value.double_value;
+    }
+
+    template <> bool & Value::as< bool >()
+    {
+        if ( type != T_BOOL )
+        {
+            clear();
+
+            type = T_BOOL;
+        }
+
+        return value.boolean_value;
+    }
+
+    template <> Object & Value::as< Object >()
+    {
+        if ( type != T_OBJECT )
+        {
+            clear();
+
+            type = T_OBJECT;
+
+            value.object_value = new Object();
+        }
+
+        return * value.object_value;
+    }
+
+    template <> Array & Value::as< Array >()
+    {
+        if ( type != T_ARRAY )
+        {
+            clear();
+
+            type = T_ARRAY;
+
+            value.array_value = new Array();
+        }
+
+        return * value.array_value;
+    }
+
+    Value & Value::operator = ( const char * _value )
+    {
+        as< String >() = _value ? _value : "";
+
+        return * this;
+    }
+
+    Value & Value::operator = ( const String & _value )
+    {
+        as< String >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( long long _value )
+    {
+        as< long long >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( int _value )
+    {
+        as< long long >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( double _value )
+    {
+        as< double >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( bool _value )
+    {
+        as< bool >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( const Object & _value )
+    {
+        as< Object >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( const Array & _value )
+    {
+        as< Array >() = _value;
+
+        return * this;
+    }
+
+    Value & Value::operator = ( const Value & _value )
+    {
+        clear();
+
+        type =_value.type;
+        value = _value.value;
+
+        switch( type )
+        {
+            case T_STRING:
+                value.string_value = new String( * value.string_value );
+                break;
+            case T_OBJECT:
+                value.object_value = new Object( * value.object_value );
+                break;
+            case T_ARRAY:
+                value.array_value = new Array( * value.array_value );
+                break;
+            default:
+                break;
+        }
+
+        return * this;
+    }
+
+    Value::Type Value::get_type() const
+    {
+        return type;
+    }
+
+    std::ostream & operator << ( std::ostream & os , const Value & value )
+    {
+        switch( value.get_type() )
+        {
+            case Value::T_NULL:
+                os << "null";
+                break;
+            case Value::T_STRING:
+                os << encode_string( * value.value.string_value );
+                break;
+            case Value::T_INT:
+                os << value.value.int_value;
+                break;
+            case Value::T_DOUBLE:
+                os << value.value.double_value;
+                break;
+            case Value::T_BOOL:
+                os << ( value.value.boolean_value ? "true" : "false" );
+                break;
+            case Value::T_OBJECT:
+                os << ( * value.value.object_value );
+                break;
+            case Value::T_ARRAY:
+                os << ( * value.value.array_value );
+                break;
+        }
+        return os;
+    }
+
+    //=============================================================================
+
+    Object::Object()
+    {
+    }
+
+    Object::Object( const Object & source )
+    :
+        map( source.map )
+    {
+    }
+
+    Object::~Object()
+    {
+    }
+
+    Value & Object::operator [] ( const String & key )
+    {
+        return map[ key ];
+    }
+
+    bool Object::has( const String & key ) const
+    {
+        return map.find( key ) != map.end();
+    }
+
+    Object::Map::iterator Object::begin()
+    {
+        return map.begin();
+    }
+
+    Object::Map::const_iterator Object::begin() const
+    {
+        return map.begin();
+    }
+
+    Object::Map::iterator Object::end()
+    {
+        return map.end();
+    }
+
+    Object::Map::const_iterator Object::end() const
+    {
+        return map.end();
+    }
+
+    std::ostream & operator<<( std::ostream & os , const Object & object )
+    {
+        bool first = true;
+        os << "{";
+        for ( Object::Map::const_iterator it = object.begin(); it != object.end(); ++it )
+        {
+            if ( first )
+            {
+                first = false;
+            }
+            else
+            {
+                os << ",";
+            }
+            os << encode_string( it->first ) << ":" << it->second;
+        }
+        os << "}";
+        return os;
+    }
+
+    String Object::stringify() const
+    {
+        std::stringstream os;
+
+        os << * this;
+
+        return os.str();
+    }
+
+    //=============================================================================
+
+    Array::Array()
+    {
+    }
+
+    Array::Array( const Array & source )
+    :
+        vector( source.vector )
+    {
+    }
+
+    Array::~Array()
+    {
+    }
+
+    bool Array::empty() const
+    {
+        return vector.empty();
+    }
+
+    std::vector< Value >::size_type Array::size() const
+    {
+        return vector.size();
+    }
+
+    void Array::clear()
+    {
+        vector.clear();
+    }
+
+    Value & Array::operator [] ( std::vector< Value >::size_type index )
+    {
+        return vector[ index ];
+    }
+
+    Value & Array::append( const Value & value )
+    {
+        vector.push_back( value );
+
+        return vector.back();
+    }
+
+    Array::Vector::iterator Array::begin()
+    {
+        return vector.begin();
+    }
+
+    Array::Vector::const_iterator Array::begin() const
+    {
+        return vector.begin();
+    }
+
+    Array::Vector::iterator Array::end()
+    {
+        return vector.end();
+    }
+
+    Array::Vector::const_iterator Array::end() const
+    {
+        return vector.end();
+    }
+
+    std::ostream & operator<<( std::ostream & os , const Array & object )
+    {
+        bool first = true;
+        os << "[";
+        for ( Array::Vector::const_iterator it = object.begin(); it != object.end(); ++it )
+        {
+            if ( first )
+            {
+                first = false;
+            }
+            else
+            {
+                os << ",";
+            }
+            os << *it;
+        }
+        os << "]";
+        return os;
+    }
+
+    String Array::stringify() const
+    {
+        std::stringstream os;
+
+        os << * this;
+
+        return os.str();
+    }
+
+    //=============================================================================
+
+    Parser::Parser()
+    {
+        JSON_config config;
+
+        init_JSON_config( & config );
+
+        config.callback = parser_callback;
+        config.callback_ctx = this;
+        config.allow_comments = 1;
+        config.depth = -1;
+
+        parser = new_JSON_parser( & config );
+    }
+
+    Parser::~Parser()
+    {
+        delete_JSON_parser( parser );
+    }
+
+    Value Parser::parse( const String & json )
+    {
+        return parse( json.c_str() );
+    }
+
+    Value Parser::parse( std::istream & stream )
+    {
+        Parser parser;
+
+        char buffer[512];
+
+        while( ! stream.eof() )
+        {
+            stream.read( buffer , sizeof( buffer ) );
+
+            if ( stream.bad() )
+            {
+                return Value();
+            }
+
+            if ( 0 == stream.gcount() )
+            {
+                break;
+            }
+
+            if ( ! parser.parse_chunk( buffer , stream.gcount() ) )
+            {
+                return Value();
+            }
+        }
+
+        return parser.finish();
+    }
+
+    Value Parser::parse( const char * json , long int length )
+    {
+        Parser parser;
+
+        parser.parse_chunk( json , length );
+
+        return parser.finish();
+    }
+
+    bool Parser::parse_chunk( const char * json , long int length )
+    {
+        if ( length <= 0 )
+        {
+            for ( const char * p = json; *p; ++p )
+            {
+                if ( ! JSON_parser_char( parser , *p ) )
+                {
+                    stack.clear();
+                    root.clear();
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            const char * p = json;
+
+            for ( long int i = 0; i < length; ++i , ++p )
+            {
+                if ( ! JSON_parser_char( parser , *p ) )
+                {
+                    stack.clear();
+                    root.clear();
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    Value Parser::finish()
+    {
+        Value result;
+
+        if ( JSON_parser_done( parser ) )
+        {
+            result = root;
+        }
+
+        stack.clear();
+
+        root.clear();
+
+        return result;
+    }
+
+    int Parser::parser_callback( void * ctx , int type , const struct JSON_value_struct * value )
+    {
+        Parser * self = ( Parser * ) ctx;
+
+        Stack & stack( self->stack );
+
+        Value new_value;
+
+        switch( type )
+        {
+            case JSON_T_ARRAY_BEGIN:
+
+                if ( stack.empty() )
+                {
+                    self->root.as< Array >();
+
+                    stack.push_back( StackPair( self->root , String() ) );
+
+                    return 1;
+                }
+
+                new_value.as< Array >();
+
+                break;
+
+            case JSON_T_ARRAY_END:
+
+                if ( stack.empty() )
+                {
+                    return 0;
+                }
+                stack.pop_back();
+                return 1;
+                break;
+
+            case JSON_T_OBJECT_BEGIN:
+
+                if ( stack.empty() )
+                {
+                    self->root.as< Object >();
+
+                    stack.push_back( StackPair( self->root , String() ) );
+
+                    return 1;
+                }
+
+                new_value.as< Object >();
+                break;
+
+            case JSON_T_OBJECT_END:
+
+                if ( stack.empty() )
+                {
+                    return 0;
+                }
+                stack.pop_back();
+                return 1;
+                break;
+
+            case JSON_T_INTEGER:
+
+                new_value.as< long long >() = value->vu.integer_value;
+                break;
+
+            case JSON_T_FLOAT:
+
+                new_value.as< double >() = value->vu.float_value;
+                break;
+
+            case JSON_T_NULL:
+
+                break;
+
+            case JSON_T_TRUE:
+
+                new_value.as< bool >() = true;
+                break;
+
+            case JSON_T_FALSE:
+
+                new_value.as< bool >() = false;
+                break;
+
+            case JSON_T_STRING:
+
+                new_value.as< String >().assign( value->vu.str.value , value->vu.str.length );
+                break;
+
+            case JSON_T_KEY:
+
+                if ( stack.empty() )
+                {
+                    return 0;
+                }
+                if ( ! stack.back().first.is< Object >() )
+                {
+                    return 0;
+                }
+                stack.back().second.assign( value->vu.str.value , value->vu.str.length );
+                return 1;
+                break;
+
+            default:
+                return 0;
+        }
+
+        if ( stack.empty() )
+        {
+            return 0;
+        }
+
+        Value & top_value( stack.back().first );
+
+        Value * to_push = 0;
+
+        if ( top_value.is< Array >() )
+        {
+            to_push = & ( top_value.as< Array >().append( new_value ) );
+        }
+        else if ( top_value.is< Object >() )
+        {
+            String & key( stack.back().second );
+
+            to_push = & ( top_value.as< Object >()[ key ] = new_value );
+        }
+        else
+        {
+            return 0;
+        }
+
+        if ( to_push->is< Array >() || to_push->is< Object >() )
+        {
+            stack.push_back( StackPair( * to_push , String() ) );
+        }
+
+        return 1;
+    }
 }
--- engine/source/json.h
+++ engine/source/json.h
@@ -1,6 +1,8 @@
 #ifndef _TRICKPLAY_JSON_H
 #define _TRICKPLAY_JSON_H
 
+#include <iostream>
+
 #include "json-glib/json-glib.h"
 
 #if ! JSON_CHECK_VERSION(0,5,0)
@@ -8,6 +10,8 @@
 #endif
 
 #include "common.h"
+
+#include "JSON_parser.h"
 
 namespace JSON
 {
@@ -42,6 +46,205 @@ namespace JSON
     // wrong, the resulting string will be empty.
 
     String stringify( lua_State * L, int index, bool pretty = false );
+};
+
+// Newer home-brewed JSON stuff
+
+namespace JSON
+{
+    class Object;
+    class Array;
+    class Null;
+
+    class Value
+    {
+    public:
+
+        enum Type
+        {
+            T_NULL ,
+            T_STRING ,
+            T_INT ,
+            T_DOUBLE ,
+            T_BOOL ,
+            T_OBJECT ,
+            T_ARRAY
+        };
+
+        Value( );
+        Value( const char *     value );
+        Value( const String &   value );
+        Value( long long        value );
+        Value( int              value );
+        Value( double           value );
+        Value( bool             value );
+        Value( const Object &   value );
+        Value( const Array &    value );
+        Value( const Value &    value );
+
+        virtual ~Value();
+
+        void clear();
+
+        bool empty() const;
+
+        template < typename T > bool is() const;
+
+        template < typename T > T & as();
+
+        Value & operator = ( const char *   value );
+        Value & operator = ( const String & value );
+        Value & operator = ( long long      value );
+        Value & operator = ( int            value );
+        Value & operator = ( double         value );
+        Value & operator = ( bool           value );
+        Value & operator = ( const Object & value );
+        Value & operator = ( const Array &  value );
+        Value & operator = ( const Value &  value );
+
+        Type get_type() const;
+
+        friend std::ostream & operator << ( std::ostream & os , const Value & value );
+
+    private:
+
+        Type    type;
+
+        union
+        {
+            long long   int_value;
+            double      double_value;
+            String *    string_value;
+            bool        boolean_value;
+            Object *    object_value;
+            Array *     array_value;
+        }
+        value;
+    };
+
+    template <> bool Value::is< Null        >() const;
+    template <> bool Value::is< String      >() const;
+    template <> bool Value::is< int         >() const;
+    template <> bool Value::is< long long   >() const;
+    template <> bool Value::is< double      >() const;
+    template <> bool Value::is< bool        >() const;
+    template <> bool Value::is< Object      >() const;
+    template <> bool Value::is< Array       >() const;
+
+    template <> String &    Value::as< String       >();
+    template <> long long & Value::as< long long    >();
+    template <> double &    Value::as< double       >();
+    template <> bool &      Value::as< bool         >();
+    template <> Object &    Value::as< Object       >();
+    template <> Array &     Value::as< Array        >();
+
+    //=============================================================================
+
+    class Null
+    {
+    };
+
+    //=============================================================================
+
+    class Object
+    {
+    public:
+
+        Object();
+        Object( const Object & source );
+        virtual ~Object();
+
+        Value & operator [] ( const String & key );
+
+        typedef std::map< String , Value > Map;
+
+        bool has( const String & key ) const;
+
+        Map::iterator begin();
+        Map::const_iterator begin() const;
+        Map::iterator end();
+        Map::const_iterator end() const;
+
+        friend std::ostream & operator<<( std::ostream & os , const Object & object );
+
+        String stringify() const;
+
+    private:
+
+        Map map;
+    };
+
+    //=============================================================================
+
+    class Array
+    {
+    public:
+
+        Array();
+        Array( const Array & source );
+        virtual ~Array();
+
+        bool empty() const;
+
+        std::vector< Value >::size_type size() const;
+
+        void clear();
+
+        Value & operator [] ( std::vector< Value >::size_type index );
+
+        Value & append( const Value & value = Value() );
+
+        typedef std::vector< Value > Vector;
+
+        Vector::iterator begin();
+        Vector::const_iterator begin() const;
+        Vector::iterator end();
+        Vector::const_iterator end() const;
+
+        friend std::ostream & operator<<( std::ostream & os , const Array & object );
+
+        String stringify() const;
+
+    private:
+
+
+        Vector vector;
+    };
+
+    //=============================================================================
+
+    class Parser
+    {
+    public:
+
+        Parser();
+
+        virtual ~Parser();
+
+        static Value parse( const String & json );
+
+        static Value parse( std::istream & stream );
+
+        static Value parse( const char * json , long int length = -1 );
+
+        bool parse_chunk( const char * json , long int length = -1 );
+
+        Value finish();
+
+    private:
+
+        static int parser_callback( void * ctx , int type , const struct JSON_value_struct * value );
+
+        typedef std::pair< Value & , String > StackPair;
+
+        typedef std::list< StackPair > Stack;
+
+        Value       root;
+
+        Stack       stack;
+
+        JSON_parser parser;
+    };
 };
 
 #endif // _TRICKPLAY_JSON_H
