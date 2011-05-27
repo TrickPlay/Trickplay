2115fb3dbf646aaa4818193b149c9f01627d6994
Heejung Kim <hjkim@trickplay.com>
2011-05-27 14:25:54 -0700

    sdk/editor

--- apps/editor/lib/ui_element.lua
+++ apps/editor/lib/ui_element.lua
@@ -5374,12 +5374,32 @@ function ui_element.menuBar(t)
 end
 --]]
 
+---[[
+
+
 
 function ui_element.tabBar(t)
     
     --default parameters
     local p = {
         font  = "DejaVu Sans 26px",
+        
+        
+        button_text_font = nil,
+    	button_text_color = nil,
+    	button_text_focus_color = nil,
+        
+    	skin = "default", 
+    	ui_width = 150,
+    	ui_height = 60, 
+        
+    	focus_color = {27,145,27,255}, 	  --"1b911b", 
+    	focus_fill_color = {27,145,27,255}, --"1b911b", 
+    	focus_text_color = {255,255,255,255}, --"1b911b", 
+    	button_border_color = {255,255,255,255}, --"FFFFFF"
+    	button_border_width = 1,
+    	button_border_corner_radius = 12,
+        
         tab_labels = {
             "Item 1",
             "Item 2",
@@ -5391,9 +5411,9 @@ function ui_element.tabBar(t)
         label_padding = 10,
         tab_position = "TOP",
         
-        display_width  = 400,
+        display_width  = 600,
         display_height = 500,
-        --space_between_tabs = 10,
+        tab_spacing = 10,
         --slant_width  = 20,
         border_width =  2,
         border_color = {255,255,255,255},
@@ -5611,7 +5631,7 @@ function ui_element.tabBar(t)
     
     create = function()
         
-        local labels, txt_h, txt_w = {},0,0
+        local labels, txt_h, txt_w 
         
 		current_index = 1
 		
@@ -5635,22 +5655,32 @@ function ui_element.tabBar(t)
                 buttons[i] = ui_element.button()
             end
             
-			buttons[i].position = {0,0}
-
+            buttons[i].position = {0,0}
+            buttons[i].skin=p.skin
+            buttons[i].ui_width=p.ui_width
+            buttons[i].ui_height=p.ui_height
+            
+            buttons[i].focus_color=p.focus_color
+            buttons[i].border_width=p.button_border_width
+            buttons[i].border_corner_radius=p.button_border_corner_radius
+            
+            
             buttons[i].label      = p.tab_labels[i]
             buttons[i].text_font  = p.font
             buttons[i].text_color = p.label_color
             buttons[i].fill_color = p.unsel_color
             buttons[i].focus_fill_color = p.fill_color
+            buttons[i].position = {0,0}
+            buttons[i].focus_text_color = p.focus_text_color
             
             buttons[i].on_focus_out()
             
             if p.tab_position == "TOP" then
-                buttons[i].x = buttons[i].w*(i-1)
+                buttons[i].x = (p.tab_spacing+buttons[i].w)*(i-1)
                 p.tabs[i].y  = buttons[i].h
             else
                 p.tabs[i].x  = buttons[i].w
-                buttons[i].y = buttons[i].h*(i-1)
+                buttons[i].y = (p.tab_spacing+buttons[i].h)*(i-1)
             end
             umbrella:add(p.tabs[i],buttons[i])
             
@@ -5742,7 +5772,6 @@ function ui_element.tabBar(t)
 
     return umbrella
 end
-
 function ui_element.arrowPane(t)
 
 	-- reference: http://www.csdgn.org/db/179
