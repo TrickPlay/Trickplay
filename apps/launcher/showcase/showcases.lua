3a47d07eee1e5fde1118158affd2e3f378705413
Pablo Pissanetzky <pablo@trickplay.com>
2011-06-09 13:51:08 -0700

    [launcher] Mountain dew showcase now launches the app.
    Former-commit-id: d061f3b012ed973addf90a00b202b550b3e3eb93
    Former-commit-id: a5814d9173838efe2f781326fc37c924af49abad

--- apps/launcher/showcase/showcases.lua
+++ apps/launcher/showcase/showcases.lua
@@ -6,17 +6,18 @@ return
         title       = "Burberry",
         dropdown    = "showcase/burberry/dropdown.png",
         enter       = function( ui ) apps:launch( "com.trickplay.burberry" ) end
-    }
-    ,
+    },
+    
+    {
+        title       = "Mountain Dew",
+        dropdown    = "showcase/mountain-dew/dropdown.png",
+        enter       = function( ui ) apps:launch( "com.trickplay.mountain_dew" ) end
+
+    },
+    
     {
         title       = "J.Y. Park 박진영",
         dropdown    = "showcase/jyp/dropdown.png",
         enter       = function( ui ) return dofile( "showcase/jyp/showcase.lua" )( ui ) end
-    }
-    ,
-    {
-        title       = "Mountain Dew",
-        dropdown    = "showcase/mountain-dew/dropdown.png",
-        enter       = function( ui ) return dofile( "showcase/mountain-dew/showcase.lua" )( ui ) end
     }
 }
