0b386ea8c097b0cb07cc143590436e56c543d91c
Alex Indaco <alex.indaco@trickplay.com>
2011-07-08 16:13:57 -0700

    [pandamonium] reorganized code. added splash screen, setup up metric for making the game harder, the further you go

--- apps/pandamonium/GameLoop.lua
+++ apps/pandamonium/GameLoop.lua
@@ -1,47 +1,46 @@
-local top_wall
 local top_wall_y = screen_h
-local walls = {}
 
 local jump_thresh = screen_h / 4
-
+local first_time = true
 local idle_loop = function( ... )
 	
-	--print(...)
-	--print("ye",panda:get_vy())
-	--physics:draw_debug()
-	
+	--if the panda crossed the jump threshhold (while jumping upwards), then scroll up
 	if panda:get_y() < jump_thresh and panda:get_vy() < 0 then
-		--print("ye",panda:get_vy())
+		
+		--the amount to scroll by
 		dy = jump_thresh - panda:get_y()
+		
+		--lower the top_wall y position upval
 		top_wall_y = top_wall_y + dy
 		
-		bg.y = (bg.y + dy/4) % bg.base_size[2] - bg.base_size[2]
+		--scroll the tiles background by a lesser amount
+		bg.y = (bg.y + dy/3) % bg.base_size[2] - bg.base_size[2]
 		
-		---[[
+		--place the panda at the threshold
 		panda:scroll_by( dy )
 		
-		for w,_ in pairs(walls) do
-            
-            if not w:scroll_by(dy) then
-				
-				walls[w] = nil
-				
-			end
-            
-        end
-		--]]
+		--move all the branches and everything down
+		World:scroll_by(dy)
+		
 	end
 	
+	--if the top walls are now in view, add 2 more walls
 	while top_wall_y > 0 do
 		
-		top_wall        = make_wall(-1,top_wall_y-screen_h)
-		walls[top_wall] = top_wall
+		--2 new walls
+		if first_time then
+			World:add_first_walls(top_wall_y-screen_h)
+			first_time = false
+		else
+			World:add_next_walls(top_wall_y-screen_h)
+		end
 		
-		top_wall        = make_wall( 1,top_wall_y-screen_h)
-		walls[top_wall] = top_wall
 		
-		top_wall_y = top_wall.y-screen_h/2
+		--update top_wall y position (need to subtract by half its height
+		--because physics bodies automatically anchor to the center)
+		top_wall_y = top_wall_y-screen_h
 		
+		--raise the panda above the new branches
 		panda:raise_to_top()
 		
 	end
--- apps/pandamonium/main.lua
+++ apps/pandamonium/main.lua
@@ -10,18 +10,16 @@ physics.pixels_per_meter = 200
 
 physics.gravity = { 0 , 15 }
 
-
-
 assets, bg         = dofile("Assets.lua")
+
 panda              = dofile("Panda.lua")
+
+Splash             = dofile("Splash_Menu.lua")
+
 branch_constructor = dofile("Branches.lua")
-make_wall          = dofile("GameWorld.lua")
+
+World              = dofile("GameWorld.lua")
+
 physics.on_step    = dofile("GameLoop.lua")
 
---make sure that the keys don't go nil 
-assert(type(panda.on_key_down) == "function")
-
-function screen:on_key_down()
-    physics:start()
-    screen.on_key_down = panda.on_key_down
-end
+Splash:fade_in()
