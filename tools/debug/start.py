2c237a855466f2205670c27c4a0c7a0fcffc7eed
Devin Abbott <devin@trickplay.com>
2011-07-13 12:24:11 -0700

    Bugfixes for changing name and other simple properties. Deactivated non-editable items

--- tools/debug/start.py
+++ tools/debug/start.py
@@ -4,9 +4,10 @@
 
 Features to add:
 
-1.   Two views, one for UI elements and oen for properties
-2.   Checkboxes for hide/show
-3.   Drag and drop UI elements?
+1.   Two views, one for UI elements and one for properties
+2.   Search by gid/name
+3.   Checkboxes for hide/show
+X.   Drag and drop UI elements?
 
 """
 
@@ -78,28 +79,101 @@ class StartQT4(QMainWindow):
         
         s = i.indexes()[0]
         
-        #print("gid of sel",  s.child(0, 0).data(Qt.Gid).toPyObject())
+        self.updatePropertyList(s)
+        
+        
+    """
+    Remove and re-append the list of UI Element properties in the property view
+    """
+    def updatePropertyList(self, inspectorElementIndex):
         
         r = self.propertyModel.invisibleRootItem()
         
-        r.setData(s,  Qt.Pointer)
+        r.setData(inspectorElementIndex,  Qt.Pointer)
         
         r.removeRows(0, r.rowCount())
     
-        self.inspectorModel.copyAttrs(s, r)
+        self.inspectorModel.copyAttrs(inspectorElementIndex, r)
         
-        # self.propertySM.select(i,  QItemSelectionModel.SelectCurrent)
         
     """
-    Change Trickplay data when data is changed (by the user) in the property view
+    Update a UI Element in the app given its gid
     """
-    def dataChanged(self,  topLeft,  bottomRight):
-        print("dataChanged",  str(topLeft.data(0).toString()),  str(bottomRight.data(0).toString())) 
+    def sendData(self,  gid,  title,  value):
+    
+        try:
+                    
+            modelToData(title, value)
+        
+        except BadDataException, (e):
+            
+            print("BadDataException",  e.value)
+            
+            return False
+            
+        # Convert the data to proper format for sending
+        title, value = modelToData(title, value)
+        
+        print('Sending:', gid, title, value)
+        
+        connection.send({'gid': gid, 'properties' : {title : value}})
+        
+        return True
+    
+    """
+    Allow the user to change the name of a UIElement from the inspector view
+    """    
+    def inspectorDataChanged(self,  topLeft,  bottomRight):
         
         if not self.preventChanges:
             
+            print("preventChanges",  self.preventChanges)
+            
+            self.preventChanges = True
+            
             # User can only select one element, so these will always be equal. This assertion can be removed later.
             if str(topLeft.data(0).toString()) ==  str(bottomRight.data(0).toString()):
+                
+                print("inspector dataChanged called")
+                
+                valueIndex = topLeft
+                
+                titleIndex = self.inspectorModel.titleFromValue(valueIndex)
+                
+                print('title',  titleIndex.data(0).toString())
+                
+                gid = pyData(valueIndex, Qt.Gid)
+                
+                value = pyData(valueIndex, 0)
+                
+                if self.sendData(gid, 'name', value):
+                    
+                    self.preventChanges = True
+                    
+                    propertyValueIndex = self.inspectorModel.findAttr(titleIndex, 'name')[1]
+                    
+                    self.inspectorModel.itemFromIndex(propertyValueIndex).setData(value, 0)
+                    
+                    self.updatePropertyList(titleIndex)
+                    
+                    self.preventChanges = False
+                    
+            self.preventChanges = False
+        
+    """
+    Update Trickplay app when data is changed (by the user) in the property view
+    """
+    def dataChanged(self,  topLeft,  bottomRight):
+        #print("dataChanged",  str(topLeft.data(0).toString()),  str(bottomRight.data(0).toString())) 
+        
+        if not self.preventChanges:
+            
+            self.preventChanges = True
+            
+            # User can only select one element, so these will always be equal. This assertion can be removed later.
+            if str(topLeft.data(0).toString()) ==  str(bottomRight.data(0).toString()):
+                
+                print("property dataChanged called")
                 
                 r = self.propertyModel.invisibleRootItem()
                 
@@ -122,30 +196,18 @@ class StartQT4(QMainWindow):
                 
                 value = pyData(propertyValueIndex, 0)
                 
-                # Verify data is OK before making any changes to model or Trickplay
-                try:
-                    
-                    modelToData(title, value)
+                # Verify data is  OK before making any changes to model or Trickplay
                 
-                except BadDataException, (e):
+                if self.sendData(gid, title, value):
+                        
+                    # Update the data in the inspector
+                    valueItem.setData(value,  0)
                     
-                    print("BadDataException",  e.value)
-                    
-                    return
-                    
-                # Convert the data to proper format for sending
-                title, value = modelToData(title, value)
-                
-                print('Sending:', gid, title, value)
-                
-                connection.send({'gid': gid, 'properties' : {title : value}})
-            
-                # Update the data in the inspector
-                valueItem.setData(value,  0)
-                
             else:
                 
                 print("ERROR >> Multiple dataChanged's")
+                
+            self.preventChanges = False
         
     """
     Initialize models, proxy models, selection models, and connections
@@ -169,8 +231,6 @@ class StartQT4(QMainWindow):
         # Inspector Selection Model
         self.inspectorSelectionModel = QItemSelectionModel(self.inspectorProxyModel)
         
-        self.inspectorSelectionModel.connect(self.inspectorSelectionModel, SIGNAL("selectionChanged(QItemSelection, QItemSelection)"), self.selectionChanged)
-
         self.ui.inspector.setSelectionMode(QAbstractItemView.SingleSelection)
         
         self.ui.inspector.setSelectionModel(self.inspectorSelectionModel)
@@ -191,11 +251,17 @@ class StartQT4(QMainWindow):
         
         self.propertyProxyModel.setFilterRole(0)
 
-        self.propertyProxyModel.setFilterRegExp(QRegExp(""))
+        self.propertyProxyModel.setFilterRegExp(QRegExp("(opacity|is_visible|scale|clip|anchor_point|x|y|z|h|w"+\
+        "|gid|type|source|src|tile|border_color|border_width|color|text)"))
         
         self.ui.property.setModel(self.propertyProxyModel)
         
-        # Property Selection Model
+        # Connections
+        self.inspectorSelectionModel.connect(self.inspectorSelectionModel, SIGNAL("selectionChanged(QItemSelection, QItemSelection)"), self.selectionChanged)
+
+        
+        self.inspectorModel.connect(self.inspectorModel, SIGNAL("dataChanged(const QModelIndex&,const QModelIndex&)"), self.inspectorDataChanged)
+        
         self.propertyModel.connect(self.propertyModel, SIGNAL("dataChanged(const QModelIndex&,const QModelIndex&)"), self.dataChanged)
         
         
