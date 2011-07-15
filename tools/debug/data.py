715a3383aacc85f2bbd3becb1153b5ddd65c6f41
Devin Abbott <devin@trickplay.com>
2011-07-15 15:07:35 -0700

    Changed source attr to display gid only. Added tile. Now for refresh
    Former-commit-id: f4a59af45c77963ecbff9ca33ffb3e9943285eff
    Former-commit-id: 80499f313ee3d66bf790733df23f7c4891de5204

--- tools/debug/data.py
+++ tools/debug/data.py
@@ -31,44 +31,57 @@ typeTable = {
     'z_rotationangle': lambda v: ('rotation-angle-z',  toFloat(v['angle'])),
 
 }
+
+dataToModelTable = {
+
+    'type': lambda v: ('type', typeTextureToImage(v)), 
+    'is_visible': lambda v: ('is_visible', bool(v)), 
+    'tile': lambda v: ('tile', tileToBool(v)), 
+    'source': lambda v: ('source', summarizeSource(v)), 
+
+}
+
+def summarizeSource(v):
+    
+    # Would like to include name, but would break on 'refresh' if name changed
+    s = str(v['gid']) 
+    
+#    name = v['name']
+#    
+#    if '' != name:
+#        
+#        s += ' : ' + name
+    
+    return  s
+
+def tileToBool(v):
+    v['x'] = bool(v['x'])
+    v['y'] = bool(v['y'])
+    return v
+
+def typeTextureToImage(v):
+    
+    if 'Texture' == v:
+    
+        return "Image"
+        
+    else:
+
+        return v
     
 def modelToData(title,  value):
     t, v = typeTable[title](value)
     return (t, v)
     
 def dataToModel(title,  value):
-    t = title
-    v = value
-    s = True
+
+    title, value = dataToModelTable.get(title, lambda v: (title, value))(value)
+    simple = not isinstance(value, dict)
     
-    if isinstance(value, dict):
-        s = False
+#    if 'source' == title:
+#        simple = True
     
-#    if "anchor_point" == t or \
-#    "clip" == t or "scale" == t or \
-#    "tile" == t or "source" == t or \
-#    "color" == t or "border_color" == t or \
-#    "size" ==t or "position" == t:
-#        s = False
-        
-    #if "gid" == v:
-    #    v = int(v)
-        
-    if "type" == t and "Texture" == v:
-        v = "Image"
-        
-    if "is_visible" == t:
-        #if v:
-        #    v = True
-        #else:
-        #    v = False
-        v = bool(v)
-            
-    if "tile" == t:
-        v['x'] = bool(v['x'])
-        v['y'] = bool(v['y'])
-    
-    return (t, v, s)
+    return (title, value, simple)
 
 def getTypeTable():
     return typeTable
