00a31d86a12ba90d39f0352717c058263b5dd1c7
Devin Abbott <devin@trickplay.com>
2011-08-19 11:04:28 -0700

    Problem with app not closing... something to do with fileSystem or editor
    Former-commit-id: f01c2b0fbf9ef7d435c9e2015d661e461333cc36
    Former-commit-id: 40ebc3ea851f85e221db9deed382afafaf0eccfe

---
+++ tools/debug/FileSystem/FileSystem.py
@@ -0,0 +1,32 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+from FileSystemModel import FileSystemModel
+
+from UI.FileSystem import Ui_FileSystem
+
+class FileSystem(QWidget):
+    
+    def __init__(self, parent = None):
+        
+        QWidget.__init__(self, parent)
+        
+        self.ui = Ui_FileSystem()
+        self.ui.setupUi(self)
+        
+        self.editorManager = None
+        
+        
+    def start(self, editorManager, path):
+        """
+        Start the FileSystem with the given app path and editor
+        """
+        
+        self.model = FileSystemModel(self.ui.view, path)
+        
+        self.editorManager = editorManager
+        
+        QObject.connect(self.ui.view,
+                        SIGNAL('doubleClicked( QModelIndex )'),
+                        self.editorManager.openFromFileSystem)
+        
