5e3846c16bfc57e2ad2bc5355c28ffebaab1ddae
Rex Fenley <rex.fenley@trickplay.com>
2011-08-23 16:48:32 -0700

    [rex-iphone] AdvancedUI now returns nil if object does not exist.
    Additionally, still working on Audio.
    
    
    Former-commit-id: 5885c31000b6f3a0100d24326013696ad988a57f
    Former-commit-id: 840fe5e47eadc41ce296e18ee69b3ed3d12eda80

--- controllers/iphone/Classes/AudioStreamer.m
+++ controllers/iphone/Classes/AudioStreamer.m
@@ -206,6 +206,7 @@ void MyAudioSessionInterruptionListener(void *inClientData, UInt32 inInterruptio
 //
 // Invoked when an error occurs, the stream ends or we have data to read.
 //
+void ASReadStreamCallBack(CFReadStreamRef aStream, CFStreamEventType eventType, void* inClientInfo);
 void ASReadStreamCallBack
 (
    CFReadStreamRef aStream,
