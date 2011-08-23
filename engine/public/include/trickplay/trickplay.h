39b0071a9fccc8ce447b13d1f443b17ca518aaf2
Pablo Pissanetzky <pablo@trickplay.com>
2011-08-23 13:08:00 -0700

    Version 1.21.0

--- engine/public/include/trickplay/trickplay.h
+++ engine/public/include/trickplay/trickplay.h
@@ -33,7 +33,7 @@ extern "C" {
 */
 
 #define TP_MAJOR_VERSION    1
-#define TP_MINOR_VERSION    20
+#define TP_MINOR_VERSION    21
 #define TP_PATCH_VERSION    0
 
 /*-----------------------------------------------------------------------------
