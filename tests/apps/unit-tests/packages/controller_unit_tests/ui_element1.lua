84bdee85411ba450f8170864af7d9af3907206eb
Peter von dem Hagen <peter@trickplay.com>
2011-08-25 10:39:55 -0700

    [unit-tests] Added more controller unit-tests and clean up the UI a bit.

---
+++ tests/apps/unit-tests/packages/controller_unit_tests/ui_element1.lua
@@ -0,0 +1,34 @@
+--[[
+Filename: input_ui1.lua
+Author: Peter von dem Hagen
+Date: April 29, 2011
+Description:  Verify that the iphone returns the correct api calls for supported functionality.
+--]]
+
+
+-- Test Set up --
+
+-- Tests --
+function test_controllers_ui_size ()
+print ("*****************************************************")
+	--dumptable (ui_size)
+	assert_equal ( ui_size[1], 320,  "ui_size not returning the correct width")
+	assert_equal ( ui_size[2], 435, "ui_size not returning the correct heigth")
+end
+
+function test_controllers_input_size ()
+	--dumptable (input_size)
+	assert_equal ( input_size[1], 320,  "input_size not returning the correct width")
+	assert_equal ( input_size[2], 435, "input_size not returning the correct heigth")
+end
+
+-- Test Tear down --
+
+
+
+
+
+
+
+
+
