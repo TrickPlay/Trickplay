96fd185610f71d529bcbf35712815fa05c76b748
Peter von dem Hagen <peter@trickplay.com>
2011-09-02 15:36:23 -0700

    [iOS tests] added acceptance tests for touch and accelerator. Also added scrolling in the test list so I could add more tests.

--- tests/apps/iOS_GUI_Tests/00009_accelerometer.lua
+++ tests/apps/iOS_GUI_Tests/00009_accelerometer.lua
@@ -1,10 +1,10 @@
 
-test_description = "Check that the accelerometer is sending movement events."
-test_steps = "Move the device around to see how the x,y and z accelerometer values change."
+test_description = "Check that the accelerometer is sending acceleration values only as it has 'L' filter."
+test_steps = "Set the device down on the table and verify that all three values are near 0.\nMoving the device should only move the values small amounts. "
 test_verify = "Need to fill this in."
 test_group = "smoke"
 test_area = "accelerometer"
-test_api = "basic movement"
+test_api = "basic 'L' movement"
 
 
 function generate_test_image (controller, factory)
@@ -14,17 +14,17 @@ function generate_test_image (controller, factory)
 
         function controller:on_accelerometer(x, y, z)
     		controller:start_accelerometer("L", 1)
-			local remaining_time = 6 - math.floor(total)
-            test_verify_txt.text = "Accelerometer at ("..tostring(x)..", "..tostring(y)..", "..tostring(z)..")\n\nAcclerometer off in "..remaining_time.." seconds."
+			local remaining_time = 10 - math.floor(total)
+            test_verify_txt.text = "Accelerometer values:\nx =\t"..tostring(x).."\t".."\ny =\t"..tostring(y).."\t".."\nz =\t"..tostring(z)
         end
 
  	controller:start_accelerometer("L", 1)
 
-	-- Stop the accelerometer after 6 seconds
+	-- Stop the accelerometer after 10 seconds
 
     function idle.on_idle( idle , seconds )
       total = total + seconds
-      if total >= 6 then
+      if total >= 10 then
         idle.on_idle = nil
 		controller:stop_accelerometer()
  		test_verify_txt.text = "Accelerometer turned off."
