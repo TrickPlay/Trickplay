8a5e89b6098b17740694e9b25ca55ec6264bd89b
Craig R. Hughes <craig@trickplay.com>
2011-09-09 17:22:48 -0700

    [ddr] Clean up the score gauge and the way it displays itself

--- apps/dance-dance-robot/robot-part/score-gauge.lua
+++ apps/dance-dance-robot/robot-part/score-gauge.lua
@@ -1,9 +1,9 @@
 local score_gauge = Group { name = "Score Gauge" }
 
-local frame = Image { src = "/assets/robot-part/gauge/GaugeFront.png" }
-local bground = Image { src = "/assets/robot-part/gauge/GaugeBack.png" }
-local heart = Image { src = "/assets/robot-part/gauge/Heart.png" }
-local gauge = Image { src = "/assets/robot-part/gauge/Gauge.png" }
+local frame = Image { name = "frame", src = "/assets/robot-part/gauge/GaugeFront.png" }
+local bground = Image { name = "bground", src = "/assets/robot-part/gauge/GaugeBack.png" }
+local heart = Image { name = "heart", src = "/assets/robot-part/gauge/Heart.png" }
+local gauge = Image { name = "gauge", src = "/assets/robot-part/gauge/Gauge.png" }
 
 bground.x = (frame.w-bground.w)/2
 bground.y = (frame.h-bground.h)/2
@@ -11,9 +11,9 @@ bground.y = (frame.h-bground.h)/2
 gauge.y = bground.y
 gauge.x = bground.x
 
-score_gauge.extras = { score = 0 }
+score_gauge.extra = { score = 0 }
 
-gauge.w = bground.w * score_gauge.extras.score/100 + 1
+gauge.w = bground.w * score_gauge.extra.score/100 + 1
 
 heart.anchor_point = { heart.w/2, 0 }
 heart.y = (frame.h-heart.h)/2
@@ -25,10 +25,11 @@ score_gauge.x = (screen.w - score_gauge.w)/2
 score_gauge.y = 100
 
 
-score_gauge.extras.set_score = function(new_score)
+score_gauge.extra.set_score = function(self,new_score)
     assert(new_score >= 0 and new_score <= 100)
-    local old_score = score_gauge.extras.score
-    score_gauge.extras.score = new_score
+    local old_score = score_gauge.extra.score
+    local gauge, bground, heart = self:find_child('gauge'),self:find_child('bground'),self:find_child('heart')
+    self.extra.score = new_score
 
     local animator = Animator {
                                 duration = 250,
@@ -38,7 +39,7 @@ score_gauge.extras.set_score = function(new_score)
                                         name = "width",
                                         keys = {
                                             { 0.0,  "LINEAR",   bground.w * old_score/100 + 1 },
-                                            { 1.0,  "EASE_IN_OUT_SINE", bground.w * score_gauge.extras.score/100 + 1 },
+                                            { 1.0,  "EASE_IN_OUT_SINE", bground.w * self.extra.score/100 + 1 },
                                         }
                                     },
                                     {
@@ -46,7 +47,7 @@ score_gauge.extras.set_score = function(new_score)
                                         name = "x",
                                         keys = {
                                             { 0.0,  "LINEAR",   gauge.x + bground.w * old_score/100 + 1 },
-                                            { 1.0,  "EASE_IN_OUT_SINE", gauge.x + bground.w * score_gauge.extras.score/100 + 1 },
+                                            { 1.0,  "EASE_IN_OUT_SINE", gauge.x + bground.w * self.extra.score/100 + 1 },
                                         }
                                     },
                                 },
