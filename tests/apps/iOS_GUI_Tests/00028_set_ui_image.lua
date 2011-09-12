327dfe6d886aa0e7d505b1e991b55abaf95265ae
Peter von dem Hagen <peter@trickplay.com>
2011-09-12 11:25:16 -0700

    [iOs] added group tests

--- tests/apps/iOS_GUI_Tests/00028_set_ui_image.lua
+++ tests/apps/iOS_GUI_Tests/00028_set_ui_image.lua
@@ -10,10 +10,7 @@ function generate_test_image (controller, factory)
 	controller.screen:clear()
 	controller:declare_resource("panda", "assets/medium_640x420_MQ_Progressive_panda.jpg")
 
-	
-	local r1 = factory:Rectangle{color = "00EB75", x = 15, y = 15, size = { 160 , 110 }}
-	controller.screen:add(r1)
-	r1:lower_to_bottom()
+
 	controller:set_ui_image ("panda", 20, 20, 150, 100 )
 	return nil
 end
