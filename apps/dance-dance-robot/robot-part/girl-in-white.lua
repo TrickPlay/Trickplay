66154636ed48e81f39c2ab9457f9c4c3aef76848
Craig R. Hughes <craig@trickplay.com>
2011-09-14 14:49:26 -0700

    [ddr] Clean up asset paths to all be relative instead of absolutish
    Former-commit-id: 7d740e34bb216d7dcfae05f57c57fd328fcc50e2
    Former-commit-id: 8617a9ec98ef5bc3ddd9816a2243703323b6af74

--- apps/dance-dance-robot/robot-part/girl-in-white.lua
+++ apps/dance-dance-robot/robot-part/girl-in-white.lua
@@ -1,31 +1,31 @@
 local my_hook = function(factory)
 
     local images = {
-                                    idle1 = "/assets/robot-part/girls/idle.0001-1.png",
-                                    idle2 = "/assets/robot-part/girls/idle.0002-1.png",
-                                    idle3 = "/assets/robot-part/girls/idle.0003-1.png",
+                                    idle1 = "assets/robot-part/girls/idle.0001-1.png",
+                                    idle2 = "assets/robot-part/girls/idle.0002-1.png",
+                                    idle3 = "assets/robot-part/girls/idle.0003-1.png",
 
-                                    knockdown1 = "/assets/robot-part/girls/knockdown.0001-1.png",
-                                    knockdown2 = "/assets/robot-part/girls/knockdown.0002-1.png",
-                                    knockdown3 = "/assets/robot-part/girls/knockdown.0003-1.png",
-                                    knockdown4 = "/assets/robot-part/girls/knockdown.0004-1.png",
+                                    knockdown1 = "assets/robot-part/girls/knockdown.0001-1.png",
+                                    knockdown2 = "assets/robot-part/girls/knockdown.0002-1.png",
+                                    knockdown3 = "assets/robot-part/girls/knockdown.0003-1.png",
+                                    knockdown4 = "assets/robot-part/girls/knockdown.0004-1.png",
 
-                                    knockdownToStand1 = "/assets/robot-part/girls/knockdownToStand.0001-1.png",
+                                    knockdownToStand1 = "assets/robot-part/girls/knockdownToStand.0001-1.png",
 
-                                    roll1 = "/assets/robot-part/girls/roll.0001-1.png",
+                                    roll1 = "assets/robot-part/girls/roll.0001-1.png",
 
-                                    rollToStand1 = "/assets/robot-part/girls/rollTostand.0001-1.png",
-                                    rollToStand2 = "/assets/robot-part/girls/rollTostand.0002-1.png",
+                                    rollToStand1 = "assets/robot-part/girls/rollTostand.0001-1.png",
+                                    rollToStand2 = "assets/robot-part/girls/rollTostand.0002-1.png",
 
-                                    run1 = "/assets/robot-part/girls/run.0001-1.png",
-                                    run2 = "/assets/robot-part/girls/run.0002-1.png",
-                                    run3 = "/assets/robot-part/girls/run.0003-1.png",
-                                    run4 = "/assets/robot-part/girls/run.0004-1.png",
-                                    run5 = "/assets/robot-part/girls/run.0005-1.png",
-                                    run6 = "/assets/robot-part/girls/run.0006-1.png",
+                                    run1 = "assets/robot-part/girls/run.0001-1.png",
+                                    run2 = "assets/robot-part/girls/run.0002-1.png",
+                                    run3 = "assets/robot-part/girls/run.0003-1.png",
+                                    run4 = "assets/robot-part/girls/run.0004-1.png",
+                                    run5 = "assets/robot-part/girls/run.0005-1.png",
+                                    run6 = "assets/robot-part/girls/run.0006-1.png",
 
-                                    runToStop1 = "/assets/robot-part/girls/runTostop.0001-1.png",
-                                    runToStop2 = "/assets/robot-part/girls/runTostop.0002-1.png",
+                                    runToStop1 = "assets/robot-part/girls/runTostop.0001-1.png",
+                                    runToStop2 = "assets/robot-part/girls/runTostop.0002-1.png",
                                 }
 
     local girl = factory(images)
