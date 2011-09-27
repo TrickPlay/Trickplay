91d96389dfbc6b9a77a4e2c0845bf99067d4aae1
Alex Indaco <alex.indaco@trickplay.com>
2011-09-27 13:13:14 -0700

    [hangman] initial commit

---
+++ apps/hangman/main.lua
@@ -0,0 +1,17 @@
+assets_path = "assets/"
+audio_path  = "audio/"
+code_path   = "code/"
+
+--I f*cking hate radians
+function sin(val) return math.sin(math.pi/180*val) end
+function cos(val) return math.cos(math.pi/180*val) end
+function tan(val) return math.tan(math.pi/180*val) end
+
+function main()
+	
+	screen_w = screen.w
+	screen_h = screen.h
+	
+end
+
+dolater(main)
