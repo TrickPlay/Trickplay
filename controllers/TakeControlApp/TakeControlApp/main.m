d00d20e210ab2b53e1e535bc15f21bb8d4966719
Craig R. Hughes <craig@trickplay.com>
2011-10-07 11:56:11 -0700

    [iphone-app] Rework project; clean up; nuke stuff not wanted; use framework from SDK directory
    Former-commit-id: f2d59628a7e2c9f1dfea9e6457ea9bfec55a5314
    Former-commit-id: f504a275d7812a25be2b2009b204b59ee042d542

--- controllers/TakeControlApp/TakeControlApp/main.m
+++ controllers/TakeControlApp/TakeControlApp/main.m
@@ -1,6 +1,6 @@
 //
 //  main.m
-//  TakeControlAgain
+//  TakeControlApp
 //
 //  Created by Rex Fenley on 10/3/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
@@ -8,13 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
-#import "TakeControlAgainAppDelegate.h"
+#import "TakeControlAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     int retVal = 0;
     @autoreleasepool {
-        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([TakeControlAgainAppDelegate class]));
+        retVal = UIApplicationMain(argc, argv, nil, NSStringFromClass([TakeControlAppAppDelegate class]));
     }
     return retVal;
 }
