e3b3a33204e4a47a78d6d18edc842c4b376229ea
Rex Fenley <rex.fenley@trickplay.com>
2011-10-24 16:41:57 -0700

    [rex-iphone] Fixed upside down orientation in TakeControlApp.
    Former-commit-id: 9cdd63e97ac5ae916a71963e16077fd8abf63383
    Former-commit-id: eef4f306ac01f8e418d00996b93eeebe3e9cd396

--- controllers/TakeControlApp/TakeControlApp/TakeControlAppViewController.m
+++ controllers/TakeControlApp/TakeControlApp/TakeControlAppViewController.m
@@ -106,7 +106,7 @@
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
     // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return (interfaceOrientation == UIInterfaceOrientationPortrait || interfaceOrientation == UIInterfaceOrientationPortraitUpsideDown);
 }
 
 #pragma mark -
