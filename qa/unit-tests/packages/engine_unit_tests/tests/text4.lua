15d426d4844bf0eb628a48cd9b579e8c26294cce
Peter von dem Hagen <peter@trickplay.com>
2011-10-27 17:55:52 -0700

    [unit tests] Added a set of Text API unit tests * Text manipulation apis like delete_text and delete_chars * Verified that all the Text API properties values could be returned.
    Former-commit-id: 58aadc22fc18cd7dc43a48cad0d38b0b24e042d5
    Former-commit-id: 3d579b72462861f48c5c53b99e3c5732db9a0472

---
+++ qa/unit-tests/packages/engine_unit_tests/tests/text4.lua
@@ -0,0 +1,69 @@
+--[[
+Filename: Text4.lua
+Author: Name
+Date: October 28, 2011
+Description:   Test the text manipulation functions - cursor position
+--]]
+
+-- Get default cursor position then remove 4 chars from end  --
+local myText1 = Text ()
+myText1.text = "Fun with Dick and Jane"
+myText1.font = "DejaVu Sans 20px"
+myText1.color = "33FF22AA"
+myText1.position = { 1200, 100 }
+myText1:delete_chars(4)
+
+function test_Text_delete_chars_basic ()
+	assert_equal (myText1.text, "Fun with Dick and ", "delete_chars did not remove the last 4 chars of the line.")
+end
+
+function test_Text_default_cursor_position ()
+	assert_equal (myText1.cursor_position, -1, "Default cursor position is not equal to -1.")
+end
+
+-- Set cursor position and delete_chars from there --
+local myText2 = Text ()
+myText2.text = "Fun with Dick and Jane"
+myText2.font = "DejaVu Sans 20px"
+myText2.color = "33FF22AA"
+myText2.position = { 1200, 130 }
+myText2.cursor_position = 17
+myText2:delete_chars (9)
+
+function test_Text_delete_chars_moved_cursor_position ()
+	assert_equal ( myText2.text, "Fun with Jane", "delete_chars did not remove the last 4 chars of the line.")
+end
+
+function test_Text_change_cursor_position ()
+	assert_equal ( myText2.cursor_position, 8, "myText2.cursor_position is returning "..myText2.cursor_position..". Expected return value is 8.")
+end
+
+
+-- Set cursor position to outside the range and verify it fails gracefully
+local myText3 = Text ()
+myText3.text = "Fun with Dick and Jane"
+myText3.font = "DejaVu Sans 20px"
+myText3.color = "33FF22AA"
+myText3.position = { 1200, 160 }
+myText3.cursor_position = 35
+
+function test_Text_cursor_position_out_of_text_range ()
+	assert_equal ( myText3.cursor_position, -1, "myText3.cursor_position is returning "..myText3.cursor_position..". Expected return value is -1.")
+end
+
+
+
+test_group:add (myText1, myText2, myText3 )
+
+
+
+
+
+
+
+
+
+
+
+
+
