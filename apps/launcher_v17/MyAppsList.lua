01d27b593ef1b33451c523c1f9fa065f79a20202
Alex Indaco <alex.indaco@trickplay.com>
2011-12-12 11:50:00 -0800

    [launcher_v17] v0.9 ready to show

---
+++ apps/launcher_v17/MyAppsList.lua
@@ -0,0 +1,455 @@
+
+local AppList = Group{name = "AppList"}
+
+local item_spacing = 102
+
+local make_entry = function(app_id,app_name)
+    
+    local g = Group{name = app_name}
+    
+    local i = Image{x=15}
+    
+    if i:load_app_icon(app_id,"launcher-icon.png") then
+    elseif i:load_app_icon(app_id,"launcher-icon.jpg") then
+    else
+        i.src = "assets/generic-app-icon.jpg"
+    end
+    
+    i.w = 48*3
+    i.h = 27*3
+    
+    i.anchor_point = {0,i.h/2}
+    i.y = item_spacing/2
+    
+    local t   = Text{
+        text  = app_name,
+        color = "#ffffff",
+        font  = "FreeSans Medium 28px",
+        x     = i.w + 25,
+    }
+    
+    t.anchor_point = {0,t.h/2}
+    
+    t.y = i.y
+    
+    g:add(i,t)
+    
+    g.anchor_point = {i.w/2,item_spacing/2}
+    
+    g.x = i.w/2
+    function g:unfocus()
+        
+        print("Unfocus "..app_name )
+        
+        g:animate{
+            duration = 100,
+            scale    = {1,1}
+        }
+        
+    end
+    
+    function g:focus()
+        
+        print("Focus "..app_name )
+        
+        
+        g:animate{
+            duration = 100,
+            scale    = {1.2,1.2}
+        }
+        
+    end
+    
+    g.icon = i
+    g.text = t
+    g.app_id = app_id
+    
+    return g
+end
+
+local list = {}
+
+
+local exiting_item
+local wrap_around_clone_ref = Clone{ x=48*3/2, anchor_point = {48*3/2,item_spacing/2}}
+
+local hl_index = 2
+
+local top_vis_i = 1
+
+local vis_len = 5
+
+local clip_table
+local hl
+--screen:add(hl)
+function AppList:init(p)
+    
+    --fill in the list
+    local app_list = apps:get_for_current_profile()
+    
+    for k,v in pairs(app_list) do
+        
+        if not v.attributes.nolauncher    and
+            k ~= "com.trickplay.launcher" and
+            k ~= "com.trickplay.empty"    and
+            k ~= "com.trickplay.editor"   then
+            
+            print(k,"\t",v.id,"\t",v.name)
+            
+            list[ #list + 1 ] = make_entry(v.id,v.name)
+            
+            if #list <= p.max_vis_len then
+                
+                list[#list].y = item_spacing*(#list-1)+item_spacing/2
+                
+                self:add(list[#list])
+                
+            end
+            
+            if #list > 4 then break end
+            
+        end
+        
+    end
+    
+    hl = p.slider
+    hl.y = item_spacing
+    hl:focus(list[hl_index].text.text,list[hl_index].icon,list[hl_index].app_id)
+    hl.logical_parent = self
+    vis_len = # list < p.max_vis_len and # list or p.max_vis_len
+    
+    clip_table = { 0,item_spacing/2,400,(vis_len - 1)*item_spacing}
+    
+    AppList.list_h = (vis_len - 1)*item_spacing
+    
+    --AppList.clip = clip_table
+    
+    return self
+    
+end
+
+local regular_speed = 250
+local fast_speed    = 100
+local double_time   = false
+local again         = false
+
+local animating = false
+local list_y = Interval(0,0)
+local hl_y   = Interval(0,0)
+
+local on_new_frame_hl_only = function(self,ms,p)
+    
+    hl.y      =     hl_y:get_value(p)
+    
+end
+
+local on_new_frame_all = function(self,ms,p)
+    
+    clip_table[2] = -list_y:get_value(p)+item_spacing/2
+    --print(clip_table[2])
+    AppList.y    = list_y:get_value(p) 
+    --AppList.clip = clip_table 
+    hl.y      = hl_y:get_value(p)
+    
+end
+
+local move_up, move_dn
+
+local update = Timeline{
+    duration = regular_speed,
+    on_completed = function(self)
+        
+        if self.on_new_frame == on_new_frame_all then
+        if animating == "UP" then
+            
+            AppList.y = AppList.y-item_spacing
+            
+            AppList:foreach_child(function(c)
+                c.y = c.y + item_spacing
+            end)
+            
+        elseif  animating == "DOWN" then
+            
+            AppList.y = AppList.y+item_spacing
+            
+            AppList:foreach_child(function(c)
+                c.y = c.y - item_spacing
+            end)
+            
+        else
+            
+            error("IMPOSSIBRU!!")
+            
+        end
+        clip_table[2] = item_spacing/2
+        --AppList.clip = clip_table 
+        end
+        
+        if exiting_item then
+            
+            exiting_item:unparent()
+            
+            --adjust y's of cliped list
+            
+        end
+        
+        if again then
+            
+            if animating == "UP" then
+                
+                move_up()
+                
+            elseif  animating == "DOWN" then
+                
+                move_dn()
+                
+            else
+                
+                error("IMPOSSIBRU!!")
+                
+            end
+            
+            again = false
+            
+        else
+            self.duration = regular_speed
+            double_time = false
+            
+            animating = false
+        end
+        
+    end,
+}
+
+update.alpha = Alpha{
+    mode     = "LINEAR",
+    timeline = update,
+}
+
+local wrap_i = function(i)
+    
+    return (i - 1) % (# list) + 1
+    
+end
+
+
+move_up = function()
+    
+        
+        list[hl_index]:unfocus()
+        
+        if hl_index == wrap_i(top_vis_i+1) then
+            
+            top_vis_i = wrap_i(top_vis_i - 1)
+            
+            if vis_len == # list then
+                
+                wrap_around_clone_ref.source = list[top_vis_i]
+                
+                AppList:add(wrap_around_clone_ref)
+                
+                wrap_around_clone_ref.y = wrap_around_clone_ref.source.y
+                
+                exiting_item = wrap_around_clone_ref
+                
+            else
+                
+                AppList:add(list[top_vis_i])
+                
+                exiting_item = list[
+                    wrap_i(top_vis_i + vis_len)
+                ]
+                
+            end
+            
+            list[top_vis_i].y = list[  wrap_i(top_vis_i + 1)  ].y - item_spacing
+            
+            list_y.from = AppList.y
+            list_y.to   = AppList.y + item_spacing
+            
+            update.on_new_frame = on_new_frame_all
+            
+        else
+            
+            update.on_new_frame = on_new_frame_hl_only
+            
+        end
+        
+        hl_index = wrap_i(hl_index - 1)
+        
+        hl_y.from = hl.y
+        hl_y.to   = wrap_i(hl_index - top_vis_i)*item_spacing
+        
+        list[hl_index]:focus()
+        hl:focus(list[hl_index].text.text,list[hl_index].icon,list[hl_index].app_id)
+        
+        update:start()
+    
+end
+
+move_dn = function()
+    
+        
+        list[hl_index]:unfocus()
+        
+        if hl_index == wrap_i( top_vis_i + vis_len - 2 ) then
+            
+            top_vis_i = wrap_i(top_vis_i + 1)
+            
+            if vis_len == # list then
+                
+                wrap_around_clone_ref.source = list[ wrap_i( top_vis_i + vis_len - 1 ) ]
+                
+                AppList:add(wrap_around_clone_ref)
+                
+                wrap_around_clone_ref.y = wrap_around_clone_ref.source.y
+                
+                exiting_item = wrap_around_clone_ref
+                
+            else
+                print("yea",wrap_i( top_vis_i + vis_len - 1 ),list[ wrap_i( top_vis_i + vis_len - 1 ) ] )
+                gl = list[ wrap_i( top_vis_i + vis_len - 1 ) ]
+                dumptable(list)
+                AppList:add(list[ wrap_i( top_vis_i + vis_len - 1 ) ])
+                print(top_vis_i,list[ wrap_i(top_vis_i) ].name)
+                exiting_item = list[
+                    wrap_i(top_vis_i-1)
+                ]
+                
+            end
+            
+            list[     wrap_i( top_vis_i + vis_len - 1 ) ].y =
+                list[ wrap_i( top_vis_i + vis_len - 2 ) ].y + item_spacing
+            
+            list_y.from = AppList.y
+            list_y.to   = AppList.y - item_spacing
+            
+            update.on_new_frame = on_new_frame_all
+            
+        else
+            
+            update.on_new_frame = on_new_frame_hl_only
+            
+        end
+        
+        hl_index = wrap_i(hl_index + 1)
+        
+        hl_y.from = hl.y
+        hl_y.to   = wrap_i(hl_index - top_vis_i)*item_spacing
+        
+        
+        list[hl_index]:focus()
+        hl:focus(list[hl_index].text.text,list[hl_index].icon,list[hl_index].app_id)
+        print(hl_index,top_vis_i)
+        update:start()
+end
+
+
+local key_events = {
+    [keys.Up] = function()
+        
+        if animating == "UP" then
+            
+            if not double_time then
+                
+                double_time = true
+                
+                update:pause()
+                local p = update.progress
+                update.duration = fast_speed
+                
+                update:start()
+                update:advance(p*fast_speed)
+                
+            end
+            
+            again = true
+            
+            return true
+            
+        elseif animating == "DOWN" then
+            --[[
+            --tl.mode = "EASE_IN_BACK"
+            if exiting_item then exiting_item = nil end
+            update:pause()
+            update.duration = regular_speed
+            update:advance(0)
+            double_time = false
+            
+            again = false
+            --]]
+            return true
+        end
+        
+        animating = "UP"
+        
+        move_up()
+        
+        return true
+    end,
+    [keys.Down] = function()
+        
+        if animating == "DOWN" then
+            
+            if not double_time then
+                
+                double_time = true
+                
+                update:pause()
+                local p = update.progress
+                update.duration = fast_speed
+                
+                update:start()
+                update:advance(p*fast_speed)
+                
+            end
+            
+            again = true
+            
+            return true
+            
+        elseif animating == "UP" then
+            
+            --[[
+            if exiting_item then exiting_item = nil end
+            update:pause()
+            update.duration = regular_speed
+            update:advance(0)
+            double_time = false
+            
+            again = false
+            --]]
+            return true
+        end
+        
+        animating = "DOWN"
+        
+        move_dn()
+        
+        return true
+        
+    end,
+    [keys.OK] = function()
+        
+        if not animating then
+            
+            hl:show_sub_menu()--selected = true
+            hl:grab_key_focus()
+        end
+        
+        return true
+    end,
+}
+
+function AppList:on_key_down(k)
+    
+    return key_events[k] and key_events[k]()
+    
+end
+
+
+
+
+
+
+return AppList
