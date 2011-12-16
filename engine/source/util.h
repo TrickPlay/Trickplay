eda7c86ab0dcb2580a8cddbb0dbeee6f55873ea4
Pablo Pissanetzky <pablo@trickplay.com>
2011-12-15 17:24:45 -0800

    [engine] Finally! Got rid of the evil rebase_path and path_to_native_path.
    Former-commit-id: ca9791cf9c8d37357efc5d4ee67c489bd311c3a6
    Former-commit-id: f24470ed3c5fc84e548bf427b1cebce58d77c908

--- engine/source/util.h
+++ engine/source/util.h
@@ -424,54 +424,6 @@ namespace Util
     String canonical_external_path( const char * path , bool abort_on_error = true );
 
     //-----------------------------------------------------------------------------
-    // Converts a path using / to a platform path in place - modifies the string
-    // passed in.
-
-    inline gchar * path_to_native_path( gchar * path )
-    {
-        if ( G_DIR_SEPARATOR == '/' )
-        {
-            return path;
-        }
-        return g_strdelimit( path, "/", G_DIR_SEPARATOR );
-    }
-
-    //-----------------------------------------------------------------------------
-    // Given a root path and some other path, it makes the path relative and
-    // appends it to the root. Return value must be destroyed with g_free.
-    // Both root and path are converted to native paths.
-    //
-    // NOTE: if path contains any .. elements, this will abort. The assumption
-    // is that root is trusted and path came from Lua - and cannot be trusted
-
-    inline gchar * rebase_path( const gchar * root, const gchar * path , bool abort = true )
-    {
-        FreeLater free_later;
-
-        if ( strstr( path, ".." ) )
-        {
-            if ( abort )
-            {
-                g_error( "Invalid relative path '%s'", path );
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        gchar * p = path_to_native_path( g_strdup( path ) );
-        free_later( p );
-
-        const gchar * last = g_path_is_absolute( p ) ? g_path_skip_root( p ) : p;
-
-        gchar * first = path_to_native_path( g_strdup( root ) );
-        free_later( first );
-
-        return g_build_filename( first, last, ( gpointer ) 0 );
-    }
-
-    //-----------------------------------------------------------------------------
 
     String make_v1_uuid();
 
