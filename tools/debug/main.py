a1fddbdbe4db09b71e6ec5bc19ab2ffc02e76011
Heejung Kim <hjkim@trickplay.com>
2012-03-01 12:17:11 -0800

    [debugger] fixed Newfile error

--- tools/debug/main.py
+++ tools/debug/main.py
@@ -665,6 +665,9 @@ class MainWindow(QMainWindow):
 
     def newFile(self):
     	file_name = self.path+'/Untitled_'+str(self.untitled_idx)+".lua"
+        
+        #TODO check if file_name is available and ... 
+
     	self.editorManager.newEditor(file_name, None, None, None, False, None, True)
     	self.untitled_idx = self.untitled_idx + 1
 		
