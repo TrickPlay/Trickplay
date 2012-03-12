2286e3f88676cefe9dc42e782da624802543df3c
Heejung Kim <hjkim@trickplay.com>
2012-03-12 10:52:48 -0700

    [debugger] bugzid : 2685
    Former-commit-id: b71434290a80ae9c10eea7e9c021f4d779b4159d
    Former-commit-id: 251036b6a80c7fe5e1ec1f1fcd1abe4b54cf5cbd

--- tools/debug/Preference.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'Preference2.ui'
-#
-# Created: Thu Mar  8 09:46:05 2012
-#      by: PyQt4 UI code generator 4.8.3
-#
-# WARNING! All changes made in this file will be lost!
-
