8b914fad9372bccb25786b8170eacfc96712a961
Steven Calwas <calwas@trickplay.com>
2012-04-05 17:53:47 -0700

    [docs] Created Localization example application
    [docs] Updated EngRef:UIElement:complete_animation() and stop_animation() functions

---
+++ tests/apps/tutorials/localization/i18n.lua
@@ -0,0 +1,27 @@
+--
+-- Localized strings and X,Y coordinates for localized screen layouts
+--
+
+-- Localized for English
+
+return{
+
+	-- Billboard image screen position
+	BILLBOARD_X				= 850,
+	BILLBOARD_Y				= 200,
+	
+	-- Text and X,Y coordinates for buttons
+	BUTTON_TOP_TEXT 		= "English",
+	BUTTON_TOP_X    		= 277,
+	BUTTON_TOP_Y			= 258,
+	
+	BUTTON_MIDDLE_TEXT		= "Français",
+	BUTTON_MIDDLE_X			= 263,
+	BUTTON_MIDDLE_Y			= 440,
+	
+	BUTTON_BOTTOM_TEXT		= "Español",
+	BUTTON_BOTTOM_X			= 268,
+	BUTTON_BOTTOM_Y			= 625,
+	
+}
+	
