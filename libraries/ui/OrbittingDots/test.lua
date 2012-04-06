3c74a98f71cec3cc7f4e7d752f71d3b7b3e53696
Alex Indaco <alex.indaco@trickplay.com>
2012-04-06 15:01:30 -0700

    [widget_library] initial commit for OrbittingDots

---
+++ libraries/ui/OrbittingDots/test.lua
@@ -0,0 +1,25 @@
+
+if not OVERRIDEMETATABLE then dofile("__UTILITIES/OverrideMetatable.lua")   end
+if not TYPECHECKING      then dofile("__UTILITIES/TypeChecking.lua")        end
+if not TABLEMANIPULATION then dofile("__UTILITIES/TableManipulation.lua")   end
+if not CANVAS            then dofile("__UTILITIES/Canvas.lua")              end
+if not COLORSCHEME       then dofile("__CORE/ColorScheme.lua")              end
+if not STYLE             then dofile("__CORE/Style.lua")                    end
+if not WIDGET            then dofile("__CORE/Widget.lua")                   end
+if not PROGRESSSPINNER   then dofile("ProgressSpinner/ProgressSpinner.lua") end
+if not ORBITTINGDOTS     then dofile("OrbittingDots/OrbittingDots.lua")     end
+
+
+
+
+
+screen:show()
+
+
+ps1 = OrbittingDots()
+
+--ps2 = ProgressSpinner{x = 200,image = "ProgressSpinner/load-sun-spin.png"}
+
+
+screen:add(ps1,ps2)
+
