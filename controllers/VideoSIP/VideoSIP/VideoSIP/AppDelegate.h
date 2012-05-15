b60e3fe90b2584256ed6043684e5657f32db0c7a
Rex Fenley <rex.fenley@trickplay.com>
2012-05-15 15:24:20 -0700

    [iOS VideoStreamerFramework] Video Streaming has successfully been refactored into its own static framework which may be applied to others apps.
    - VideoStreamerFramework represents a static iOS framework that may be used to
      stream video via SIP from the device.
    - This framework may be added to an app similarly to how any other framework
      is installed to apps.

--- controllers/VideoSIP/VideoSIP/VideoSIP/AppDelegate.h
+++ controllers/VideoSIP/VideoSIP/VideoSIP/AppDelegate.h
@@ -8,9 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
+#import "VideoStreamer.h"
+
 @class ViewController;
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, VideoStreamerDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
