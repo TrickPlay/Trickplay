9c6f984c94c3d6b0b8b20d255a6535db66eb4baf
Alex Indaco <alex.indaco@trickplay.com>
2012-05-17 15:38:11 -0700

    [widget_library] needed to update MenuButton in order to support changes in LayoutManager

--- libraries/ui/MenuButton/test.lua
+++ libraries/ui/MenuButton/test.lua
@@ -1,1 +1,46 @@
 
+
+if not OVERRIDEMETATABLE then dofile("__UTILITIES/OverrideMetatable.lua")   end
+if not TYPECHECKING      then dofile("__UTILITIES/TypeChecking.lua")        end
+if not TABLEMANIPULATION then dofile("__UTILITIES/TableManipulation.lua")   end
+if not CANVAS            then dofile("__UTILITIES/Canvas.lua")              end
+if not MISC              then dofile("__UTILITIES/Misc.lua")                end
+if not COLORSCHEME       then dofile("__CORE/ColorScheme.lua")              end
+if not STYLE             then dofile("__CORE/Style.lua")                    end
+if not WIDGET            then dofile("__CORE/Widget.lua")                   end
+if not LISTMANAGER       then dofile("__UTILITIES/ListManagement.lua")      end
+if not LAYOUTMANAGER     then dofile("LayoutManager/LayoutManager.lua")     end
+if not BUTTON            then dofile("Button/Button.lua")                   end
+if not MENUBUTTON        then dofile("MenuButton/MenuButton.lua")           end
+
+local test_group = Group()
+
+screen:add(test_group)
+local tests = {
+    
+}
+
+for i,test in ipairs(tests) do
+    
+    if not test() then print("test "..i.." failed") end
+    test_group:clear()
+end
+
+test_group:unparent()
+
+
+
+
+
+
+
+screen:show()
+
+mb1 = MenuButton{
+    items = {
+        Button(),Button()--Rectangle{w=100,h=100},Rectangle{w=100,h=100},
+    }
+}
+
+screen:add(mb1)
+
