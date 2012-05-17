ab29549c159bda3698ed9833eac8f5e08262f843
Heejung Kim <hjkim@trickplay.com>
2012-05-17 10:43:24 -0700

    [VE2] Widget Lib Updated
    Former-commit-id: 1f6192cb9beb514d00702a6bc2a9119f14359628
    Former-commit-id: 189e68eb3d4b2934a3694ac5d26a6da677db131b

--- tools/ve2/VE/__CORE/ColorScheme.lua
+++ tools/ve2/VE/__CORE/ColorScheme.lua
@@ -83,6 +83,17 @@ ColorScheme = function(parameters)
             end
             
         end,
+        set = function(_,t)
+            
+            if type(t) ~= "table" then
+                error("Expects a table. Received "..type(t),2)
+            end
+            
+            for k, v in pairs(t) do
+                instance[k] = v
+            end
+            
+        end,
         on_changed = function(self,object,update_function)
             
             children_using_this_style[object] = update_function
