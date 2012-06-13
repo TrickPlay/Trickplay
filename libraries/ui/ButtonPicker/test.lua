e70169dc125e657d94478e5a720978b3d67fcd1d
Alex Indaco <alex.indaco@trickplay.com>
2012-06-13 14:30:41 -0700

    [widget_library] setting ButtonPickers arrow color now works

--- libraries/ui/ButtonPicker/test.lua
+++ libraries/ui/ButtonPicker/test.lua
@@ -20,7 +20,11 @@ screen:show()
 
 
 bp1 = ButtonPicker{items={"one","two","333","for"}}
-bp2 = ButtonPicker{x = 400,window_h = 200,orientation="vertical",items={"item1","item2","ite","itdddem2"}}
+bp2 = ButtonPicker{style = false, x = 400,window_h = 200,orientation="vertical",items={"item1","item2","ite","itdddem2"}}
+print("MEEEEEERP")
+bp2.style.arrow.colors.default = "009999"
+bp2.style.arrow.size = 40
+bp2.style.arrow.offset = -45
 
 screen:add(Rectangle{size=screen.size,color="666600"},bp1,bp2)
 
