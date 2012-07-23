76f511387a2e058f3813e9bfbf3f4d5376a177a9
Craig R. Hughes <craig.git@rungie.com>
2012-07-23 11:23:54 -0700

    [free-tv] Preload images; optionally use async
    Conflicts:
    	apps/freetv-splash/preload_configure.lua

--- apps/freetv-splash/posters.lua
+++ apps/freetv-splash/posters.lua
@@ -1,10 +1,11 @@
-local directory = ...
+local directory,load_async = ...
+if nil == load_async then load_async = true end
 
 local poster_names = dofile(directory.."list.lua")
 local posters = {}
 
 for i=1,#poster_names do
-    local my_image = Widget_Image { src = directory..poster_names[i] }
+    local my_image = Widget_Image { src = directory..poster_names[i], async=load_async }
     posters[i] = NineSlice{
         x = screen.w/2,
         y = screen.h/2,
@@ -31,6 +32,12 @@ for i=1,#poster_names do
         }
     }
     posters[i].anchor_point = { posters[i].w/2, posters[i].h/2 }
+    if load_async then my_image.on_loaded = function(img,fail)
+            posters[i].w = img.base_size[1]+1+1
+            posters[i].h = img.base_size[2]+1+1
+            posters[i].anchor_point = { posters[i].w/2, posters[i].h/2 }
+        end
+    end
 end
 
 poster_names = nil
