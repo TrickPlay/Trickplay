966e88c5235e8e93d97a1ed3d07fc5503b7cafdc
Craig R. Hughes <craig.git@rungie.com>
2012-07-31 16:43:58 -0700

    [free-tv] Startup optimization: create a queue of assets to load, then preload them more spread-outedly for faster start and smoother smoothness, in theory
    Former-commit-id: 3b7a078076097f74c5ff92d1e743758093ea0f9f
    Former-commit-id: 775eddd7cdc5ca74c01740d20cedd2c0947d7548

--- apps/freetv-splash/posters.lua
+++ apps/freetv-splash/posters.lua
@@ -1,43 +1,11 @@
-local directory,load_async = ...
-if nil == load_async then load_async = true end
+local directory,all_posters_list = ...
 
 local poster_names = dofile(directory.."list.lua")
 local posters = {}
 
+-- Create a queue with filenames and a destination into which to insert them when they're loaded
 for i=1,#poster_names do
-    local my_image = Widget_Image { src = directory..poster_names[i], async=load_async }
-    posters[i] = NineSlice{
-        x = screen.w/2,
-        y = screen.h/2,
-        w = my_image.w + 1 + 1,
-        h = my_image.h + 1 + 1,
-        cells = {
-            default = {
-                {
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                },
-                {
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                    my_image,
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                },
-                {
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                    Widget_Rectangle{ w=1,h=1,color="white", opacity=64 },
-                },
-            },
-        }
-    }
-    posters[i].anchor_point = { posters[i].w/2, posters[i].h/2 }
-    if load_async then my_image.on_loaded = function(img,fail)
-            posters[i].w = img.base_size[1]+1+1
-            posters[i].h = img.base_size[2]+1+1
-            posters[i].anchor_point = { posters[i].w/2, posters[i].h/2 }
-        end
-    end
+    table.insert( all_posters_list, { file = directory..poster_names[i], dest = posters } )
 end
 
 poster_names = nil
