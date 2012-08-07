fa4ba9450686b2890ff44ae812b52ab1586dd83a
Alex Indaco <alex.indaco@trickplay.com>
2012-08-07 14:12:31 -0700

    [hangman] removed garbage
    Former-commit-id: 390d0257bda93af47096a057ebcdc4e4b74c338e
    Former-commit-id: d761ba4e8d9c22856d1c5fc855ad78651663c77f

--- apps/hangman/code/visual objects/Front_Page__Main.lua
+++ apps/hangman/code/visual objects/Front_Page__Main.lua
@@ -423,9 +423,7 @@ function self:won_against(entry)
         
         game_history:set_wins( g_user.wins )
         
-    end)<<<<<<<<
-presence type="unavailable" from="room1@mug.internal.trickplay.com/1_q" to="ca5cd5d0-e00a-11e1-8fce-109add46f936@internal.trickplay.com/smarttv"><game xmlns="http://jabber.org/protocol/mug"><item affiliation="none" role="p2" jid="d63c53fe-dce9-11e1-bd68-6b00b2f10aa2@internal.trickplay.com"/></game></presence>
-<<<<<<<<
+    end)
 
     
 end
