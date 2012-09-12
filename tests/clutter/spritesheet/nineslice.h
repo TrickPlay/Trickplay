e0f905b639066faba51a509c948f0d8dd3f1b375
Josh Steelman <joshua.steelman@trickplay.com>
2012-09-11 17:09:20 -0700

    [spritesheet] moved spritesheet to string hashes, cleaned up code

--- tests/clutter/spritesheet/nineslice.h
+++ tests/clutter/spritesheet/nineslice.h
@@ -10,14 +10,27 @@
 #include <clutter/clutter.h>
 
 typedef struct SpriteSheet {
-  CoglMaterial* *material;
-  CoglHandle* *texture;
+  GHashTable *map;
+  CoglMaterial **material;
+  CoglHandle **texture;
   gint *w;
   gint *h;
   gint n;
 } SpriteSheet;
 
-ClutterEffect* nineslice_effect_new_from_source(gchar* source[], gboolean tiled);
-ClutterEffect* nineslice_effect_new_from_spritesheet(SpriteSheet *sheet, gint offset, gboolean tiled);
+static void spritesheet_get_sprite(SpriteSheet *sheet, gchar *name, CoglMaterial **material, CoglHandle **texture, gint *w, gint *h);
+
+static void spritesheet_get_sprite(SpriteSheet *sheet, gchar *name, CoglMaterial **material, CoglHandle **texture, gint *w, gint *h) {
+  gpointer p = g_hash_table_lookup(sheet->map, name);
+  if (p != NULL) {
+    gint i = GPOINTER_TO_INT(p) - 1;
+    if (material != NULL) *material = sheet->material[i];
+    if (texture != NULL) *texture = sheet->texture[i];
+    if (w != NULL) *w = sheet->w[i];
+    if (h != NULL) *h = sheet->h[i];
+  }
+}
+
+ClutterEffect* nineslice_effect_new_from_names(gchar* names[], SpriteSheet *sheet, gboolean tiled);
 
 #endif
