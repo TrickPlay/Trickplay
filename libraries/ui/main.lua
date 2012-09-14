210c1191aa2ebbede2c0575841cb178c01f2306f
trickplay <trickplay@ubuntu.(none)>
2012-09-14 15:02:36 -0700

    [widget_library] added animation to main.lua because Parrallels sucks and some verbosities
    Former-commit-id: 520950125dece22ec9fcbfe699a389fe20c9d7a0
    Former-commit-id: 60c480fe8ecb4a9d25a7b09b856afabe622c8fbf

--- libraries/ui/main.lua
+++ libraries/ui/main.lua
@@ -17,13 +17,24 @@ if not PROGRESSSPINNER   then dofile("ProgressSpinner/ProgressSpinner.lua") end
 if not TOASTALERT        then dofile("ToastAlert/ToastAlert.lua")         end
 if not DIALOGBOX         then dofile("DialogBox/DialogBox.lua")           end
 
-add_verbosity("STYLE_SUBSCRIPTIONS")
-add_verbosity("DEBUG")
+--add_verbosity("STYLE_SUBSCRIPTIONS")
+--add_verbosity("DEBUG")
+add_verbosity("TABBAR")
+add_verbosity("ArrayManager")
 
 
-dofile("NineSlice/test.lua")
+dofile("MenuButton/test.lua")
 screen:show()
 controllers:start_pointer()
+
+r = Rectangle{w=10,h=10,y = 1070}
+screen:add(r)
+
+r:animate{
+    loop = true,
+    duration = 60000,
+    w = screen.w
+}
 --[=[
 g = Group{name="Layer1"}
 
