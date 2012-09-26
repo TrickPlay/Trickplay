abfc77ffc5c1ab9c6c582b875d998970f44cd6c9
Alex Indaco <alex.indaco@trickplay.com>
2012-09-26 15:34:32 -0700

    [widget_library] refactored TextInput
    Former-commit-id: 96dca91f93dc762a68bcc8d3873440457dda1806
    Former-commit-id: 795c0c01f6881c053aef09457756ec7f6e09163d

--- libraries/ui/TextInput/test.lua
+++ libraries/ui/TextInput/test.lua
@@ -7,6 +7,9 @@ if not COLORSCHEME       then dofile("__CORE/ColorScheme.lua")            end
 if not STYLE             then dofile("__CORE/Style.lua")                  end
 if not WIDGET            then dofile("__CORE/Widget.lua")                 end
 if not TEXTINPUT         then dofile("TextInput/TextInput.lua")           end
+if not LISTMANAGER       then dofile("__UTILITIES/ListManagement.lua")    end
+if not LAYOUTMANAGER     then dofile("LayoutManager/LayoutManager.lua")   end
+if not NINESLICE         then dofile("NineSlice/NineSlice.lua")           end
 
 
 
@@ -15,15 +18,16 @@ text = "This is a sample message. This is a sample message. This is a sample mes
 screen:show()
 
 
-t1 = TextInput{style = false}
-
-t1.style.fill_colors.default = "660000"
-
-t2 = TextInput{style = false,h=400,x = 200,text = "default"}
+t1 = TextInput{style = false,}
+--[[
+print("t2 t2 t2 t2 t2 t2 t2 t2 t2 t2 t2 ")
+t2 = TextInput{style = false,h=400,w=200,x = 200,text = "default"}
 
 t2.style.text.colors.default = "00d000"
 --t2.style.text.single_line = false
-
+--]]
+print("changing fill colors")
+--t1.style.fill_colors.default = "660000"
 screen:add(t1,t2)
 
 
