c448670fa94237b285af20b1a8ed89504a5e3a51
Craig R. Hughes <craig.git@rungie.com>
2012-10-05 10:39:47 -0700

    [engine] Remove debug prints

--- engine/source/toast.cpp
+++ engine/source/toast.cpp
@@ -106,10 +106,8 @@ private:
 
     static gboolean captured_event( ClutterActor * actor , ClutterEvent * event ,  ToastUpAction * me )
     {
-        printf("1\n");
         if ( event && event->any.type == CLUTTER_KEY_PRESS && event->key.keyval == TOAST_ACTION_KEY )
         {
-            printf("2\n");
             return me->selected() ? TRUE : FALSE;
         }
 
