0f2a8489259190dfde648c2b33f09af27c2658b4
Alex Indaco <alex.indaco@trickplay.com>
2012-10-16 19:52:15 -0700

    [liberty] main menu & store menu

---
+++ apps/liberty/FourMoviesIcon.lua
@@ -0,0 +1,71 @@
+
+local collapsed_spacing = 20
+
+local ws = {
+    160,
+    140,
+    120,
+    100,
+}
+local hs = {
+    210,
+    190,
+    170,
+    150,
+}
+
+local tot_w = 0
+for i,w in ipairs(ws) do tot_w = tot_w + w end
+local collapsed_w = ws[1] + collapsed_spacing*(#ws-1)
+print("collapsed_w",collapsed_w)
+
+
+return function(items)
+    local instance = Group{w = tot_w}
+    if items == nil then
+        items = {}
+        for i = 1,#ws do
+            items[i] = Rectangle{
+                w = ws[i],
+                h = hs[i],
+                y = hs[1] - hs[i],
+                color = {rand(),rand(),rand(),}
+            }
+        end
+    end
+    
+    local intervals = {}
+    
+    for i,c in ipairs(items) do
+        instance:add(c) 
+        c:lower_to_bottom() 
+        intervals[i] = Interval(
+            intervals[i-1] and collapsed_spacing +(ws[i-1] - ws[i])+intervals[i-1].from or (tot_w - collapsed_w)/2,
+            ws[i-1] and ws[i-1]+intervals[i-1].to or 0
+        )
+    end
+    for i,c in ipairs(instance.children) do 
+        c.x = intervals[#intervals-i+1].to
+    end
+    
+    local expand = Timeline{
+        duration = 200,
+        on_new_frame = function(tl,ms,p)
+            for i,c in ipairs(instance.children) do
+                c.x = intervals[#intervals - i+1]:get_value(p)
+            end
+        end
+    }
+    local collapse = Timeline{
+        duration = 200,
+        on_new_frame = function(tl,ms,p)
+            for i,c in ipairs(instance.children) do
+                c.x = intervals[#intervals - i+1]:get_value(1-p)
+            end
+        end
+    }
+    instance.on_key_focus_in  = function(self) expand:start()   end
+    instance.on_key_focus_out = function(self) collapse:start() end
+    
+    return instance
+end
