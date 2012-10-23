3a89656b0e2552111eea67f524480460016e5dfe
Craig R. Hughes <craig.git@rungie.com>
2012-10-23 15:48:12 -0700

    [engine] Trigger Lua garbage collection before every clutter redraw

--- engine/source/app.cpp
+++ engine/source/app.cpp
@@ -864,6 +864,14 @@ int App::lua_panic_handler( lua_State * L )
     throw LUA_ERRRUN;
 }
 
+
+static gboolean lua_gc_every_frame( gpointer state )
+{
+    lua_State *L = (lua_State *)state;
+    lua_gc( L, LUA_GCCOLLECT, 0);
+    return true;
+}
+
 //-----------------------------------------------------------------------------
 
 App::App( TPContext * c, const App::Metadata & md, const String & dp, const LaunchInfo & _launch )
@@ -878,7 +886,8 @@ App::App( TPContext * c, const App::Metadata & md, const String & dp, const Laun
     cookie_jar( NULL ),
     screen( NULL ),
     launch( _launch ),
-    stage_allocation_handler( 0 )
+    stage_allocation_handler( 0 ),
+    lua_gc_func ( 0 )
 
 #ifndef TP_PRODUCTION
 
@@ -1240,6 +1249,11 @@ void App::run_part2( const StringSet & allowed_names , RunCallback run_callback 
     LuaAPIPlugin::call_open( L );
 
     //.........................................................................
+    // Collect garbage every frame
+
+    lua_gc_func = clutter_threads_add_repaint_func( lua_gc_every_frame, L, 0 );
+
+    //.........................................................................
     // Run the script
 
     FreeLater free_later;
@@ -1317,6 +1331,10 @@ App::~App()
     // Close plugins
 
     LuaAPIPlugin::call_close( L );
+
+    // Remove GC callback per frame
+
+    clutter_threads_remove_repaint_func( lua_gc_func );
 
     // Close Lua
 
