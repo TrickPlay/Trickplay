dc7176707e0422bf322f1eb256878d1bae7bcc52
Craig R. Hughes <craig.git@rungie.com>
2012-10-30 14:45:34 -0700

    [stitcher] Adding header files; refactor half way to get things to compile and run
    Former-commit-id: 4fa9ab1ed16d26051653e6ce87345c5006bb6b88
    Former-commit-id: 326e7c76d0182e12448a69190c9572b3bda08fb6

---
+++ tools/stitcher/item.h
@@ -0,0 +1,25 @@
+#ifndef __ITEM_H__
+#define __ITEM_H__
+
+#include <gio/gio.h>
+#include <glib.h>
+#include <magick/MagickCore.h>
+
+#include "main.h"
+
+typedef struct Item {
+  int x, y, w, h, area;
+  char  * id,
+        * path;
+  GFile * file;
+  Image * source;
+  gboolean placed;
+} Item;
+
+
+Item * item_new ( char * id );
+void item_set_source( Item * item, Image * source, gboolean add_buffer_pixels, Page *minimum, Page *smallest, int *output_size_step );
+void item_add_to_items ( Item * item, GSequence *items, int input_size_limit, int output_size_limit, gboolean copy_large_images, GPtrArray  * large_images, gboolean allow_multiple_sheets );
+void item_load ( GFile * file, GFile * base, char * base_path, GPtrArray * input_patterns, gboolean recursive, gboolean add_buffer_pixels, Page *minimum, Page *smallest, int *output_size_step, GSequence *items, int input_size_limit, int output_size_limit, gboolean copy_large_images, GPtrArray  * large_images, gboolean allow_multiple_sheets );
+
+#endif
--- tools/stitcher/leaf.c
+++ tools/stitcher/leaf.c
@@ -1,12 +1,15 @@
+#include "leaf.h"
+#include <stdlib.h>
+
 /*
 GSequence * get_sequence ( GHashTable * table, int key )
 {
     gpointer ptr = GINT_TO_POINTER( key + 1 );
-    
+
     GSequence * seq = g_hash_table_lookup( table, ptr );
     if ( seq == NULL )
         g_hash_table_insert( table, ptr, ( seq = g_sequence_new( NULL ) ) );
-    
+
     return seq;
 }
 */
@@ -17,7 +20,7 @@ void g_sequence_remove_sorted ( GSequence * seq, gpointer data, GCompareDataFunc
     GSequenceIter * sj, * si = g_sequence_lookup( seq, data, cmp_func, cmp_data );
     if ( si == NULL )
         return;
-    
+
     sj = si;
     while ( !g_sequence_iter_is_end( sj ) )
     {
@@ -28,7 +31,7 @@ void g_sequence_remove_sorted ( GSequence * seq, gpointer data, GCompareDataFunc
             break;
         sj = g_sequence_iter_next( sj );
     }
-    
+
     sj = si;
     while ( !g_sequence_iter_is_begin( sj ) )
     {
@@ -41,20 +44,12 @@ void g_sequence_remove_sorted ( GSequence * seq, gpointer data, GCompareDataFunc
     }
 }
 
-#define AREA GINT_TO_POINTER(1)
-#define WIDTH GINT_TO_POINTER(2)
-#define HEIGHT GINT_TO_POINTER(3)
-
-typedef struct Leaf {
-    int x, y, w, h, area;
-} Leaf;
-
 int leaf_compare ( gconstpointer a, gconstpointer b, gpointer user_data )
 {
     Leaf * aa = (Leaf *) a, * bb = (Leaf *) b;
-    return user_data == AREA   ? aa->area - bb->area :
-           user_data == WIDTH  ? aa->w    - bb->w    :
-           user_data == HEIGHT ? aa->h    - bb->h    : 0;
+    return user_data == LEAF_AREA_COMPARE   ? aa->area - bb->area :
+           user_data == LEAF_WIDTH_COMPARE  ? aa->w    - bb->w    :
+           user_data == LEAF_HEIGHT_COMPARE ? aa->h    - bb->h    : 0;
 }
 
 Leaf * leaf_new ( int x, int y, int w, int h )
@@ -65,19 +60,18 @@ Leaf * leaf_new ( int x, int y, int w, int h )
     leaf->w = w;
     leaf->h = h;
     leaf->area = w * h;
-    
-    g_sequence_insert_sorted( leaves_sorted_by_area, leaf, leaf_compare, AREA );
+
     return leaf;
 }
 
-void leaf_cut ( Leaf * leaf, int w, int h )
+void leaf_cut ( Leaf * leaf, int w, int h, GSequence *leaves_sorted_by_area, const Page *smallest )
 {
     gboolean b = leaf->w - w > leaf->h - h;
-    if ( leaf->w - w > smallest.width )
+    if ( leaf->w - w > smallest->width )
         leaf_new( leaf->x + w, leaf->y, leaf->w - w, b ? leaf->h : h );
-    if ( leaf->h - h > smallest.height )
+    if ( leaf->h - h > smallest->height )
         leaf_new( leaf->x, leaf->y + h, b ? w : leaf->w, leaf->h - h );
-    
-    g_sequence_remove_sorted( leaves_sorted_by_area, leaf, leaf_compare, AREA );
+
+    g_sequence_remove_sorted( leaves_sorted_by_area, leaf, leaf_compare, LEAF_AREA_COMPARE );
     free( leaf );
 }
