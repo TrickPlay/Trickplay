ef4e8ffa7f40942958f0877c51d3497e9a20ca07
Alex Indaco <alex.indaco@trickplay.com>
2012-11-06 16:01:49 -0800

    [widget_library] fixed bug where load_layer couldn't load LayoutManagers with placeholders
    Former-commit-id: c9bbccd5b370662c6685ec996179cbfe2958c9b1
    Former-commit-id: 08eab990c9f36229b73b955fcdb42630b21d77e5

--- libraries/ui/load_json.lua
+++ libraries/ui/load_json.lua
@@ -64,7 +64,6 @@ screen_json = screen_json..']'
 
 ]=]
 --------------------------------------------------------------------------------
-
 local color_scheme_uri = 'app/color_schemes.json'
 local style_uri        = 'app/styles.json'
 local layer_dir        = 'app/'
@@ -72,43 +71,42 @@ local layer_dir        = 'app/'
 local color_schemes
 
 local function load_color_schemes()
-    
+
     local input = color_scheme_json--readfile(color_scheme_uri)
-    
+
     if input == nil then
-        
+
         print("'"..color_scheme_uri.."' does not exist, Default colors will be used")
-        
+
         color_schemes = {}
-        
+
         return
-        
+
     end
-    
+
     input = json:parse(input)
-    
+
     if input == nil then
-        
-        error("'"..color_scheme_uri.."' is invalid json",2)
-        
+
+        error("'"..color_scheme_uri.."' is invalid json",3)
+
     end
-    
+
     if type(input) ~= "table" then
-        
+
         error("JSON in '"..color_scheme_uri.."' is not formatted as expected.",2)
-        
+
     end
-    
+
     color_schemes = {}
-    
+
     for k,v in pairs(input) do
-        
-		v.name = k
-		
+
+        v.name = k
         color_schemes[k] = ColorScheme(v)
-        
+
     end
-    
+
 end
 
 --------------------------------------------------------------------------------
@@ -116,294 +114,170 @@ end
 local styles
 
 function load_styles(str)
-    
-        
+
+
     if type(styles) == "table" then
-        
+
         print("WARNING. Styles table already exists")
-        
+
     end
-    
+
     if type(str) ~= "string" then
-        
+
         error("Expected string. Received "..type(str),2)
-        
+
     end
-    
+
     styles = json:parse(str)
-    
+
     if type(styles) ~= "table" then
-        
+
         error("String is not valid json",2)
-        
+
     end
-    
+
     for name,attributes in pairs(styles) do
-        
-        styles[name] = Style(name):set(attributes)
-        
+
+        styles[name] = WL.Style(name):set(attributes)
+
     end
-    
+
     return styles
-        
+
 end
 
---[[
-local function load_styles()
-    
-    if not color_schemes then load_color_schemes() end
-    
-    local input = styles_json--readfile(style_uri)
-    
-    if input == nil then
-        
-        print("'"..style_uri.."' does not exist, Default styles and colors will be used")
-        
-        styles = {}
-        
-        return
-        
-    end
-    
-    input = json:parse(input)
-    
-    if input == nil then
-        
-        error("'"..style_uri.."' is invalid json",2)
-        
-    end
-    
-    if type(input) ~= "table" or not (input.arrow and input.border and input.text and input.style) then
-        
-        error("JSON in '"..style_uri.."' is not formatted as expected.",2)
-        
-    end
-    
-    styles = {}
-    
-    local arrow_styles, text_styles, border_styles = {},{},{}
-    
-    for k,v in pairs(input.arrow)  do
-        --v.colors = type(v.colors) == "string" and color_schemes[v.colors] or v.colors
-        v.name = k
-		arrow_styles[k]  = ArrowStyle(v)
-    end
-    for k,v in pairs(input.border) do
-        --v.colors = type(v.colors) == "string" and color_schemes[v.colors] or v.colors
-        v.name = k
-		border_styles[k] = BorderStyle(v)
-    end
-    for k,v in pairs(input.text)   do
-        --v.colors = type(v.colors) == "string" and color_schemes[v.colors] or v.colors
-        v.name = k
-		text_styles[k]   = TextStyle(v)
-    end
-    
-    for k,v in pairs(input.style) do
-        
-        --v.arrow       = type(v.arrow)       == "string" and arrow_styles[v.arrow]        or v.arrow
-        --v.border      = type(v.border)      == "string" and border_styles[v.border]      or v.border
-        --v.text        = type(v.text)        == "string" and text_styles[v.text]          or v.text
-        --v.fill_colors = type(v.fill_colors) == "string" and color_schemes[v.fill_colors] or v.fill_colors
-        v.name = k
-        styles[k] = Style(v)
-        
-    end
-    
-end
-]]
-
---------------------------------------------------------------------------------
 
 local names
 local neigbor_info
 local curr_neighbors
 local obj
 
-local construct
-construct = function(t)
-    
-    if type(t) ~= "table" then
-        
-        return error("Expects table, received "..type(t),2)
-        
+local concat_elements = function(t1,t2)
+    if t2 == nil then return t1 end
+    local t3 = {}
+    for k,v in pairs(t2) do
+        t3[k]=v
     end
-    
+    --t1 has precendence over t2
+    for k,v in pairs(t1) do
+        t3[k]=v
+    end
+    return t3
+end
+
+
+local obj_to_elements_map = {}
+local construct
+
+
+construct = function(t)
+
+    if type(t) ~= "table" then
+
+        return error("Expects table, received "..type(t),2)
+
+    end
+
+    local elements
     if t.type == "LayoutManager" then
+        elements = {}
         for i,row in ipairs(t.cells) do
             for j,v in ipairs(row) do
-                
-                t.cells[i][j] = construct(v)
-                
+                if v then -- v == false when no item was specified
+                    t.cells[i][j] = construct(v)
+    
+                    elements[t.cells[i][j].name] = t.cells[i][j]
+                    elements = concat_elements(elements,obj_to_elements_map[t.cells[i][j]])
+                end
             end
         end
     elseif t.type == "ListManager" then
+        elements = {}
         for i,v in ipairs(t.cells) do
-            
-            t.cells[i] = construct(v)
-            
+
+            if v then -- v == false when no item was specified
+                t.cells[i] = construct(v)
+    
+                elements[t.cells[i].name] = t.cells[i]
+                elements = concat_elements(elements,obj_to_elements_map[t.cells[i]])
+            end
         end
     elseif t.children then
+        elements = {}
         for i,v in ipairs(t.children) do
-            
+
+
             t.children[i] = construct(v)
-            
+            print ( t.children[i] )
+            elements[t.children[i].name] = t.children[i]
+            elements = concat_elements(elements,obj_to_elements_map[t.children[i]])
+
         end
     end
     curr_neighbors = t.neighbors
-    obj = _G[t.type] and _G[t.type](t) or
-        
+    --obj = WL[t.type] and WL[t.type](t) or
+    obj = _G[t.type] and _G[t.type](t) or WL[t.type] and WL[t.type](t) or
+
         error("Received invalid type: "..t.type)
-    
+
     names[obj.name] = obj
     neigbor_info[obj] = curr_neighbors
-    
+    obj_to_elements_map[obj] = elements
     return obj
-    
+
 end
+
+
 function load_layer(str)
-    
+
     names = {}
     neigbor_info = {}
-    
+
       --load_styles should be called before load_layer
     if type(styles) ~= "table" then
-        
+
         print("WARNING. Styles table is empty")
-        
+
     end
-    
+
     --load_layer expects to receive a json string
     if type(str) ~= "string" then
-        
+
         error("Expected string. Received "..type(str),2)
-        
+
     end
-    
+
     --parse the json
     local layer = json:parse(str)
-    
+
     --load_layer expects valid json
     if type(layer) ~= "table" then
-        
+
         error("String is not valid json",2)
-        
+
     end
-    
-    --the setter for Widget_Group.children calls the appropriate 
+
+    --the setter for Widget_Group.children calls the appropriate
     --constructors when it receives an attributes table as an entry
     layer = construct(layer)
-    
-    for obj,neighbors in pairs(neigbor_info) do
-        
-        for k,v in pairs(neighbors) do
-            
-            obj.neighbors[k] = names[v]
-            
+    --layer.elements = names
+    --layer.elements = {}
+
+    for i,v in ipairs(layer.children) do
+        if string.find(v.name, "Layer") then
+            v.elements = obj_to_elements_map[v]
+            --layer.elements[ string.gsub( v.name, " ", "_" ) ] = v
         end
-        
     end
-    
+
+    for obj,neighbors in pairs(neigbor_info) do
+
+        for k,v in pairs(neighbors) do
+
+            obj.neighbors[k] = names[v]
+
+        end
+    end
+
     return layer
 end
-
---[[
-construct = function(t)
-    
-    if type(t) ~= "table" then
-        
-        return error("Expects table, received "..type(t))
-        
-    end
-    
-    if t.children then
-        
-        for i,v in ipairs(t.children) do
-            
-            t.children[i] = construct(t)
-            
-        end
-        
-    end
-    
-    return constructors[t.type] and constructors[t.type](t) or
-        
-        error("Received invalid type "..t.type)
-    
-end
-
---------------------------------------------------------------------------------
-
-load_layer = function(layer_name)
-    
-    if type(layer_name) ~= "string" then
-        
-        error("'load_layer()' expects type 'string'. Recieved "..type(layer_name),2)
-        
-    end
-    
-    --the first time this function is called, styles will get set up
-    if not styles then load_styles() end
-    
-    --load the json
-    local layer = readfile(layer_name)
-    
-    if layer == nil then
-        
-        error("Layer '"..layer_name.."' does not exist.",2)
-        
-    end
-    
-    --parse it
-    layer = json:parse(layer)
-    
-    if layer == nil then
-        
-        error("Layer '"..layer_name.."' has invalid json",2)
-        
-    end
-    
-    if type(layer) ~= "table" then
-        
-        error("Layer '"..layer_name.."' contained expected json. Top-Level is not an array.",2)
-        
-    end
-    
-    --make it a group
-    local layer_object = Group()
-    
-    layer_object.elements = {}
-    
-    for i,v in ipairs(layer) do
-        print(v.name,v.style)
-        v.style = type(v.style) == "string" and styles[v.style] or v.style
-        print(v.name,v.style.name)
-        local current_object = construct(v)
-        
-        if type(current_object) ~= "userdata" and not current_object.__types__.actor then
-            
-            error(i.."-th entry in the '"..layer_name.."' layer is not a UIElement",2)
-            
-        end
-        
-        if type(current_object.name) ~= "string" then
-            
-            error(i.."-th entry in the '"..layer_name.."' layer does not have a name",2)
-            
-        end
-        
-        layer_object.elements[ string.gsub( current_object.name, " ", "_" ) ] = current_object
-        print(current_object.name,current_object.style.name)
-        layer_object:add(  current_object  )
-        
-    end
-    
-    return layer_object
-    
-end
-]]
-
---l = load_layer("fake")
-
---screen:add(l)
---]]
