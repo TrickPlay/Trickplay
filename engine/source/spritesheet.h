549752deb1f01cef969c2ddae07c707563241132
Josh Steelman <joshua.steelman@trickplay.com>
2012-11-13 17:21:30 -0800

    [spritesheet] spritesheet.cpp now holds structure and loading code. Need to figure out how to do load-as-needed

--- engine/source/spritesheet.h
+++ engine/source/spritesheet.h
@@ -3,11 +3,39 @@
 
 #include <clutter/clutter.h>
 #include <stdlib.h>
+#include "app_resource.h"
 #include "util.h"
+
+#ifdef CLUTTER_VERSION_1_10
+#define TP_COGL_TEXTURE(t) (COGL_TEXTURE(t))
+#define TP_CoglTexture CoglTexture *
+#else
+#define TP_COGL_TEXTURE(t) (t)
+#define TP_CoglTexture CoglHandle
+#endif
 
 class SpriteSheet : public RefCounted
 {
 public:
+    
+    class Source
+    {
+    public:
+        Source( SpriteSheet * sheet ) : sheet( sheet ), texture( NULL ), refs( 0 ) {};
+        ~Source();
+        
+        void load_image( Image * image );
+        TP_CoglTexture get_texture();
+        void ref_inc() { refs += 1; g_message( "Ref'ed, now %i", refs ); }
+        void ref_dec();
+        
+        SpriteSheet * sheet;
+        
+    private:
+        TP_CoglTexture texture;
+        int refs;
+    };
+    
   
     static bool class_initialized;
     inline static void unref( SpriteSheet * sheet ) { RefCounted::unref( sheet ); }
@@ -15,10 +43,10 @@ public:
     SpriteSheet();
     ~SpriteSheet();
 
-    int add_texture( CoglHandle texture );
-    bool is_initialized();
+    Source * add_source();
+    //bool is_initialized();
 
-    bool map_subtexture( const char * id, int tex, int x, int y, int w, int h );
+    void map_subtexture( const char * id, int x, int y, int w, int h );
     CoglHandle get_subtexture( const char * id );
     GList * get_ids();
     void dump();
@@ -27,10 +55,10 @@ public:
     GObject * extra;
 
 private:
-    GHashTable * map;
-    GPtrArray  * textures;
+    GHashTable * sprites;
+    GPtrArray  * sources;
 
-    inline void check_initialized() { if ( ! g_ptr_array_index( this->textures, 0 ) ) g_error( "SpriteSheet not initialized" ); }
+    //inline void check_initialized() { if ( ! g_ptr_array_index( this->textures, 0 ) ) g_error( "SpriteSheet not initialized" ); }
 };
 
 #endif
