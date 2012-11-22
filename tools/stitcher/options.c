8dbba93683b67d3071743c936f9750e4b676a82c
Josh Steelman <joshua.steelman@trickplay.com>
2012-11-21 18:27:48 -0800

    [stitcher] now using glib's option parsing. Not sure what's up with -B.

--- tools/stitcher/options.c
+++ tools/stitcher/options.c
@@ -57,110 +57,77 @@ gboolean options_take_unique_id ( Options * options, const char * id )
     return TRUE;
 }
 
-enum {
-    INPUT_PATHS,
-    SET_FORGET = 'f',
-    SET_INPUT  = 'i',
-    SET_JSON_MERGE = 'j',
-    SET_OUTPUT = 'o',
-    NO_ARGS,
-};
+gboolean opt_forget( const gchar * option_name, const gchar * value, gpointer * data, GError ** error )
+{
+    options_take_unique_id( (Options *) data, strdup( value ) );
+    return TRUE;
+}
+
+gboolean opt_filter( const gchar * option_name, const gchar * value, gpointer * data, GError ** error )
+{
+    g_ptr_array_add( ((Options *) data)->input_patterns, g_pattern_spec_new( strdup( value ) ) );
+    return TRUE;
+}
+
+gboolean opt_json( const gchar * option_name, const gchar * value, gpointer * data, GError ** error )
+{
+    g_ptr_array_add( ((Options *) data)->json_to_merge, strdup( (char *) value ) );
+    return TRUE;
+}
+
+gboolean opt_input( const gchar * option_name, const gchar * value, gpointer * data, GError ** error )
+{
+    g_ptr_array_add( ((Options *) data)->input_paths, strdup( (char *) value ) );
+    return TRUE;
+}
 
 Options * options_new_from_arguments ( int argc, char ** argv )
 {
     Options * options = options_new();
+    
+    GOptionContext * context = g_option_context_new( "- stitch together many source sprites into a single spritesheet");
+    g_option_context_set_summary( context, "stitcher will accept a list of directories and/or images, and things that might be convertible to images (ie. SVG). Experiment to see what input formats work for your case." );
 
-    int state = INPUT_PATHS;
-
-    for ( int i = 1; i < argc; i++ )
+    GOptionEntry entries[] =
     {
-        char * arg = argv[i];
-        size_t l = strlen( arg );
-        if ( (char) arg[0] == '-' )
-            for ( size_t j = 1; j < l; j++ )
-                switch( (char) arg[j] )
-                {
-                    case 'f':
-                    case 'i':
-                    case 'j':
-                    case 'o':
-                        state = arg[j];
-                        break;
-
-                    case 'b':
-                        options->add_buffer_pixels = TRUE;
-                        state = NO_ARGS;
-                        break;
-
-                    case 'c':
-                        options->copy_large_items = TRUE;
-                        state = NO_ARGS;
-                        break;
-
-                    case 'm':
-                        options->allow_multiple_sheets = TRUE;
-                        state = NO_ARGS;
-                        break;
-
-                    case 'r':
-                        options->recursive = TRUE;
-                        state = NO_ARGS;
-                        break;
-
-                    case 'h':
-                        system( g_strdup_printf( "man -l %s.man", argv[0] ) );
-                        exit(0);
-
-                    default:
-                        fprintf( stderr, "Unknown flag '-%s'.\n", (char *) &arg[j] );
-                        break;
-                }
-        else
-        {
-            gboolean arg_is_int = TRUE;
-            for ( size_t j = 0; j < l; j++ )
-                if ( !g_ascii_isdigit( arg[j] ) )
-                    arg_is_int = FALSE;
-
-            switch( state )
-            {
-                case SET_FORGET:
-                    options_take_unique_id( options, arg );
-                    break;
-
-                case SET_INPUT:
-                    if ( arg_is_int )
-                        options->input_size_limit = strtoul( arg, NULL, 0 );
-                    else
-                        g_ptr_array_add( options->input_patterns, g_pattern_spec_new( arg ) );
-                    break;
-
-                case SET_OUTPUT:
-                    if ( arg_is_int )
-                        options->output_size_limit = strtoul( arg, NULL, 0 );
-                    else
-                        options->output_path = arg;
-                    break;
-
-                case SET_JSON_MERGE:
-                    g_ptr_array_add( options->json_to_merge, arg );
-                    break;
-
-                case INPUT_PATHS:
-                {
-                    char * lc = &arg[ strlen( arg ) - 1 ];
-                    if ( * lc == '/' )
-                         * lc = '\0';
-                    g_ptr_array_add( options->input_paths, arg );
-                    break;
-                }
-
-                default:
-                    fprintf( stderr, "Ambiguous argument %s.\n", arg );
-                    break;
-            }
-        }
+        { G_OPTION_REMAINING, 0, G_OPTION_FLAG_FILENAME,    G_OPTION_ARG_CALLBACK, 
+            & opt_input,                         NULL, "PATH ..." },
+        { "no-buffer-pixels", 'B', G_OPTION_FLAG_REVERSE,     G_OPTION_ARG_NONE, 
+            & options->add_buffer_pixels,        "Do not place buffer pixels around sprite edges", NULL },
+        { "no-copy",        'C', G_OPTION_FLAG_REVERSE,     G_OPTION_ARG_NONE, 
+            & options->copy_large_items,         "Do not copy over files that fail the input size filter as stand-alone images", NULL },
+        { "forget",         'f', 0,                         G_OPTION_ARG_CALLBACK,
+            & opt_forget,                        "Name of a sprite to skip or forget in output", "ID" },
+        { "input-filter",   'i', 0,                         G_OPTION_ARG_CALLBACK, 
+            & opt_filter,                        "Inclusive wildcard (?, *) filter for files within input directories (default: *)", "FILTER" },
+        { "merge-json",     'j', G_OPTION_FLAG_FILENAME,    G_OPTION_ARG_CALLBACK,
+            & opt_json,                          "Path to the JSON file of a spritesheet to merge into this one", "PATH" },
+        { "no-multiple",    'M', G_OPTION_FLAG_REVERSE,     G_OPTION_ARG_NONE, 
+            & options->allow_multiple_sheets,    "Do not allow the tool to output multiple images", NULL },
+        { "output-path",    'o', 0,                         G_OPTION_ARG_STRING, 
+            & options->output_path,              "Output path for the files *.json and *.png (default: first input path)", "PATH" },
+        { "no-recursive",   'R', G_OPTION_FLAG_REVERSE,     G_OPTION_ARG_NONE, 
+            & options->recursive,                "Do not recursively enter subdirectories", NULL },
+        { "input-size",     's', 0,                         G_OPTION_ARG_INT, 
+            & options->input_size_limit,         "Limit on the maximum size of input images (default 512)", "INT" },
+        { "output-size",    'S', 0,                         G_OPTION_ARG_INT, 
+            & options->output_size_limit,        "Limit on the maximum size of output images (default: 4096)", "INT" },
+        { NULL }
+    };
+    
+    g_message( "buffer %i", options->add_buffer_pixels );
+    
+    GOptionGroup * group = g_option_group_new( "all", NULL, NULL, options, NULL );
+    g_option_group_add_entries( group, entries );
+    g_option_context_set_main_group( context, group );
+    
+    if ( !g_option_context_parse( context, &argc, &argv, NULL ) )
+    {
+        fprintf( stderr, "Could not parse arguments.\n" ); // this doesn't seem to be working right
+        exit( 1 );
     }
+    
+    g_option_context_free( context );
 
     if ( options->input_paths->len + options->json_to_merge->len == 0 )
         fprintf( stderr, "No inputs given.\n" );
@@ -168,11 +135,16 @@ Options * options_new_from_arguments ( int argc, char ** argv )
     if ( options->output_path == NULL )
     {
         if ( options->input_paths->len > 0 )
+        {
             options->output_path = g_ptr_array_index( options->input_paths, 0 );
         //else if ( options->json_to_merge->len > 0 )
         //    options->output_path = g_ptr_array_index( options->json_to_merge, 0 );
+        }
         else
+        {
             fprintf( stderr, "Ambiguous output path.\n" );
+            exit( 1 );
+        }
     }
 
     return options;
