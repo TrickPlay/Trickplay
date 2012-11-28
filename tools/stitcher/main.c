98417a2c71ab311184a43599cd97b3f5e89775dc
Josh Steelman <joshua.steelman@trickplay.com>
2012-11-28 11:23:24 -0800

    [stitcher] more logical structure in main.c
    Former-commit-id: daf55f87eaa6830092ce8d35dc9f398c3233ecef
    Former-commit-id: 5c9417cc8e6c5db67950eb544226d5b441c1c52c

--- tools/stitcher/main.c
+++ tools/stitcher/main.c
@@ -5,17 +5,16 @@
 #include "output.h"
 #include "layout.h"
 
-Layout * place_attempt(Output *output, Options *options)
+Layout * layout_attempt( Output * output, Options * options )
 {
     Layout * best = layout_new( 0 );
-
-    unsigned int i;
-    for ( i = 0; i <= options->output_size_limit; i++ )
+    
+    for ( unsigned i = 0; i <= options->output_size_limit; i++ )
     {
         Layout * layout = layout_new_from_output( output, i, options );
         Layout * better = layout_choose( layout, best, options );
 
-        if(layout == better)
+        if ( layout == better )
         {
             layout_free( best );
             best = layout;
@@ -25,22 +24,7 @@ Layout * place_attempt(Output *output, Options *options)
             layout_free( layout );
         }
     }
-
-    if ( !options->allow_multiple_sheets && (!best || best->items_skipped != 0) )
-    {
-        fprintf( stderr, "Can't fit all files within output dimensions (%i x %i).\n",
-                 options->output_size_limit, options->output_size_limit );
-        exit( 1 );
-    }
-    else if ( options->allow_multiple_sheets && best->items_placed == 0 )
-    {
-        fprintf( stderr, "Failed to fit all of the images.\n" );
-        exit( 1 );
-    }
-
-
-    output_add_layout( output, best, options );
-
+    
     return best;
 }
 
@@ -56,16 +40,36 @@ int main ( int argc, char ** argv )
 
     if ( options->allow_multiple_sheets )
     {
-        // fit sprites into sheets in the densest way possible, until we run out of sprites
-
-        while ( place_attempt(output, options)->items_skipped )
+        while ( TRUE )
         {
-            continue;
+            Layout * best = layout_attempt( output, options );
+            
+            if ( best->items_placed == 0 )
+            {
+                fprintf( stderr, "Failed to fit all of the images.\n" );
+                exit( 1 );
+            }
+            
+            output_add_layout( output, best, options );
+            
+            if ( !best->items_skipped )
+            {
+                break;
+            }
         }
     }
     else
     {
-        place_attempt(output,options);
+        Layout * best = layout_attempt( output, options );
+        
+        if ( !best->items_placed || best->items_skipped )
+        {
+            fprintf( stderr, "Can't fit all files within output dimensions (%i x %i).\n",
+                     options->output_size_limit, options->output_size_limit );
+            exit( 1 );
+        }
+        
+        output_add_layout( output, best, options );
     }
 
     output_export_files( output, options );
