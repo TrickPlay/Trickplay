a9f03405302e3b25b5136c3e6b9b8297d6ed1d1f
Alex Indaco <alex.indaco@trickplay.com>
2012-11-30 11:10:16 -0800

    [widget_library] modified tests to remove fake styles
    Former-commit-id: d73ccba02a2d58c994acc38edf8ba5020b064a07
    Former-commit-id: 73b223f69ae40310292a10909ad993649c55ea2d

--- libraries/ui/ButtonPicker/test.lua
+++ libraries/ui/ButtonPicker/test.lua
@@ -6,8 +6,8 @@ bp1 = WL.ButtonPicker{items={"one","two","333","for"}}
 print("fjfhjfjfjfjfjfjfjf")
 bp1.items = {"one","two"}
 bp1.orientation ="vertical"
---[[
-bp2 = WL.ButtonPicker{style = false, x = 400,window_h = 200,orientation="vertical",items={"item1","item2","ite","itdddem2"}}
+---[[
+bp2 = WL.ButtonPicker{style = "new style", x = 400,window_h = 200,orientation="vertical",items={"item1","item2","ite","itdddem2"}}
 
 bp2.style.arrow.colors.default = "009999"
 bp2.style.arrow.size = 40
