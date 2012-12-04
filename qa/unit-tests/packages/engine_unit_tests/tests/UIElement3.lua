1c0387e46c2d98c32405597914589f5933fc5c1f
Craig R. Hughes <craig.git@rungie.com>
2012-12-04 11:25:26 -0800

    Partial Revert "[unit-tests] Remove actor:animate({loop=true}) and don't call foo_rotation multiple times."
    Only put looping in animate() back in; leave the foo_rotation stuff
    
    This reverts commit 44ec94080b14bb4672a877444d5363467d4f5294.

--- qa/unit-tests/packages/engine_unit_tests/tests/UIElement3.lua
+++ qa/unit-tests/packages/engine_unit_tests/tests/UIElement3.lua
@@ -18,7 +18,7 @@ image1.w = 100
 
 test_group:add(image1)
 
-image1:animate{duration=20000, x=400, y=400, h=400, w=400, opacity=160, x_rotation=90, y_rotation=90, z_rotation=90}
+image1:animate{duration=20000, loop=true, x=400, y=400, h=400, w=400, opacity=160, x_rotation=90, y_rotation=90, z_rotation=90}
 
 -- Tests --
 
