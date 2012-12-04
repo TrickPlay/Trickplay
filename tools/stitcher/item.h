6b73826d752cbf0bcf72892627848543bd9a5411
Josh Steelman <joshua.steelman@trickplay.com>
2012-12-03 18:06:25 -0800

    [stitcher] made item_new_from_source the allowed instantiation method.
    Former-commit-id: 4f73e7774d1afbb59c1c13f3004d50ffbf97d4b9
    Former-commit-id: 294f96712efe031d1710a6244375f4cb6ac9a84f

--- tools/stitcher/item.h
+++ tools/stitcher/item.h
@@ -15,9 +15,7 @@ typedef struct Item {
 
 Item * item_new ( const char * id );
 void item_free ( Item * item );
-void item_set_source( Item * item, Image * source, Options * options );
 Item * item_new_with_source( const char * id, Image * source, Options * options );
-//Item * item_new_from_file ( const char * id, const char * directory, Options * options );
 gint item_compare ( gconstpointer a, gconstpointer b, gpointer user_data __attribute__((unused)) );
 
 #endif
