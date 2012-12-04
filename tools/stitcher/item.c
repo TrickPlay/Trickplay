2100c34b5dd6caabab5cfc3f00504267ba8dae76
Josh Steelman <joshua.steelman@trickplay.com>
2012-12-03 18:06:25 -0800

    [stitcher] made item_new_from_source the allowed instantiation method.

--- tools/stitcher/item.c
+++ tools/stitcher/item.c
@@ -26,49 +26,19 @@ void item_free ( Item * item )
     free( item );
 }
 
-void item_set_source( Item * item, Image * source, Options * options )
+Item * item_new_with_source( const char * id, Image * source, Options * options )
 {
+    Item * item = item_new( id );
+    
     unsigned int bp = options->add_buffer_pixels ? 2 : 0;
     item->source = source;
     item->w = (unsigned int) source->columns + bp;
     item->h = (unsigned int) source->rows    + bp;
     item->area = item->w * item->h;
-}
-
-Item * item_new_with_source( const char * id, Image * source, Options * options )
-{
-    Item * item = item_new( id );
-    item_set_source( item, source, options );
+    
     return item;
 }
-/*
-Item * item_new_from_file ( const char * id, const char * directory, Options * options )
-{
-    Item * item = item_new( id );
 
-    const char * path = directory ? g_build_filename( directory, item->id, NULL ) : item->id;
-
-    ExceptionInfo * exception = AcquireExceptionInfo();
-    ImageInfo * input_info = AcquireImageInfo();
-    CopyMagickString( input_info->filename, path, MaxTextExtent );
-    Image * source = ReadImage( input_info, exception );
-
-    if ( exception->severity != UndefinedException )
-    {
-        item_free( item );
-        item = NULL;
-    }
-    else
-    {
-        item_set_source( item, source, options );
-    }
-
-    DestroyImageInfo( input_info );
-    DestroyExceptionInfo( exception );
-
-    return item;
-}
-*/
 gint item_compare ( gconstpointer a, gconstpointer b, gpointer user_data __attribute__((unused)) )
 {
     Item * aa = (Item *) a, * bb = (Item *) b;
