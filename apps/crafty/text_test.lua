f922e4563629e5062cc62af6605f626d7d79b5c3
Craig R. Hughes <craig.git@rungie.com>
2012-12-18 16:19:24 -0800

    [craftymind] Increase size to full-screen at full resolution

--- apps/crafty/text_test.lua
+++ apps/crafty/text_test.lua
@@ -32,11 +32,11 @@ local function makeBorder(text)
 end
 
 text_test.extra.start = function()
-    textblock1 = Text { wrap = true, color="black", position = {0,      0}, size = {200, 300}, text = concatStr(english, 4),  font = "Champignon 16px" }
-    textblock2 = Text { wrap = true, color="black", position = {0,    300}, size = {200, 300}, text = concatStr(english, 3),  font = "Starjedi 16px"   }
-    textblock3 = Text { wrap = true, color="black", position = {200,    0}, size = {800, 600}, text = concatStr(english, 10),  font = "FreeSerif 16px"  }
-    textblock4 = Text { wrap = true, color="black", position = {1000,   0}, size = {200, 300}, text = concatStr(russian, 3),  font = "FreeSerif 16px"  }
-    textblock5 = Text { wrap = true, color="black", position = {1000, 300}, size = {200, 300}, text = concatStr(japanese, 3), font = "Sazanami Mincho Regular 16px"  }
+    textblock1 = Text { wrap = true, color="black", position = {0,      0}, size = {320,  480}, text = concatStr(english, 4),  font = "Champignon 26px" }
+    textblock2 = Text { wrap = true, color="black", position = {0,    480}, size = {320,  480}, text = concatStr(english, 3),  font = "Starjedi 26px"   }
+    textblock3 = Text { wrap = true, color="black", position = {320,    0}, size = {1280, 960}, text = concatStr(english, 10),  font = "FreeSerif 26px"  }
+    textblock4 = Text { wrap = true, color="black", position = {1600,   0}, size = {320,  480}, text = concatStr(russian, 3),  font = "FreeSerif 26px"  }
+    textblock5 = Text { wrap = true, color="black", position = {1600, 480}, size = {320,  480}, text = concatStr(japanese, 3), font = "Sazanami Mincho Regular 26px"  }
 
     border1=makeBorder(textblock1)
     border2=makeBorder(textblock2)
@@ -48,17 +48,17 @@ text_test.extra.start = function()
     text_test:add(textblock1,textblock2,textblock3,textblock4,textblock5)
 
     local expand = true
-    local expandwidth = 200;
+    local expandwidth = 320;
 
     idle:add_onidle_listener(function()
         if(expand) then
             expandwidth = expandwidth + 1
-            if(expandwidth == 400) then
+            if(expandwidth == 640) then
                 expand = false
             end
         else
             expandwidth = expandwidth - 1
-            if(expandwidth == 200) then
+            if(expandwidth == 320) then
                 expand = true
             end
         end
@@ -66,13 +66,13 @@ text_test.extra.start = function()
         border1.width = expandwidth                         textblock1.width = expandwidth-2
         border2.width = expandwidth                         textblock2.width = expandwidth-2
 
-        border3.width = (1200-(expandwidth*2))              textblock3.width = (1200-(expandwidth*2))-2
+        border3.width = (1920-(expandwidth*2))              textblock3.width = (1920-(expandwidth*2))-2
         border3.x = expandwidth                             textblock3.x = expandwidth+1
 
         border4.width = expandwidth                         textblock4.width = expandwidth-2
-        border4.x = (1200-expandwidth)                      textblock4.x = (1200-expandwidth)+1
+        border4.x = (1920-expandwidth)                      textblock4.x = (1920-expandwidth)+1
         border5.width = expandwidth                         textblock5.width = expandwidth-2
-        border5.x = (1200-expandwidth)                      textblock5.x = (1200-expandwidth)+1
+        border5.x = (1920-expandwidth)                      textblock5.x = (1920-expandwidth)+1
     end)
 end
 
