e25954110facf553f37ffacb91b0e680e335bbf3
Josh Steelman <joshua.steelman@trickplay.com>
2012-12-26 14:08:32 -0800

    [stitcher] added -p, working progress feedback

--- tools/stitcher/progress.h
+++ tools/stitcher/progress.h
@@ -1,22 +1,27 @@
 #ifndef __PROGRESS_H__
 
 #include <glib.h>
+#include "options.h"
 
-typedef struct {
+typedef struct Progress Progress;
+
+typedef struct ProgressChunk {
     float estimate, 
           progress;
+    Progress * parent;
 } ProgressChunk;
 
-typedef struct {
+struct Progress {
     GPtrArray * chunks;
     unsigned percent;
-} Progress;
+    gboolean print;
+};
 
 #define __PROGRESS_H__
 
-Progress * progress_new();
+Progress * progress_new( Options * options );
 void progress_free( Progress * progress );
 void progress_recalculate( Progress * progress );
-ProgressChunk * progress_new_chunk( Progress * progress, unsigned estimate );
+ProgressChunk * progress_new_chunk( Progress * progress, float estimate );
 
 #endif
