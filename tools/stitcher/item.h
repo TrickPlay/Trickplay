27795e769086bc184ff2153f67ab171478c4df1c
Josh Steelman <joshua.steelman@trickplay.com>
2013-01-14 11:11:57 -0800

    [stitcher] documented the intentions of each header file

--- tools/stitcher/item.h
+++ tools/stitcher/item.h
@@ -1,6 +1,16 @@
 #ifndef __ITEM_H__
 #define __ITEM_H__
 
+/*
+
+item.h
+
+An Item represents a single sprite + id pair to be included in the spritesheet. Items can have hierarchical 'children', which define virtual duplicates or subimages of their parent Item, in which case only the topmost parent Item will take up real space in the spritesheet.
+
+Items handle how they are converted to lines in the JSON result, but not how they are composited into the spritesheet textures (see state.c).
+
+*/
+
 #include <magick/MagickCore.h>
 #include "options.h"
 
