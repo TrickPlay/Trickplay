79082eb0cef047346069da882e32295c7409c86b
Alex Indaco <alex.indaco@trickplay.com>
2013-01-22 10:33:36 -0800

    [widget_library] tweaked main.lua to add testing for load layer

--- libraries/ui/main.lua
+++ libraries/ui/main.lua
@@ -1,5 +1,18 @@
 
-
+do
+    local old_print = print
+    local old_dumptable = dumptable
+    
+    dumptable = function(...) 
+        old_print( (( {pcall(error,"Dumptable:",3)} )[2]) ) 
+        old_dumptable(...)
+    end
+    print = function(...) 
+        local m = ""
+        for i,v in ipairs({...}) do   m = m..tostring(v).." "   end
+        old_print( (( {pcall(error,m,3)} )[2]) )
+    end
+end
 
 WL = dofile("Widget_Library.lua")
 dofile("load_json.lua")
@@ -9,7 +22,7 @@ dofile("load_json.lua")
 --add_verbosity("ArrayManager")
 
 
-dofile("TabBar/test.lua")
+dofile("Button/test.lua")
 --[[
 wg = WL.Widget_Group{name='wg'}
 
@@ -29,8 +42,22 @@ controllers:start_pointer()
 r = Rectangle{w=10,h=10,y = 1070}
 screen:add(r)
 
-r:animate{
+tl = Timeline{
     loop = true,
-    duration = 60000,
-    w = screen.w
+    duration = 10000,
+    on_new_frame = function(tl,ms,p)
+        --print(p)
+        r.w = 1920*p
+    end
 }
+tl:start()
+
+--[[
+w = WL.Widget_Group()
+g = Group()
+g:add(w)
+screen:add(g)
+w:unparent()
+screen:add(w)
+print(w:to_json())
+--]]
