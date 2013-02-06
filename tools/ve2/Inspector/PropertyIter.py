284270dbef8c57bdbd2f538ef4f43363262b7ff0
Heejung Kim <hjkim@trickplay.com>
2013-02-06 13:02:12 -0800

    [ve2] fixed inspector property names (bugzid:3749)

--- tools/ve2/Inspector/PropertyIter.py
+++ tools/ve2/Inspector/PropertyIter.py
@@ -491,6 +491,211 @@ PROP_LIST = [
     #'children', 
 ]
 
+PROP_S_LIST = {
+    #'gid',
+    #'name', 
+    #'source', 
+    #'src', 
+    #'text',
+    #'font', 
+    #'position', 
+    #'x',
+    #'y',
+    #'z',
+    #'w',
+    #'h',
+    #'size',
+    'separator_y':'separator height',
+    #'opacity', 
+    #'tile',
+    'is_visible':'visible', 
+    #'color',
+    # ButtonPicker
+    #'orientation',
+    'window_w':'window width', 
+    'window_h':'window height', 
+    'animate_duration':'animate duration', 
+    'border_color':'border color',
+    'anchor_point':'anchor point',
+    #'scale',
+    #'clip',
+    'x_rotation':'x rotation',
+    'y_rotation':'y rotation',
+    'z_rotation':'z rotation',
+    # Widget
+    #'style', 
+    #'focused',
+    #'enabled',
+    # Widget Style
+    #'border',
+    #'arrow',
+    #'fill_colors',
+    # Style Text
+    'x_offset':'x offset',
+    'y_offset':'y offset',
+    #'colors',
+    # Style Border
+    #'width',
+    'corner_radius':'corner radius',
+    #'colors',
+    # Style Arrow 
+    #'offset',
+    #'colors',
+    # Color Scheme 
+    'default_color':'default color', 
+    'focus_color':'focus color', 
+    'activate_color':'activate color', 
+    'select_color':'select color', 
+    # Color
+    'hex_string':'hex string', 
+    'rgb_table':'rgb table',
+    'color_table':'color table',
+    # Button
+    #'label', 
+    # Toggle Button
+    #'selected', 
+    #'group', 
+    'images_selection' : 'images',
+    # Radio Button Group
+    'selected_item':'selected items',
+    # Layout Manager
+    'number_of_rows':'rows',
+    'number_of_columns':'columns',
+    'cell_width':'cell width', 
+    'cell_height':'cell height', 
+    'horizontal_cell_spacing':'horizontal cell spacing', 
+    'vertical_cell_spacing':'vertical cell spacing', 
+    'cell_timing':'cell timing', 
+    'cell_timing_offset':'cell timing offset', 
+    'cells_focusable':'cell focusable', 
+    # Text Input
+    # Progress Bar
+    #'progress',
+    'fill_style' : 'fill style',
+    'empty_style' : 'empty style', 
+    # Progress Spinner
+    #'duration',
+    #'image', 
+    #'animating', 
+    # Obitting Dots
+    #'image', 
+    'num_dots' : 'number of dots', 
+    'dot_size' : 'dot size', 
+    # Dialog Box
+    #'title', 
+    #'content', 
+    #'image', 
+    # Toast Alert 
+    #'icon', 
+    #'message', 
+    'message_font':'message font', 
+    'message_color':'message color', 
+    'horizontal_message_padding' : 'horizontal message padding', 
+    'vertical_message_padding' : 'vertical message padding', 
+    'horizontal_icon_padding' : 'horizontal icon padding', 
+    'vertical_icon_padding' : 'vertical icon padding', 
+    'animate_in_duration':'animate in duration',
+    'animate_out_duration':'animate out duration',
+    'on_screen_duration':'on screen duration',
+    # Menu Button
+    #'button',
+    'x_offset' : 'x offset',
+    'y_offset' : 'y offset',
+    'x_alignment' : 'x alignment',
+    'y_alignment' : 'y alignment',
+    # Button Picker
+    #'items',
+    #'derection',
+    # Tab Bar
+    #'direction', #"horizontal"
+    'dist_from_pane' : 'distance from pane',
+    'focus_opens_tab' : 'focus opens tab',
+    # ArrowPane
+    'children_want_focus' : 'children want focus', 
+    #'clip',
+    'clip_to_size' : 'clip to size',
+    #'count',
+    #'depth',
+    #'has_clip',
+    #'horizontal_alignment',
+    #'horizontal_spacing',
+    #'is_animating',
+    #'is_rotated',
+    #'is_scaled',
+    'arrow_move_by':'arrow move by',
+    'note_constructor' : 'note_constructor',
+    'number_of_cols' : 'columns',
+    'number_of_rows' : 'rows',
+    'pane_h': 'pane height', 
+    'pane_w': 'pane width', 
+    #'placeholder', 
+    'request_mode':'request mode', # "HEIGHT_FOR_WIDTH"
+    #'vertical_alignment', 
+    #'vertical_spacing', 
+    'virtual_x' : 'virtual x',
+    'virtual_y' : 'virtual y',
+    'virtual_w' : 'virtual w',
+    'virtual_h' : 'virtual h',
+    # ScroolPane
+    'slider_thickness':'slider thickness',
+    # TabBar
+    'tab_h':'tab height',
+    'tab_location':'tab location',
+    'tab_w':'tab width',
+
+    # MenuButton
+    'item_spacing':'item spacing', 
+    'popup_offset':'popup offset', 
+
+    # Scroll Pane
+    'horizontal_slider':'horizontal slider',
+    'vertical_slider':'vertical slider',
+    # Slider 
+    #'value', 
+    #'ratio',
+    #'grip', # userdata ? 
+    'grip_w':'grip width', 
+    'grip_h':'grip height', 
+    #'track', #userdata ?
+    'track_w':'track width', 
+    'track_h':'track height', 
+    # Clippig Region
+    'virtual_width':'virtual width',
+    'virtual_height':'virtual height',
+
+    # Widget Text
+    #'alignment',
+    #'baseline',
+    'cursor_position' : 'cursor position',
+    'cursor_size' : 'cursor size',
+    #'editable',
+    #'justify',
+    'line_spacing' : 'line spacing',
+    #'markup',
+    'max_length' : 'max length',
+    'password_char' : 'password char',
+    'selected_text' : 'selected text',
+    'selected_end' : 'selected end',
+    'single_line' : 'single line',
+    'use_markup' : 'use markup',
+    'wants_enter' : 'wants enter',
+    'wrap_mode' : 'wrap mode',
+    
+    # Widget Image
+    #'async',
+    #'loaded',
+    #'read_tags',
+    #'tags',
+
+    # Widget Rectangle
+    #'border_width',
+    #'neighbors',
+
+    # Hidden
+    #'type',
+    #'children', 
+}
+
 NESTED_PROP_LIST = {
     #'style' : ['arrow', 'border', 'fill_colors', 'text'], 
     'style' : ['text', 'spritesheet_map'], 
@@ -527,4 +732,11 @@ NESTED_PROP_LIST = {
     #'tabs' : [], 
     #ButtonPicker
     #'items' : []
+}
+
+NESTED_PROP_S_LIST = {
+    'spritesheet_map' : 'spritesheet map', 
+    'corner_radius' : 'corner radius',
+    'x_offset' : 'x offset', 
+    'y_offset' : 'y offset'
 }
