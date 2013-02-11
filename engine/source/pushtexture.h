fdc978080ad98b27de60029bbb9ed069340288a0
Jiang Xuan <jxuan@trickplay.com>
2013-02-11 15:46:16 -0800

    [spritesheet] Use pointers to reference instances of Sprite and Source to avoid unneeded copy of classes
    Former-commit-id: b8aabb0714b969e6d5391b908cd08726a26055eb
    Former-commit-id: 416c16e14deaf957c8cadb1540ba659e0b3fb5b2

--- engine/source/pushtexture.h
+++ engine/source/pushtexture.h
@@ -75,7 +75,7 @@ public:
     };
 
     PushTexture() : cache( false ), failed( false ), texture( NULL ), can_signal( true ), real( false ) {};
-    ~PushTexture();
+    virtual ~PushTexture();
 
     CoglHandle get_texture();
     void set_texture( CoglHandle texture, bool real );
