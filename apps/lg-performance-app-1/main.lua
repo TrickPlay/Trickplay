7e3c9034487641721cfd325cab3e3c9d97046e3d
Alex Indaco <alex.indaco@trickplay.com>
2013-02-25 15:17:46 -0800

    [lg-perf] grid:delete() now has an optional third parameter that specifies the number of times in a row that delete is to occur, modal_menu:focus()/:unfocus() now makes a modal menu bounce in and out
    Former-commit-id: 6e708321ee13d7885724521b04fc1402d9681415
    Former-commit-id: 0c3008b3a5fc231d7ff2389b272372b22d571d0c

--- apps/lg-performance-app-1/main.lua
+++ apps/lg-performance-app-1/main.lua
@@ -41,11 +41,12 @@ local function make_grid(items,cell_w,cell_h,x_spacing,y_spacing)
         end
     end
 
-    local w = (cell_w+x_spacing)*#items[1]-x_spacing
+    local w = (cell_w+x_spacing)*#entries[1]-x_spacing
     instance.anchor_point = {w/2,0}
 
     local deletion_duration = 250
-    function instance:delete(d_r,d_c)
+    function instance:delete(d_r,d_c,n)
+        n = n or 1
 
         local sliding_left = Group()
         local wrapping_around = Group()
@@ -191,6 +192,14 @@ local function make_grid(items,cell_w,cell_h,x_spacing,y_spacing)
             end
 
             entries[#entries][#entries[#entries]] = nil
+            if #entries[#entries] == 0 then entries[#entries] = nil end
+            if n > 1 then
+                dolater(
+                    instance.delete,
+                    instance,
+                    d_r,d_c,n-1
+                )
+            end
         end
 --]=]
         dolater(a.start,a)
@@ -200,6 +209,7 @@ local function make_grid(items,cell_w,cell_h,x_spacing,y_spacing)
 
 end
 
+-------------------------------------------------------------
 local items = {}
 for i=1,4 do
     items[i] = {}
@@ -212,4 +222,46 @@ grid = make_grid(items,100,100,80,80)
 grid.x = screen_w/2
 grid.y = 400
 screen:add(grid)
+
+-------------------------------------------------------------
+items = {}
+for i=1,4 do
+    items[i] = {}
+    for j=1,9 do
+        items[i][j] = "Icon "..i.." "..j
+    end
+end
+modal_menu = make_grid(items,100,100,80,80)
+
+modal_menu.x = screen_w/2
+modal_menu.y = 100
+
+r = Rectangle{w=1700,h=800,x=-50,y=-50,color = "red"}
+
+modal_menu:add(r)
+r:lower_to_bottom()
+
+modal_menu.opacity = 0
+
+function modal_menu:focus()
+    modal_menu:animate{
+        duration = 250,
+        opacity = 255,
+    }
+    modal_menu:animate{
+        duration = 300,
+        mode = "EASE_OUT_BOUNCE",
+        z = 0,
+    }
+end
+function modal_menu:unfocus()
+    modal_menu:animate{
+        duration = 250,
+        opacity = 0,
+        z = -100,
+    }
+end
+
+screen:add(modal_menu)
+modal_menu.z = -100
 
