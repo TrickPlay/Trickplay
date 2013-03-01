0f5e4711c0a4ce99738c5a7870754332585f296b
Alex Indaco <alex.indaco@trickplay.com>
2013-03-01 14:35:15 -0800

    [lg-perf] added some assets the the cube

--- apps/lg-performance-app-1/cube_test.lua
+++ apps/lg-performance-app-1/cube_test.lua
@@ -1,4 +1,11 @@
-
+local bg = Rectangle{
+    size = screen.size,
+    color = "bbbbbb"
+}
+local card_src = Image{
+    src = "assets/card.png"
+}
+card_src:hide()
 w = screen.w-200
 h = 700
 
@@ -10,25 +17,72 @@ local function make_side(w,h,bg_color)
     }
 
 
-    local r = Rectangle{
+    local bg = Rectangle{
         w=w,
         h=h,
         --position={w/2,h/2},
         color=bg_color
     }
 
-
-    local items = {}
-    for i=1,4 do
-        items[i] = {}
-        for j=1,8 do
-            items[i][j] = "Icon "..i.." "..j
+    -----------------------------------------------
+    local top_left = Rectangle{
+        x=26,
+        y=32,
+        w=530,
+        h=400,
+        --position={w/2,h/2},
+        color="pink"
+    }
+    local btm_left = Rectangle{
+        x=top_left.x,
+        y=top_left.y+top_left.h,
+        w=top_left.w,
+        h=260,
+        --position={w/2,h/2},
+        color="yellow"
+    }
+    -----------------------------------------------
+    local items1 = {}
+    for i=1,3 do
+        items1[i] = {}
+        for j=1,3 do
+            items1[i][j] = "Icon "..i.." "..j
         end
     end
-    local grid = make_grid(items,100,100,80,80)
-    grid.x = w/2
+    local grid1 = make_grid(items1,90,140,80,80)
+    grid1.x = w*3/6
+    grid1.y = top_left.y+20
+    -----------------------------------------------
+    local items2 = {}
+    for i=1,3 do
+        items2[i] = {}
+        for j=1,3 do
+            items2[i][j] = "Icon "..i.." "..j
+        end
+    end
+    local grid2 = make_grid(items2,150,100,30,120)
+    grid2.x = w*5/6+5
+    grid2.y = top_left.y+40
+    -----------------------------------------------
     --grid.y = h/2
-    instance:add(r,grid)
+    instance:add(
+        bg,
+        Clone{
+            source = card_src,
+        },
+        Clone{
+            source = card_src,
+            x      = w*2/6
+        },
+        Clone{
+            source = card_src,
+            x      = w*4/6
+        },
+        top_left,
+        btm_left,
+        grid1,
+        grid2
+    )
 
     return instance
 end
@@ -42,6 +96,18 @@ g = Group{
 }
 
 
+local btm_row_backing = Image{
+    src="assets/bottom-bar.png"
+}
+btm_row_backing.y = screen.h-btm_row_backing.h
+local btm_row_backing_text = Text{
+    text = "More",
+    font = "Lato 40px",
+    color = "white",
+    x  = screen.w/2,
+    y  = btm_row_backing.y,
+}
+btm_row_backing_text.anchor_point = {btm_row_backing_text.w/2,0}
 local items = {}
 for i=1,1 do
     items[i] = {}
@@ -51,10 +117,10 @@ for i=1,1 do
 end
 local grid = make_grid(items,100,100,80,80)
 grid.x = screen.w/2
-grid.y = 850
+grid.y = 900
 
 g:add(r1,r2)
-screen:add(grid,g)
+screen:add(card_src,bg,btm_row_backing,btm_row_backing_text,grid,g)
 
 
 local phase_one, phase_two
