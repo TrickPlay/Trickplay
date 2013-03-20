b9a6c590ec542a135de7a1666c3fc20169601d8e
Heejung Kim <hjkim@trickplay.com>
2013-03-20 14:51:55 -0700

    [ve2]Widget Lib update
    Former-commit-id: 249e0410b746f9993d2732143843a9332cda40d1
    Former-commit-id: 6ca70002f8a137972f148c14cd28ec7370428e8f

--- tools/ve2/VE/LIB/Widget/TabBar/TabBar.lua
+++ tools/ve2/VE/LIB/Widget/TabBar/TabBar.lua
@@ -1,4 +1,3 @@
-TABBAR = true
 
 local external = ({...})[1] or _G
 local _ENV     = ({...})[2] or _ENV
@@ -25,14 +24,14 @@ local top_tabs = function(self,state)
     --[[
 	local c = Canvas(self.w,self.h)
     mesg("TABBAR",0,"TabBar make top_tab",self.gid,state)
-	
+
 	c.op = "SOURCE"
-	
+
 	c.line_width = self.style.border.width
-	
+
 	local r     = self.style.border.corner_radius
     local inset = c.line_width/2
-    
+
     c:move_to( inset, inset+r)
     --top-left corner
     c:arc( inset+r, inset+r, r,180,270)
@@ -44,15 +43,15 @@ local top_tabs = function(self,state)
     c:line_to( inset, c.h + inset)
     --bottom-left corner
     c:line_to( inset, inset+r)
-    
+
 	c:set_source_color( self.style.fill_colors[state] or "00000000" )
-	
+
 	c:fill(true)
-    
+
 	c:set_source_color( self.style.border.colors[state] or "ffffff" )
-	
+
 	c:stroke(true)
-	
+
 	return c:Image()
 	--]]
 end
@@ -75,18 +74,18 @@ local side_tabs = function(self,state)
                                 --se   = style["TabBar/default/se.png"],
                             }
             }
-    
-            
+
+
     --[[
 	local c = Canvas(self.w,self.h)
     mesg("TABBAR",0,"TabBar make side_tab",self.gid,state)
 	c.op = "SOURCE"
-	
+
 	c.line_width = self.style.border.width
-	
+
 	local r     = self.style.border.corner_radius
     local inset = c.line_width/2
-    
+
     c:move_to( inset, inset+r)
     --top-left corner
     c:arc( inset+r, inset+r, r,180,270)
@@ -98,16 +97,16 @@ local side_tabs = function(self,state)
     --bottom-left corner
     c:arc( inset+r, c.h - (inset+r), r,90,180)
     c:line_to( inset, inset+r)
-    
-    
+
+
 	c:set_source_color( self.style.fill_colors[state] or "00000000" )
-	
+
 	c:fill(true)
-    
+
 	c:set_source_color( self.style.border.colors[state] or "ffffff" )
-	
+
 	c:stroke(true)
-	
+
 	return c:Image()
 	--]]
 end
@@ -120,30 +119,30 @@ local default_parameters = {tab_w = 200,tab_h = 50,pane_w = 400,pane_h = 300, ta
 -- TabBar's base is a ListManager, this allows for easier alignment when
 --         switching the location of the tabs
 -- This ListManager contains 2 items: an ArrowPane and a Group
--- The ArrowPane contains a ListManager of ToggleButtons, linked with a 
+-- The ArrowPane contains a ListManager of ToggleButtons, linked with a
 -- RadioButtonGroup
 -----------------------------------------------------------------------------
 TabBar = setmetatable(
     {},
     {
         __index = function(self,k)
-            
+
             return getmetatable(self)[k]
-            
+
         end,
         __call = function(self,p)
-            
+
             return self:declare():set(p or {})
-            
+
         end,
-        
+
         subscriptions = {
         },
         public = {
             properties = {
                 enabled = function(instance,_ENV)
                     return nil,
-                    function(oldf,self,v)  
+                    function(oldf,self,v)
                         oldf(self,v)
                         for i = 1,tabs_lm.length do
                             tabs_lm.cells[i].enabled = v
@@ -151,10 +150,35 @@ TabBar = setmetatable(
                         tab_pane.enabled = v
                     end
                 end,
+                contents_offset = function(instance,_ENV)
+                    return function(oldf,self)
+--dumptable(rbg.items[rbg.selected].contents.contents_offset)
+                        --[[
+                        local x,y = unpack(
+                            panes[rbg.selected] and
+                            panes[rbg.selected].contents_offset or
+                            {0,0}
+                        )
+
+                        return {
+                            ((tab_location == "left") and
+                            (x+tab_w) or x),
+                            ((tab_location == "top") and
+                            (y+tab_h) or y)
+                        }
+                        --]]
+                        return {
+                            ((tab_location == "left") and
+                            (tab_w) or 0),
+                            ((tab_location == "top") and
+                            (tab_h) or 0)
+                        }
+                    end
+                end,
                 pane_w = function(instance,_ENV) -- TODO check need for these upvals
                     return function(oldf) return   pane_w     end,
-                    function(oldf,self,v)  
-                        pane_w = v 
+                    function(oldf,self,v)
+                        pane_w = v
                         panes_obj.w = v
                         if tab_location == "top" then
                             tab_pane.pane_w    = pane_w
@@ -163,8 +187,8 @@ TabBar = setmetatable(
                 end,
                 pane_h = function(instance,_ENV)
                     return function(oldf) return   pane_h     end,
-                    function(oldf,self,v)   
-                        pane_h = v 
+                    function(oldf,self,v)
+                        pane_h = v
                         panes_obj.h = v
                         if tab_location == "left" then
                             tab_pane.pane_h    = pane_h
@@ -173,34 +197,34 @@ TabBar = setmetatable(
                 end,
                 tab_w = function(instance,_ENV)
                     return function(oldf) return   tab_w     end,
-                    function(oldf,self,v)          
-                        tab_w = v 
+                    function(oldf,self,v)
+                        tab_w = v
                         resize_tabs = true
                     end
                 end,
                 tab_images = function(instance,_ENV)
                     return function(oldf) return   tab_images     end, -- TODO either return clone, or metatable for changes
-                    function(oldf,self,v)          
+                    function(oldf,self,v)
                         local old_images = tab_images or {}
                         tab_images = v
-                        
+
                         for k,v in pairs(v) do
                             add(instance,v)
                             v:hide()
                         end
-                        
+
                         for i = 1,tabs_lm.length do
-                            
+
                             local clones = {}
-                            
+
                             for k,v in pairs(v) do
                                 clones[k] = Clone{source=v}
                             end
-                            
+
                             tabs_lm.cells[i].images = clones
-                            
+
                         end
-                        
+
                         for k,v in pairs(old_images) do
                             v:unparent()
                         end
@@ -208,8 +232,8 @@ TabBar = setmetatable(
                 end,
                 tab_h = function(instance,_ENV)
                     return function(oldf) return   tab_h     end,
-                    function(oldf,self,v)          
-                        tab_h = v 
+                    function(oldf,self,v)
+                        tab_h = v
                         resize_tabs = true
                     end
                 end,
@@ -222,15 +246,15 @@ TabBar = setmetatable(
                 end,
                 tabs = function(instance,_ENV)
                     return function(oldf)  return   tabs_interface      end,
-                    function(oldf,self,v)    
+                    function(oldf,self,v)
                         if type(v) ~= "table" then error("Expected table. Received: ",2) end
-                        new_tabs = v  
+                        new_tabs = v
                         resize_tabs = true
                     end
                 end,
                 tab_location = function(instance,_ENV)
                     return function(oldf) return   tab_location     end,
-                    function(oldf,self,v)  
+                    function(oldf,self,v)
                         mesg("TABBAR",0,"TabBar.tab_location =",v)
                         if tab_location == v then return end
                         new_tab_location = true
@@ -273,11 +297,11 @@ TabBar = setmetatable(
                         tab_location = v
                     end
                 end,
-    
+
                 attributes = function(instance,_ENV)
-                    return function(oldf,self) 
+                    return function(oldf,self)
                         local t = oldf(self)
-                        
+
                         t.length               = nil
                         t.number_of_cols       = nil
                         t.number_of_rows       = nil
@@ -288,66 +312,67 @@ TabBar = setmetatable(
                         t.cell_h = nil
                         t.cell_w = nil
                         t.cells  = nil
-                        
+
                         t.style = instance.style.name
-                        
+
+                        t.contents_offset = instance.contents_offset
                         t.tab_w  = instance.tab_w
                         t.tab_h  = instance.tab_h
                         t.pane_w = instance.pane_w
                         t.pane_h = instance.pane_h
                         t.tabs   = instance.tabs
                         t.tab_location = instance.tab_location
-                        
+
                         t.tabs = {}
-                        
+
                         for i = 1,tabs_lm.length do
                             t.tabs[i]    = {
                                 label    = tabs_lm.cells[i].label,
                                 contents = tabs_lm.cells[i].contents.attributes
                             }
                         end
-                        
+
                         t.type = "TabBar"
-                        
+
                         return t
                     end
                 end,
-                
-    
+
+
             },
             functions = {
             },
-        },  
-        
+        },
+
         private = {
-        
+
             update = function(instance,_ENV)
                 return function()
                     mesg("TABBAR",{0,5},"TabBar update called")
                     if restyle_tabs then
                         restyle_tabs = false
                         for i = 1,tabs_lm.length do
-                                
+
                             tabs_lm.cells[i].style:set(instance.style.attributes)
-                            
+
                         end
                     end
                     if restyle_arrows then
                         restyle_arrows = false
-                        
+
                         tab_pane.style.arrow:set(instance.style.arrow.attributes)
                     end
                     if resize_tabs then
                         resize_tabs = false
                         if not new_tabs then
                             for i = 1,tabs_lm.length do
-                                
+
                                 tabs_lm.cells[i].size = {tab_w,tab_h}
-                                
+
                             end
                         end
                         if tab_location == "top" then
-                            
+
                             tab_pane.pane_h        = tab_h
                             tab_pane.virtual_h     = tab_h
                             tab_pane.arrow_move_by = tab_w + tabs_lm.spacing
@@ -397,9 +422,9 @@ TabBar = setmetatable(
                         else
                             error("Expected 'top' or 'left'. Received "..v,2)
                         end
-                        
+
                     end
-                    
+
                     old_update()
                     print("here")
                     if  new_selection then
@@ -407,15 +432,15 @@ TabBar = setmetatable(
                         rbg.selected = new_selection
                         new_selection = false
                     end
-                    
+
                     --tabs_lm__ENV:call_update()
                 end
             end,
         },
         declare = function(self,parameters)
-            
+
             parameters = parameters or {}
-            
+
             local instance,_ENV = ListManager:declare{vertical_alignment = "top",spacing=0}
             style_flags = {
                 border      = "restyle_tabs",
@@ -423,7 +448,7 @@ TabBar = setmetatable(
                 fill_colors = "restyle_tabs",
                 arrow       = "restyle_arrows",
             }
-            
+
             function instance:on_key_focus_in()
                 if tabs_lm.length > 0 then
                     rbg.items[rbg.selected]:grab_key_focus()
@@ -445,11 +470,11 @@ TabBar = setmetatable(
                     end
                 end
             }
-            
-            
-            
+
+
+
             old_update = update
-            
+
             pane_w = 400
             pane_h = 300
             panes_obj = Widget_Group{
@@ -462,22 +487,22 @@ TabBar = setmetatable(
                     end
                 end
             }
-            
+
             WL_parent_redirect[panes_obj] = instance
-            
+
             tab_w = 200
             tab_h = 50
-            tab_images   = nil 
+            tab_images   = nil
             tab_style    = nil
             tab_location = "top"
             resize_tabs = true
             new_selection = 1
-            
+
             local function make_tab_interface(tb)
                 --prevents the user from getting/setting any of the other fields of the ToggleButtons
                 local setter = {
                     label    = function(v) tb.label = v end,
-                    contents = function(v) 
+                    contents = function(v)
                         tb.contents:unparent()
                         tb.contents = v
                         panes_obj:add(v)
@@ -498,41 +523,41 @@ TabBar = setmetatable(
                 })
             end
             tab_to_interface_map = {}
-            
+
             tabs_lm = ListManager:declare{
                 name = "Tabs ListManager",
                 spacing = 0,
                 vertical_alignment = "top",
                 direction = "horizontal",
                 node_constructor = function(obj)
-                    
-                    
-                    
+
+
+
                     mesg("TABBAR",{0,3},"New Tab Button")
-                    if obj == nil then 
+                    if obj == nil then
                         obj = {label = "Tab",contents = Widget_Group()}
                     elseif type(obj) ~= "table" then
                         error("Expected tab entry to be a string. Received "..type(obj),2)
                     elseif type(obj.label) ~= "string" then
                         error("Received a tab without a label",2)
                     end
-                    if type(obj.contents) == "table" and obj.contents.type then 
-                        
+                    if type(obj.contents) == "table" and obj.contents.type then
+
                         obj.contents = _ENV[obj.contents.type](obj.contents)
-                        
-                    elseif type(obj.contents) ~= "userdata" and obj.contents.__types__.actor then 
-                        
-                        error("Must be a UIElement or nil. Received "..obj.contents,2) 
+
+                    elseif type(obj.contents) ~= "userdata" and obj.contents.__types__.actor then
+
+                        error("Must be a UIElement or nil. Received "..obj.contents,2)
                     end
                     local pane = obj.contents
-                    
+
                     --local style = instance.style.attributes
                     --style.name = style
                     --style.border.colors.selection = style.border.colors.selection or "ffffff"
                     local clones
                     if tab_images then
                         clones = {}
-                        
+
                         for k,v in pairs(tab_images) do
                             clones[k] = Clone{source=v}
                         end
@@ -567,7 +592,7 @@ TabBar = setmetatable(
                         obj.style:set(tab_style.attributes) -- causes extra redraw
                     end
                     --]]
-                    
+
                     tab_to_interface_map[obj] = make_tab_interface(obj)
                     --table.insert(tabs,obj)
                     --obj.pane = pane
@@ -576,17 +601,17 @@ TabBar = setmetatable(
                     panes_obj:add(obj.contents)
                     obj.contents.w = pane_w
                     obj.contents.h = pane_h
-                    
+
                     if sel then new_selection = sel end
                     return obj
                 end
             }
-            
+
             tabs_interface = setmetatable({},{
                 __index = function(_,k)
                     local v = tabs_lm.cells[k]
-                    return type(k) == "number" and v and 
-                        tab_to_interface_map[v] or 
+                    return type(k) == "number" and v and
+                        tab_to_interface_map[v] or
                         v
                 end,
                 --pass through to the ListManager
@@ -594,9 +619,9 @@ TabBar = setmetatable(
                     tabs_lm = v
                 end,
             })
-            
+
             tabs_lm__ENV = get_env(tabs_lm)
-            
+
             --TODO roll into a single set
             tab_pane = ArrowPane{
                 name = "ArrowPane",
@@ -612,14 +637,14 @@ TabBar = setmetatable(
             --tab_pane.style.border.colors.default = "00000000"
             --tab_pane.style.fill_colors.default   = "00000000"
             tab_pane:add(tabs_lm)
-            
+
             instance.cells = {tab_pane,panes_obj}
-            
+
             setup_object(self,instance,_ENV)
-            
-            dumptable(get_children(instance))
+
+            --dumptable(get_children(instance))
             return instance, _ENV
-            
+
         end
     }
 )
