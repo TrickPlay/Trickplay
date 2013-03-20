ec18a021786a46aa6c85636f3af39aff9f63f7c9
Heejung Kim <hjkim@trickplay.com>
2013-03-20 14:51:55 -0700

    [ve2]Widget Lib update

--- tools/ve2/VE/LIB/Widget/__UTILITIES/TypeChecking.lua
+++ tools/ve2/VE/LIB/Widget/__UTILITIES/TypeChecking.lua
@@ -1,4 +1,3 @@
-TYPECHECKING = true
 
 local external = ({...})[1] or _G
 local _ENV     = ({...})[2] or _ENV
@@ -10,28 +9,28 @@ function expects(type_wanted, received)
     )
 end
 
-function is_ui_element(obj) 
-    return type(obj) == "userdata" and obj.__types__ and obj.__types__.actor 
+function is_ui_element(obj)
+    return type(obj) == "userdata" and obj.__types__ and obj.__types__.actor
 end
 
 function is_table_or_nil(name,input)
-    
+
     return input == nil and {} or
-        
-        type(input) == "table" and input or 
-        
+
+        type(input) == "table" and input or
+
         error(name.." requires a table or nil as input",3)
-    
+
 end
 
 --This function needs a better name
 function matches_nil_table_or_type(constructor,req_type,input)
-    
+
     return input == nil and constructor() or
         type(input) == "string" and constructor(input) or
         type(input) == "table"  and (input.type == req_type and input or constructor(input)) or
         error("input did not match nil, table, or "..req_type,2)
-    
+
 end
 
 
@@ -39,22 +38,22 @@ end
 check_name = function(curr_names,instance,name,generic)
     --print(generic)
     if name == nil then name = generic end
-    
+
     if curr_names[name] == nil then
-        
+
         curr_names[name] = instance
-        
+
     else
-        
+
         local i = 1
-        
+
         while curr_names[name.." ("..i..")"] ~= nil do    i = i + 1    end
-        
+
         curr_names[name.." ("..i..")"] = instance
-        
+
         name = name.." ("..i..")"
     end
-    
+
     return name
-    
+
 end
