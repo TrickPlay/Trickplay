72ad6c5f635b8a961dcc46c01ffba531296d5a3c
Alex Indaco <alex.indaco@trickplay.com>
2013-03-22 15:33:45 -0700

    [widget_library] simplified the main.lua file for the widget test

--- tests/apps/widget-library/main.lua
+++ tests/apps/widget-library/main.lua
@@ -7,8 +7,6 @@ end
 
 --If the locations of this test app or of the widget library change,
 --then these must change as well
-local relative_path_to_test_app       = "tests/apps/widget-library/"
-local relative_path_to_widget_library = "libraries/ui/"
 local absolute_path_to_test_app       = editor.app_path
 
 local absolute_path_to_widget_library =
@@ -16,40 +14,41 @@ local absolute_path_to_widget_library =
     absolute_path_to_test_app:sub(
         1,
         absolute_path_to_test_app:len() -
-        relative_path_to_test_app:len() + 1
+        ("tests/apps/widget-library/"):len() + 1
     ) ..
     --appends the path to the widget_library
-    relative_path_to_widget_library
+    "libraries/ui/"
 
 ---------------------------------------------------------------------
 
 local tests = {}
-local t
 local tests_group = Group()
 for i,file in ipairs(editor:readdir(absolute_path_to_test_app)) do
 
     if file ~= "app" and file ~= "main.lua" then
+
         file = file:sub(1,file:len()-4)
-        print("waht",file)
-        --tests[file] = loadfile(file)
-        t      = Text{
-            text=file,
-            color="white",
-            font="Lato 40px",
-            y = 100*(#tests+1),
-            x = 100,
-        }
-        t.file = loadfile(file)
-        table.insert(tests,t)
+
+        table.insert(tests,Text{
+            text     = file,
+            color    = "white",
+            font     = "Lato 40px",
+            y        = 100*(#tests+1),
+            x        = 100,
+            extra    = {
+                file = loadfile(file),
+            }
+        })
     end
     tests_group:add(unpack(tests))
 
 end
 
+---------------------------------------------------------------------
 local load_test_menu
 load_test_menu = function()
     screen:add(Rectangle{color="880000",y = 80,h=100,w=500},tests_group)
-    screen:show()
+
     tests_group.y = 0
     local i = 1
     local key_events
@@ -60,7 +59,7 @@ load_test_menu = function()
             i = i + 1
 
             tests_group:stop_animation()
-            tests_group:animate{duration=100,y = -100*(i-1)}
+            tests_group:animate{duration=100,y = -tests[i].y+100}
         end,
         [keys.Up] = function()
             if i == 1 then return end
@@ -68,12 +67,13 @@ load_test_menu = function()
             i = i - 1
 
             tests_group:stop_animation()
-            tests_group:animate{duration=100,y = -100*(i-1)}
+            tests_group:animate{duration=100,y = -tests[i].y+100}
         end,
         [keys.OK] = function()
             screen:clear()
             key_events = {
                 [keys.BACK] = function()
+                    screen:grab_key_focus()
                     screen:clear()
                     load_test_menu()
                 end,
@@ -87,55 +87,11 @@ load_test_menu = function()
     end
 end
 
+controllers:start_pointer()
+screen:show()
 load_test_menu()
 ---------------------------------------------------------------------
 editor:change_app_path(absolute_path_to_widget_library)
 WL = dofile("Widget_Library.lua")
 
 dofile("load_json.lua")
-
-print(editor.app_path)
-dumptable(getmetatable(editor))
-dumptable(tests)
---[=[]]
-local l = "/Users/trickplay/tp/trickplay/"
---dumptable(getmetatable(editor))
-local path_to_WL = "/Users/trickplay/tp/trickplay/libraries/ui/"
-editor:change_app_path(path_to_WL)
-print(editor.app_path)
-
-
-WL = dofile("Widget_Library.lua")
-dofile("load_json.lua")
---add_verbosity("STYLE_SUBSCRIPTIONS")
---add_verbosity("DEBUG")
---add_verbosity("TABBAR")
---add_verbosity("ArrayManager")
-
-
-dofile("OrbittingDots/test.lua")
---[[
-wg = WL.Widget_Group{name='wg'}
-
-wg:add(WL.MenuButton{
-    name = "lm",
-    y = 400,
-})
-
-str = wg:to_json()
-print(str)
-screen:add(load_layer(str))
---]]
-screen:show()
-controllers:start_pointer()
-
----------------------------------------------------------------------
-r = Rectangle{w=10,h=10,y = 1070}
-screen:add(r)
-
-r:animate{
-    loop = true,
-    duration = 60000,
-    w = screen.w
-}
---]=]
