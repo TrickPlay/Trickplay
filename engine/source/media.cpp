9fbd85e9b2df7a3f0fba8dfc0048d282bbb9a7a8
Jiang Xuan <jxuan@trickplay.com>
2013-05-20 16:40:11 -0700

    [media] Replace MediaExtra with Media in media.lb, update media.cpp and media.h
    Former-commit-id: ae2290e2e234bede922407e6123c1f783c776173
    Former-commit-id: 14506d816a4dc5ca48397113425de6dc039b1507

--- engine/source/media.cpp
+++ engine/source/media.cpp
@@ -1,7 +1,81 @@
+#include "clutter_util.h"
+#include "tp-clutter.h"
 #include "media.h"
 
-Media::Media()
-{}
+Media * Media::get( ClutterActor * _actor, lua_State *l )
+{
+    g_assert( _actor );
 
-Media::~Media()
-{}
+    Media* instance = ( Media* ) g_object_get_data( G_OBJECT( _actor ), "media-extra" );
+
+    if ( instance ) return instance;
+
+    g_assert( l );
+
+    instance = new Media();
+    instance->L = l;
+
+    if ( _actor ) instance->actor = _actor;
+
+    g_object_set_data_full( G_OBJECT( _actor ), "media-extra", instance, ( GDestroyNotify ) Media::destroy );
+
+    return instance;
+}
+
+void Media::destroy( Media * instance )
+{
+    delete instance;
+}
+
+void Media::loaded()
+{
+    lb_invoke_callbacks( L, G_OBJECT( actor ), "MEDIA_METATABLE", "on_loaded", 0, 0 );
+}
+
+void Media::error( int code, const char * message )
+{
+    lua_pushinteger( L, code );
+    lua_pushstring( L, message );
+    lb_invoke_callbacks( L, G_OBJECT( actor ), "MEDIA_METATABLE", "on_error", 2, 0 );
+}
+
+void Media::end_of_stream()
+{
+    lb_invoke_callbacks( L, G_OBJECT( actor ), "MEDIA_METATABLE", "on_end_of_stream", 0, 0 );
+}
+
+bool Media::load_media()
+{
+    // Don't load when constructing. Not all properties may have been set yet
+    if ( constructing ) return false;
+
+    g_assert( L );
+    g_assert( actor );
+
+    //int result;
+
+    tags         = JSON::Object();
+    loaded_flag  = false;
+
+    //.........................................................................
+    // Get the media source
+    char * uri = ( char * ) g_object_get_data( G_OBJECT( actor ), "tp-src" );
+    if ( !uri ) return false;
+
+    /*MediaPlayer * mp = extra->get_player();
+    if ( mp )
+    {
+        AppResource resource = AppResource( L , uri , 0 , mp->get_valid_schemes() );
+
+        result = resource ? mp->load( resource.get_uri().c_str(), "" )
+                          : TP_MEDIAPLAYER_ERROR_INVALID_URI;
+    }
+    else
+    {
+        result = TP_MEDIAPLAYER_ERROR_NO_MEDIAPLAYER;
+    }*/
+
+    g_warning( "AT %s", Util::where_am_i_lua(L).c_str() );
+
+    return true;
+}
--- engine/source/media.h
+++ engine/source/media.h
@@ -1,13 +1,44 @@
-#include <clutter-gst/clutter-gst.h>
+#ifndef _TRICKPLAY_MEDIA_H
+#define _TRICKPLAY_MEDIA_H
+
+#include "lb.h"
 
 class Media
 {
   public:
-    Media();
-    ~Media();
+    Media() : constructing( false), loaded_flag( false ), pre_load( false )
+            , read_tags( false ), L( NULL ), actor( NULL )//, mute( 0 ), volume( 0.0 )
+    {}
+
+    ~Media() {}
+
+    static Media * get( ClutterActor * _actor, lua_State *l );
+
+    bool load_media();
+
+    // Related to .lb
+    bool           constructing;
+    bool           loaded_flag;
+    bool           pre_load;
+    bool           read_tags;
+    JSON::Object   tags;
   private:
-    int             mute;
-    double          volume;
-    ClutterMedia  * cm;
+
+    static void destroy( Media * instance );
+
+    void loaded();
+    void error( int code, const char * message );
+    void end_of_stream();
+
+    lua_State     * L;
+    ClutterActor  * actor;
+
+    // Related to media file/url
+
+    // Related to mediaplayer status
+    //int             mute;
+    //double          volume;
+    //ClutterMedia  * cm;
 };
 
+#endif // _TRICKPLAY_MEDIA_H
