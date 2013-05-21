52a1ce6d5fdc754b952440c6e4acbc794bfbb80c
Jiang Xuan <jxuan@trickplay.com>
2013-05-20 17:25:25 -0700

    [Media] Adding load function
    Former-commit-id: a7f0de1f72963052ed9385f612858ffa1b78b40c
    Former-commit-id: 95917f75f23f00c3bf9f25fdf4ce5fb7c571d198

--- engine/source/media.cpp
+++ engine/source/media.cpp
@@ -6,7 +6,7 @@ Media * Media::get( ClutterActor * _actor, lua_State *l )
 {
     g_assert( _actor );
 
-    Media* instance = ( Media* ) g_object_get_data( G_OBJECT( _actor ), "media-extra" );
+    Media * instance = ( Media * ) g_object_get_data( G_OBJECT( _actor ), "media-extra" );
 
     if ( instance ) return instance;
 
@@ -17,20 +17,26 @@ Media * Media::get( ClutterActor * _actor, lua_State *l )
 
     if ( _actor ) instance->actor = _actor;
 
+    StringVector s = split_string( App::get( l )->get_context()->get( TP_MEDIAPLAYER_SCHEMES, TP_MEDIAPLAYER_SCHEMES_DEFAULT ), "," );
+    instance->schemes.insert( s.begin(), s.end() );
+
     g_object_set_data_full( G_OBJECT( _actor ), "media-extra", instance, ( GDestroyNotify ) Media::destroy );
 
     return instance;
 }
+
 
 void Media::destroy( Media * instance )
 {
     delete instance;
 }
 
+
 void Media::loaded()
 {
     lb_invoke_callbacks( L, G_OBJECT( actor ), "MEDIA_METATABLE", "on_loaded", 0, 0 );
 }
+
 
 void Media::error( int code, const char * message )
 {
@@ -39,10 +45,12 @@ void Media::error( int code, const char * message )
     lb_invoke_callbacks( L, G_OBJECT( actor ), "MEDIA_METATABLE", "on_error", 2, 0 );
 }
 
+
 void Media::end_of_stream()
 {
     lb_invoke_callbacks( L, G_OBJECT( actor ), "MEDIA_METATABLE", "on_end_of_stream", 0, 0 );
 }
+
 
 bool Media::load_media()
 {
@@ -52,7 +60,7 @@ bool Media::load_media()
     g_assert( L );
     g_assert( actor );
 
-    //int result;
+    int result;
 
     tags         = JSON::Object();
     loaded_flag  = false;
@@ -62,20 +70,51 @@ bool Media::load_media()
     char * uri = ( char * ) g_object_get_data( G_OBJECT( actor ), "tp-src" );
     if ( !uri ) return false;
 
-    /*MediaPlayer * mp = extra->get_player();
-    if ( mp )
-    {
-        AppResource resource = AppResource( L , uri , 0 , mp->get_valid_schemes() );
+    AppResource resource = AppResource( L , uri , 0 , schemes );
 
-        result = resource ? mp->load( resource.get_uri().c_str(), "" )
-                          : TP_MEDIAPLAYER_ERROR_INVALID_URI;
-    }
-    else
-    {
-        result = TP_MEDIAPLAYER_ERROR_NO_MEDIAPLAYER;
-    }*/
+    //result = resource ? load( resource.get_uri().c_str(), "" )
+    //                  : TP_MEDIAPLAYER_ERROR_INVALID_URI;
 
     g_warning( "AT %s", Util::where_am_i_lua(L).c_str() );
 
     return true;
 }
+
+/*int Media::load( const char* uri, const char* extra )
+{
+    MPLOCK;
+
+    // Get us back to IDLE
+    reset();
+
+    TPMediaPlayer* mp = get_mp();
+
+    if ( !mp->load )
+    {
+        g_warning( "MP[%p]    load NOT IMPLEMENTED", mp );
+        return TP_MEDIAPLAYER_ERROR_NOT_IMPLEMENTED;
+    }
+
+    gchar* unescaped_uri = g_uri_unescape_string( uri , 0 );
+
+    if ( ! unescaped_uri )
+    {
+        g_warning( "MP[%p] INVALID URI '%s'" , mp , uri );
+        return TP_MEDIAPLAYER_ERROR_INVALID_URI;
+    }
+
+    tplog( "[%p] <- load('%s','%s')", mp, unescaped_uri , extra );
+
+    if ( int result = mp->load( mp, unescaped_uri, extra ) )
+    {
+        g_warning( "MP[%p]    FAILED %d", mp, result );
+        g_free( unescaped_uri );
+        return result;
+    }
+
+    g_free( unescaped_uri );
+
+    state = TP_MEDIAPLAYER_LOADING;
+
+    return 0;
+}*/
--- engine/source/media.h
+++ engine/source/media.h
@@ -1,20 +1,43 @@
 #ifndef _TRICKPLAY_MEDIA_H
 #define _TRICKPLAY_MEDIA_H
 
+#include "app_resource.h"
+#include "context.h"
 #include "lb.h"
+
+#define MPLOCK Util::GSRMutexLock lock(&mutex)
 
 class Media
 {
   public:
-    Media() : constructing( false), loaded_flag( false ), pre_load( false )
-            , read_tags( false ), L( NULL ), actor( NULL )//, mute( 0 ), volume( 0.0 )
-    {}
 
-    ~Media() {}
+    Media() : constructing( false ), loaded_flag( false ), pre_load( false )
+            , read_tags( false ), L( NULL ), actor( NULL )//, mute( 0 ), volume( 0.0 )
+    {
+#ifndef GLIB_VERSION_2_32
+        g_static_rec_mutex_init( &mutex );
+#else
+        g_rec_mutex_init( &mutex );
+#endif
+    }
+
+    ~Media()
+    {
+        {
+            MPLOCK;
+        }
+#ifndef GLIB_VERSION_2_32
+    g_static_rec_mutex_free( &mutex );
+#else
+    g_rec_mutex_clear( &mutex );
+#endif
+    }
 
     static Media * get( ClutterActor * _actor, lua_State *l );
 
     bool load_media();
+
+    const StringSet& get_valid_schemes() const { return schemes; }
 
     // Related to .lb
     bool           constructing;
@@ -22,9 +45,11 @@ class Media
     bool           pre_load;
     bool           read_tags;
     JSON::Object   tags;
+
   private:
 
     static void destroy( Media * instance );
+    //int load( const char* uri, const char* extra );
 
     void loaded();
     void error( int code, const char * message );
@@ -39,6 +64,13 @@ class Media
     //int             mute;
     //double          volume;
     //ClutterMedia  * cm;
+
+#ifndef GLIB_VERSION_2_32
+    GStaticRecMutex mutex;
+#else
+    GRecMutex mutex;
+#endif
+    StringSet         schemes;
 };
 
 #endif // _TRICKPLAY_MEDIA_H
