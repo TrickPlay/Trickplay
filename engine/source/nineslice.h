380d523ced04907182cc88793a12cb3f0ba0be41
Jiang Xuan <jxuan@trickplay.com>
2013-05-28 17:29:00 -0700

    [nineslice] Update dump format

--- engine/source/nineslice.h
+++ engine/source/nineslice.h
@@ -3,9 +3,14 @@
 
 #include <clutter/clutter.h>
 #include "spritesheet.h"
+#include "log.h"
+#include "clutter_util.h"
+#include "spritesheet.h"
 
 typedef SpriteSheet::Sprite Sprite;
 typedef PushTexture::PingMe PingMe;
+
+extern const char * keys[9];
 
 GType nineslice_layout_get_type( void );
 
@@ -16,7 +21,29 @@ GType nineslice_layout_get_type( void );
 #define IS_NINESLICE_LAYOUT_CLASS(klass)  (G_TYPE_CHECK_CLASS_CAST((klass),   TYPE_NINESLICE_LAYOUT))
 #define NINESLICE_LAYOUT_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS((obj),   TYPE_NINESLICE_LAYOUT, NineSliceLayoutClass))
 
+#define LB_GET_SPRITESHEET(L,i) ((SpriteSheet*)lb_get_udata_check(L,i,"SpriteSheet"))
+
 struct NineSliceLayout;
+
+GObject* nineslice_layout_new();
+
+void nineslice_layout_set_sprite( NineSliceLayout* layout, unsigned i, SpriteSheet::Sprite* sprite, bool async );
+
+bool nineslice_layout_is_done( NineSliceLayout* layout );
+bool nineslice_layout_is_loaded( NineSliceLayout* layout );
+void nineslice_layout_signal_loaded_later( NineSliceLayout* layout );  // fires "load_finished" signal if true
+
+// There are 6 tile parameters (see nineslice.lb); set/get them individually or as an array
+
+bool nineslice_layout_get_tile( NineSliceLayout* layout, unsigned i );
+void nineslice_layout_get_tile( NineSliceLayout* layout, gboolean tile[6] );
+void nineslice_layout_set_tile( NineSliceLayout* layout, unsigned i, bool t, bool guess, bool constructing );
+void nineslice_layout_set_tile( NineSliceLayout* layout, gboolean tile[6] );
+void nineslice_redraw( NineSliceLayout* layout );
+
+std::vector< int >* nineslice_layout_get_borders( NineSliceLayout* layout );
+
+void nineslice_layout_init_tablelayout( NineSliceLayout* self, ClutterActor * _actor );
 
 class Slice
 {
@@ -68,24 +95,144 @@ struct NineSliceLayoutClass
     GObjectClass parent_class;
 };
 
-GObject* nineslice_layout_new();
+/* TODO: Need to make it clear how objects are destructed, instead of no code/comments
+ * TODO: Leverage ClutterTableLayout to simplify the implementation
+ * TODO: Need to verify calculations of coordinates of x and y in the json file. Right now if it starts from 1 instead of 0, some sprites are being missed
+ * TODO: Cloning - Two instances of the same NineSlice should share SpriteSheet, Sprites
+ *
+ * Class heirarchy: NineSliceBinding -> NineSliceLayout -> NineSliceLayoutPrivate -> Slice
+ */
 
-void nineslice_layout_set_sprite( NineSliceLayout* layout, unsigned i, SpriteSheet::Sprite* sprite, bool async );
+struct NineSliceBinding
+{
+    NineSliceBinding( NineSliceLayout * _layout, ClutterActor * _actor ) : async( false ), constructing( false ), sheet( NULL ), layout( _layout ), action( NULL )
+    {
+        g_assert( layout );
+        g_assert( _actor );
 
-bool nineslice_layout_is_done( NineSliceLayout* layout );
-bool nineslice_layout_is_loaded( NineSliceLayout* layout );
-void nineslice_layout_signal_loaded_later( NineSliceLayout* layout );  // fires "load_finished" signal if true
+        nineslice_layout_init_tablelayout( layout, _actor );
+    }
 
-// There are 6 tile parameters (see nineslice.lb); set/get them individually or as an array
+    ~NineSliceBinding()
+    {
+        if ( async && action )
+        {
+            Action::cancel( action );
+            action = NULL;
+        }
 
-bool nineslice_layout_get_tile( NineSliceLayout* layout, unsigned i );
-void nineslice_layout_get_tile( NineSliceLayout* layout, gboolean tile[6] );
-void nineslice_layout_set_tile( NineSliceLayout* layout, unsigned i, bool t, bool guess, bool constructing );
-void nineslice_layout_set_tile( NineSliceLayout* layout, gboolean tile[6] );
-void nineslice_redraw( NineSliceLayout* layout );
+        unsubscrube_sprites();
 
-std::vector< int >* nineslice_layout_get_borders( NineSliceLayout* layout );
+        layout->priv->parent_valid = false; // NineSliceBinding is gone
+    }
 
-void nineslice_layout_init_tablelayout( NineSliceLayout* self, ClutterActor * _actor );
+    NineSliceLayout * get_layout() { return layout; }
+
+    bool get_tile( unsigned i )
+    {
+        if ( i >= 6 )
+        {
+            tpwarn("Invalid NineSlice tile index %d. The value must be between 0 and 6", i);
+            return false;
+        }
+
+        return nineslice_layout_get_tile( layout, i );
+    }
+
+    void set_tile( unsigned i, bool t, bool guess )
+    {
+        if ( i >= 6 )
+        {
+            tpwarn("Invalid NineSlice tile index %d. The value must be between 0 and 6", i);
+            return;
+        }
+
+        nineslice_layout_set_tile( layout, i, t, guess, constructing );
+    }
+
+    bool is_loaded() { return sheet && nineslice_layout_is_loaded( layout ); };
+
+    std::vector< int > * get_borders() { return nineslice_layout_get_borders( layout ); }
+
+    std::string & get_id( unsigned i )
+    {
+        if ( i >= 9 )
+        {
+            tpwarn("Invalid NineSlice id index %d. The value must be between 0 and 9", i);
+
+            static std::string e = "";
+            return e;
+        }
+
+        return ids[i];
+    }
+
+    void set_id( unsigned i, const std::string & new_id )
+    {
+        if ( i >= 9 )
+        {
+            tpwarn("Invalid NineSlice id index %d. The value must be between 0 and 9", i);
+            return;
+        }
+
+        ids[i] = new_id;
+
+        if (!constructing) nineslice_layout_set_sprite( layout, i, sheet ? sheet->get_sprite( ids[i].c_str() ) : NULL, async );
+    }
+
+    SpriteSheet * get_sheet() { return sheet; }
+
+    void set_sheet( SpriteSheet * _sheet )
+    {
+        sheet = _sheet;
+
+        if (constructing) return;
+
+        if (sheet) set_all_sprites();
+    }
+
+    void set_all_sprites()
+    {
+        if (!sheet) return;
+
+        for ( unsigned i = 0; i < 9; ++i )
+        {
+            if ( !(ids[i].empty()) )
+            {
+                nineslice_layout_set_sprite( layout, i, sheet->get_sprite( ids[i].c_str() ), async );
+            }
+        }
+    }
+
+    void signal_loaded_later()
+    {
+        if ( async )
+        {
+            nineslice_layout_signal_loaded_later( layout );
+        }
+    }
+
+    void unsubscrube_sprites()
+    {
+        Slice * slices = layout->priv->slices;
+
+        if (slices)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                slices[i].unset_sprite();
+            }
+        }
+    }
+
+    bool async;
+    bool constructing;
+
+private:
+    SpriteSheet      * sheet;
+    NineSliceLayout  * layout;
+    std::string        ids[9];
+    Action           * action;
+};
 
 #endif
